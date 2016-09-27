5f76e0e1c0f15b1edbf1390bb81b9957efd0b6ec
judell <jon@jonudell.info>
2016-09-27 11:29:30 -0700

    hide 'see all activity' from canvas preview, remove unrelated code

--- export/utils.js
+++ export/utils.js
@@ -2,6 +2,12 @@ var output = '';
 var displayed_in_thread = [];
 var query = 'https://hypothes.is/api/search?limit=200&offset=__OFFSET__';
 
+function all_activity_for(user) {
+   var activity_url = 'http://jonudell.net/h/facet.html?facet=user&search=' + user;
+   document.getElementById('all_activity_href').href = activity_url;
+   document.getElementById('all_activity_user').innerText = user;
+}
+
 var token_ux = function(){/*
 <p>
 <input onchange="javascript:set_token()" type="password" value="" size="40" id="token"> <br> <span class="small">(for private group annotations, include your <a target="token" href="https://hypothes.is/profile/developer">API token</a>)</span> 
@@ -14,8 +20,15 @@ function show_token_ux() {
     document.querySelector('#token').value = token;
 }
 
+var see_all_ux = function(){/*
+<p>See <a target="all_activity" id="all_activity_href" href="">all Hypothesis activity for <span id="all_activity_user"></span></a></p>
+*/};
 
-function load(offset, rows, replies) {
+function show_see_all_ux() {
+    document.querySelector('#see_all_ux').innerHTML = heredoc(see_all_ux);
+}
+
+function load(user, offset, rows, replies) {
     var limit = 400;
     var _query = query.replace('__OFFSET__', offset);
     $.ajax({
@@ -38,9 +51,11 @@ function load(offset, rows, replies) {
             if (data.rows.length == 0 || rows.length > limit) {
                 process(rows, replies);
                 show_token_ux();
+                show_see_all_ux();
+                all_activity_for(user);
                 }
             else
-                load(offset + 200, rows, replies);
+                load(user, offset + 200, rows, replies);
         }
     });
 }
@@ -88,89 +103,6 @@ function organize(url_updates) {
     return reverse_chron_urls;
 }
 
-var export_template = function(){/*
-<html>
-<head><meta charset="utf-8"></head>
-<title>Hypothesis activity for the query __FACET__ = __QUERY__</title>
-<style>
-body { font-family:verdana; word-break:break-word; margin:10px; } 
-.url { font-family:italic; margin-bottom:10px; color:gray; }
-a { text-decoration: none; color: brown } 
-a.toggle {font-weight: bold; }
-a.visit { color: #151414 } 
-a.expand_all { font-weight:bold; margin-right:20px; font-size:larger; float:right }
-a.collapse_all { font-weight:bold; margin-right:20px; font-size:larger; float:right }
-.checkbox { display:none }
-img { width: 90%; margin: 8px } 
-.user { font-weight:bold } 
-.search_term { background-color:antiquewhite }
-.timestamp { font-style:italic; font-size:smaller } 
-.thread, .annotation { border:thin solid lightgray;padding:10px;margin:10px; } 
-.annotations { display:none; } 
-.annotation-quote { color: #777; font-style: italic;padding: 0 .615em; border-left: 3px solid #d3d3d3; margin-top:12px; margin-bottom: 12px } 
- .tag-item { margin: 2px; text-decoration: none; border: 1px solid #BBB3B3; border-radius: 2px; padding: 3px; color: #4B4040; background: #f9f9f9; } 
-.anno-count { } 
-.tags { line-height: 2 }
-#selections { display: none }
-#expander { display: none }
-</style>
-</head>
-<body>
-<h1>Hypothesis activity for the query __FACET__ = __QUERY__</h1>
-__EXPORT__
-<script>
-function toggle(dom_id) {
-    var element = document.getElementById('a' + dom_id);
-    var display = element.style['display'];
-    var annos = element.querySelectorAll('.annotation');
-    var toggler = document.getElementById('d' + dom_id).querySelector('.toggle');
-    var toggler_text = toggler.querySelector('.anno-count');
-    if (display == 'none' || display == '') {
-        toggler_text.innerText = toggler_text.innerText.replace('+','-');
-        toggler.title = 'collapse annotations';
-        element.style['display'] = 'block';
-        for (var i = 0; i < annos.length; i++)
-            annos[i].style.display = 'block';
-    }
-    else {
-        toggler_text.innerText = toggler_text.innerText.replace('-','+');
-        toggler.title = 'expand annotations';
-        element.style['display'] = 'none';
-        for (var i = 0; i < annos.length; i++)
-            annos[i].style.display = 'none';
-    }
-}
-</script>
-</body>
-</html>
-*/};
-
-function process(rows, replies) {
-    var gathered = gather(rows);
-
-    if ($('#format_html').is(":checked")) {
-        document_view('exported_html', gathered, replies);
-        var html = heredoc(export_template);
-        html = html.replace(/__QUERY__/g, search);
-        html = html.replace(/__FACET__/g, facet);
-        html = html.replace(/__EXPORT__/g, document.getElementById('exported_html').innerHTML);
-        download(html, 'html');
-        rows = [];
-    }
-    else if ($('#format_csv').is(":checked")) {
-        to_csv(rows);
-    }
-    else if ($('#format_md').is(":checked")) {
-        to_markdown(rows);
-    }
-    else if ($('#format_text').is(":checked")) {
-        to_text(rows);
-    }
-    else {
-        console.log('?')
-    }
-}
-
 var docview_template = function(){/*
 <div id="d__INDEX__">
 <input checked="" onchange="javascript:item_checked(__INDEX__)" class="checkbox" id="c__INDEX__" type="checkbox">
@@ -201,12 +133,6 @@ function document_view(element, gathered, replies) {
 
         var s = heredoc(docview_template);
 
-        if ( is_exporting() ) {
-            var selected_ids = get_ids();
-            if ( selected_ids.length>0 && selected_ids.indexOf(i) == -1 )
-                continue;
-            }
-
         s = s.replace(/__INDEX__/g, i);
 
         var url = reverse_chron_urls[i][0];
@@ -228,9 +154,6 @@ function document_view(element, gathered, replies) {
         s = s.replace(/__THREAD__/, output);
         elt.append(s);
     }
-
-    all_or_none();
-
 }
 
 function show_thread(annos, id, level, replies) {
@@ -254,14 +177,11 @@ function show_thread(annos, id, level, replies) {
             stripIgnoreTagBody: ['script']
         };
         html = filterXSS(html, options);
-        html = wrap_search_term(html);
         var tags = '';
         if (anno.tags.length) 
 			tags = make_tags(anno.tags);
-        html = wrap_search_term(html);
         var user = anno.user;
         var quote = filterXSS(anno.quote, {});
-		quote = wrap_search_term(quote);
         var anno_scope = ''
         if ( ! anno.permissions.read[0].startsWith('group') )
             anno_scope = '(Only Me)';
@@ -299,35 +219,22 @@ function show_thread(annos, id, level, replies) {
     }
 }
 
-function annotation_view(rows) {
-    for (var i = 0; i < rows.length; i++) {
-        var row = rows[i];
-        var anno = parse_annotation(row);
-        show_annotation(anno);
-    }
-}
-
 function show_annotation(anno) {
     var margin = 20;
     var dt = new Date(anno.updated);
     var user = anno.user.replace('acct:','').replace('@hypothes.is','')
     user = user;
-//    var url = wrap_search_term(anno.url);
     var url = anno.url;
     var quote = '';
     if (anno.quote) {
-      quote = wrap_search_term(anno.quote);
       quote = filterXSS(quote, {});
 	  }
-    var title = wrap_search_term(anno.title);
     var dt_str = dt.toLocaleDateString() + ' ' + dt.toLocaleTimeString();
     var converter = new Showdown.converter();
-    var text = wrap_search_term(anno.text);
     var html = '';
     if (text) {
         html = converter.makeHtml(text);
         html = filterXSS(html, options);
-        html = wrap_search_term(html);
         if ( ! html ) {
             html = '';
         }
@@ -357,17 +264,6 @@ function show_annotation(anno) {
         '<div>' + html + '</div>' +
         '</div>';
     output += template.toString();
-}
-
-function wrap_search_term(s) {
-    if ( ! s || typeof(search) == 'undefined' )
-        return s;
-    var re = new RegExp( search, 'i');
-    var m = s.match(re);
-    if ( m )
-        return s.replace(m[0], '<span class="search_term">' + m[0] + '</span>');
-    else 
-        return s;
 }
 
 function parse_annotation(row) {
@@ -454,170 +350,6 @@ function add_css(css) {
     head.appendChild(s);
 }
 
-function toggle(dom_id) {
-    var element = document.getElementById('a' + dom_id);
-    var display = element.style['display'];
-    var annos = element.querySelectorAll('.annotation');
-    var toggler = document.getElementById('d' + dom_id).querySelector('.toggle');
-    var toggler_text = toggler.querySelector('.anno-count');
-    if (display == 'none' || display == '') {
-        toggler_text.innerText = toggler_text.innerText.replace('+','-');
-        toggler.title = 'collapse annotations';
-        element.style['display'] = 'block';
-        for (var i = 0; i < annos.length; i++)
-            annos[i].style.display = 'block';
-    }
-    else {
-        toggler_text.innerText = toggler_text.innerText.replace('-','+');
-        toggler.title = 'expand annotations';
-        element.style['display'] = 'none';
-        for (var i = 0; i < annos.length; i++)
-            annos[i].style.display = 'none';
-    }
-}
-
-var export_form = function(){/*
-<div id="export_ux">
-<hr>
-Export results to:
-<p>
-<input id="format_html" type="radio" name="format" value="HTML" checked="checked"> HTML
-<input id="format_csv" type="radio" name="format" value="CSV"> CSV
-<input id="format_text" type="radio" name="format" value="TEXT"> TEXT
-<input id="format_md" type="radio" name="format" value="MARKDOWN"> MARKDOWN
-</p>
-<p>
-<input type="button" onclick="_select()" value="select">
-<span class="small">(if you only want some stuff)</span>
-</p>
-<p>
- <input type="button" onclick="_export()" value="export">
- <span class="small">(to get your stuff in your preferred format)</span>
-</p>
-<div id="exported_html" style="display:none"></div>
-<div id="export_done"></div><p style="font-size:smaller">(Click <i>documents</i> or <i>annotations</i> to reset)</div>
-*/};
-
-function add_export(el) {
-    document.getElementById(el).innerHTML = heredoc(export_form);
-}
-function add_doc() {
-    document.getElementById('doc').innerHTML = '<hr><p>Results are organized as follows:</p><ol><li> Annotations are grouped into threads.<li> Threads are grouped by URL.<li> URLs appear in reverse order by recency of annotation.</ol><hr><p>Source: <a href="https://github.com/judell/h_export">https://github.com/judell/h_export</a>';
-}
-
-function download(text, type) {
-    var a = document.createElement('a');
-    a.href = 'data:attachment/' + type + ',' + encodeURIComponent(text);
-    a.target = '_blank';
-    a.download = 'hypothesis.' + type;
-    document.body.appendChild(a);
-    a.click();
-}
-
-function get_mode() {
-    var mode = gup('mode');
-	if ( mode == '' || mode == 'documents' )
-		return 'documents';
-    else
-		return 'annotations';
-}
-
-function save_ids(ids) {
-  localStorage.setItem('h_export_selections', JSON.stringify(ids) );
-}
-
-function get_ids() {
-    return JSON.parse(localStorage.getItem('h_export_selections'));
-}
-
-function all_or_none() {
-    if ( !is_selecting) return;
-    if ( is_selecting() && !is_exporting() ) {
-		document.getElementById('selections').style.display = 'block';
-		var choice = getRVBN('selections');
-		var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-		var ids = [];
-		if (choice == 'none') {
-			for (var i = 0; i < checkboxes.length; i++) {
-				checkboxes[i].checked = false;
-				checkboxes[i].style.display = 'inline';
-			}
-		}
-		else {
-			for (var i = 0; i < checkboxes.length; i++) {
-				checkboxes[i].style.display = 'inline';
-				checkboxes[i].checked = true;
-				ids.push(i);
-			}
-		}
-		save_ids(ids);
-    }
-}
-
-function item_checked(id) {
-    var ids = get_ids();
-    var is_saved = ids.indexOf(id) != -1;
-    var checkbox = document.getElementById('c' + id);
-    var is_checked = checkbox.checked;
-    if ( ! is_checked && is_saved ) {
-       var index = ids.indexOf(id);
-       if ( index > -1 ) {
-           ids.splice(index, 1);
-           save_ids(ids);
-           console.log('unsave ' + id);
-           }
-       }
-    if ( is_checked && ! is_saved ) {
-       ids.push(id);
-       save_ids(ids);
-       console.log('save ' + id);
-       }
-}
-
-function getRVBN(rName) {
-    var radioButtons = document.getElementsByName(rName);
-    for (var i = 0; i < radioButtons.length; i++) {
-        if (radioButtons[i].checked)
-            return radioButtons[i].value;
-    }
-    return '';
-}
-
-function is_selecting() {
-    return localStorage.getItem('h_is_selecting')=='true';
-}
-
-function is_exporting() {
-    return localStorage.getItem('h_is_exporting')=='true';
-}
-
-function init_selections() {
-    if ( gup('selections') == '' ) {
-        localStorage.setItem('h_export_selections', JSON.stringify([]));
-        localStorage.setItem('h_is_selecting','false');
-        localStorage.setItem('h_is_exporting','false');
-        }
-    }
-
-function start_selections() {
-  localStorage.setItem('h_is_selecting','true');
-  localStorage.setItem('h_is_exporting','false');
-  }
-
-function menu(facet) {
-  var items = ['user', 'group', 'uri', 'tag', 'any'];
-  var html_items = [];
-  for (var i=0; i<items.length; i++) {
-    if ( items[i] == facet ) {
-      html_items.push('<b>' + facet + '</b>');
-	  }
-	else {
-		
-	  html_items.push('<a href="facet.html?facet=' + items[i] + '">' + items[i] + '</a>');
-      }
-  }
-  return html_items.join('  &#8226; ');
-}
 
 function heredoc(fn) {
  var a = fn.toString();
@@ -626,110 +358,8 @@ function heredoc(fn) {
 }
 
 
-var form = function(){/*
-<div>
-<p>
-<input onchange="_search('__FACET__', 'documents')" value="" id="facet"></input> <br><span class="small">__MSG1__</span>
-</p>
-<p>
-<input type="password" value="" size="40" id="token"></input> <br> <span class="small">__MSG2__</span> 
-</p>
-<p>
-View by <input type="button" onclick="_search('__FACET__', 'documents')" value="documents"></input> or
-<input type="button" onclick="_search('__FACET__', 'annotations')" value="annotations"></input>
-</p>
-</div>
-*/};
-
-function add_form(facet, mode) {
-  var token_msg_1 = '(for private annotations, include your <a href="https://hypothes.is/profile/developer">API token</a>)';
-  var token_msg_2 = '(your <a href="https://hypothes.is/profile/developer">API token</a>)';
-
-  var s = heredoc(form);
-  s = s.replace(/__FACET__/g, facet);
-  s = s.replace(/__MODE__/g, mode);
-  switch (facet) {
-    case 'user':
-	  s = s.replace('__MSG1__', '(a Hypothesis username)');
-	  s = s.replace('__MSG2__', token_msg_1);
-	  break;
-	case 'group':
-	  s = s.replace('__MSG1__', '(a Hypothesis group ID from https://hypothes.is/groups/<span style="font-weight:bold">ID</span>)');
-	  s = s.replace('__MSG2__', token_msg_2);
-	  break;
-	case 'uri':
-	  s = s.replace('__MSG1__', '(URL of an annotated document)');
-	  s = s.replace('__MSG2__', token_msg_1);
-	  break;
-	case 'tag':
-	  s = s.replace('__MSG1__', '(a Hypothesis tag)');
-	  s = s.replace('__MSG2__', token_msg_1);
-	  break;
-	case 'any':
-	  s = s.replace('__MSG1__', '(user, URL, tag. annotation quote or body)');
-	  s = s.replace('__MSG2__', token_msg_1);
-	  break;
-	default:
-	    console.log('add_form unexpected facet ' + facet);	
-    }	  
-  document.getElementById('form').innerHTML = s;
-  var token = getLocalStorageItem('h_token');
-  document.getElementById('token').value = token;
-  }
-
-function add_menu(facet) {
-  document.getElementById('menu').innerHTML = menu(facet);
-  }
-
-function _search(facet, mode) {
-  localStorage.setItem('h_token', document.getElementById('token').value);
-  var search = document.getElementById('facet').value;
-  var href = 'facet.html?facet=' + facet + '&mode=' + mode + '&search=' + search;
-  location.href = href;
-}
-
-function _export() {
-  query += '&' + facet + '=' + search;
-  localStorage.setItem('h_is_exporting','true');
-  localStorage.setItem('h_is_selecting','false');
-  load(0, [], []);
-}
-
-function _select() {
-  start_selections();
-  search = document.getElementById('facet').value;
-  location.href = 'facet.html?facet=' + facet + '&mode=' + mode + '&search=' + search + '&selections=yes';
-}
-
-function expand_all() {
-    document.getElementById('expander').style.display = 'none';
-    document.getElementById('collapser').style.display = 'inline';
-    document.getElementById('collapser').title = 'collapse all';
-    var annos = document.querySelectorAll('.annotations');
-    for (var i = 0; i < annos.length; i++)
-        annos[i].style.display = 'block';
-}
-
-function collapse_all() {
-    document.getElementById('expander').style.display = 'inline';
-    document.getElementById('collapser').title = 'expand all';
-    document.getElementById('collapser').style.display = 'none';
-    var annos = document.querySelectorAll('.annotations');
-    for (var i = 0; i < annos.length; i++)
-        annos[i].style.display = 'none';
-}
-
-function compare(a,b) {
-  if (a.updated > b.updated)
-    return -1;
-  else if (a.updated < b.updated)
-    return 1;
-  else 
-    return 0;
-}
-
 function make_tags(tags) {
-  var links = tags.map(function (x) { return '<a target="_tag" href="facet.html?mode=' + mode + '&facet=tag&search=' + x.replace('#', '') + '">' + wrap_search_term(x) + '</a>' });
+  var links = tags.map(function (x) { return '<a target="_tag" href="facet.html?mode=' + mode + '&facet=tag&search=' + x.replace('#', '') + '">' + x + '</a>' });
   var tags = '<div class="tags">' +
       '<span class="tag-item">' +
       links.join('</span><span class="tag-item">') +
