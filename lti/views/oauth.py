76ee311cc30e236f2736d82d6d76a95704c6d261
Sean Hammond <git@snhmnd.fastmail.com>
2017-07-27 13:29:14 +0100

    Fix lint violations in oauth.py
    Fix most of the linter rule violations in oauth.py.

--- lti/views/oauth.py
+++ lti/views/oauth.py
@@ -21,35 +21,35 @@ def token_callback(request):
     return oauth_callback(request, type_='token')
 
 
-@view_config( route_name='refresh_callback' )
+@view_config(route_name='refresh_callback')
 def refresh_callback(request):
-    return oauth_callback(request, type='refresh')
+    return oauth_callback(request, type_='refresh')
 
 
-def oauth_callback(request, type=None):
+def oauth_callback(request, type_=None):
     """
     I'm not sure yet.
-    
+
     Canvas called back with an authorization code. Use it to get or refresh an
     API token.
-    
+
     """
     try:
-        log.info ( 'oauth_callback: %s' % request.query_string )
-        q = urlparse.parse_qs(request.query_string)
-        code = q['code'][0]
-        state = q['state'][0]
-        dict = util.unpack_state(state)
-        log.info ( 'oauth_callback: %s' % state)
+        log.info('oauth_callback: {query_string}', query_string=request.query_string)
+        parsed_query_string = urlparse.parse_qs(request.query_string)
+        code = parsed_query_string['code'][0]
+        state = parsed_query_string['state'][0]
+        unpacked_state = util.unpack_state(state)
+        log.info('oauth_callback: {state}', state=state)
 
-        course = dict[constants.CUSTOM_CANVAS_COURSE_ID]
-        user = dict[constants.CUSTOM_CANVAS_USER_ID]
-        oauth_consumer_key = dict[constants.OAUTH_CONSUMER_KEY]
-        ext_content_return_url = dict[constants.EXT_CONTENT_RETURN_URL]
+        course = unpacked_state[constants.CUSTOM_CANVAS_COURSE_ID]
+        user = unpacked_state[constants.CUSTOM_CANVAS_USER_ID]
+        oauth_consumer_key = unpacked_state[constants.OAUTH_CONSUMER_KEY]
+        ext_content_return_url = unpacked_state[constants.EXT_CONTENT_RETURN_URL]
 
-        assignment_type = dict[constants.ASSIGNMENT_TYPE]
-        assignment_name = dict[constants.ASSIGNMENT_NAME]
-        assignment_value = dict[constants.ASSIGNMENT_VALUE]
+        assignment_type = unpacked_state[constants.ASSIGNMENT_TYPE]
+        assignment_name = unpacked_state[constants.ASSIGNMENT_NAME]
+        assignment_value = unpacked_state[constants.ASSIGNMENT_VALUE]
 
         canvas_client_secret = request.auth_data.get_lti_secret(
             oauth_consumer_key)
@@ -57,22 +57,22 @@ def oauth_callback(request, type=None):
             oauth_consumer_key)
         canvas_server = request.auth_data.get_canvas_server(oauth_consumer_key)
         url = '%s/login/oauth2/token' % canvas_server
-        grant_type = 'authorization_code' if type == 'token' else 'refresh_token'
-        params = { 
-                'grant_type': grant_type,
-                'client_id': oauth_consumer_key,
-                'client_secret': canvas_client_secret,
-                'redirect_uri': '%s/token_init' % request.registry.settings['lti_server'] # this uri must match the uri in Developer Keys but is not called from
-                }                                                                         # canvas. rather it calls token_callback or refresh callback
-        if grant_type == 'authorization_code': 
+        grant_type = 'authorization_code' if type_ == 'token' else 'refresh_token'
+        params = {
+            'grant_type': grant_type,
+            'client_id': oauth_consumer_key,
+            'client_secret': canvas_client_secret,
+            'redirect_uri': '%s/token_init' % request.registry.settings['lti_server']  # this uri must match the uri in Developer Keys but is not called from
+        }                                                                              # canvas. rather it calls token_callback or refresh callback
+        if grant_type == 'authorization_code':
             params['code'] = code
         else:
             params['refresh_token'] = lti_refresh_token
-        r = requests.post(url, params)
-        dict = r.json()
-        lti_token = dict['access_token']
-        if dict.has_key('refresh_token'): # does it ever not?
-            lti_refresh_token = dict['refresh_token']
+        response = requests.post(url, params)
+        unpacked_state = response.json()
+        lti_token = unpacked_state['access_token']
+        if 'refresh_token' in unpacked_state:  # Is it ever not?
+            lti_refresh_token = unpacked_state['refresh_token']
         request.auth_data.set_tokens(oauth_consumer_key,
                                      lti_token,
                                      lti_refresh_token)
