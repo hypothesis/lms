0e7cf9bfbe9f27d500326d07358ccd235054c20c
Sean Hammond <git@snhmnd.fastmail.com>
2017-07-27 12:42:00 +0100

    Move route definitions into routes.py file
    This doesn't achieve anything except keeping the code tidier by
    splitting into separate files, but - move all the Pyramid route
    definitions (`add_route()` calls) into a separate `routes.py` file like
    `h` does. The main `app.py` then does `config.include('lti.routes')`,
    which runs the `includeme()` function in `app.py`, which is where the
    `add_route()`s are now.

---
+++ lti/routes.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+
+def includeme(config):
+    config.add_route('about', '/')
+
+    config.add_route('token_callback', '/token_callback')
+    config.add_route('refresh_callback', '/refresh_callback')
+
+    config.add_route('config_xml', '/config.xml')
+
+    config.add_route('lti_credentials', '/lti_credentials')
+    config.add_route('lti_setup', '/lti_setup')
+    config.add_route('lti_submit', '/lti_submit')
+    config.add_route('lti_export', '/lti_export')
+
+    config.add_route('lti_serve_pdf', '/viewer/web/{file}.pdf')
+    config.add_route('catchall_pdf', '/viewer/*subpath')
