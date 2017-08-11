3b9cd67d2778f96632e0ee25612425060d12cf97
Sean Hammond <git@snhmnd.fastmail.com>
2017-08-11 15:21:59 +0100

    Delete some unused imports

--- lti/app.py
+++ lti/app.py
@@ -1,11 +1,8 @@
-﻿import urllib
-import urlparse
-import requests
+﻿import requests
 import traceback
 import re
 import logging
 import filelock
-from pyramid.httpexceptions import HTTPFound
 from pyramid.view import view_config
 from pyramid.response import FileResponse
 from requests_oauthlib import OAuth1
