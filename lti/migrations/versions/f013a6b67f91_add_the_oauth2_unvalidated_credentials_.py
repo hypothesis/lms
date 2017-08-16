91456b1a7114bed2c8d5979d5db8d330173b88fb
Sean Hammond <git@snhmnd.fastmail.com>
2017-08-16 16:32:29 +0100

    Add migration for oauth2_unvalidated_credentials
    Add a database migration script for the oauth2_unvalidated_credentials
    table that has already been added to the model code.
    
    This is our first migration script, so also add migration tooling
    (alembic dependency, alembic config file, env.py and script.py.mako
    files for alembic) and add a section to the README about how to create a
    DB migration.

---
+++ lti/migrations/versions/f013a6b67f91_add_the_oauth2_unvalidated_credentials_.py
@@ -0,0 +1,29 @@
+"""
+Add the oauth2_unvalidated_credentials table.
+
+Revision ID: f013a6b67f91
+Revises: None
+Create Date: 2017-08-16 16:49:03.696908
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f013a6b67f91'
+down_revision = None
+
+
+def upgrade():
+    op.create_table('oauth2_unvalidated_credentials',
+        sa.Column('id', sa.Integer, autoincrement=True, primary_key=True),
+        sa.Column('client_id', sa.UnicodeText),
+        sa.Column('client_secret', sa.UnicodeText),
+        sa.Column('authorization_server', sa.UnicodeText),
+        sa.Column('email_address', sa.UnicodeText),
+    )
+
+
+def downgrade():
+    op.drop_table('oauth2_unvalidated_credentials')
