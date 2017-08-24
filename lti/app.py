6704a92eac43312a356d3fc0d5e46b8938e52b4e
Sean Hammond <git@snhmnd.fastmail.com>
2017-08-24 14:51:50 +0100

    Move lti_setup() into own file, lint, and test

--- lti/app.py
+++ lti/app.py
@@ -1,5 +1,4 @@
 ﻿import requests
-import traceback
 import logging
 from pyramid.view import view_config
 from pyramid.response import FileResponse
@@ -10,9 +9,6 @@ from pyramid.renderers import render
 from lti.config import configure
 from lti import util
 from lti import constants
-from lti.views import oauth
-from lti.views import web
-from lti.views import pdf
 
 
 log = logging.getLogger(__name__)
@@ -24,117 +20,6 @@ def serve_file(path=None, file=None, request=None, content_type=None):
                             content_type=content_type)
     return response
 
-
-@view_config( route_name='lti_setup' )
-@view_config( route_name='canvas_resource_selection' )
-def lti_setup(request):
-    """
-    LTI-launched from a Canvas assignment's Find interaction to present choice of doc (PDF or URL) to annotate.
-  
-    LTI-launched again when the Canvas assignment opens.
-  
-    In those two cases we have LTI params in the HTTP POST -- if we have a Canvas API token.
-
-    If there is no token, or the token is expired, called instead by way of OAuth redirect. 
-    In that case we expect params in the query string.
-    """
-    auth_data_svc = request.find_service(name='auth_data')
-
-    log.info ( 'lti_setup: query: %s' % request.query_string )
-    log.info ( 'lti_setup: post: %s' % request.POST )
-    post_data = util.requests.capture_post_data(request)
-
-    oauth_consumer_key = util.requests.get_post_or_query_param(request, constants.OAUTH_CONSUMER_KEY)
-    if oauth_consumer_key is None:
-        log.error( 'oauth_consumer_key cannot be None %s' % request.POST )
-    oauth_consumer_key = oauth_consumer_key.strip()
-    lis_outcome_service_url = util.requests.get_post_or_query_param(request, constants.LIS_OUTCOME_SERVICE_URL)
-    lis_result_sourcedid = util.requests.get_post_or_query_param(request, constants.LIS_RESULT_SOURCEDID)
-
-    course = util.requests.get_post_or_query_param(request, constants.CUSTOM_CANVAS_COURSE_ID)
-    if course is None:
-        log.error ( 'course cannot be None' )
-        return util.simple_response('No course number. Was Privacy set to Public for this installation of the Hypothesis LTI app? If not please do so (or ask someone who can to do so).')
-    
-    post_data[constants.ASSIGNMENT_TYPE] = util.requests.get_post_or_query_param(request, constants.ASSIGNMENT_TYPE)
-    post_data[constants.ASSIGNMENT_NAME] = util.requests.get_post_or_query_param(request, constants.ASSIGNMENT_NAME)
-    post_data[constants.ASSIGNMENT_VALUE] = util.requests.get_post_or_query_param(request, constants.ASSIGNMENT_VALUE)
-
-    log.info ( 'lti_setup: post_data: %s' % post_data )
-
-    try:
-        lti_token = auth_data_svc.get_lti_token(oauth_consumer_key)
-    except:
-        response = "We don't have the Consumer Key %s in our database yet." % oauth_consumer_key
-        log.error ( response )
-        log.error ( traceback.print_exc() )
-        return util.simple_response(response)
-
-    if lti_token is None:
-        log.info ( 'lti_setup: getting token' )
-        return oauth.make_authorization_request(request, util.pack_state(post_data))
-
-    sess = requests.Session()  # ensure we have a token before calling lti_pdf or lti_web
-    canvas_server = auth_data_svc.get_canvas_server(oauth_consumer_key)
-    log.info ( 'canvas_server: %s' % canvas_server )
-    url = '%s/api/v1/courses/%s/files?per_page=100' % (canvas_server, course)
-    r = sess.get(url=url, headers={'Authorization':'Bearer %s' % lti_token })
-    if r.status_code == 401:
-      log.info ( 'lti_setup: refreshing token' )
-      return oauth.make_authorization_request(
-            request, util.pack_state(post_data), refresh=True)
-    files = r.json()
-    while ('next' in r.links):
-        url = r.links['next']['url']
-        r = sess.get(url=url, headers={'Authorization':'Bearer %s' % lti_token })
-        files = files + r.json()
-    log.info ('files: %s' % len(files))
-
-    #return HTTPFound(location='http://h.jonudell.info:3000/courses/2/external_content/success/external_tool_dialog?return_type=lti_launch_url&url=http%3A%2F%2F98.234.245.185%3A8000%2Flti_setup%3FCUSTOM_CANVAS_COURSE_ID%3D2%26type%3Dpdf%26name%3Dfilename%26value%3D9')
-    
-    assignment_type = post_data[constants.ASSIGNMENT_TYPE]
-    assignment_name = post_data[constants.ASSIGNMENT_NAME]
-    assignment_value = post_data[constants.ASSIGNMENT_VALUE]
-
-    if assignment_type == 'pdf':
-        return pdf.lti_pdf(request, oauth_consumer_key=oauth_consumer_key, lis_outcome_service_url=lis_outcome_service_url, lis_result_sourcedid=lis_result_sourcedid, course=course, name=assignment_name, value=assignment_value)
-
-    if assignment_type == 'web':
-        return web.web_response(request,
-                                auth_data_svc,
-                                oauth_consumer_key=oauth_consumer_key,
-                                course=course,
-                                lis_outcome_service_url=lis_outcome_service_url,
-                                lis_result_sourcedid=lis_result_sourcedid,
-                                name=assignment_name,
-                                value=assignment_value)
-
-    return_url = util.requests.get_post_or_query_param(request, constants.EXT_CONTENT_RETURN_URL)
-    if return_url is None: # this is an oauth redirect so get what we sent ourselves
-        return_url = util.requests.get_post_or_query_param(request, 'return_url')
-
-    log.info ( 'return_url: %s' % return_url )
-
-    launch_url_template = '%s/lti_setup?assignment_type=__TYPE__&assignment_name=__NAME__&assignment_value=__VALUE__&return_url=__RETURN_URL__' % request.registry.settings['lti_server']
-
-    log.info ( 'key %s, course %s, token %s' % (oauth_consumer_key, course, lti_token) )
-
-    pdf_choices = ''
-    if len(files) > 0:
-        pdf_choices += '<ul>'
-        for file in files:
-            id = str(file['id'])
-            name = file['display_name']
-            if not name.lower().endswith('.pdf'):
-                continue
-            pdf_choices += '<li><input type="radio" name="pdf_choice" onclick="javascript:go()" value="%s" id="%s">%s</li>' % (name, id, name) 
-        pdf_choices += '</ul>'
-   
-    return Response(render('lti:templates/document_chooser.html.jinja2', dict(
-        return_url=return_url,
-        launch_url=launch_url_template,
-        pdf_choices=pdf_choices,
-    )))
 
 @view_config( route_name='lti_submit' )
 def lti_submit(request, oauth_consumer_key=None, lis_outcome_service_url=None, lis_result_sourcedid=None, export_url=None):
@@ -185,7 +70,6 @@ def lti_serve_pdf(request):
 
     return util.simple_response('You are not logged in to Canvas')
 
-from pyramid.response import Response
 from pyramid.static import static_view
 
 def create_app(global_config, **settings):  # pylint: disable=unused-argument
---
+++ tests/lti/views/setup_test.py
@@ -0,0 +1,475 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import urllib
+
+import pytest
+import mock
+
+from lti import constants
+from lti.views import setup
+
+pytestmark = pytest.mark.usefixtures(  # pylint:disable=invalid-name
+    'simple_response',
+    'requests',
+    'render',
+    'pack_state',
+    'oauth',
+    'web',
+    'pdf',
+)
+
+
+class SharedLTISetupTests(object):
+
+    def test_theres_no_course_param_it_returns_an_error_page(
+            self, pyramid_request, simple_response):
+        del pyramid_request.POST[constants.CUSTOM_CANVAS_COURSE_ID]
+
+        returned = setup.lti_setup(pyramid_request)
+
+        simple_response.assert_called_once_with(
+            'No course number. Was Privacy set to Public for this '
+            'installation of the Hypothesis LTI app? If not please do so (or '
+            'ask someone who can to do so).')
+        assert returned == simple_response.return_value
+
+    def test_it_gets_the_access_token_from_the_db(self, pyramid_request, auth_data_svc):
+        setup.lti_setup(pyramid_request)
+
+        auth_data_svc.get_lti_token.assert_called_once_with(
+            'TEST_OAUTH_CONSUMER_KEY')
+
+    def test_if_we_dont_have_the_consumer_key_it_returns_an_error_page(
+            self, pyramid_request, simple_response, auth_data_svc):
+        # get_lti_token() raises KeyError if the given consumer key doesn't
+        # exist in the db.
+        auth_data_svc.get_lti_token.side_effect = KeyError
+
+        returned = setup.lti_setup(pyramid_request)
+
+        error_message = (
+            "We don't have the Consumer Key TEST_OAUTH_CONSUMER_KEY in our "
+            "database yet.")
+        simple_response.assert_called_once_with(error_message)
+        assert returned == simple_response.return_value
+
+
+class TestLTISetupWhenWeDontHaveAnAccessToken(SharedLTISetupTests):
+    """
+    Unit tests for lti_setup() when we don't have an access token.
+
+    These test what lti_setup() does when we _do_ have the consumer key in the
+    db, but we don't yet have an access token for that consumer key.
+
+    """
+
+    def test_it_packs_the_launch_params_into_a_string(
+            self, pyramid_request, pack_state):
+        auth_data_svc = pyramid_request.find_service(name='auth_data')
+        auth_data_svc.get_lti_token.return_value = None
+
+        setup.lti_setup(pyramid_request)
+
+        pack_state.assert_called_once_with({
+            constants.ASSIGNMENT_VALUE: 'TEST_ASSIGNMENT_VALUE',
+            constants.ASSIGNMENT_NAME: 'TEST_ASSIGNMENT_NAME',
+            constants.ASSIGNMENT_TYPE: 'TEST_ASSIGNMENT_TYPE',
+            constants.CUSTOM_CANVAS_ASSIGNMENT_ID: 'TEST_ASSIGNMENT_ID',
+            constants.CUSTOM_CANVAS_COURSE_ID: 'TEST_COURSE_ID',
+            constants.CUSTOM_CANVAS_USER_ID: 'TEST_CANVAS_USER_ID',
+            constants.EXT_CONTENT_RETURN_TYPES: 'TEST_EXT_CONTENT_RETURN_TYPES',
+            constants.EXT_CONTENT_RETURN_URL: 'TEST_EXT_CONTENT_RETURN_URL',
+            constants.LIS_OUTCOME_SERVICE_URL: 'TEST_LIS_OUTCOME_SERVICE_URL',
+            constants.LIS_RESULT_SOURCEDID: 'TEST_LIS_RESULT_SOURCEDID',
+            constants.OAUTH_CONSUMER_KEY: 'TEST_OAUTH_CONSUMER_KEY',
+        })
+
+    def test_it_starts_an_oauth_flow_to_get_an_access_token(self,
+                                                            pyramid_request,
+                                                            pack_state,
+                                                            oauth):
+        auth_data_svc = pyramid_request.find_service(name='auth_data')
+        auth_data_svc.get_lti_token.return_value = None
+
+        returned = setup.lti_setup(pyramid_request)
+
+        oauth.make_authorization_request.assert_called_once_with(
+            pyramid_request, pack_state.return_value)
+        assert returned == oauth.make_authorization_request.return_value
+
+
+class TestLTISetupWhenWeOurAccessTokenHasExpired(SharedLTISetupTests):
+    """Unit tests for lti_setup() when our access token is expired."""
+
+    def test_it_gets_the_canvas_servers_url_from_the_database(self,
+                                                              pyramid_request,
+                                                              auth_data_svc):
+        setup.lti_setup(pyramid_request)
+
+        auth_data_svc.get_canvas_server.assert_called_once_with(
+            'TEST_OAUTH_CONSUMER_KEY')
+
+    def test_it_gets_the_courses_files(self, pyramid_request, requests):
+        setup.lti_setup(pyramid_request)
+
+        # It makes a network request to the Canvas API, synchronously, to get
+        # the first page of the list of files that have been uploaded to the
+        # Canvas course.
+        requests.Session.return_value.get.assert_called_once_with(
+            url='https://TEST_CANVAS_SERVER.com/api/v1/courses/TEST_COURSE_ID/files?per_page=100',
+            headers={'Authorization': 'Bearer TEST_OAUTH_ACCESS_TOKEN'},
+        )
+
+    def test_it_starts_an_oauth_flow_to_refresh_the_access_token(self,
+                                                                 pyramid_request,
+                                                                 oauth,
+                                                                 pack_state):
+        returned = setup.lti_setup(pyramid_request)
+
+        oauth.make_authorization_request.assert_called_once_with(
+            pyramid_request, pack_state.return_value, refresh=True)
+        assert returned == oauth.make_authorization_request.return_value
+
+    @pytest.fixture
+    def requests(self, requests):
+        requests.Session.return_value.get.return_value.status_code = 401
+        return requests
+
+
+class TestLTISetupWhenWeHaveAValidAccessToken(SharedLTISetupTests):
+    """Unit tests for lti_setup() when we have a valid access token."""
+
+    def test_it_requests_every_page_of_the_files_list(self,
+                                                      pyramid_request,
+                                                      requests):
+        setup.lti_setup(pyramid_request)
+
+        assert requests.Session.return_value.get.call_args_list == [
+            mock.call(
+                headers={'Authorization': 'Bearer TEST_OAUTH_ACCESS_TOKEN'},
+                url='https://TEST_CANVAS_SERVER.com/api/v1/courses/TEST_COURSE_ID/files?per_page=100',
+            ),
+            mock.call(
+                headers={'Authorization': 'Bearer TEST_OAUTH_ACCESS_TOKEN'},
+                url='SECOND_PAGE',
+            ),
+            mock.call(
+                headers={'Authorization': 'Bearer TEST_OAUTH_ACCESS_TOKEN'},
+                url='THIRD_PAGE',
+            ),
+        ]
+
+    # FIXME: This should be a "when it's a pdf assignment" class.
+    def test_it_returns_lti_pdf_if_its_a_pdf_assignment(self,
+                                                        pyramid_request,
+                                                        pdf):
+        # FIXME: We shouldn't have to actually encode a query string in tests
+        # like this.
+        pyramid_request.query_string = urllib.urlencode({
+            constants.ASSIGNMENT_VALUE: 'TEST_ASSIGNMENT_VALUE',
+            constants.ASSIGNMENT_NAME: 'TEST_ASSIGNMENT_NAME',
+            constants.ASSIGNMENT_TYPE: 'pdf',
+        })
+
+        returned = setup.lti_setup(pyramid_request)
+
+        pdf.lti_pdf.assert_called_once_with(
+            pyramid_request,
+            oauth_consumer_key='TEST_OAUTH_CONSUMER_KEY',
+            lis_outcome_service_url='TEST_LIS_OUTCOME_SERVICE_URL',
+            lis_result_sourcedid='TEST_LIS_RESULT_SOURCEDID',
+            course='TEST_COURSE_ID',
+            name='TEST_ASSIGNMENT_NAME',
+            value='TEST_ASSIGNMENT_VALUE',
+        )
+        assert returned == pdf.lti_pdf.return_value
+
+    # FIXME: This should be a "when it's a web assignment" class.
+    def test_it_returns_web_response_if_its_a_web_assignment(self,
+                                                             pyramid_request,
+                                                             web,
+                                                             auth_data_svc):
+        # FIXME: We shouldn't have to actually encode a query string in tests
+        # like this.
+        pyramid_request.query_string = urllib.urlencode({
+            constants.ASSIGNMENT_VALUE: 'TEST_ASSIGNMENT_VALUE',
+            constants.ASSIGNMENT_NAME: 'TEST_ASSIGNMENT_NAME',
+            constants.ASSIGNMENT_TYPE: 'web',
+        })
+
+        returned = setup.lti_setup(pyramid_request)
+
+        web.web_response.assert_called_once_with(
+            pyramid_request,
+            auth_data_svc,
+            oauth_consumer_key='TEST_OAUTH_CONSUMER_KEY',
+            course='TEST_COURSE_ID',
+            lis_outcome_service_url='TEST_LIS_OUTCOME_SERVICE_URL',
+            lis_result_sourcedid='TEST_LIS_RESULT_SOURCEDID',
+            name='TEST_ASSIGNMENT_NAME',
+            value='TEST_ASSIGNMENT_VALUE',
+        )
+        assert returned == web.web_response.return_value
+
+    # FIXME: This should be a "when it's a document chooser" class.
+    def test_it_returns_a_document_chooser_page_if_theres_no_assignment_type(
+            self, pyramid_request, render):
+        # FIXME: We shouldn't have to actually encode a query string in tests
+        # like this.
+        pyramid_request.query_string = urllib.urlencode({
+            constants.ASSIGNMENT_VALUE: 'TEST_ASSIGNMENT_VALUE',
+            constants.ASSIGNMENT_NAME: 'TEST_ASSIGNMENT_NAME',
+            # No assignment_type param here.
+        })
+
+        response = setup.lti_setup(pyramid_request)
+
+        render.assert_called_once_with(
+            'lti:templates/document_chooser.html.jinja2',
+            {
+                'return_url': 'TEST_EXT_CONTENT_RETURN_URL',
+                'launch_url': 'http://TEST_LTI_SERVER.com/lti_setup'
+                              '?assignment_type=__TYPE__'
+                              '&assignment_name=__NAME__'
+                              '&assignment_value=__VALUE__'
+                              '&return_url=__RETURN_URL__',
+                'pdf_choices': '<ul>'
+                               '<li>'
+                               '<input type="radio" '
+                               'name="pdf_choice" '
+                               'onclick="javascript:go()" '
+                               'value="File One.pdf" '
+                               'id="1">'
+                               'File One.pdf'
+                               '</li>'
+                               '<li>'
+                               '<input type="radio" '
+                               'name="pdf_choice" '
+                               'onclick="javascript:go()" '
+                               'value="File Two.pdf" '
+                               'id="2">'
+                               'File Two.pdf'
+                               '</li>'
+                               '<li>'
+                               '<input type="radio" '
+                               'name="pdf_choice" '
+                               'onclick="javascript:go()" '
+                               'value="File Three.pdf" '
+                               'id="3">'
+                               'File Three.pdf'
+                               '</li>'
+                               '<li>'
+                               '<input type="radio" '
+                               'name="pdf_choice" '
+                               'onclick="javascript:go()" '
+                               'value="File Four.pdf" '
+                               'id="4">'
+                               'File Four.pdf'
+                               '</li>'
+                               '<li>'
+                               '<input type="radio" '
+                               'name="pdf_choice" '
+                               'onclick="javascript:go()" '
+                               'value="File Five.pdf" '
+                               'id="5">'
+                               'File Five.pdf'
+                               '</li>'
+                               '<li>'
+                               '<input type="radio" '
+                               'name="pdf_choice" '
+                               'onclick="javascript:go()" '
+                               'value="File Six.pdf" '
+                               'id="6">'
+                               'File Six.pdf'
+                               '</li>'
+                               '<li>'
+                               '<input type="radio" '
+                               'name="pdf_choice" '
+                               'onclick="javascript:go()" '
+                               'value="File Seven.pdf" '
+                               'id="7">'
+                               'File Seven.pdf'
+                               '</li>'
+                               '<li>'
+                               '<input type="radio" '
+                               'name="pdf_choice" '
+                               'onclick="javascript:go()" '
+                               'value="File Eight.pdf" '
+                               'id="8">'
+                               'File Eight.pdf'
+                               '</li>'
+                               '</ul>'
+            }
+        )
+        assert response.body == render.return_value
+        assert response.content_type == 'text/html'
+        assert response.status_int == 200
+
+    def test_it_omits_non_pdf_files(self, pyramid_request, requests, render):
+        # Make Canvas return three uploaded files, one of them is not a PDF
+        # file.
+        requests.Session.return_value.get.side_effect = None
+        requests.Session.return_value.get.return_value = mock.MagicMock(
+            json=mock.MagicMock(return_value=[
+                {'id': 1, 'display_name': 'File One.pdf'},
+                {'id': 2, 'display_name': 'File Two.doc'},
+                {'id': 3, 'display_name': 'File Three.pdf'},
+            ]),
+        )
+
+        setup.lti_setup(pyramid_request)
+
+        # Only the two PDF files are rendered.
+        assert render.call_args[0][1]['pdf_choices'] == (
+            '<ul>'
+            '<li>'
+            '<input type="radio" '
+            'name="pdf_choice" '
+            'onclick="javascript:go()" '
+            'value="File One.pdf" '
+            'id="1">'
+            'File One.pdf'
+            '</li>'
+            '<li>'
+            '<input type="radio" '
+            'name="pdf_choice" '
+            'onclick="javascript:go()" '
+            'value="File Three.pdf" '
+            'id="3">'
+            'File Three.pdf'
+            '</li>'
+            '</ul>'
+        )
+
+    def test_PDF_files_are_still_rendered(self, pyramid_request, requests, render):
+        # The filename ends in ".PDF" not ".pdf".
+        requests.Session.return_value.get.side_effect = None
+        requests.Session.return_value.get.return_value = mock.MagicMock(
+            json=mock.MagicMock(return_value=[
+                {'id': 1, 'display_name': 'File One.PDF'},
+            ]),
+        )
+
+        setup.lti_setup(pyramid_request)
+
+        assert render.call_args[0][1]['pdf_choices'] == (
+            '<ul>'
+            '<li>'
+            '<input type="radio" '
+            'name="pdf_choice" '
+            'onclick="javascript:go()" '
+            'value="File One.PDF" '
+            'id="1">'
+            'File One.PDF'
+            '</li>'
+            '</ul>'
+        )
+
+    def test_it_can_also_read_return_url_as_return_url(self,
+                                                       pyramid_request,
+                                                       render):
+        del pyramid_request.POST[constants.EXT_CONTENT_RETURN_URL]
+        pyramid_request.query_string = urllib.urlencode({
+            'return_url': 'OAUTH_REDIRECT_RETURN_URL',
+        })
+
+        setup.lti_setup(pyramid_request)
+
+        assert render.call_args[0][1]['return_url'] == 'OAUTH_REDIRECT_RETURN_URL'
+
+    @pytest.fixture
+    def requests(self, requests):
+        # Make get() return a list of three responses one after another.
+        # The first two responses include 'next' URLs.
+        requests.Session.return_value.get.side_effect = [
+            mock.MagicMock(
+                json=mock.MagicMock(return_value=[
+                    {'id': 1, 'display_name': 'File One.pdf'},
+                    {'id': 2, 'display_name': 'File Two.pdf'},
+                    {'id': 3, 'display_name': 'File Three.pdf'},
+                ]),
+                links={'next': {'url': 'SECOND_PAGE'}},
+            ),
+            mock.MagicMock(
+                json=mock.MagicMock(return_value=[
+                    {'id': 4, 'display_name': 'File Four.pdf'},
+                    {'id': 5, 'display_name': 'File Five.pdf'},
+                    {'id': 6, 'display_name': 'File Six.pdf'},
+                ]),
+                links={'next': {'url': 'THIRD_PAGE'}},
+            ),
+            mock.MagicMock(
+                json=mock.MagicMock(return_value=[
+                    {'id': 7, 'display_name': 'File Seven.pdf'},
+                    {'id': 8, 'display_name': 'File Eight.pdf'},
+                ]),
+                links={},
+            ),
+        ]
+
+        return requests
+
+
+@pytest.fixture
+def pyramid_request(pyramid_request):
+    # These params need to actually be in the query string because our code
+    # actually parses request.query_string to get them.
+    #
+    # FIXME: It looks like these params _also_ appear in the request body, so
+    # our code should just get them from the request body like the others.
+    pyramid_request.query_string = urllib.urlencode({
+        constants.ASSIGNMENT_VALUE: 'TEST_ASSIGNMENT_VALUE',
+        constants.ASSIGNMENT_NAME: 'TEST_ASSIGNMENT_NAME',
+        constants.ASSIGNMENT_TYPE: 'TEST_ASSIGNMENT_TYPE',
+    })
+
+    # Add the params that Canvas POSTs to us in the body of its launch request.
+    pyramid_request.POST.update({
+        constants.CUSTOM_CANVAS_ASSIGNMENT_ID: 'TEST_ASSIGNMENT_ID',
+        constants.CUSTOM_CANVAS_COURSE_ID: 'TEST_COURSE_ID',
+        constants.CUSTOM_CANVAS_USER_ID: 'TEST_CANVAS_USER_ID',
+        constants.EXT_CONTENT_RETURN_TYPES: 'TEST_EXT_CONTENT_RETURN_TYPES',
+        constants.EXT_CONTENT_RETURN_URL: 'TEST_EXT_CONTENT_RETURN_URL',
+        constants.LIS_OUTCOME_SERVICE_URL: 'TEST_LIS_OUTCOME_SERVICE_URL',
+        constants.LIS_RESULT_SOURCEDID: 'TEST_LIS_RESULT_SOURCEDID',
+        constants.OAUTH_CONSUMER_KEY: 'TEST_OAUTH_CONSUMER_KEY',
+    })
+
+    return pyramid_request
+
+
+@pytest.fixture
+def pack_state(patch):
+    return patch('lti.views.setup.util.pack_state')
+
+
+@pytest.fixture
+def simple_response(patch):
+    return patch('lti.views.setup.util.simple_response')
+
+
+@pytest.fixture
+def requests(patch):
+    return patch('lti.views.setup.requests')
+
+
+@pytest.fixture
+def render(patch):
+    return patch('lti.views.setup.render')
+
+
+@pytest.fixture
+def oauth(patch):
+    return patch('lti.views.setup.oauth')
+
+
+@pytest.fixture
+def pdf(patch):
+    return patch('lti.views.setup.pdf')
+
+
+@pytest.fixture
+def web(patch):
+    return patch('lti.views.setup.web')
