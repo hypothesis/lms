4252385a026f2e45fa692cfdb6190f3a7b991da1
Sean Hammond <git@snhmnd.fastmail.com>
2017-08-31 14:29:02 +0100

    Use Via for PDF assignments
    This is just a test of whether the Canvas app can just point Via to the
    Canvas file's download URL, rather than downloading the file from Canvas
    to its own disk and serving it, having its own vendored copy of PDF.js,
    and pointing PDF.js at the downloaded file.
    
    This will certainly be slower, because Via is slow, but we hope to
    replace Via with something faster soon anyway.
    
    If this works it simplifies the Canvas app a lot.
    
    I'll need to merge this and push it to QA in order to test it.

--- lti/app.py
+++ lti/app.py
@@ -1,7 +1,6 @@
 ﻿import requests
 import logging
 from pyramid.view import view_config
-from pyramid.response import FileResponse
 from requests_oauthlib import OAuth1
 
 from pyramid.renderers import render
@@ -12,13 +11,6 @@ from lti import constants
 
 
 log = logging.getLogger(__name__)
-
-
-def serve_file(path=None, file=None, request=None, content_type=None):
-    response = FileResponse('%s/%s' % (path, file),
-                            request=request,
-                            content_type=content_type)
-    return response
 
 
 @view_config( route_name='lti_submit' )
@@ -60,17 +52,6 @@ def lti_submit(request, oauth_consumer_key=None, lis_outcome_service_url=None, l
         response = 'Something is wrong. %s %s' % (r.status_code, r.text)        
     return util.simple_response(response)
 
-@view_config( route_name='lti_serve_pdf' )
-def lti_serve_pdf(request):
-    if request.referer is not None and 'pdf.worker.js' in request.referer:
-        return serve_file(path=request.registry.settings['lti_files_path'],
-                      file=request.matchdict['file'] + '.pdf',
-                      request=request,
-                      content_type='application/pdf')
-
-    return util.simple_response('You are not logged in to Canvas')
-
-from pyramid.static import static_view
 
 def create_app(global_config, **settings):  # pylint: disable=unused-argument
     config = configure(settings=settings)
@@ -85,8 +66,6 @@ def create_app(global_config, **settings):  # pylint: disable=unused-argument
     config.include('lti.routes')
     config.include('lti.services')
 
-    pdf_view = static_view('lti:static/pdfjs')
-    config.add_view(pdf_view, route_name='catchall_pdf')
     config.add_static_view(name='export', path='lti:static/export')
 
     config.add_static_view(name='static', path='lti:static')
--- tests/lti/util/filecache_test.py
+++ tests/lti/util/filecache_test.py
@@ -22,21 +22,6 @@ class TestExistsHTML(object):
         assert filecache.exists_html(hash_='abc123', settings=pyramid_request.registry.settings) == isfile_return_value
 
 
-@pytest.mark.usefixtures('os_fixture')
-class TestExistsPDF(object):
-
-    def test_it_calls_isfile_with_the_correct_path(self, os_fixture, pyramid_request):
-        filecache.exists_pdf(hash_='abc123', settings=pyramid_request.registry.settings)
-
-        os_fixture.path.isfile.assert_called_once_with('/var/lib/lti/abc123.pdf')
-
-    @pytest.mark.parametrize('isfile_return_value', [True, False])
-    def test_it_returns_what_isfile_returns(self, os_fixture, isfile_return_value, pyramid_request):
-        os_fixture.path.isfile.return_value = isfile_return_value
-
-        assert filecache.exists_pdf(hash_='abc123', settings=pyramid_request.registry.settings) == isfile_return_value
-
-
 @pytest.fixture
 def os_fixture(patch):
     return patch('lti.util.filecache.os')
