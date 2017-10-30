1bd0c3b93538bb74766141e730558ef2a501ba56
Nick Benoit <nick.benoit14@gmail.com>
2017-10-30 15:54:04 -0600

    add env script

---
+++ env.sh
@@ -0,0 +1,5 @@
+export LTI_SERVER="http://localhost:8001"
+export LTI_CREDENTIALS_URL="http://localhost:8001/lti_credentials"
+export CLIENT_ORIGIN="http://localhost:5000"
+export VIA_URL="http://localhost:9080"
+
