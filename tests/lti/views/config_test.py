1b6ea712c106b0ec8065e76103e1af965965b75d
Nick Benoit <nick.benoit14@gmail.com>
2017-10-31 07:09:13 -0600

    remove old tests

--- tests/lti/views/config_test.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from lti.views import config
-
-
-def test_it_returns_config_xml_object(pyramid_request):
-    actual = config.config_xml(pyramid_request)
-    assert actual['launch_url'] == 'http://example.com/lti_setup'
-    assert actual['resource_selection_url'] == 'http://example.com/canvas/resource_selection'
