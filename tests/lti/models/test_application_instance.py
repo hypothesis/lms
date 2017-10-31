6b3de241a3571aa0d0b4bdca0090c28a65d8a9bc
Nick Benoit <nick.benoit14@gmail.com>
2017-10-31 10:59:44 -0600

    adds application instance test

--- tests/lti/models/test_application_instance.py
+++ tests/lti/models/test_application_instance.py
@@ -5,20 +5,20 @@ from lti.models import ApplicationInstance
 
 
 class TestApplicationInstance(object):
-  def test_test(self):
-#    a = ApplicationInstance()
-#    import pdb; pdb.set_trace()
-    assert True
-#    def test_it_persists_the_client_id_access_token_and_refresh_token(self,
-#                                                                      db_session,
-#                                                                      oauth2_credentials):
-#        db_session.add(OAuth2AccessToken(credentials=oauth2_credentials,
-#                                         access_token="TEST_ACCESS_TOKEN",
-#                                         refresh_token="TEST_REFRESH_TOKEN"))
-#
-#        persisted = db_session.query(OAuth2AccessToken).one()
-#        assert persisted.client_id == oauth2_credentials.client_id
-#        assert persisted.access_token == "TEST_ACCESS_TOKEN"
-#        assert persisted.refresh_token == "TEST_REFRESH_TOKEN"
-#
+    def test_it_persists_the_client_id_access_token_and_refresh_token(self,
+        db_session):
+      db_session.add(
+        ApplicationInstance(
+          consumer_key="TEST_CONSUMER_KEY",
+          shared_secret="TEST_SHARED_SECRET",
+          lms_url="TEST_LMS_URL",
+        )
+      )
+
+      persisted = db_session.query(ApplicationInstance).one()
+
+      assert persisted.consumer_key == "TEST_CONSUMER_KEY"
+      assert persisted.shared_secret == "TEST_SHARED_SECRET"
+      assert persisted.lms_url == "TEST_LMS_URL"
+
 
