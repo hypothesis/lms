0623b3c9236ff118b4a62d8fe3c65f0cad01f2d0
Nick Benoit <nick.benoit14@gmail.com>
2017-11-01 07:33:20 -0600

    adds lti install page

--- lti/views/application_instance.py
+++ lti/views/application_instance.py
@@ -1,17 +1,19 @@
 
 from pyramid.view import view_config
+from pyramid.response import Response
 
 from pyramid.renderers import render
+from lti.models import application_instance as ai
 
 @view_config(route_name='application_instance')
 def create_application_instance(request):
-    html = render('lti:templates/html_assignment.html.jinja2', dict(
-        name=name,
-        url=request.registry.settings['via_url'] + '/' + url,
-        oauth_consumer_key=oauth_consumer_key,
-        lis_outcome_service_url=lis_outcome_service_url,
-        lis_result_sourcedid=lis_result_sourcedid,
-        lti_server=request.registry.settings['lti_server'],
-        client_origin=request.registry.settings['client_origin'],
-    ))
+    instance = ai.build_from_lms_url("")
+    request.db.add(instance)
+    html = render(
+      'lti:templates/application_instance/show_application_instance.html.jinja2', 
+       {
+         "consumer_key": instance.consumer_key,
+         "shared_secret": instance.shared_secret
+       }
+     )
     return Response(html.encode('utf-8'), content_type='text/html')
