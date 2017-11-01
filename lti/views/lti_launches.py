8a16eb4719c4fe1906aee28e510409351a03cdfa
Nick Benoit <nick.benoit14@gmail.com>
2017-11-01 08:51:37 -0600

    look up instance

--- lti/views/lti_launches.py
+++ lti/views/lti_launches.py
@@ -1,15 +1,17 @@
 from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPFound
 from pylti.common import verify_request_common
+from lti.models import application_instance as ai
 
 @view_config(route_name="lti_launches", renderer="json", request_method="POST")
 def lti_launches(request):
-  # TODO obviously this should not be hard coded
-  consumers = {
-    "hypothesis": {
-      "secret": "fake_secret"
-    }
-  }
+  consumer_key = request.params["oauth_consumer_key"]
+  instance = request.db.query(ai.ApplicationInstance).filter(
+    ai.ApplicationInstance.consumer_key == consumer_key).one()
+
+  consumers = {}
+
+  consumers[consumer_key] = { "secret": instance.shared_secret }
 
   # TODO rescue from an invalid lti launch
   verify_request_common(consumers, request.url, request.method, dict(request.headers), dict(request.params))
