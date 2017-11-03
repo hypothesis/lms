410393b874b5f6d0034cc058a1db12119def5ee0
Keith Richards <keith.richards@atomicjolt.com>
2017-11-03 12:17:08 -0600

    Remove debug code.

--- lti/views/lti_launches.py
+++ lti/views/lti_launches.py
@@ -5,7 +5,6 @@ from lti.util.lti_launch import lti_launch
 @view_config(route_name='lti_launches', renderer='lti:templates/lti_launches/create_lti_launch.html.jinja2', request_method='POST')
 @lti_launch
 def lti_launches(request):
-  import pdb; pdb.set_trace()
   return {}
 
 
--- lti/views/module_item_configurations.py
+++ lti/views/module_item_configurations.py
@@ -6,9 +6,6 @@ import pdb
 @view_config(route_name='module_item_configurations', renderer='lti:templates/module_item_configurations/new_module_item_configuration.html.jinja2', request_method='POST')
 @lti_launch
 def new_module_item_configuration(request):
-  # pdb.set_trace()
-  # ext_content_return_url
-  # launch_presentation_return_url
   return {
     'launch_presentation_return_url': request.params['launch_presentation_return_url'],
     'lti_launch_url': request.route_url('lti_launches') + '?url='
