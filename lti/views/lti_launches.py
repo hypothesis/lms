61fe514c7a05c0b9653eead9fd6b68d664c091b1
dittonjs <jditton.atomic@gmail.com>
2017-11-03 16:24:11 -0600

    adds student view and fixes security hole

--- lti/views/lti_launches.py
+++ lti/views/lti_launches.py
@@ -5,6 +5,9 @@ from lti.util.lti_launch import lti_launch
 @view_config(route_name='lti_launches', renderer='lti:templates/lti_launches/create_lti_launch.html.jinja2', request_method='POST')
 @lti_launch
 def lti_launches(request):
-  return {}
+  import pdb; pdb.set_trace()
+  return {
+    'hypothesis_url': 'https://via.hypothes.is/' + request.params['url']
+  }
 
 
