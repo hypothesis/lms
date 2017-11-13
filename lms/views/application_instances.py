8f946d3d2736f0266acb76e17fdce589b4f18718
Keith Richards <keith.richards@atomicjolt.com>
2017-11-13 16:35:03 -0700

    Add pyramid_mailer to requirments and config files.

--- lms/views/application_instances.py
+++ lms/views/application_instances.py
@@ -1,3 +1,4 @@
+from pyramid_mailer.message import Message
 from pyramid.view import view_config
 from lms.models import application_instance as ai
 
@@ -9,6 +10,14 @@ def create_application_instance(request):
 
     instance = ai.build_from_lms_url(request.params['lms_url'])
     request.db.add(instance)
+
+    # TODO: Send email about signing up
+    message = Message(subject="hello world",
+                      sender="admin@mysite.com",
+                      recipients=["keith.richards@atomicjolt.com"],
+                      body="hello, keith")
+    mailer = request.mailer
+    mailer.send(message)
 
     return {
         'consumer_key': instance.consumer_key,
