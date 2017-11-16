9a0686cf8f24cb566d539acb87312ef0160ecae1
Keith Richards <keith.richards@atomicjolt.com>
2017-11-16 10:54:11 -0700

    Test logging missing ini settings

--- tests/lms/views/test_application_instance.py
+++ tests/lms/views/test_application_instance.py
@@ -11,4 +11,20 @@ class TestApplicationInstance(object):
         }
         create_application_instance(pyramid_request)
         assert pyramid_request.db.query(ApplicationInstance).filter(
-            ApplicationInstance.lms_url == pyramid_request.params['lms_url']).count() == 1
+            ApplicationInstance.lms_url == pyramid_request.params[
+                'lms_url']).count() == 1
+
+    def test_exception_no_mta(self, pyramid_request, capfd):
+        pyramid_request.method = 'POST'
+        pyramid_request.params = {
+            'lms_url': 'canvas.example.com',
+            'email': 'email@example.com',
+        }
+
+        create_application_instance(pyramid_request)
+
+        out, err = capfd.readouterr()
+        assert 'new_lms_email_recipient' in err
+        assert 'new_lms_email_recipient' in err
+
+
