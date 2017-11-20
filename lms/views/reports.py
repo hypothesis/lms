bbda495ac3c0e2727ba1f3d01eea1331461e54d8
Keith Richards <keith.richards@atomicjolt.com>
2017-11-20 16:37:32 -0700

    List all application instances

--- lms/views/reports.py
+++ lms/views/reports.py
@@ -1,7 +1,10 @@
 from pyramid.view import view_config
+from lms.models.application_instance import ApplicationInstance
 
 
 @view_config(route_name='reports',
              renderer="lms:templates/reports/application_report.html.jinja2")
-def list_application_instances(_):
-    return {}
+def list_application_instances(request):
+    return {
+        'apps': request.db.query(ApplicationInstance).all()
+    }
