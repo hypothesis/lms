a3ddfda524dae52ee55db87acdbd0207f026cba9
Keith Richards <keith.richards@atomicjolt.com>
2017-11-20 17:13:07 -0700

    Add email and created to report template

--- lms/views/reports.py
+++ lms/views/reports.py
@@ -2,9 +2,12 @@ from pyramid.view import view_config
 from lms.models.application_instance import ApplicationInstance
 
 
+# TODO: add authentication
 @view_config(route_name='reports',
              renderer="lms:templates/reports/application_report.html.jinja2")
 def list_application_instances(request):
+    # TODO: Is it a security issue that this dictionary has the secret in it?
+    # Maybe better to create a new dict, leaving out secret.
     return {
         'apps': request.db.query(ApplicationInstance).all()
     }
