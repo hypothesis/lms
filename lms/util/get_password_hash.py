0760ad615fd47541c8d5c0401fd46458aad52762
Keith Richards <keith.richards@atomicjolt.com>
2017-11-28 16:39:08 -0700

    Add tool to find password hash

---
+++ lms/util/get_password_hash.py
@@ -0,0 +1,17 @@
+import hashlib
+import os
+import binascii
+
+password = input("Please enter a password: ").encode('utf8')
+salt = input("Please enter a salt (leave blank to have one created): ")
+if salt == '':
+    salt = binascii.hexlify(os.urandom(8))
+else:
+    salt = salt.encode("utf8")
+
+pw_hash = binascii.hexlify(
+    hashlib.pbkdf2_hmac('sha256', password, salt, 1000000)
+)
+
+print(f"password hash: {pw_hash}")
+print(f"salt: {salt}")
