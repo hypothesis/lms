49bf2560aaece3de21f6c4026a8f75df47dd3053
Keith Richards <keith.richards@atomicjolt.com>
2017-11-29 12:15:49 -0700

    Prevent auto run of get_password_hash.py

--- lms/util/get_password_hash.py
+++ lms/util/get_password_hash.py
@@ -2,16 +2,17 @@ import hashlib
 import os
 import binascii
 
-password = input("Please enter a password: ").encode('utf8')
-salt = input("Please enter a salt (leave blank to have one created): ")
-if salt == '':
-    salt = binascii.hexlify(os.urandom(8))
-else:
-    salt = salt.encode("utf8")
+if __name__ == "__main__":
+    password = input("Please enter a password: ").encode('utf8')
+    salt = input("Please enter a salt (leave blank to have one created): ")
+    if salt == '':
+        salt = binascii.hexlify(os.urandom(8))
+    else:
+        salt = salt.encode("utf8")
 
-pw_hash = binascii.hexlify(
-    hashlib.pbkdf2_hmac('sha256', password, salt, 1000000)
-)
+    pw_hash = binascii.hexlify(
+        hashlib.pbkdf2_hmac('sha256', password, salt, 1000000)
+    )
 
-print(f"password hash: {pw_hash}")
-print(f"salt: {salt}")
+    print(f"password hash: {pw_hash}")
+    print(f"salt: {salt}")
