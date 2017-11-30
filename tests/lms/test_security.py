6aea2d0c2020293bc13a06f1d1ab137077f22c01
Keith Richards <keith.richards@atomicjolt.com>
2017-11-29 17:33:04 -0700

    Code formating

--- tests/lms/test_security.py
+++ tests/lms/test_security.py
@@ -11,14 +11,14 @@ class TestSecurityPassword(object):
 
     def test_wrong_password(self):
         salt = 'fbe82ee0da72b77b'
-        #password = 'asdf'
+        # password = 'asdf'
         wrong_password = 'wrooong'
         expected_hash = 'e46df2a5b4d50e259b5154b190529483a5f8b7aaaa22a50447e377d33792577a'
 
         assert not security.check_password(wrong_password, expected_hash, salt)
 
     def test_wrong_salt(self):
-        #salt = 'fbe82ee0da72b77b'
+        # salt = 'fbe82ee0da72b77b'
         wrong_salt = 'nopenope'
         password = 'asdf'
         expected_hash = 'e46df2a5b4d50e259b5154b190529483a5f8b7aaaa22a50447e377d33792577a'
