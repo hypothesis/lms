a5d8047be293412096e8caeea884bda1374c8305
dittonjs <jditton.atomic@gmail.com>
2017-12-01 09:12:13 -0700

    adds basic canvas api support

--- lms/routes.py
+++ lms/routes.py
@@ -7,6 +7,7 @@ def includeme(config):
     config.add_route('welcome', '/welcome')
     config.add_route('config_xml', '/config_xml')
     config.add_route('module_item_configurations', '/module_item_configurations')
+    config.add_route('test_api', '/test_api')
 
     # lms routes
     config.add_route('lti_launches', '/lti_launches')
