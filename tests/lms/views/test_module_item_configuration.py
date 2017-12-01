19aaa2d17208a30c1eb36a37fba3e23227401327
Keith Richards <keith.richards@atomicjolt.com>
2017-12-01 10:03:59 -0700

    Test bad jwt token

--- tests/lms/views/test_module_item_configuration.py
+++ tests/lms/views/test_module_item_configuration.py
@@ -11,3 +11,20 @@ class TestModuleItemConfiguration(object):
         create_module_item_configuration(authenticated_request)
         new_count = authenticated_request.db.query(ModuleItemConfiguration).count()
         assert new_count == initial_count + 1
+
+    def test_bad_jwt_fails_authentication(self, authenticated_request):
+        initial_count = authenticated_request.db.query(
+            ModuleItemConfiguration).count()
+        authenticated_request.params['document_url'] = 'https://www.example.com'
+        authenticated_request.params['resource_link_id'] = 'test'
+        authenticated_request.params['tool_consumer_instance_guid'] = 'test'
+        authenticated_request.params['jwt_token'] = 'wrongjwttoken'
+
+        response = create_module_item_configuration(authenticated_request)
+
+        new_count = authenticated_request.db.query(
+            ModuleItemConfiguration).count()
+        assert new_count == initial_count
+        assert 'Unauthenticated Request' in str(response.body)
+
+
