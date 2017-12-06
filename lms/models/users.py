94e43f6b426d040a79de2f56a69a5626717ef5c8
Nick Benoit <nick.benoit14@gmail.com>
2017-12-06 15:18:23 -0700

    Fix oauth and users lint errors

--- lms/models/users.py
+++ lms/models/users.py
@@ -3,7 +3,7 @@ from lms.db import BASE
 
 
 class User(BASE):
-
+    """Class to represent a single lti user"""
     __tablename__ = 'users'
 
     id = sa.Column(sa.Integer, autoincrement=True, primary_key=True)
@@ -11,12 +11,14 @@ class User(BASE):
     lms_guid = sa.Column(sa.String, index=True)
 
 
-def find_by_lms_guid(session, lms_guid):
-  return session.query(User).filter(
-    User.lms_guid == lms_guid).one_or_none()
+def find_by_lms_guid(session, user_lms_guid):
+    """Find a user from their unique lti launch user_id guid"""
+    return session.query(User).filter(
+        User.lms_guid == user_lms_guid).one_or_none()
 
 
 def build_from_lti_params(lti_launch_params):
+    """Build a user from an lti launch POST request"""
     return User(
       lms_guid=lti_launch_params['user_id'],
     )
--- lms/views/oauth.py
+++ lms/views/oauth.py
@@ -1,48 +1,43 @@
+"""Oauth endpoint views"""
+import json
 from pyramid.view import view_config
-from pyramid.httpexceptions import HTTPFound
 from requests_oauthlib import OAuth2Session
 from lms.models.tokens import update_user_token, build_token_from_oauth_response
 from lms.models.oauth_state import find_user_from_state, find_by_state
 from lms.util.lti_launch import get_application_instance
-from lms.util.lti_launch import lti_launch
-from lms.util.view_renderer import view_renderer
-from lms.models.oauth_state import find_by_state
 from lms.views.content_item_selection import content_item_form
-from lms.models.application_instance import find_by_oauth_consumer_key
-import json
 
-token_endpoint = 'login/oauth2/token'
-
-
-def build_canvas_token_url(lms_url, token_endpoint):
-    return lms_url + '/' + token_endpoint
+def build_canvas_token_url(lms_url):
+    """Builds a canvas token url from the base lms url and the token"""
+    return lms_url + '/login/oauth2/token'
 
 @view_config(route_name='canvas_oauth_callback', request_method='GET')
 def canvas_oauth_callback(request):
-  client_id = request.registry.settings['oauth.client_id']
-  client_secret = request.registry.settings['oauth.client_secret']
+    """Route to handle content item selection oauth response"""
+    client_id = request.registry.settings['oauth.client_id']
+    client_secret = request.registry.settings['oauth.client_secret']
 
-  state = request.params['state']
-  oauth_state = find_by_state(request.db, state)
-  lti_params = json.loads(oauth_state.lti_params)
-  consumer_key = lti_params['oauth_consumer_key']
-  application_instance = get_application_instance(request.db, consumer_key)
-  token_url = build_canvas_token_url(application_instance.lms_url,
-          token_endpoint)
+    state = request.params['state']
+    oauth_state = find_by_state(request.db, state)
+    lti_params = json.loads(oauth_state.lti_params)
+    consumer_key = lti_params['oauth_consumer_key']
+    application_instance = get_application_instance(request.db, consumer_key)
+    token_url = build_canvas_token_url(application_instance.lms_url)
 
-  session = OAuth2Session(client_id, state=state)
-  oauth_resp = session.fetch_token(token_url, client_secret=client_secret,
-                             authorization_response=request.url, code=request.params['code'])
+    session = OAuth2Session(client_id, state=state)
+    oauth_resp = session.fetch_token(token_url, client_secret=client_secret,
+                                     authorization_response=request.url,
+                                     code=request.params['code'])
 
-  user = find_user_from_state(request.db, state)
+    user = find_user_from_state(request.db, state)
 
-  new_token = build_token_from_oauth_response(oauth_resp)
-  token = update_user_token(request.db, new_token, user)
+    new_token = build_token_from_oauth_response(oauth_resp)
+    update_user_token(request.db, new_token, user)
 
-  return content_item_form(
-      request,
-      lti_params=lti_params,
-      lms_url=application_instance.lms_url,
-      content_item_return_url=lti_params['content_item_return_url'],
-      jwt=None
-  )
+    return content_item_form(
+        request,
+        lti_params=lti_params,
+        lms_url=application_instance.lms_url,
+        content_item_return_url=lti_params['content_item_return_url'],
+        jwt=None
+    )
