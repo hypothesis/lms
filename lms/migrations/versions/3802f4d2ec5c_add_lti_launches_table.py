8edecd2aab131924b91034efeabc9e0e48fef9b5
Keith Richards <keith.richards@atomicjolt.com>
2017-12-11 16:57:32 -0700

    Add lti_launches table

---
+++ lms/migrations/versions/3802f4d2ec5c_add_lti_launches_table.py
@@ -0,0 +1,33 @@
+"""
+Add lti_launches table
+
+Revision ID: 3802f4d2ec5c
+Revises: 58f2693de313
+Create Date: 2017-12-11 14:21:53.686455
+
+"""
+from datetime import datetime
+
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '3802f4d2ec5c'
+down_revision = '58f2693de313'
+
+
+def upgrade():
+    op.create_table(
+        'lti_launches',
+        sa.Column('id', sa.Integer, autoincrement=True, primary_key=True),
+        sa.Column('created', sa.TIMESTAMP, default=datetime.utcnow),
+        sa.Column('context_id', sa.Integer),
+        sa.Column(
+            'application_instances_id',
+            sa.Integer,
+            sa.ForeignKey('application_instances.id'))
+    )
+
+
+def downgrade():
+    op.drop_table('lti_launches')
---
+++ lms/models/lti_launches.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+import sqlalchemy as sa
+from lms.db import BASE
+
+
+class LtiLaunches(BASE):
+    """Track each LTI launch"""
+
+    __tablename__ = 'lti_launches'
+
+    id = sa.Column(sa.Integer, autoincrement=True, primary_key=True)
+    created = sa.Column(sa.TIMESTAMP, default=datetime.utcnow())
+    context_id = sa.Column(sa.Integer)
+    application_instance_id = sa.Column(
+        sa.Integer,
+        sa.ForeignKey('ApplicationInstance.id'))
