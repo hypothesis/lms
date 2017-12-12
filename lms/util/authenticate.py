1d1109478546993d15b0611e419f5eed0ef8e929
Nick Benoit <nick.benoit14@gmail.com>
2017-12-12 10:25:36 -0700

    Add user support to authentication util

--- lms/util/authenticate.py
+++ lms/util/authenticate.py
@@ -2,6 +2,7 @@
 import jwt
 from lms.config.settings import env_setting
 from pyramid.response import Response
+from lms.models.users import find_by_lms_guid
 
 
 def authenticate(view_function):
@@ -9,9 +10,19 @@ def authenticate(view_function):
     def wrapper(request):
         """Validate the JWT signature."""
         try:
-            decoded_jwt = jwt.decode(request.params['jwt_token'], env_setting('JWT_SECRET'), algorithms=['HS256'])
+
+            jwt_token = request.headers['Authorization'] or request.params['jwt_token']
+            decoded_jwt = jwt.decode(
+                                    jwt_token,
+                                    env_setting('JWT_SECRET'),
+                                    algorithms=['HS256'])
+
+            lms_guid = decoded_jwt['user_id']
+            user = find_by_lms_guid(request.db, lms_guid)
+
+
         except (jwt.exceptions.DecodeError, KeyError):
             return Response('<p>Error: Unauthenticated Request</p>')
 
-        return view_function(request, decoded_jwt)
+        return view_function(request, decoded_jwt, user=user)
     return wrapper
--- lms/views/canvas_proxy.py
+++ lms/views/canvas_proxy.py
@@ -1,7 +1,13 @@
+import jwt
+
 from pyramid.view import view_config
 from pyramid.response import Response
 from lms.models import application_instance as ai
+#from lms.users import find_by_lms_guid 
 from lms.util.canvas_api import CanvasApi, GET
+from lms.config.settings import env_setting
+from lms.util.authenticate import authenticate 
+
 
 # TODO
 # * Add proxy api
@@ -10,15 +16,20 @@ from lms.util.canvas_api import CanvasApi, GET
 #    * Look up user from jwt
 #
 #Use http://niemeyer.net/mocker for mocking 
+
+@authenticate
 @view_config(route_name='canvas_proxy', renderer='json')
-def canvas_proxy(request):
+def canvas_proxy(request, decoded_jwt, user):
     # Request canvas files
+
 #    canvas_api = CanvasApi(
 #      new_token.access_token,
 #      application_instance.lms_url
 #    )
+    
+#    response = canvas_api.get_canvas_course_files(1773, {})
+    import pdb; pdb.set_trace()
 
-    response = canvas_api.get_canvas_course_files(1773, {})
     return Response({
       'bad': True    
     }, status=200)
