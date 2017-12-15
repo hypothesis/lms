5e14a84d431383cdb41179461e54ee815a8c45aa
Nick Benoit <nick.benoit14@gmail.com>
2017-12-15 14:53:16 -0700

    Add canvas proxy view docs

--- lms/views/canvas_proxy.py
+++ lms/views/canvas_proxy.py
@@ -9,10 +9,17 @@ from lms.util.canvas_api import canvas_api
 @authenticate
 @canvas_api
 def canvas_proxy(request, decoded_jwt, user, canvas_api):
+    """
+    Route to support proxying client side XHR requests
+
+    We can't send the canvas token obtained through the oauth process
+    to the client side application, so we must proxy requets through
+    this view to add the canvas token before requests reach canvas
+    """
     result = canvas_api.proxy(
-             request.params['endpoint_url'],
-             request.params['method'],
-             request.params['params'])
+        request.params['endpoint_url'],
+        request.params['method'],
+        request.params['params'])
     response = None
     try:
         response = result.json()
