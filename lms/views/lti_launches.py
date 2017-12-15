c8ab331a00193ed9f33b41bd06065b243e6f3167
Nick Benoit <nick.benoit14@gmail.com>
2017-12-14 20:16:49 -0700

    Refactor lti_launches

--- lms/views/lti_launches.py
+++ lms/views/lti_launches.py
@@ -13,6 +13,22 @@ def can_configure_module_item(roles):
     return any(role in lower_cased_roles for role in allowed_roles)
 
 
+def is_canvas_file(request):
+    return 'canvas_file' in request.params
+
+def is_url_configured(request):
+    return 'url' in request.params
+
+def is_authorized_to_configure(request):
+    return can_configure_module_item(request.params['roles'])
+
+def is_db_configured(request):
+    config = request.db.query(ModuleItemConfiguration).filter(
+            ModuleItemConfiguration.resource_link_id == request.params['resource_link_id'] and
+            ModuleItemConfiguration.tool_consumer_instance_guid == request.params['tool_consumer_instance_guid']
+        )
+    return config.count == 1
+
 @view_config(route_name='lti_launches', request_method='POST')
 @lti_launch()
 @associate_user
@@ -29,27 +45,30 @@ def lti_launches(request, jwt, user=None):
     3. If a teacher launches and no document has been configured, ict renders a form that allows
     them to configure the document.
     """
-    if 'url' not in request.params:
+
+    if is_url_configured(request): # We are launching from the provided url
+        return _view_document(request, document_url=request.params['url'], jwt=jwt)
+    elif is_db_configured(request): # We are launching a module item saved in the db
         config = request.db.query(ModuleItemConfiguration).filter(
             ModuleItemConfiguration.resource_link_id == request.params['resource_link_id'] and
             ModuleItemConfiguration.tool_consumer_instance_guid == request.params['tool_consumer_instance_guid']
         )
-        if config.count() >= 1:
-            return _view_document(request, document_url=config.one().document_url, jwt=jwt)
-        elif can_configure_module_item(request.params['roles']):
-            consumer_key = request.params['oauth_consumer_key']
-            application_instance = get_application_instance(request.db, consumer_key)
-            return content_item_form(
-                request,
-                lti_params=request.params,
-                content_item_return_url=request.route_url('module_item_configurations'),
-                lms_url=application_instance.lms_url,
-                jwt=jwt
-            )
+        return _view_document(request, document_url=config.one().document_url, jwt=jwt)
+    elif is_canvas_file(request): # We are launching a canvas file
+        pass
+        # TODO Get a public viewing url
+    elif is_authorized_to_configure(request):
+        consumer_key = request.params['oauth_consumer_key']
+        application_instance = get_application_instance(request.db, consumer_key)
+        return content_item_form(
+            request,
+            lti_params=request.params,
+            content_item_return_url=request.route_url('module_item_configurations'),
+            lms_url=application_instance.lms_url,
+            jwt=jwt
+        )
+    else: # Not configured
         return _unauthorized(request)
-
-    return _view_document(request, document_url=request.params['url'], jwt=jwt)
-
 
 @view_renderer(renderer='lms:templates/lti_launches/new_lti_launch.html.jinja2')
 def _view_document(request, document_url, jwt):
