0802bd3970a2dbe76b66a9ad03bb22d21005c8d9
Nick Benoit <nick.benoit14@gmail.com>
2017-12-20 08:09:55 -0700

    Send correct resource_link_id and tool_consumer_instance_guid

--- lms/views/content_item_selection.py
+++ lms/views/content_item_selection.py
@@ -40,10 +40,7 @@ def content_item_selection(request, _, _user=None):
 @view_renderer(
     renderer='lms:templates/content_item_selection/new_content_item_selection.html.jinja2')
 def content_item_form(request, lti_params, lms_url, content_item_return_url, jwt=None):
-    params = {
-        'content_item_return_url': content_item_return_url,
-        'lti_launch_url': request.route_url('lti_launches'),
-        'form_fields': {
+    form_fields = {
             'lti_message_type': 'ContentItemSelection',
             'lti_version': lti_params['lti_version'],
             'oauth_version': lti_params['oauth_version'],
@@ -51,10 +48,16 @@ def content_item_form(request, lti_params, lms_url, content_item_return_url, jwt
             'oauth_consumer_key': lti_params['oauth_consumer_key'],
             'oauth_signature_method': lti_params['oauth_signature_method'],
             'oauth_signature': lti_params['oauth_signature'],
-            'resource_link_id': lti_params['oauth_signature'],
-            'tool_consumer_instance_guid': lti_params['oauth_signature'],
             'jwt_token': jwt
-        },
+        }
+    if('resource_link_id' in lti_params):
+            form_fields['resource_link_id'] = lti_params['resource_link_id']
+    if('tool_consumer_instance_guid' in lti_params):
+            form_fields['tool_consumer_instance_guid'] = lti_params['tool_consumer_instance_guid']
+    params = {
+        'content_item_return_url': content_item_return_url,
+        'lti_launch_url': request.route_url('lti_launches'),
+        'form_fields': form_fields,
         'google_client_id': request.registry.settings['google_client_id'],
         'google_developer_key': request.registry.settings['google_developer_key'],
         'google_app_id': request.registry.settings['google_app_id'],
