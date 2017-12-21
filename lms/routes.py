0e7e73a8f7c822b190f0fc131d7ea1666c66524e
dittonjs <jditton.atomic@gmail.com>
2017-12-21 07:49:06 -0700

    remove test route

--- lms/routes.py
+++ lms/routes.py
@@ -10,7 +10,6 @@ def includeme(config):
     config.add_route('reports', '/reports')
     config.add_route('config_xml', '/config_xml')
     config.add_route('module_item_configurations', '/module_item_configurations')
-    config.add_route('test_api', '/test_api')
     config.add_route('canvas_proxy', '/canvas_proxy')
 
     # lms routes
