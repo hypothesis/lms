68fdbe1bc350b1779376e306b03f67bd182f12a9
Nick Benoit <nick.benoit14@gmail.com>
2018-03-01 20:44:54 -0700

    Update report data

--- lms/views/reports.py
+++ lms/views/reports.py
@@ -8,8 +8,13 @@ from lms.models.lti_launches import LtiLaunches
              renderer="lms:templates/reports/application_report.html.jinja2",
              permission='view')
 def list_application_instances(request):
+    launches = request.db.execute("SELECT context_id, count(context_id), \
+      lms_url, requesters_email, consumer_key FROM lti_launches LEFT JOIN \
+      application_instances on \
+      lti_launches.lti_key=application_instances.consumer_key GROUP BY \
+      context_id, consumer_key, requesters_email, lms_url;").fetchall()
     return {
         'apps': request.db.query(ApplicationInstance).all(),
-        'launches': request.db.query(LtiLaunches).all(),
+        'launches': launches,
         'logout_url': request.route_url('logout')
     }
