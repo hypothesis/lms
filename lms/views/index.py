d9240e65840e17dd524aafcd2b6b92e7312a7e2c
Lyza Danger Gardner <lyza@lyza.com>
2018-07-10 13:42:08 -0400

    fixup! Add verification meta tag to base template and `index` view

--- lms/views/index.py
+++ lms/views/index.py
@@ -3,6 +3,6 @@ from pyramid.view import view_config
 
 @view_config(route_name='index',
              renderer="lms:templates/base.html.jinja2")
-def index(request):
-    '''Render an empty page that contains a needed Google verification meta tag'''
+def index():
+    """Render an empty page that contains a needed Google verification meta tag."""
     return {}
