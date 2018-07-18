0ecaf5136b77c20912cf6d377f80f3d32cebf9b0
Sean Hammond <git@snhmnd.fastmail.com>
2018-07-18 18:59:34 +0100

    Add generate_display_name() function
    This adds a function for generating h display names from LTI launch
    request parameters.
    
    Nothing calls this function yet.

--- lms/util/h.py
+++ lms/util/h.py
@@ -12,8 +12,32 @@ USERNAME_ALLOWED_CHARS = string.ascii_letters + string.digits + "._"
 """The characters that are allowed in h usernames."""
 
 
+DISPLAY_NAME_MAX_LENGTH = 30
+"""The maximum length of an h display name."""
+
+
 USERNAME_MAX_LENGTH = 30
 """The maximum length of an h username."""
+
+
+def generate_display_name(request_params):
+    full_name = (request_params.get("lis_person_name_full") or "").strip()
+    given_name = (request_params.get("lis_person_name_given") or "").strip()
+    family_name = (request_params.get("lis_person_name_family") or "").strip()
+
+    if full_name:
+        display_name = full_name
+    else:
+        display_name = " ".join((given_name, family_name))
+
+    display_name = display_name.strip()
+
+    display_name = display_name or "Anonymous"
+
+    if len(display_name) > DISPLAY_NAME_MAX_LENGTH:
+        display_name = display_name[:DISPLAY_NAME_MAX_LENGTH - 1].rstrip() + "…"
+
+    return display_name
 
 
 def generate_username(request_params):
