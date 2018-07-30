1bb4a05c0d859bdae852cd5cb0c2fb682880b1fa
Sheetal Umesh Kumar <sheetaluk@hypothes.is>
2018-07-30 09:21:35 -0400

    Style error pages so they look better.
    See https://github.com/hypothesis/lms/issues/142

---
+++ lms/views/error.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from pyramid import httpexceptions
+from pyramid import i18n
+from pyramid.view import view_config
+from pyramid.view import view_defaults
+from pyramid.settings import asbool
+
+
+_ = i18n.TranslationStringFactory(__package__)
+
+
+@view_defaults(renderer='lms:templates/error.html.jinja2')
+class ErrorController(object):
+    """Show user an error page and an appropriate message."""
+
+    def __init__(self, exc, request):
+        """Store request and exception information."""
+        self.exc = exc
+        self.request = request
+
+    @view_config(context=httpexceptions.HTTPError)
+    @view_config(context=httpexceptions.HTTPServerError)
+    def httperror(self):
+        self.request.response.status_int = self.exc.status_int
+        # If code raises an HTTPError or HTTPServerError we assume this was
+        # deliberately raised and:
+        # 1. Show the user an error page including specific error message
+        # 2. _Do not_ report the error to Sentry
+        return {'message': str(self.exc)}
+
+    @view_config(context=Exception)
+    def error(self):
+        # In debug mode re-raise exceptions so that they get printed in the
+        # terminal.
+        if asbool(self.request.registry.settings.get('debug')):
+            raise self.exc
+
+        self.request.response.status_int = 500
+
+        # If code raises a non-HTTPException exception we assume it was a bug
+        # and:
+        # 1. Show the user a generic error page
+        # 2. Report the details of the error to Sentry
+        self.request.raven.captureException()
+        return {'message': _("Sorry, but something went wrong. "
+                             "The issue has been reported and we'll try to "
+                             "fix it.")}
---
+++ tests/lms/views/error_test.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from pyramid import httpexceptions
+import pytest
+
+from lms.views import error
+
+
+class TestErrorController(object):
+    def test_it_sets_correct_message_for_http_error(self, pyramid_request):
+        exc = httpexceptions.HTTPError('test http error msg', status_code=500)
+
+        error_controller = error.ErrorController(exc, pyramid_request)
+        resp = error_controller.httperror()
+
+        assert resp['message'] == 'test http error msg'
+
+    def test_it_sets_correct_request_status_int_for_http_error(self, pyramid_request):
+        exc = httpexceptions.HTTPError('test http error msg', status_code=500)
+
+        error_controller = error.ErrorController(exc, pyramid_request)
+        error_controller.httperror()
+
+        assert pyramid_request.response.status_int == exc.status_int
+
+    def test_it_sets_correct_message_for_http_server_error(self, pyramid_request):
+        exc = httpexceptions.HTTPServerError('test server error msg', status_code=500)
+
+        error_controller = error.ErrorController(exc, pyramid_request)
+        resp = error_controller.httperror()
+
+        assert resp['message'] == 'test server error msg'
+
+    def test_it_sets_correct_request_status_int_for_http_server_error(self, pyramid_request):
+        exc = httpexceptions.HTTPServerError('test server error msg', status_code=500)
+
+        error_controller = error.ErrorController(exc, pyramid_request)
+        error_controller.httperror()
+
+        assert pyramid_request.response.status_int == exc.status_int
+
+    def test_it_sets_correct_message_for_non_http_error(self, pyramid_request):
+        exc = Exception()
+
+        error_controller = error.ErrorController(exc, pyramid_request)
+        resp = error_controller.error()
+
+        assert resp['message'] == 'Sorry, but something went wrong. The issue has been reported and we\'ll try to fix it.'
+
+    def test_it_logs_non_http_error_in_sentry(self, pyramid_request):
+        exc = Exception()
+
+        error_controller = error.ErrorController(exc, pyramid_request)
+        error_controller.error()
+
+        pyramid_request.raven.captureException.assert_called_once()
+
+    def test_it_re_raises_exception_in_debug_mode(self, pyramid_request):
+        exc = Exception()
+        pyramid_request.registry.settings = {'debug': True}
+
+        error_controller = error.ErrorController(exc, pyramid_request)
+
+        with pytest.raises(Exception):
+            error_controller.error()
