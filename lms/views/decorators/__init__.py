d75d05961fa36ba26c245f1ffb88ba6b82591c2c
Sean Hammond <git@snhmnd.fastmail.com>
2018-07-31 11:17:23 +0100

    Create users in h on LTI launches
    Whenever the app is LTI launched, whether during assignment creation or
    when launching an assignment, call the h API to create a user in h
    corresponding to the authorized LTI user, if such an h user doesn't
    already exist.

---
+++ lms/views/decorators/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Python decorators meant to be used for decorating Pyramid view functions."""
+
+from __future__ import unicode_literals
+
+from lms.views.decorators.h_api import create_h_user
+
+__all__ = (
+    "create_h_user",
+)
---
+++ lms/views/decorators/h_api.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+"""View decorators for working with the h API."""
+
+from __future__ import unicode_literals
+
+import json
+import requests
+
+from pyramid.httpexceptions import HTTPBadGateway
+from pyramid.httpexceptions import HTTPBadRequest
+from pyramid.httpexceptions import HTTPGatewayTimeout
+
+from lms import util
+from lms.util import MissingToolConsumerIntanceGUIDError
+from lms.util import MissingUserIDError
+
+
+def create_h_user(wrapped):  # noqa: MC0001
+    """
+    Create a user in h if one doesn't exist already.
+
+    Call the h API to create a user for the authorized LTI user, if one doesn't
+    exist already.
+    """
+    def wrapper(request, jwt):  # pylint: disable=too-many-branches
+        params = request.params
+
+        try:
+            oauth_consumer_key = params["oauth_consumer_key"]
+        except KeyError:
+            raise HTTPBadRequest('Required paramer "oauth_consumer_key" missing from LTI params')
+
+        # Only create users for application instances that we've enabled the
+        # auto provisioning features for.
+        enabled_consumer_keys = request.registry.settings["auto_provisioning"]
+        if oauth_consumer_key not in enabled_consumer_keys:
+            return wrapped(request, jwt)
+
+        # Our OAuth 2.0 client_id and client_secret for authenticating to the h API.
+        client_id = request.registry.settings["h_client_id"]
+        client_secret = request.registry.settings["h_client_secret"]
+
+        # The authority that we'll create h users and groups in.
+        authority = request.registry.settings["h_authority"]
+
+        # The URL of h's create user API endpoint.
+        create_user_api_url = request.registry.settings["h_api_url"] + "/users"
+
+        try:
+            username = util.generate_username(params)
+            provider = util.generate_provider(params)
+            provider_unique_id = util.generate_provider_unique_id(params)
+        except MissingToolConsumerIntanceGUIDError:
+            raise HTTPBadRequest('Required parameter "tool_consumer_instance_guid" missing from LTI params')
+        except MissingUserIDError:
+            raise HTTPBadRequest('Required parameter "user_id" missing from LTI params')
+
+        # The user data that we will post to h.
+        user_data = {
+            "username": username,
+            "display_name": util.generate_display_name(params),
+            "authority": authority,
+            "identities": [
+                {
+                    "provider": provider,
+                    "provider_unique_id": provider_unique_id,
+                }
+            ],
+        }
+
+        # Call the h API to create the user in h if it doesn't exist already.
+        try:
+            response = requests.post(
+                create_user_api_url,
+                auth=(client_id, client_secret),
+                data=json.dumps(user_data),
+                timeout=1,
+            )
+        except requests.exceptions.ReadTimeout:
+            raise HTTPGatewayTimeout(explanation="Connecting to Hypothesis failed")
+
+        if response.status_code == 200:
+            # User was created successfully.
+            pass
+        elif response.status_code == 409:
+            # The user already exists in h, so we don't need to do anything.
+            pass
+        else:
+            # Something unexpected went wrong when trying to create the user
+            # account in h. Abort and show the user an error page.
+            raise HTTPBadGateway(explanation="Connecting to Hypothesis failed")
+
+        return wrapped(request, jwt)
+
+    return wrapper
