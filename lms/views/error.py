6fa36f0a409375904f1f6c5ac4f6ea4d32b7cc2d
Sheetal Umesh Kumar <sheetaluk@hypothes.is>
2018-07-31 14:36:30 -0400

    Show user the error page when context_id pnd resource_link_id params are missing and lti launch fails.
    Fixes https://github.com/hypothesis/lms/issues/145
    Fixes https://github.com/hypothesis/lms/issues/138

--- lms/views/error.py
+++ lms/views/error.py
@@ -8,6 +8,7 @@ from pyramid.view import view_config
 from pyramid.view import view_defaults
 from pyramid.settings import asbool
 
+from lms.exceptions import MissingLTILaunchParamError
 
 _ = i18n.TranslationStringFactory(__package__)
 
@@ -48,3 +49,18 @@ class ErrorViews(object):
         return {'message': _("Sorry, but something went wrong. "
                              "The issue has been reported and we'll try to "
                              "fix it.")}
+
+
+    @view_config(context=MissingLTILaunchParamError)
+    def missing_lti_launch_param_error(self):
+        """
+        Catch MissingLTILaunchParamError, render a 400 page and report the exception to Sentry.
+
+        If code raises a MissingLTILaunchParamError it means that a required parameter was
+        missing from an LTI launch request that we received:
+        1. Show the user an error page including specific error message
+        2. Report the error to Sentry
+        """
+        self.request.response.status_int = 400
+        self.request.raven.captureException()
+        return {'message': str(self.exc)}
