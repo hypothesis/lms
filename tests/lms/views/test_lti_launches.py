1da925db6dcf74d393049f43f7e9bd178ac25716
Sheetal Umesh Kumar <sheetaluk@hypothes.is>
2018-08-01 12:12:27 -0400

    Show user the error page when resource link id param is missing and lti launch fails.
    See https://github.com/hypothesis/lms/issues/145

--- tests/lms/views/test_lti_launches.py
+++ tests/lms/views/test_lti_launches.py
@@ -46,3 +46,9 @@ class TestLtiLaunches(object):
     def test_raises_for_missing_resource_link_id_param(self, lti_launch_request):
         with pytest.raises(MissingLTILaunchParamError, match="LTI data parameter resource_link_id is required for launch."):
             lti_launches(lti_launch_request)
+
+    def test_raises_for_missing_resource_link_id_param(self, lti_launch_request):
+        lti_launch_request.params.pop('resource_link_id', None)
+
+        with pytest.raises(MissingLtiLaunchParamError, match="Resource link id is required for lti launch."):
+            lti_launches(lti_launch_request)
