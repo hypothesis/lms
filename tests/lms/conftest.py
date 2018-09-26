2e1a6af678ae8c9ed85e4342031f66279e0394f3
Sean Hammond <git@snhmnd.fastmail.com>
2018-09-26 17:18:45 +0100

    Add js-hypothesis-config to LTI launch HTML
    Add a new variable `{{ context.hypothesis_config }}` to the context for
    all templates.
    
    This variable contains a js-hypothesis-config JSON object, suitable for
    rendering in a `<script type="application/json" class="js-hypothesis-config">`
    tag in order to configure the client. See:
    
    https://h.readthedocs.io/projects/client/en/latest/publishers/config/#configuring-the-client-using-json
    
    Use this variable in the new_lti_launch.html.jinja2 template to render a
    js-hypothesis-config JSON script.
    
    Nothing uses this js-hypothesis-config JSON script yet. A Via iframe is
    injected into the LTI launch response, but the Hypothesis client itself
    is _not_ injected in the LTI launch HTML response itself, so it will not
    read this js-hypothesis-config script. The script needs to be read and
    passed to the client by something else, which will be done in a future
    commit.
    
    The changes in this commit require two new config settings that're
    needed to generate the `grantToken` part of the client config. Two new
    environment variables are added to the install instructions:
    
    * H_JWT_CLIENT_ID
    * H_JWT_CLIENT_SECRET

--- tests/lms/conftest.py
+++ tests/lms/conftest.py
@@ -146,6 +146,8 @@ def pyramid_config(pyramid_request):
         },
         'h_client_id': 'TEST_CLIENT_ID',
         'h_client_secret': 'TEST_CLIENT_SECRET',
+        'h_jwt_client_id': 'TEST_JWT_CLIENT_ID',
+        'h_jwt_client_secret': 'TEST_JWT_CLIENT_SECRET',
         'h_authority': 'TEST_AUTHORITY',
         'h_api_url': 'https://example.com/api',
         'auto_provisioning': 'Hypothesise3f14c1f7e8c89f73cefacdd1d80d0ef Hypothesisf6f3a575c0c73e20ab41aa6be09b9c20',
@@ -210,6 +212,9 @@ def lti_launch_request(monkeypatch, pyramid_request):
     pyramid_request.params['custom_canvas_course_id'] = '1'
     pyramid_request.params['context_id'] = 'fake_context_id'
     monkeypatch.setattr('pylti.common.verify_request_common', lambda a, b, c, d, e: True)
+
+    pyramid_request.context = {"hypothesis_config": {}}
+
     yield pyramid_request
 
 
