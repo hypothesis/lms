5de60a7d0eb5635503d3e6b55931fab3243ff65c
Sean Hammond <git@snhmnd.fastmail.com>
2018-11-21 17:56:31 +0000

    Parametrize some duplicated tests

--- tests/lms/views/test_content_item_selection.py
+++ tests/lms/views/test_content_item_selection.py
@@ -16,51 +16,15 @@ class TestContentItemSelection:
 
 
 class TestContentItemForm:
-    def test_raises_for_lti_version_param(self, lti_launch_request):
-        del lti_launch_request.params["lti_version"]
-        with pytest.raises(
-            MissingLTIContentItemParamError,
-            match="Required LTI data param for content item selection missing: lti_version",
-        ):
-            content_item_form(
-                lti_launch_request,
-                lti_params=lti_launch_request.params,
-                lms_url="test_lms_url",
-                content_item_return_url="content_item_return_url",
-            )
+    @pytest.mark.parametrize(
+        "param", ["lti_version", "oauth_version", "oauth_nonce", "oauth_signature"]
+    )
+    def test_it_raises_if_a_required_launch_param_is_missing(
+        self, lti_launch_request, param
+    ):
+        del lti_launch_request.params[param]
 
-    def test_raises_for_oauth_version_param(self, lti_launch_request):
-        del lti_launch_request.params["oauth_version"]
-        with pytest.raises(
-            MissingLTIContentItemParamError,
-            match="Required LTI data param for content item selection missing: oauth_version",
-        ):
-            content_item_form(
-                lti_launch_request,
-                lti_params=lti_launch_request.params,
-                lms_url="test_lms_url",
-                content_item_return_url="content_item_return_url",
-            )
-
-    def test_raises_for_oauth_nonce_param(self, lti_launch_request):
-        del lti_launch_request.params["oauth_nonce"]
-        with pytest.raises(
-            MissingLTIContentItemParamError,
-            match="Required LTI data param for content item selection missing: oauth_nonce",
-        ):
-            content_item_form(
-                lti_launch_request,
-                lti_params=lti_launch_request.params,
-                lms_url="test_lms_url",
-                content_item_return_url="content_item_return_url",
-            )
-
-    def test_raises_for_oauth_signature_param(self, lti_launch_request):
-        del lti_launch_request.params["oauth_signature"]
-        with pytest.raises(
-            MissingLTIContentItemParamError,
-            match="Required LTI data param for content item selection missing: oauth_signature",
-        ):
+        with pytest.raises(MissingLTIContentItemParamError, match=param):
             content_item_form(
                 lti_launch_request,
                 lti_params=lti_launch_request.params,
