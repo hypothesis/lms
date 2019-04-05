b85dafd795386ac2f5611c918881de12691a2f88
Sean Hammond <git@snhmnd.fastmail.com>
2019-04-05 18:57:01 +0100

    Add lms.values.LTIUser value type
    Add a "value object" type for an LTI user -- an immutable object with no
    methods that just exposes a set of user-related fields: user_id,
    oauth_consumer_key (more can be added in future).
    
    In the future an instance of this class will be exposed as
    `request.user` whenever an LTI user is authenticated, to contain more
    info about the user than just `request.authenticated_userid`.

---
+++ lms/values.py
@@ -0,0 +1,15 @@
+"""Immutable value objects for use throughout the app."""
+from typing import NamedTuple
+
+
+__all__ = ("LTIUser",)
+
+
+class LTIUser(NamedTuple):
+    """An LTI user."""
+
+    user_id: str
+    """The user_id LTI launch parameter."""
+
+    oauth_consumer_key: str
+    """The oauth_consumer_key LTI launch parameter."""
