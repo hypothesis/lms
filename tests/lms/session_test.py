6c8ec103c92558a4467c203249d0c0f465f593d1
Sean Hammond <git@snhmnd.fastmail.com>
2019-04-23 12:47:38 +0100

    Add a Pyramid session to the app
    This is needed right now in order to be able to use flash messages but
    the session might end up getting used for other things in the future (a
    Pyramid app can only have one session) so I've paid basic attention to
    security.
    
    Add a new required envvar: `COOKIE_SIGNING_SECRET` and docs.
    
    Add new `session.py` module that sets up a standard Pyramid signed (not
    encrypted) cookie session using the new secret.

---
+++ tests/lms/session_test.py
@@ -0,0 +1,13 @@
+from pyramid.interfaces import ISessionFactory
+
+from lms.session import includeme
+
+
+def test_includeme(pyramid_config):
+    pyramid_config.registry.settings["session_cookie_secret"] = "test_secret"
+
+    includeme(pyramid_config)
+
+    session_factory = pyramid_config.registry.queryUtility(ISessionFactory)
+    assert session_factory is not None
+    assert session_factory._cookie_secure is True
