62fd9970c625784b49e72a931a892bbba312cf15
Robert Knight <robertknight@gmail.com>
2019-05-06 17:42:51 +0100

    Setup build + test tooling for building React/Preact UI components
    Setup the build tooling that will be needed by the new LMS file picker
    and its tests.

--- lms/static/scripts/bootstrap.js
+++ lms/static/scripts/bootstrap.js
@@ -1,1 +1,8 @@
+// Expose sinon assertions.
 sinon.assert.expose(assert, { prefix: null });
+
+// Configure Enzyme for UI tests.
+require('preact/debug');
+import { configure } from 'enzyme';
+import { Adapter } from 'enzyme-adapter-preact-pure';
+configure({ adapter: new Adapter() });
