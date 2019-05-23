f1067759daa58c3522db9ad6e82ea1c2e4af1c3d
Sean Hammond <git@snhmnd.fastmail.com>
2019-05-23 14:03:25 +0100

    Add report_lti_launch view decorator
    This can be added to LTI launch views like so:
    
        @view_config(
            decorator="lms.views.decorators.report_lti_launch",
            ...
        )
        def my_lti_launch_view(context, request):
    
    For details see the decorator parameter documented here:
    
    https://docs.pylonsproject.org/projects/pyramid/en/latest/narr/viewconfig.html?highlight=decorator#non-predicate-arguments
    
    It will add a row to the `lti_launches` DB table each time the view is
    called. Rows from this table are the data used by the `/reports` page.
    
    New views will use this instead of a helper function that the legacy
    views used to append to the same table:
    
    https://github.com/hypothesis/lms/blob/d06cae8bb0778867588c3f7e95c2050385a8776d/lms/views/lti_launches.py#L80-L93
    
    Nothing uses the new view decorator yet.

--- lms/views/decorators/__init__.py
+++ lms/views/decorators/__init__.py
@@ -1,9 +1,35 @@
-# -*- coding: utf-8 -*-
+"""
+Custom view decorators.
 
-"""Python decorators meant to be used for decorating Pyramid view functions."""
+These are Pyramid view decorators, to be used with the ``decorators`` view
+config parameter. For example:
 
+    @view_config(
+        ...,
+        decorator=[
+            "lms.views.decorators.decorator_2",
+            "lms.views.decorators.decorator_1",
+        ]
+    )
+    def my_view(context, request):
+        ...
+
+For documentation see:
+
+https://docs.pylonsproject.org/projects/pyramid/en/latest/narr/viewconfig.html#non-predicate-arguments
+"""
 from lms.views.decorators.h_api import upsert_h_user
 from lms.views.decorators.h_api import create_course_group
 from lms.views.decorators.h_api import add_user_to_group
+from lms.views.decorators.reports import report_lti_launch
 
-__all__ = ("upsert_h_user", "create_course_group", "add_user_to_group")
+__all__ = (
+    "report_lti_launch",
+    # Legacy view decorators. These are used as normal Python @decorators
+    # applied directly to the view function, rather than with Pyramid's
+    # decorators= view config parameter. See the docstring in
+    # lms/views/decorators/h_api.py for details.
+    "upsert_h_user",
+    "create_course_group",
+    "add_user_to_group",
+)
