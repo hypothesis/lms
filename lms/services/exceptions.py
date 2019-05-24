e8d3bcf485fc0a0623d8be7b3dc02de6a26f61bc
Sean Hammond <git@snhmnd.fastmail.com>
2019-05-24 19:08:49 +0100

    Raise CanvasAPIError if public_url request fails
    This actually needs to be done centrally for all Canvas API requests.
    But for now just doing it for the public_url API will allow the view to
    know whether it needs to ask the user for authorization or not.

--- lms/services/exceptions.py
+++ lms/services/exceptions.py
@@ -67,3 +67,42 @@ class HAPIError(HTTPInternalServerError):  # pylint: disable=too-many-ancestors
 
 class HAPINotFoundError(HAPIError):  # pylint: disable=too-many-ancestors
     """A 404 error from an API request."""
+
+
+class CanvasAPIError(HTTPInternalServerError):  # pylint: disable=too-many-ancestors
+    """
+    A problem with a Canvas API request.
+
+    This exception class is raised whenever a Canvas API request times out or
+    when an unsuccessful, invalid or unexpected response is received from the
+    Canvas API.
+
+    An CanvasAPIError is a 500 Internal Server Error response
+    (HTTPInternalServerError subclass) rather than, say, a 502 Bad Gateway
+    because Cloudflare intercepts gateway error responses and replaces our
+    error page with its own error page, and we don't want that.
+
+    Any arguments or keyword arguments other than ``response`` are passed
+    through to HTTPInternalServerError.
+
+    :param response: The response from the HTTP request to the h API
+    :type response: requests.Response
+    """
+
+    def __init__(self, *args, response=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.response = response
+
+    def __str__(self):
+        if self.response is None:
+            return super().__str__()
+
+        # Log the details of the Canvas API response. This goes to both Sentry
+        # and the application's logs. It's helpful for debugging to know how
+        # Canvas responded.
+        parts = [
+            str(self.response.status_code or ""),
+            self.response.reason,
+            self.response.text,
+        ]
+        return " ".join([part for part in parts if part])
