18d54ac4ddb66649e92e77825b2d1b19bc0777f5
Sean Hammond <git@snhmnd.fastmail.com>
2019-05-24 11:21:32 +0100

    Rename the h_api decorators to legacy_*
    New versions of these decorators are going to be made that work as
    proper Pyramid view decorators, and these now legacy_* ones will
    eventually be deleted.

--- lms/views/decorators/__init__.py
+++ lms/views/decorators/__init__.py
@@ -18,18 +18,19 @@ For documentation see:
 
 https://docs.pylonsproject.org/projects/pyramid/en/latest/narr/viewconfig.html#non-predicate-arguments
 """
-from lms.views.decorators.h_api import upsert_h_user
-from lms.views.decorators.h_api import create_course_group
-from lms.views.decorators.h_api import add_user_to_group
 from lms.views.decorators.reports import report_lti_launch
+
+from lms.views.decorators.legacy_h_api import legacy_upsert_h_user
+from lms.views.decorators.legacy_h_api import legacy_create_course_group
+from lms.views.decorators.legacy_h_api import legacy_add_user_to_group
 
 __all__ = (
     "report_lti_launch",
     # Legacy view decorators. These are used as normal Python @decorators
     # applied directly to the view function, rather than with Pyramid's
     # decorators= view config parameter. See the docstring in
-    # lms/views/decorators/h_api.py for details.
-    "upsert_h_user",
-    "create_course_group",
-    "add_user_to_group",
+    # lms/views/decorators/legacy_h_api.py for details.
+    "legacy_upsert_h_user",
+    "legacy_create_course_group",
+    "legacy_add_user_to_group",
 )
