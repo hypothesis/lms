9ed2f96509639be23535cc47a387e293ae1cd563
Sean Hammond <git@snhmnd.fastmail.com>
2019-05-31 15:22:46 +0100

    Fix ModuleItemConfiguration docstring

--- lms/models/module_item_configuration.py
+++ lms/models/module_item_configuration.py
@@ -3,7 +3,20 @@ from lms.db import BASE
 
 
 class ModuleItemConfiguration(BASE):
-    """Class that links a document url to a specific lms module (Not needed for canvas)."""
+    """
+    A module item or assignment configuration.
+
+    When an LMS doesn't support LTI content-item selection/deep linking (so it
+    doesn't support storing an assignment's document URL in the LMS and passing
+    it back to us in launch requests) then we store the document URL in the
+    database instead.
+
+    Each persisted ModuleItemConfiguration object represents a DB-stored
+    assignment configuration, with the
+    ``(tool_consumer_instance_guid, resource_link_id)`` launch params
+    identifying the LTI resource (module item or assignment) and the
+    ``document_url`` being the URL of the document to be annotated.
+    """
 
     __tablename__ = "module_item_configurations"
     __table_args__ = (
@@ -11,6 +24,17 @@ class ModuleItemConfiguration(BASE):
     )
 
     id = sa.Column(sa.Integer, autoincrement=True, primary_key=True)
+
     resource_link_id = sa.Column(sa.String, nullable=False)
+    """The resource_link_id launch param of the module item or assignment."""
+
     tool_consumer_instance_guid = sa.Column(sa.String, nullable=False)
+    """
+    The tool_consumer_instance_guid launch param of the LMS.
+
+    This is needed because resource_link_id's aren't guaranteed to be unique
+    across different LMS's.
+    """
+
     document_url = sa.Column(sa.String, nullable=False)
+    """The URL of the document to be annotated for this assignment."""
