58ab93276720ccdacdeb22465c9e9a9bc0be2425
Kyle Keating <kkeating@hypothes-is>
2019-10-14 10:13:09 -0700

    Add grader endpoint for submitting a grade
    - Also stub out the end point for fetching the grade (to be implemented later)

---
+++ lms/static/scripts/frontend_apps/utils/test/grader-service-test.js
@@ -0,0 +1,53 @@
+import { submitGrade, $imports } from '../grader-service';
+
+describe('grader-services', () => {
+  const fakeApiCall = sinon.stub().resolves({});
+
+  beforeEach(() => {
+    $imports.$mock({
+      './api': {
+        apiCall: fakeApiCall,
+      },
+    });
+  });
+
+  afterEach(() => {
+    $imports.$restore();
+  });
+
+  context('submitGrade', () => {
+    it('returns a promise', async () => {
+      const result = submitGrade({
+        student: {
+          LISResultSourcedId: 0,
+          LISOutcomeServiceUrl: 'url',
+        },
+        grade: 1,
+        authToken: 'auth',
+      });
+      assert.isTrue(result instanceof Promise);
+    });
+
+    it('calls submitGrade with the provided parameters', async () => {
+      await submitGrade({
+        student: {
+          LISResultSourcedId: 0,
+          LISOutcomeServiceUrl: 'url',
+        },
+        grade: 1,
+        authToken: 'auth',
+      });
+      assert.isTrue(
+        fakeApiCall.calledWithMatch({
+          authToken: 'auth',
+          path: '/api/lti/result',
+          data: {
+            lis_result_sourcedid: 0,
+            lis_outcome_service_url: 'url',
+            score: 1,
+          },
+        })
+      );
+    });
+  });
+});
