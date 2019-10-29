41ae36bd975caadcab7eb10565751bc17b993edf
Sean Hammond <git@snhmnd.fastmail.com>
2019-10-29 17:29:57 +0000

    Rollback DB session before calling exception views
    Add a tween that rolls back the DB session before exception views are
    called to prevent crashes when exception views try to access the DB.
    This is the same as one we already have in h:
    
    https://github.com/hypothesis/h/pull/5746

--- lms/app.py
+++ lms/app.py
@@ -57,6 +57,7 @@ def create_app(global_config, **settings):  # pylint: disable=unused-argument
     config.include("lms.views.error")
     config.include("lms.services")
     config.include("lms.validation")
+    config.include("lms.tweens")
     config.add_static_view(name="export", path="lms:static/export")
     config.add_static_view(name="static", path="lms:static")
 
