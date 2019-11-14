924209618e2d07961408edb9c5a5fd546dcdb7a4
Jon Betts <jon.betts@hypothes.is>
2019-11-14 00:10:27 +0000

    Adding doc strings and tidying up param names

--- lms/authentication/password_hash.py
+++ lms/authentication/password_hash.py
@@ -1,3 +1,5 @@
+"""Functions for hashing and checking passwords."""
+
 import binascii
 import os
 from hashlib import pbkdf2_hmac
@@ -5,24 +7,39 @@ from hashlib import pbkdf2_hmac
 __all__ = ["hash_password", "check_password"]
 
 
-def check_password(password: str, expected_pw_hash: str, salt: str):
+def check_password(password: str, expected_hash: str, salt: str):
+    """
+    Check that a password matches the expected hash.
+
+    :param password: The password to check
+    :param expected_hash: The expected hash
+    :param salt: The salt used to generate the hash
+    :rtype: bool
+    :return: Does these credentials match?
+    """
     calculated_hash, _ = hash_password(password, salt)
 
-    return calculated_hash == expected_pw_hash.encode("utf8")
+    return calculated_hash == expected_hash.encode("utf8")
 
 
-def hash_password(pw_to_hash, salt_for_hash: str = ""):
-    if isinstance(pw_to_hash, str):
-        pw_to_hash = pw_to_hash.encode("utf8")
+def hash_password(password: str, salt: str = ""):
+    """
+    Create a hash and possibly salt for a password.
 
-    if salt_for_hash == "":
-        salt_for_hash = binascii.hexlify(os.urandom(8))
+    :param password: The password to hash
+    :param salt: An optional salt. If this is not passed, then a random salt
+                 will be generated.
+    :return: A tuple of hash and salt
+    """
+    if isinstance(password, str):
+        password = password.encode("utf8")
 
-    elif isinstance(salt_for_hash, str):
-        salt_for_hash = salt_for_hash.encode("utf8")
+    if salt == "":
+        salt = binascii.hexlify(os.urandom(8))
 
-    hash_pw = binascii.hexlify(
-        pbkdf2_hmac("sha256", pw_to_hash, salt_for_hash, 1_000_000)
-    )
+    elif isinstance(salt, str):
+        salt = salt.encode("utf8")
 
-    return hash_pw, salt_for_hash
+    hash_pw = binascii.hexlify(pbkdf2_hmac("sha256", password, salt, 1_000_000))
+
+    return hash_pw, salt
