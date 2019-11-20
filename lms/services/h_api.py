2ab60dcf90cb37b4459a2df58cb2216854a21f10
Jon Betts <jon.betts@hypothes.is>
2019-11-20 13:52:35 +0000

    Removing un-used options in api_request, fixing the name and doc strings
    We had an attribute _request and a function _request. Not very bright

--- lms/services/h_api.py
+++ lms/services/h_api.py
@@ -1,3 +1,4 @@
+"""H API service for modifying users and groups."""
 import json
 
 import requests
@@ -11,11 +12,6 @@ __all__ = ["HAPIClient"]
 
 class HAPIClient:
     """
-    High-level h API service.
-
-    This service supports high-level operations (eg. fetching a user) on h
-    resources via the low-level "hapi" client.
-
     :raises HAPIError: On any unhandled exception during calls to the H API
     """
 
@@ -24,136 +20,148 @@ class HAPIClient:
 
         settings = request.registry.settings
 
-        self._client_id = settings["h_client_id"]
-        self._client_secret = settings["h_client_secret"]
         self._authority = settings["h_authority"]
+
+        self._http_auth = (settings["h_client_id"], settings["h_client_secret"])
         self._base_url = settings["h_api_url_private"]
 
     def get_user(self, username):
         """
-        Fetch an `HUser` given their username.
+        Get an `HUser` given their username.
 
         :rtype: HUser
         """
-        authority = self._request.registry.settings["h_authority"]
-        userid = HUser(authority, username).userid
-        user_info = self._request("GET", path=f"users/{userid}").json()
+        userid = HUser(self._authority, username).userid
+        user_info = self._api_request("GET", path=f"users/{userid}").json()
 
         return HUser(
-            authority=authority,
+            authority=self._authority,
             username=username,
             display_name=user_info["display_name"],
         )
 
     def create_user(self, h_user, provider, provider_unique_id):
+        """
+        Create a user in H.
+
+        :param h_user: An `HUser` object to send to H
+        :param provider: The provider (hosting LMS)
+        :param provider_unique_id: The id of the provider
+        :return:
+        """
         user_data = {
             "username": h_user.username,
             "display_name": h_user.display_name,
-            "authority": self._request.registry.settings["h_authority"],
+            "authority": self._authority,
             "identities": [
                 {"provider": provider, "provider_unique_id": provider_unique_id,}
             ],
         }
 
-        self._request("POST", "users", user_data)
-
-    def update_user(self, h_user):
-        """
-
-        :param h_user:
-        :return:
-        """
-        self._request(
-            "PATCH", f"users/{h_user.username}", {"display_name": h_user.display_name},
-        )
+        self._api_request("POST", "users", data=user_data)
 
     def upsert_user(self, h_user, provider, provider_unique_id):
+        """
+        Create or update a user in H as appropriate.
+
+        This is equivalent to calling `update_user()` then `create_user()` if
+        that fails.
+
+        :param h_user: An `HUser` object to send to H
+        :param provider: The provider (hosting LMS)
+        :param provider_unique_id: The id of the provider
+        :return:
+        """
         try:
             self.update_user(h_user)
         except HAPINotFoundError:
             self.create_user(h_user, provider, provider_unique_id)
 
+    def update_user(self, h_user):
+        """
+        Update details for a user in H.
+
+        Currently this only updates the display name.
+
+        :param h_user: An `HUser` object to update
+        :return:
+        """
+        self._api_request(
+            "PATCH", f"users/{h_user.username}", {"display_name": h_user.display_name},
+        )
+
     def create_group(self, group_id, group_name, h_user):
-        self._request(
+        """
+        Create a group in H.
+
+        :param group_id: The id of the group
+        :param group_name: The display name for the group
+        :param h_user: An `HUser` to associate with the group
+        :return:
+        """
+        self._api_request(
             "PUT",
             f"groups/{group_id}",
             {"groupid": group_id, "name": group_name,},
-            user_id=h_user.userid,
+            headers={"X-Forwarded-User": h_user.userid},
         )
 
     def update_group(self, group_id, group_name):
         """
+        Update a group in H.
 
-        :param group_id:
-        :param group_name:
+        Currently this only updates the name of the group.
+
+        :param group_id: The id of the group
+        :param group_name: The display name for the group
         :return:
         """
-        self._request(
+        self._api_request(
             "PATCH", f"groups/{group_id}", {"name": group_name},
         )
 
     def add_user_to_group(self, h_user, group_id):
         """
+        Add the user as a member of the group.
 
-        :param h_user:
-        :param group_id:
+        :param h_user: An `HUser` to associate with the group
+        :param group_id: The id of the group
         :return:
         """
-        self._request("POST", f"groups/{group_id}/members/{h_user.userid}")
+        self._api_request("POST", f"groups/{group_id}/members/{h_user.userid}")
 
-    def _request(
-        self, method, path, data=None, userid=None, statuses=None
-    ):  # pylint:disable=too-many-arguments
+    def _api_request(
+        self, method: str, path: str, data: dict = None, headers: dict = None
+    ):
         """
         Send any kind of HTTP request to the h API and return the response.
 
-        If the request fails for any reason (for example a network connection error
-        or a timeout) :exc:`~lms.services.HAPIError` is raised.
-        :exc:`~lms.services.HAPIError` is also raised if a 4xx or 5xx response is
-        received. Use the optional keyword argument ``statuses`` to supply a list
-        of one or more 4xx or 5xx statuses for which :exc:`~lms.services.HAPIError`
-        should not be raised -- the 4xx or 5xx response will be returned instead.
+        :param method: the HTTP request method to use, for example "GET",
+                       "POST", "PUT", "PATCH" or "DELETE"
+        :param path: the h API path to post to, relative to
+                     ``settings["h_api_url_private"]``, for example:
+                     ``"users"`` or ``"groups/<GROUPID>/members/<USERID>"``
+        :param data: the data to post as JSON in the request body
+        :param headers: extra headers to pass with the request
 
-        :arg method: the HTTP request method to use, for example "GET", "POST",
-          "PUT", "PATCH" or "DELETE"
-        :type method: str
-        :arg path: the h API path to post to, relative to
-          ``settings["h_api_url_private"]``, for example: ``"users"`` or
-          ``"groups/<GROUPID>/members/<USERID>"``
-        :type path: str
-        :arg data: the data to post as JSON in the request body
-        :type data: dict
-        :arg userid: the userid of the user to post as (using an
-          X-Forwarded-User header)
-        :type userid: str
-        :arg statuses: the list of 4xx and 5xx statuses that should not trigger an
-          exception, for example: ``[409, 410]``
-        :type statuses: list of ints
-
-        :raise ~lms.services.HAPIError: if the request fails for any reason,
-          including if a 4xx or 5xx response is received
-
+        :raise HAPINotFoundError: If the request fails with 404
+        :raise HAPIError: if the request fails for any other reason
         :return: the response from the h API
         :rtype: requests.Response
         """
-        statuses = statuses or []
+        headers = headers or {}
+        headers["Hypothesis-Application"] = "lms"
 
-        request_args = {"headers": {"Hypothesis-Application": "lms"}}
-
-        if path.startswith("/"):
-            path = path[1:]
+        request_args = {"headers": headers}
 
         if data is not None:
             request_args["data"] = json.dumps(data)
 
-        if userid is not None:
-            request_args["headers"]["X-Forwarded-User"] = userid
-
         try:
             response = requests.request(
                 method=method,
-                url=self._base_url + path,
-                auth=(self._client_id, self._client_secret),
+                url=self._base_url + path.lstrip("/"),
+                auth=self._http_auth,
                 timeout=10,
                 **request_args,
             )
@@ -163,11 +171,11 @@ class HAPIClient:
             response = getattr(err, "response", None)
             status_code = getattr(response, "status_code", None)
 
-            if status_code is None or status_code not in statuses:
-                exception_class = HAPINotFoundError if status_code == 404 else HAPIError
-
-                raise exception_class(
-                    explanation="Connecting to Hypothesis failed", response=response
+            if status_code == 404:
+                raise HAPINotFoundError(
+                    "Could not find requested resource", response
                 ) from err
+
+            raise HAPIError("Connecting to Hypothesis failed", response) from err
 
         return response
