8e3a67ba07dd04d3260b068cb769bed8ca3a79db
Jon Betts <jon.betts@hypothes.is>
2019-11-20 15:26:40 +0000

    Fixed a test and used better casing for h_api

--- tests/unit/lms/services/h_api_test.py
+++ tests/unit/lms/services/h_api_test.py
@@ -4,6 +4,7 @@ import pytest
 from h_matchers import Any
 from pyramid.testing import DummyRequest
 from requests import (
+    ConnectionError,
     HTTPError,
     ReadTimeout,
     RequestException,
@@ -20,13 +21,13 @@ AUTHORITY = "test-authority"
 
 
 class TestHAPI:
-    def test_get_user_works(self, hapi):
-        hapi._api_request.return_value.json.return_value = {
+    def test_get_user_works(self, h_api):
+        h_api._api_request.return_value.json.return_value = {
             "display_name": sentinel.display_name
         }
-        user = hapi.get_user("username")
+        user = h_api.get_user("username")
 
-        hapi._api_request.assert_called_once_with(
+        h_api._api_request.assert_called_once_with(
             "GET", path="users/acct:username@test-authority"
         )
 
@@ -35,10 +36,10 @@ class TestHAPI:
         assert user.username == "username"
         assert user.display_name == sentinel.display_name
 
-    def test_create_user_works(self, hapi, h_user):
-        hapi.create_user(h_user, sentinel.provider, sentinel.provider_unique_id)
+    def test_create_user_works(self, h_api, h_user):
+        h_api.create_user(h_user, sentinel.provider, sentinel.provider_unique_id)
 
-        hapi._api_request.assert_called_once_with(
+        h_api._api_request.assert_called_once_with(
             "POST",
             "users",
             data={
@@ -54,10 +55,10 @@ class TestHAPI:
             },
         )
 
-    def test_update_user_works(self, hapi, h_user):
-        hapi.update_user(h_user)
+    def test_update_user_works(self, h_api, h_user):
+        h_api.update_user(h_user)
 
-        hapi._api_request.assert_called_once_with(
+        h_api._api_request.assert_called_once_with(
             "PATCH",
             "users/sentinel.username",
             data={"display_name": h_user.display_name},
@@ -66,37 +67,37 @@ class TestHAPI:
     @patch.object(HAPI, "create_user")
     @patch.object(HAPI, "update_user")
     def test_upsert_calls_calls_create_if_update_fails(
-        self, update_user, create_user, hapi, h_user
+        self, update_user, create_user, h_api, h_user
     ):
         update_user.side_effect = HAPINotFoundError()
-        hapi.upsert_user(h_user, sentinel.provider, sentinel.provider_unique_id)
+        h_api.upsert_user(h_user, sentinel.provider, sentinel.provider_unique_id)
 
         update_user.assert_called_once_with(h_user)
         create_user.assert_called_once_with(
             h_user, sentinel.provider, sentinel.provider_unique_id
         )
 
-    def test_create_groups_works(self, hapi, h_user):
-        hapi.create_group(sentinel.group_id, sentinel.group_name, h_user)
+    def test_create_groups_works(self, h_api, h_user):
+        h_api.create_group(sentinel.group_id, sentinel.group_name, h_user)
 
-        hapi._api_request.assert_called_once_with(
+        h_api._api_request.assert_called_once_with(
             "PUT",
             "groups/sentinel.group_id",
             data={"groupid": sentinel.group_id, "name": sentinel.group_name},
             headers=Any.dict.containing({"X-Forwarded-User": h_user.userid}),
         )
 
-    def test_update_group_works(self, hapi):
-        hapi.update_group(sentinel.group_id, sentinel.group_name)
+    def test_update_group_works(self, h_api):
+        h_api.update_group(sentinel.group_id, sentinel.group_name)
 
-        hapi._api_request.assert_called_once_with(
+        h_api._api_request.assert_called_once_with(
             "PATCH", "groups/sentinel.group_id", data={"name": sentinel.group_name}
         )
 
-    def test_add_user_to_group(self, hapi, h_user):
-        hapi.add_user_to_group(h_user, sentinel.group_id)
+    def test_add_user_to_group(self, h_api, h_user):
+        h_api.add_user_to_group(h_user, sentinel.group_id)
 
-        hapi._api_request.assert_called_once_with(
+        h_api._api_request.assert_called_once_with(
             "POST",
             "groups/sentinel.group_id/members/acct:sentinel.username@test-authority",
         )
@@ -110,35 +111,35 @@ class TestHAPI:
         )
 
     @pytest.yield_fixture
-    def hapi(self, p_request):
-        hapi = HAPI(sentinel.context, p_request)
+    def h_api(self, p_request):
+        h_api = HAPI(sentinel.context, p_request)
 
         with patch.object(HAPI, "_api_request"):
-            yield hapi
+            yield h_api
 
 
 class TestHAPIRequest:
-    def test_it_passes_expected_defaults(self, hapi, requests):
-        hapi._api_request(sentinel.method, "dummy-path")
+    def test_it_passes_expected_defaults(self, h_api, requests):
+        h_api._api_request(sentinel.method, "dummy-path")
 
         self._assert_called_requests_with(
             requests, auth=(sentinel.client_id, sentinel.client_secret), timeout=10
         )
 
-    def test_it_passes_method_and_path(self, hapi, requests):
-        hapi._api_request(sentinel.method, "/path")
+    def test_it_passes_method_and_path(self, h_api, requests):
+        h_api._api_request(sentinel.method, "/path")
 
         self._assert_called_requests_with(
             requests, method=sentinel.method, url=BASE_URL + "path",
         )
 
-    def test_id_dumps_json_body(self, hapi, requests):
-        hapi._api_request(sentinel.method, "dummy-path", {"a": 1, "b": [2]})
+    def test_id_dumps_json_body(self, h_api, requests):
+        h_api._api_request(sentinel.method, "dummy-path", {"a": 1, "b": [2]})
 
         self._assert_called_requests_with(requests, data='{"a":1,"b":[2]}')
 
-    def test_it_adds_expected_headers(self, hapi, requests):
-        hapi._api_request(
+    def test_it_adds_expected_headers(self, h_api, requests):
+        h_api._api_request(
             sentinel.method, "dummy-path", headers={"X-Header": sentinel.header}
         )
 
@@ -147,35 +148,38 @@ class TestHAPIRequest:
             headers={"X-Header": sentinel.header, "Hypothesis-Application": "lms"},
         )
 
-    def test_it_raises_HAPINotFoundError_for_404(self, hapi, requests):
+    def test_it_raises_HAPINotFoundError_for_404(self, h_api, requests):
         response = Response()
         response.status_code = 404
         exception = RequestException(response=response)
         requests.request.return_value.raise_for_status.side_effect = exception
 
         with pytest.raises(HAPINotFoundError) as exc_info:
-            hapi._api_request(sentinel.method, "dummy-path")
+            h_api._api_request(sentinel.method, "dummy-path")
 
         # It records the requests exception that caused the HAPIError.
         assert exc_info.value.__cause__ == exception
 
     @pytest.mark.parametrize(
-        "exception", [ConnectionError(), HTTPError(), ReadTimeout(), TooManyRedirects()]
+        "exception_class", [ConnectionError, HTTPError, ReadTimeout, TooManyRedirects]
     )
-    def test_it_raises_HAPIError_for_other_http_errors(self, hapi, requests, exception):
+    def test_it_raises_HAPIError_for_other_http_errors(
+        self, h_api, requests, exception_class
+    ):
+        exception = exception_class()
         requests.request.side_effect = exception
 
         with pytest.raises(HAPIError) as exc_info:
-            hapi._api_request(sentinel.method, "dummy-path")
+            h_api._api_request(sentinel.method, "dummy-path")
 
         # It records the requests exception that caused the HAPIError.
         assert exc_info.value.__cause__ == exception
 
-    def test_other_exceptions_are_raised_normally(self, hapi, requests):
+    def test_other_exceptions_are_raised_normally(self, h_api, requests):
         requests.request.side_effect = OSError()
 
         with pytest.raises(OSError):
-            hapi._api_request(sentinel.method, "dummy-path")
+            h_api._api_request(sentinel.method, "dummy-path")
 
     def _assert_called_requests_with(self, requests, **extras):
         values = {
@@ -191,7 +195,7 @@ class TestHAPIRequest:
         requests.request.assert_called_once_with(**values)
 
     @pytest.fixture
-    def hapi(self, p_request):
+    def h_api(self, p_request):
         return HAPI(sentinel.context, p_request)
 
 
