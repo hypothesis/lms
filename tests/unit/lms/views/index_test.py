f5fa6c5cb3419f49a310148c0155515646c53812
Jon Betts <jon.betts@hypothes.is>
2019-11-20 15:55:15 +0000

    Adding a little test to bump coverage over the previous value
    The ratchet in effect. As I've removed code our overall coverage
    is down.

---
+++ tests/unit/lms/views/index_test.py
@@ -0,0 +1,8 @@
+from lms.views.index import index
+
+
+class TestIndexView:
+    def test_it_does_nowt(self, pyramid_request):
+        template_params = index(pyramid_request)
+
+        assert template_params == {}
