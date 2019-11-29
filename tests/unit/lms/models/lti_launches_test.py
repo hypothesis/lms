3df478b9358ac3026cb657d884c748215ce5f604
Sean Hammond <git@snhmnd.fastmail.com>
2019-11-29 17:29:14 +0000

    Fix LtiLaunches.created default value
    Use the current time as the default value, not the time at which the app
    started up.

--- tests/unit/lms/models/lti_launches_test.py
+++ tests/unit/lms/models/lti_launches_test.py
@@ -1,10 +1,16 @@
+from datetime import datetime
+
 from lms.models import LtiLaunches
 
 
 class TestLTILaunches:
     def test_add_adds_an_lti_launch_record_to_the_db(self, db_session):
+        before = datetime.utcnow()
         LtiLaunches.add(db_session, "TEST_CONTEXT_ID", "TEST_OAUTH_CONSUMER_KEY")
 
         lti_launch = db_session.query(LtiLaunches).one()
+
+        after = datetime.utcnow()
         assert lti_launch.context_id == "TEST_CONTEXT_ID"
         assert lti_launch.lti_key == "TEST_OAUTH_CONSUMER_KEY"
+        assert lti_launch.created >= before <= after
