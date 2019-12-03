a5eae33d08656ea25ddfbb8ca4cc6c1cc24a2503
Jon Betts <jon.betts@hypothes.is>
2019-12-03 12:48:42 +0000

    Adding the concept of Higher Order Gherkin - Gherkin defined in Gherkin
     * This introduces the higher order gherking injector
     * This injector reads special feature step files
     * These are written in Gherkin but define steps not scenarios
     * These scenarios are parsed and written out to a python file
     * This python file is read by behave and interpretted as normal steps

--- tests/bdd/environment.py
+++ tests/bdd/environment.py
@@ -1,15 +1,25 @@
 """Entry point and hooks for behave."""
 
+from pkg_resources import resource_filename
+
 from lms.app import create_app
+from tests.bdd.higher_order_gherkin import Injector
 from tests.bdd.step_context import StepContextManager
 from tests.conftest import TEST_SETTINGS
 
 TEST_SETTINGS["session_cookie_secret"] = "notasecret"
 
+# Create the compiled step file before steps are read
+Injector.create_step_file(
+    source_dir=resource_filename("tests", "bdd/steps/feature_steps/"),
+    target_file=resource_filename("tests", "bdd/steps/_compiled_feature_steps.py"),
+)
+
 
 def before_all(context):
     # This will actually take effect the next time you run the tests, but it
     # still keeps you mostly up to date when developing
+
     StepContextManager.before_all(context, app=create_app(None, **TEST_SETTINGS))
 
 
---
+++ tests/bdd/steps/_compiled_feature_steps.py
@@ -0,0 +1,91 @@
+"""
+This code is auto-generated.
+
+From /home/jon/projects/lms/tests/bdd/steps/feature_steps/.
+"""
+
+from behave import step
+
+
+@step("standard authentication setup")
+def standard_authentication_setup(context):
+    # From: tests/bdd/steps/feature_steps/auth.feature: line 1
+    context.execute_steps(
+        """
+    Given the OAuth 1 consumer key is 'Hypothesis4dd96539c449ca5c3d57cc3d778d6bf3'
+      And the OAuth 1 shared secret is 'TEST_SECRET'
+
+    Given I create an LMS DB 'ApplicationInstance'
+        | Field            | Value                                      |
+        | consumer_key     | Hypothesis4dd96539c449ca5c3d57cc3d778d6bf3 |
+        | shared_secret    | TEST_SECRET                                |
+        | lms_url          | test_lms_url                               |
+        | requesters_email | test_requesters_email                      |
+    """
+    )
+
+
+@step("standard setup for LTI section {section}")
+def standard_setup_for_lti_section_section(context, section):
+    # From: tests/bdd/steps/feature_steps/lti.feature: line 1
+    context.execute_steps(
+        """
+    Given fixtures are located in '/lti_certification_1_1/section_{section}'
+      And standard authentication setup
+      And I load the fixture 'most_common.ini' as 'params'
+    """.format(
+            section=section
+        )
+    )
+
+
+@step("I start an LTI launch request")
+def i_start_an_lti_launch_request(context):
+    # From: tests/bdd/steps/feature_steps/lti.feature: line 6
+    context.execute_steps(
+        """
+    Given I start a 'POST' request to 'http://localhost/lti_launches'
+      And I set the request header 'Accept' to 'text/html'
+      And I set the request header 'Content-Type' to 'application/x-www-form-urlencoded'
+    """
+    )
+
+
+@step("I sign the LTI launch request")
+def i_sign_the_lti_launch_request(context):
+    # From: tests/bdd/steps/feature_steps/lti.feature: line 11
+    context.execute_steps(
+        """
+    Given I OAuth 1 sign the fixture 'params'
+      And I set the form parameters from the fixture 'params'
+    """
+    )
+
+
+@step("I make an LTI launch request")
+def i_make_an_lti_launch_request(context):
+    # From: tests/bdd/steps/feature_steps/lti.feature: line 15
+    context.execute_steps(
+        """
+    Given I start an LTI launch request
+    And   I sign the LTI launch request
+
+    When I send the request to the app
+    """
+    )
+
+
+@step("the app redirects to the LTI tool with message matching '{regex}'")
+def the_app_redirects_to_the_lti_tool_with_message_matching_regex(context, regex):
+    # From: tests/bdd/steps/feature_steps/lti.feature: line 21
+    context.execute_steps(
+        """
+     Then  the response status code is 302
+      And   the response header 'Location' is the URL
+      And   the fixture 'params' key 'launch_presentation_return_url' is the value
+      And   the url matches the value
+      And   the url query parameter 'lti_msg' matches '{regex}'
+    """.format(
+            regex=regex
+        )
+    )
