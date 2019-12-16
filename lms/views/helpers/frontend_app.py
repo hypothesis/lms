e293fd38f59be59d5c872be5f4f1751a4e5e5ef0
Jon Betts <jon.betts@hypothes.is>
2019-12-16 14:39:57 +0000

    Fixing indentation of grading javascript config
     * Previouly if grading is disabled, we still run the whole block
     * This means we look up all students for every call
     * This will be generating lots of unnecessary DB calls

--- lms/views/helpers/frontend_app.py
+++ lms/views/helpers/frontend_app.py
@@ -19,35 +19,35 @@ def configure_grading(request, js_config):
     ):
         js_config["lmsGrader"] = True
 
-    js_config["grading"] = {
-        "courseName": request.params.get("context_title"),
-        "assignmentName": request.params.get("resource_link_title"),
-    }
+        js_config["grading"] = {
+            "courseName": request.params.get("context_title"),
+            "assignmentName": request.params.get("resource_link_title"),
+        }
 
-    lis_result_sourcedid_svc = request.find_service(name="lis_result_sourcedid")
-    lis_result_sourcedids = lis_result_sourcedid_svc.fetch_students_by_assignment(
-        oauth_consumer_key=request.lti_user.oauth_consumer_key,
-        context_id=request.params.get("context_id"),
-        resource_link_id=request.params.get("resource_link_id"),
-    )
-    students = []
-    for student in lis_result_sourcedids:
-        # Using ``HUser`` NamedTuple to get at the ``userid`` prop
-        h_user = HUser(
-            authority=request.registry.settings["h_authority"],
-            username=student.h_username,
-            display_name=student.h_display_name,
+        lis_result_sourcedid_svc = request.find_service(name="lis_result_sourcedid")
+        lis_result_sourcedids = lis_result_sourcedid_svc.fetch_students_by_assignment(
+            oauth_consumer_key=request.lti_user.oauth_consumer_key,
+            context_id=request.params.get("context_id"),
+            resource_link_id=request.params.get("resource_link_id"),
         )
-        students.append(
-            {
-                "userid": h_user.userid,
-                "displayName": h_user.display_name,
-                "LISResultSourcedId": student.lis_result_sourcedid,
-                "LISOutcomeServiceUrl": student.lis_outcome_service_url,
-            }
-        )
+        students = []
+        for student in lis_result_sourcedids:
+            # Using ``HUser`` NamedTuple to get at the ``userid`` prop
+            h_user = HUser(
+                authority=request.registry.settings["h_authority"],
+                username=student.h_username,
+                display_name=student.h_display_name,
+            )
+            students.append(
+                {
+                    "userid": h_user.userid,
+                    "displayName": h_user.display_name,
+                    "LISResultSourcedId": student.lis_result_sourcedid,
+                    "LISOutcomeServiceUrl": student.lis_outcome_service_url,
+                }
+            )
 
-    js_config["grading"]["students"] = students
+        js_config["grading"]["students"] = students
 
 
 def _is_assignment_gradable(request):
