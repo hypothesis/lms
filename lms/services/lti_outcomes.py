5a756dbc09ee78c98976ed52b7247c20a46f6680
Jon Betts <jon.betts@hypothes.is>
2019-12-20 15:34:41 +0000

    LTI Outcomes: Python dicts for XML generation and separate Canvas specific stuff (#1287)
    * Use python dicts for XML generation and separate Canvas specific stuff
    
    Changes based on review feedback:
    
     * kwargs to canvas_extras to show they currently only relate to Canvas
     * _pox_wrapper to _pox_envelope, dropping the previous name
     * Reformatted the doc string
     * Added a link to canvas docs

--- lms/services/lti_outcomes.py
+++ lms/services/lti_outcomes.py
@@ -2,81 +2,13 @@ from typing import NamedTuple
 from xml.etree import ElementTree
 
 import requests
-from jinja2 import Template
+import xmltodict
 from requests import RequestException
 from requests_oauthlib import OAuth1
 
 from lms.services.exceptions import LTIOutcomesAPIError
 
 __all__ = ["LTIOutcomesClient", "LTIOutcomesRequestParams"]
-
-
-# Envelope for LTI Outcome Service requests sent to the LMS
-#
-# See https://www.imsglobal.org/specs/ltiomv1p0/specification for specs for all
-# messages.
-#
-# nb. To be a valid XML document it is important that the template has no
-# whitespace at the start.
-LTI_OUTCOME_REQUEST_TEMPLATE = Template(
-    """<?xml version="1.0" encoding="UTF-8"?>
-<imsx_POXEnvelopeRequest xmlns="http://www.imsglobal.org/services/ltiv1p1/xsd/imsoms_v1p0">
-  <imsx_POXHeader>
-    <imsx_POXRequestHeaderInfo>
-      <imsx_version>V1.0</imsx_version>
-      <imsx_messageIdentifier>999999123</imsx_messageIdentifier>
-    </imsx_POXRequestHeaderInfo>
-  </imsx_POXHeader>
-  <imsx_POXBody>
-    {{ body }}
-  </imsx_POXBody>
-</imsx_POXEnvelopeRequest>
-"""
-)
-
-# Template for a `replaceResult` XML message to send to the LMS.
-REPLACE_RESULT_REQUEST_TEMPLATE = Template(
-    """
-<replaceResultRequest>
-  <resultRecord>
-    <sourcedGUID>
-      <sourcedId>{{ lis_result_sourcedid | e }}</sourcedId>
-    </sourcedGUID>
-    <result>
-      {% if score %}
-      <resultScore>
-        <language>en</language>
-        <textString>{{ score | e }}</textString>
-      </resultScore>
-      {% endif %}
-      {% if lti_launch_url %}
-      <resultData>
-        <ltiLaunchUrl>{{ lti_launch_url | e }}</ltiLaunchUrl>
-      </resultData>
-      {% endif %}
-    </result>
-  </resultRecord>
-  {% if submitted_at %}
-  <submissionDetails>
-    <submittedAt>{{ submitted_at.isoformat() }}</submittedAt>
-  </submissionDetails>
-  {% endif %}
-</replaceResultRequest>
-"""
-)
-
-# Template for a `readResult` XML message to send to the LMS.
-READ_RESULT_REQUEST_TEMPLATE = Template(
-    """
-<readResultRequest>
-  <resultRecord>
-    <sourcedGUID>
-      <sourcedId>{{ lis_result_sourcedid | e }}</sourcedId>
-    </sourcedGUID>
-  </resultRecord>
-</readResultRequest>
-"""
-)
 
 
 class LTIOutcomesRequestParams(NamedTuple):
@@ -118,11 +50,19 @@ class LTIOutcomesClient:
 
         :return: The last-submitted score or `None` if no score has been submitted.
         """
-        body = READ_RESULT_REQUEST_TEMPLATE.render(
-            lis_result_sourcedid=outcomes_request_params.lis_result_sourcedid
-        )
 
-        result = self._send_request(outcomes_request_params, body)
+        result = self._send_request(
+            outcomes_request_params,
+            request_body={
+                "readResultRequest": {
+                    "resultRecord": {
+                        "sourcedGUID": {
+                            "sourcedId": outcomes_request_params.lis_result_sourcedid
+                        }
+                    }
+                }
+            },
+        )
 
         try:
             score = self.find_element(
@@ -135,11 +75,7 @@ class LTIOutcomesClient:
             return None
 
     def record_result(  # pylint:disable=no-self-use
-        self,
-        outcomes_request_params,
-        score=None,
-        lti_launch_url=None,
-        submitted_at=None,
+        self, outcomes_request_params, score=None, **canvas_extras,
     ):
         """
         Record a score or grading view launch URL for an assignment in the LMS.
@@ -159,14 +95,41 @@ class LTIOutcomesClient:
             If the submission date matches an existing submission then the
             existing submission is updated rather than creating a new submission.
         """
-        body = REPLACE_RESULT_REQUEST_TEMPLATE.render(
-            lis_result_sourcedid=outcomes_request_params.lis_result_sourcedid,
-            score=score,
-            lti_launch_url=lti_launch_url,
-            submitted_at=submitted_at,
+
+        request = {
+            "resultRecord": {
+                "sourcedGUID": {
+                    "sourcedId": outcomes_request_params.lis_result_sourcedid
+                }
+            }
+        }
+
+        if score:
+            request["resultRecord"]["result"] = {
+                "resultScore": {"language": "en", "textString": score}
+            }
+
+        # Canvas specific adaptations
+        self._canvas_request_modification(request, **canvas_extras)
+
+        self._send_request(
+            outcomes_request_params, request_body={"replaceResultRequest": request}
         )
 
-        self._send_request(outcomes_request_params, body)
+    @classmethod
+    def _canvas_request_modification(
+        cls, request, lti_launch_url=None, submitted_at=None, **_
+    ):
+        # For details of Canvas extensions see:
+        # https://erau.instructure.com/doc/api/file.assignment_tools.html
+
+        if lti_launch_url:
+            request["resultRecord"].setdefault("result", {})["resultData"] = {
+                "ltiLaunchUrl": lti_launch_url
+            }
+
+        if submitted_at:
+            request["submissionDetails"] = {"submittedAt": submitted_at.isoformat()}
 
     @classmethod
     def find_element(cls, xml_element, path):
@@ -176,16 +139,17 @@ class LTIOutcomesClient:
         return xml_element.find(f".//{xpath}")
 
     @classmethod
-    def _send_request(cls, outcomes_request_params, pox_body):
+    def _send_request(cls, outcomes_request_params, request_body):
         """
         Send a signed request to an LMS's Outcome Management Service endpoint.
 
-        :arg pox_body: The content of the `imsx_POXBody` element in the request
+        :arg request_body: The content to send as the body element of the
+                           request
         :return: Parsed XML response
         :rtype: ElementTree.Element
         """
 
-        xml_body = LTI_OUTCOME_REQUEST_TEMPLATE.render(body=pox_body)
+        xml_body = xmltodict.unparse(cls._pox_envelope(request_body))
 
         # Sign request using OAuth 1.0.
         oauth_client = OAuth1(
@@ -233,3 +197,30 @@ class LTIOutcomesClient:
             raise LTIOutcomesAPIError("LTI outcome request failed")
 
         return xml
+
+    @staticmethod
+    def _pox_envelope(body):
+        """
+        Return ``body`` wrapped in an imsx_POXEnvelopeRequest envelope.
+
+        Return an xmltodict-format XML dict - an XML document represented as
+        a dict, suitable for converting into an XML string by passing it to
+        ``xmltodict.unparse()``.
+
+        The returned dict renders to an ``<imsx_POXEnvelopeRequest>`` document
+        with the given ``body`` as the contents of the ``<imsx_POXBody>`` tag.
+
+        :rtype: dict
+        """
+        return {
+            "imsx_POXEnvelopeRequest": {
+                "@xmlns": "http://www.imsglobal.org/services/ltiv1p1/xsd/imsoms_v1p0",
+                "imsx_POXHeader": {
+                    "imsx_POXRequestHeaderInfo": {
+                        "imsx_version": "V1.0",
+                        "imsx_messageIdentifier": "999999123",
+                    }
+                },
+                "imsx_POXBody": body,
+            }
+        }
