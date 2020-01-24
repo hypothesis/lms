01ca15687b58f41d318e506399333ee5b88fd3d5
Sean Hammond <git@snhmnd.fastmail.com>
2020-01-24 14:16:34 +0000

    Return the section groups in the sync API response

--- lms/views/api/h/sync.py
+++ lms/views/api/h/sync.py
@@ -5,6 +5,6 @@ from pyramid.view import view_config
 def sync_lti_data_to_h(request):
     lti_h_service = request.find_service(name="lti_h")
     lti_h_service.upsert_h_user()
-    lti_h_service.upsert_course_groups()
+    groups = lti_h_service.upsert_course_groups()
     lti_h_service.add_user_to_groups()
-    return {}
+    return groups
