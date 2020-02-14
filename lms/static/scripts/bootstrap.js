2d5715963460866a58063d79d38bf80ce65eaf12
Robert Knight <robertknight@gmail.com>
2020-02-14 13:43:07 +0000

    Patch `propTypes.{element, node}` to work with Preact
    The standard implementations of these functions work with React elements
    but not Preact elements because they rely on React internals via the
    `react-is` package.
    
    Add implementations that work with Preact elements instead and
    monkey-patch the standard exports. I'm unsure that monkey-patching is
    the best approach, as opposed to just importing the custom prop types
    directly, but it does avoid confusion when developers try to use
    `propTypes.{node, element}`.

--- lms/static/scripts/bootstrap.js
+++ lms/static/scripts/bootstrap.js
@@ -3,6 +3,10 @@ sinon.assert.expose(assert, { prefix: null });
 
 // Configure Enzyme for UI tests.
 require('preact/debug');
+import { patchPropTypes } from './frontend_apps/utils/prop-types';
+import propTypes from 'prop-types';
+patchPropTypes(propTypes);
+
 import { configure } from 'enzyme';
 import { Adapter } from 'enzyme-adapter-preact-pure';
 configure({ adapter: new Adapter() });
