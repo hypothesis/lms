e96890f3c9adcffdfb966a6cd9922c01895d427c
Kyle Keating <kkeating@hypothes.is>
2020-03-11 10:56:02 -0700

    Fix a11y for Dialog
    - Dialog may now have either a “dialog” or an “alertdialog” role depending what its used for.
    - Dialog no longer has an `onKeyDown` hanlder. (a11y issue)
    - `Escape` keyboard event is now handled with useElementShouldClose
    - Add aria-modal=“true” prop to Dialog
    - Dialog `id` prop is now generated with useUniqueId
    - Expand Dialog `button` prop types for better accuracy

---
+++ lms/static/scripts/frontend_apps/common/dom.js
@@ -0,0 +1,23 @@
+/**
+ * Attach listeners for one or multiple events to an element and return a
+ * function that removes the listeners.
+ *
+ * @param {Element}
+ * @param {string[]} events
+ * @param {(event: Event) => any} listener
+ * @param {boolean} [options.useCapture]
+ * @return {function} Function which removes the event listeners.
+ */
+export function listen(element, events, listener, { useCapture = false } = {}) {
+  if (!Array.isArray(events)) {
+    events = [events];
+  }
+  events.forEach(event =>
+    element.addEventListener(event, listener, useCapture)
+  );
+  return () => {
+    events.forEach(event =>
+      element.removeEventListener(event, listener, useCapture)
+    );
+  };
+}
