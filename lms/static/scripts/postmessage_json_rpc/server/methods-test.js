16e03266b713c5ddac9c3ad1c11e94d0bbdd767f
Sean Hammond <git@snhmnd.fastmail.com>
2020-03-13 20:26:10 +0000

    Add RPC server requestGroups method
    Nothing actually calls this yet. But I have hacked the client to call it
    locally, and verified that it returns the list of one group ID.
    
    Fixes https://github.com/hypothesis/lms/issues/1555

--- lms/static/scripts/postmessage_json_rpc/server/methods-test.js
+++ lms/static/scripts/postmessage_json_rpc/server/methods-test.js
@@ -1,4 +1,4 @@
-import { requestConfig } from './methods';
+import { requestConfig, requestGroups } from './methods';
 
 describe('postmessage_json_rpc/methods#requestConfig', () => {
   let configEl;
@@ -19,5 +19,27 @@ describe('postmessage_json_rpc/methods#requestConfig', () => {
 
   it('returns the config object', () => {
     assert.deepEqual(requestConfig(), { foo: 'bar' });
+  });
+});
+
+describe('postmessage_json_rpc/methods#requestGroups', () => {
+  let configEl;
+
+  beforeEach('inject the client config into the document', () => {
+    configEl = document.createElement('script');
+    configEl.setAttribute('type', 'application/json');
+    configEl.classList.add('js-config');
+    configEl.textContent = JSON.stringify({
+      hypothesisClient: { services: [{ groups: ['groupid1', 'groupid2'] }] },
+    });
+    document.body.appendChild(configEl);
+  });
+
+  afterEach('remove the client config from the document', () => {
+    configEl.parentNode.removeChild(configEl);
+  });
+
+  it('returns the list of groups', () => {
+    assert.deepEqual(requestGroups(), ['groupid1', 'groupid2']);
   });
 });
