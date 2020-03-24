a3f8c7f36ed68032288d637f735bfc23951713f5
Sean Hammond <git@snhmnd.fastmail.com>
2020-03-24 15:02:43 +0000

    Add unique section groupid function
    Add a method, `h_section_groupid()`, for deterministically generating a
    unique h groupid for a Canvas course section.
    
    Fixes https://github.com/hypothesis/lms/issues/1410

--- lms/resources/lti_launch.py
+++ lms/resources/lti_launch.py
@@ -106,6 +106,34 @@ class LTILaunchResource:
         """
         return f"group:{self.h_authority_provided_id}@{self._authority}"
 
+    def h_section_groupid(self, tool_consumer_instance_guid, context_id, section):
+        """
+        Return a unique h groupid for a given Canvas course section.
+
+        The groupid is deterministic and is unique to the course section.
+        Calling this function again with params representing the same course
+        section will always return the same groupid. Calling this function with
+        different params will always return a different groupid.
+
+        :param tool_consumer_instance_guid: the tool_consumer_instance_guid LTI
+            launch param from the Canvas instance that the section belongs to.
+            This is a unique identifier for the Canvas instance
+        :type tool_consumer_instance_guid: str
+
+        :param context_id: the context_id LTI launch param from the Canvas
+            course that the section belongs to. This is a unique identifier for
+            the course within the Canvas instance
+        :type context_id: str
+
+        :param section: a section dict as received from the Canvas API
+        :type section: dict
+        """
+        hash_object = hashlib.sha1()
+        hash_object.update(tool_consumer_instance_guid.encode())
+        hash_object.update(context_id.encode())
+        hash_object.update(section["id"].encode())
+        return f"group:section-{hash_object.hexdigest()}@{self._authority}"
+
     @property
     def h_group_name(self):
         """
