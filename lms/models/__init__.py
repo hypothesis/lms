a303867ae7650b460479c60ee10f734bea960615
Sean Hammond <git@snhmnd.fastmail.com>
2020-04-10 13:31:53 +0100

    Remove an unnecessary __all__

--- lms/models/__init__.py
+++ lms/models/__init__.py
@@ -5,15 +5,6 @@ from lms.models.lti_launches import LtiLaunches
 from lms.models.module_item_configuration import ModuleItemConfiguration
 from lms.models.oauth2_token import OAuth2Token
 
-__all__ = (
-    "ApplicationInstance",
-    "GroupInfo",
-    "GradingInfo",
-    "LtiLaunches",
-    "ModuleItemConfiguration",
-    "OAuth2Token",
-)
-
 
 def includeme(config):  # pylint: disable=unused-argument
     pass
