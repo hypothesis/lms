e574374a2fb7182b43074e7693651cd3623db9ee
Sean Hammond <git@snhmnd.fastmail.com>
2020-04-23 16:08:26 +0100

    Extract group_name() into model
    Extract the `_group_name()` helper method out of `LTILaunchResource`
    into the domain model. This reduces some test duplication, and also
    makes it so that future code outside of `LTILaunchResource` can call
    `models.h_group_name()` without duplicating any code.
    
    Same approach as with `display_name()` for `models.LTIUser`

--- lms/models/h_group.py
+++ lms/models/h_group.py
@@ -7,3 +7,16 @@ class HGroup(NamedTuple):
 
     def groupid(self, authority):
         return f"group:{self.authority_provided_id}@{authority}"
+
+
+def h_group_name(name):
+    """Return an h-compatible group name from the given string."""
+    name = name.strip()
+
+    # The maximum length of an h group name.
+    group_name_max_length = 25
+
+    if len(name) > group_name_max_length:
+        name = name[: group_name_max_length - 1].rstrip() + "…"
+
+    return name
