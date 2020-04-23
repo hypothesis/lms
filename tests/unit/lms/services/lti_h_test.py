e5ca3b7dc34d6e3d20d626c0a55d6189ce16d9e4
Jon Betts <jon.betts@hypothes.is>
2020-04-23 14:43:35 +0100

    Add Bulk API with feature flag fallback for serial insertion (#1673)
    * Add h-api to the requirements and bump h-matchers
    * Add new bulk API
    * Fix rebasing errors and refactoring tests to separate things we are about to delete
    * Apply factory for fixtures
    * Rename the bulk action method, fix factories for fixture

--- tests/unit/lms/services/lti_h_test.py
+++ tests/unit/lms/services/lti_h_test.py
@@ -1,11 +1,13 @@
 from unittest.mock import create_autospec
 
 import pytest
+from h_api.bulk_api import CommandBuilder
 from pyramid.httpexceptions import HTTPInternalServerError
 
 from lms.models import GroupInfo, HGroup
 from lms.services import HAPIError
 from lms.services.lti_h import LTIHService
+from tests import factories
 
 
 class TestSync:
@@ -14,30 +16,63 @@ class TestSync:
     ):
         ai_getter.provisioning_enabled.return_value = False
 
-        lti_h_svc.sync(groups=[h_group])
+        lti_h_svc.sync(h_groups=[h_group])
 
-        h_api.upsert_user.assert_not_called()
+        h_api.execute_bulk.assert_not_called()
 
-    def test_sync_catches_HAPIErrors(self, h_api, lti_h_svc, h_group):
-        h_api.upsert_user.side_effect = HAPIError
+    def test_sync_catches_HAPIErrors(
+        self, h_api, lti_h_svc, h_group, group_info_service
+    ):
+        h_api.execute_bulk.side_effect = HAPIError
 
         with pytest.raises(HTTPInternalServerError):
-            lti_h_svc.sync(groups=[h_group])
+            lti_h_svc.sync(h_groups=[h_group])
+
+        group_info_service.assert_not_called()
+
+    def test_sync_calls_bulk_action_correctly(self, h_api, h_user, lti_h_svc):
+        groups = factories.HGroup.create_batch(2)
+
+        lti_h_svc.sync(h_groups=groups)
+
+        _, kwargs = h_api.execute_bulk.call_args
+
+        assert "commands" in kwargs
+
+        # pylint: disable=protected-access
+        assert [command.raw for command in kwargs["commands"]] == [
+            CommandBuilder.user.upsert(
+                {
+                    "authority": lti_h_svc._authority,
+                    "username": h_user.username,
+                    "display_name": h_user.display_name,
+                    "identities": [
+                        {
+                            "provider": h_user.provider,
+                            "provider_unique_id": h_user.provider_unique_id,
+                        }
+                    ],
+                },
+                "user_0",
+            ).raw
+        ] + [
+            CommandBuilder.group.upsert(
+                {
+                    "authority": lti_h_svc._authority,
+                    "name": group.name,
+                    "authority_provided_id": group.authority_provided_id,
+                },
+                f"group_{i}",
+            ).raw
+            for i, group in enumerate(groups)
+        ] + [
+            CommandBuilder.group_membership.create("user_0", f"group_0").raw,
+            CommandBuilder.group_membership.create("user_0", f"group_1").raw,
+        ]
 
 
-class TestGroupUpdating:
-    def test_it_upserts_the_group(self, h_api, lti_h_svc, h_group):
-        lti_h_svc.sync([h_group])
-
-        h_api.upsert_group.assert_called_once_with(h_group)
-
-    def test_it_raises_if_upserting_the_group_fails(self, h_api, lti_h_svc, h_group):
-        h_api.upsert_group.side_effect = HAPIError("Oops")
-
-        with pytest.raises(HTTPInternalServerError, match="Oops"):
-            lti_h_svc.sync([h_group])
-
-    def test_it_upserts_the_GroupInfo_into_the_db(
+class TestGroupInfoUpdating:
+    def test_sync_upserts_the_GroupInfo_into_the_db(
         self, params, group_info_service, lti_h_svc, pyramid_request, h_group
     ):
         lti_h_svc.sync([h_group])
@@ -47,18 +82,6 @@ class TestGroupUpdating:
             consumer_key=pyramid_request.lti_user.oauth_consumer_key,
             params=params,
         )
-
-    def test_it_doesnt_upsert_GroupInfo_into_the_db_if_upserting_the_group_fails(
-        self, group_info_service, h_api, lti_h_svc, h_group
-    ):
-        h_api.upsert_group.side_effect = HAPIError("Oops")
-
-        try:
-            lti_h_svc.sync([h_group])
-        except:  # pylint: disable=bare-except
-            pass
-
-        group_info_service.assert_not_called()
 
     @pytest.fixture
     def params(self):
@@ -72,6 +95,21 @@ class TestGroupUpdating:
         return pyramid_request
 
 
+@pytest.mark.usefixtures("use_serial_api")
+class TestGroupUpdating:
+    def test_it_upserts_the_group(self, h_api, lti_h_svc, h_group):
+        lti_h_svc.sync([h_group])
+
+        h_api.upsert_group.assert_called_once_with(h_group)
+
+    def test_it_raises_if_upserting_the_group_fails(self, h_api, lti_h_svc, h_group):
+        h_api.upsert_group.side_effect = HAPIError("Oops")
+
+        with pytest.raises(HTTPInternalServerError, match="Oops"):
+            lti_h_svc.sync([h_group])
+
+
+@pytest.mark.usefixtures("use_serial_api")
 class TestUserUpserting:
     def test_it_calls_h_api_for_user_update(self, h_api, lti_h_svc, h_user, h_group):
         lti_h_svc.sync([h_group])
@@ -96,6 +134,7 @@ class TestUserUpserting:
         h_api.upsert_user.assert_not_called()
 
 
+@pytest.mark.usefixtures("use_serial_api")
 class TestAddingUserToGroups:
     def test_it_adds_the_user_to_the_group(self, h_api, lti_h_svc, h_user, h_group):
         lti_h_svc.sync([h_group])
@@ -120,6 +159,11 @@ def lti_h_svc(pyramid_request):
 @pytest.fixture
 def h_user(pyramid_request):
     return pyramid_request.lti_user.h_user
+
+
+@pytest.fixture
+def use_serial_api(pyramid_request):
+    pyramid_request.feature = lambda item: item == "use_serial_api"
 
 
 @pytest.fixture
