fdeb17552ea3ea93741d20b33e84f74945aceb6d
Sean Hammond <git@snhmnd.fastmail.com>
2020-06-01 14:20:50 +0100

    Remove an unnecessary __all__

--- lms/models/application_instance.py
+++ lms/models/application_instance.py
@@ -9,8 +9,6 @@ from sqlalchemy.ext.mutable import MutableDict
 
 from lms.db import BASE
 
-__all__ = ["ApplicationInstance"]
-
 
 class ApplicationInstance(BASE):  # pylint:disable=too-few-public-methods
     """Class to represent a single lms install."""
