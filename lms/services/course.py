e2a0f3d39b84414213ade5a050ec32398dc0c68b
Jon Betts <jon.betts@hypothes.is>
2020-06-04 16:05:43 +0100

    Add a method to check if any course in an instance has a particular setting

--- lms/services/course.py
+++ lms/services/course.py
@@ -1,3 +1,5 @@
+import json
+
 from lms.models import Course, CourseGroupsExportedFromH
 
 
@@ -10,6 +12,27 @@ class CourseService:
     def get_or_create(self, authority_provided_id):
         """Add the current course to the `course` table if it's not there already."""
         return self._get(authority_provided_id) or self._create(authority_provided_id)
+
+    def any_with_setting(self, group, key, value=True):
+        """
+        Return whether any course has the specified setting.
+
+        Note! This will only work for courses that have the key and the value,
+        so if the key is missing entirely it will not be counted.
+
+        :param group: Setting group name
+        :param key: Setting key
+        :param value: Expected value
+        """
+
+        return bool(
+            self._db.query(Course)
+            .filter(Course.consumer_key == self._consumer_key)
+            # pylint: disable=protected-access
+            .filter(Course._settings[group][key] == json.dumps(value))
+            .limit(1)
+            .count()
+        )
 
     def _get(self, authority_provided_id):
         return self._db.query(Course).get((self._consumer_key, authority_provided_id))
--- tests/factories/__init__.py
+++ tests/factories/__init__.py
@@ -1,3 +1,4 @@
+from tests.factories.course import Course
 from tests.factories.grading_info import GradingInfo
 from tests.factories.h_group import HGroup
 from tests.factories.h_user import HUser
