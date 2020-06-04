24c3f28493f2a35cb594c618f7f2c9f09cdc3425
Jon Betts <jon.betts@hypothes.is>
2020-06-04 18:13:58 +0100

    Changing fixtures based on review feedback
     * Move pymark to the end
     * Use pre-defined factories for consumer key

--- tests/factories/course.py
+++ tests/factories/course.py
@@ -1,10 +1,11 @@
 import factory
 
 from lms import models
+from tests.factories._attributes import OAUTH_CONSUMER_KEY
 
 Course = factory.make_factory(  # pylint:disable=invalid-name
     models.Course,
-    consumer_key=factory.Faker("hexify", text="^" * 40),
+    consumer_key=OAUTH_CONSUMER_KEY,
     authority_provided_id=factory.Faker("hexify", text="^" * 40),
     _settings=factory.lazy_attribute(lambda o: {}),
 )
