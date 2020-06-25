9dab810f669c26f19ea647950e4f93e2eeed71f5
Robert Knight <robertknight@gmail.com>
2020-06-25 18:51:55 +0100

    Add a new context resource for the Canvas OAuth error view

--- lms/resources/__init__.py
+++ lms/resources/__init__.py
@@ -9,4 +9,5 @@ See the traversal-related sections in the Pyramid docs:
 * https://docs.pylonsproject.org/projects/pyramid/en/latest/narr/hybrid.html
 """
 from lms.resources.default import DefaultResource
+from lms.resources.frontend_app import FrontendAppResource
 from lms.resources.lti_launch import LTILaunchResource
