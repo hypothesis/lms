03cef26ed44cf971bccc506d16a98be75b887370
Sean Hammond <git@snhmnd.fastmail.com>
2020-07-02 12:46:11 +0100

    Enable Sentry's SQLAlchemy integration

--- lms/sentry.py
+++ lms/sentry.py
@@ -12,6 +12,7 @@ def includeme(config):
         {
             "h_pyramid_sentry.filters": [filter_canvas_api_access_token_error],
             "h_pyramid_sentry.retry_support": True,
+            "h_pyramid_sentry.sqlalchemy_support": True,
         }
     )
     config.include("h_pyramid_sentry")
