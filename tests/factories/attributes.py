a11543108607eebdc43441f342400e4b2bd499cc
Jon Betts <jon.betts@hypothes.is>
2020-07-17 18:56:38 +0100

    Add a token store which manages the OAuth2Token
     * Also added an OAuth2Token factory boy factory
     * Ditto for ApplicationInstance
    
    The `conftest.py` looks a bit goofy, but it's used again later.

--- tests/factories/attributes.py
+++ tests/factories/attributes.py
@@ -4,11 +4,8 @@ from factory import Faker
 
 OAUTH_CONSUMER_KEY = Faker("hexify", text="Hypothesis" + "^" * 32)
 SHARED_SECRET = Faker("hexify", text="^" * 64)
+ACCESS_TOKEN = REFRESH_TOKEN = Faker("hexify", text="^" * 32)
 
 USER_ID = Faker("hexify", text="^" * 40)
-
 H_USERNAME = Faker("hexify", text="^" * 30)
-
 H_DISPLAY_NAME = Faker("name")
-
-ACCESS_TOKEN = REFRESH_TOKEN = Faker("hexify", text="^" * 32)
