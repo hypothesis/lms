08d747ff92ccbdf2ed5ab9c79d5db15d598a80f8
Sean Hammond <git@snhmnd.fastmail.com>
2020-07-20 18:08:35 +0100

    Fix the Course test factory
    Test factories for SQLAlchemy models should have
    `FACTORY_CLASS=SQLAlchemyModelFactory`. This lets Factory Boy know that
    it's a SQLAlchemy factory and means that it automatically adds it to the
    test DB session when you do `factories.course()`,
    `factories.course.create()` or `factories.course.create_batch()`.
    
    If you want to make one _without_ adding it to the DB session (or
    without even needing to have a DB session) use `build()` or
    `build_batch()`.
    
    See `factories.GradingInfo` for an existing example of this.

--- tests/factories/course.py
+++ tests/factories/course.py
@@ -1,10 +1,12 @@
 import factory
+from factory.alchemy import SQLAlchemyModelFactory
 
 from lms import models
 from tests.factories._attributes import OAUTH_CONSUMER_KEY
 
 Course = factory.make_factory(  # pylint:disable=invalid-name
     models.Course,
+    FACTORY_CLASS=SQLAlchemyModelFactory,
     consumer_key=OAUTH_CONSUMER_KEY,
     authority_provided_id=factory.Faker("hexify", text="^" * 40),
     settings=factory.lazy_attribute(lambda o: {}),
