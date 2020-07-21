697e3e52e5b6f5dfa78c48c8cb7f0a6c5d014366
Sean Hammond <git@snhmnd.fastmail.com>
2020-07-21 09:27:45 +0100

    Clean up the functional tests
    Clean out some junk from our functional tests:
    
    * Get rid of JSON fixtures.
    
      Let's just use pytest's fixtures system and keep our fixtures in
      Python code, like we do everywhere else. This is for consistency, but
      it's also much simpler, and it's going to allow pytest fixture
      composition (where one fixture uses values created by another fixture)
      to be used as normal in a future PR.
    
      The base class for all tests classes to inherit from with the
      `json_fixture()` method that streams JSON fixtures from disk has been
      deleted. The two JSON fixtures that were in use have each been inlined
      into the Python code in the one place that each was used.
    
      It's worth remembering that the JSON format was (indirectly) based on
      Python's list and dict literals format. The two formats are almost
      exactly the same. You can copy some JSON data and paste it into Python
      code with almost no modification. So there's really little point in
      putting data in separate JSON files and then writing code to read them
      from disk and parse them. You can just put the data in Python files.
    
      If you don't want test data cluttering up the test code, put the
      fixtures in a separate Python code. In this (and most) cases though,
      maintenance is easier if the fixtures live in the same file with the
      tests that use them.
    
    * Delete a bunch of unused files.
    
      There was a `tests/functional/lti_certification/` dir with nothing in
      it.

--- tests/functional/test_sanity.py
+++ tests/functional/test_sanity.py
@@ -1,7 +1,4 @@
-from tests.functional.base_class import TestBaseClass
-
-
-class TestSanity(TestBaseClass):
+class TestSanity:
     """Some basic tests to ensure the world exists etc."""
 
     def test_sanity(self, app):
@@ -10,9 +7,7 @@ class TestSanity(TestBaseClass):
     def test_we_can_read_and_send_params(self, app):
         # We aren't really running this test because we care, but just
         # to prove the testing framework
-        result = app.get(
-            "/flags/test", params=self.json_fixture("sanity/bar_on.json"), status=200
-        )
+        result = app.get("/flags/test", params={"feature_flags.bar": True}, status=200)
 
         assert result.json_body["bar"] == "Bar feature flag is enabled"
         assert result.json_body["foo"] == "Foo feature flag is disabled"
