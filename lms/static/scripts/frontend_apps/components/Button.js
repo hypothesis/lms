5797e8e8f0e6c057356de7b6e0180a59aa21a771
Robert Knight <robertknight@gmail.com>
2020-07-24 06:47:42 +0100

    Convert `Button` props documentation from prop-types to JSDoc

--- lms/static/scripts/frontend_apps/components/Button.js
+++ lms/static/scripts/frontend_apps/components/Button.js
@@ -2,6 +2,18 @@ import classNames from 'classnames';
 import propTypes from 'prop-types';
 import { createElement } from 'preact';
 
+/**
+ * @typedef ButtonProps
+ * @prop {string} [className]
+ * @prop {boolean} [disabled]
+ * @prop {string} label
+ * @prop {() => any} onClick
+ * @prop {string} [type]
+ */
+
+/**
+ * @param {ButtonProps} props
+ */
 export default function Button({
   className = '',
   disabled = false,
