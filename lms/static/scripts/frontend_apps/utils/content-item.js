c1340d6b78e7d3d21ee1cd19cda8cfc14991d3bf
Robert Knight <robertknight@gmail.com>
2020-07-29 15:36:12 +0100

    Refactor `contentItemFromXYZ` functions and add missing tests
    Refactor the `contentItemFromUrl` and `contentItemFromFile` functions so
    that it is easier to add new types of LMS assignment in future and add
    missing tests for this module.

--- lms/static/scripts/frontend_apps/utils/content-item.js
+++ lms/static/scripts/frontend_apps/utils/content-item.js
@@ -2,21 +2,37 @@
  * @typedef {import('../api-types').File} File
  */
 
+import { stringify } from 'querystring';
+
 /**
  * Return a JSON-LD `ContentItem` representation of the LTI activity launch
  * URL for a given document URL.
+ *
+ * @param {string} ltiLaunchUrl
+ * @param {Object.<string,string>} params - Query parameters for the generated URL
  */
-export function contentItemForUrl(ltiLaunchUrl, documentUrl) {
+function contentItemWithParams(ltiLaunchUrl, params) {
   return {
     '@context': 'http://purl.imsglobal.org/ctx/lti/v1/ContentItem',
     '@graph': [
       {
         '@type': 'LtiLinkItem',
         mediaType: 'application/vnd.ims.lti.v1.ltilink',
-        url: `${ltiLaunchUrl}?url=${encodeURIComponent(documentUrl)}`,
+        url: `${ltiLaunchUrl}?${stringify(params)}`,
       },
     ],
   };
+}
+
+/**
+ * Return a JSON-LD `ContentItem` representation of the LTI activity launch
+ * URL for a given document URL.
+ *
+ * @param {string} ltiLaunchUrl
+ * @param {string} documentUrl
+ */
+export function contentItemForUrl(ltiLaunchUrl, documentUrl) {
+  return contentItemWithParams(ltiLaunchUrl, { url: documentUrl });
 }
 
 /**
@@ -27,14 +43,8 @@ export function contentItemForUrl(ltiLaunchUrl, documentUrl) {
  * @param {File} file
  */
 export function contentItemForLmsFile(ltiLaunchUrl, file) {
-  return {
-    '@context': 'http://purl.imsglobal.org/ctx/lti/v1/ContentItem',
-    '@graph': [
-      {
-        '@type': 'LtiLinkItem',
-        mediaType: 'application/vnd.ims.lti.v1.ltilink',
-        url: `${ltiLaunchUrl}?canvas_file=true&file_id=${file.id}`,
-      },
-    ],
-  };
+  return contentItemWithParams(ltiLaunchUrl, {
+    canvas_file: 'true',
+    file_id: file.id,
+  });
 }
