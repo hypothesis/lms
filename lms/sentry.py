9bb317ae3648465750aa417e8f63e9891138dd40
Sean Hammond <git@snhmnd.fastmail.com>
2020-07-29 16:31:59 +0100

    Enable Sentry's performance monitoring

--- lms/sentry.py
+++ lms/sentry.py
@@ -10,6 +10,8 @@ def filter_canvas_api_access_token_error(event):
 def includeme(config):
     config.add_settings(
         {
+            "h_pyramid_sentry.init.traces_sample_rate": 0.25,
+            "h_pyramid_sentry.init._experiments": {"auto_enabling_integrations": True},
             "h_pyramid_sentry.filters": [filter_canvas_api_access_token_error],
             "h_pyramid_sentry.retry_support": True,
             "h_pyramid_sentry.sqlalchemy_support": True,
