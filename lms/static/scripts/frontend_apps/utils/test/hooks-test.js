76dc656819c0986e110b4cb851f7cf44d2ef609e
Robert Knight <robertknight@gmail.com>
2020-08-13 10:10:57 +0100

    Call hooks from within a component in tests
    Hooks should only be called from within a functional component and
    Preact >= 10.4.7 throws if trying to call a hook from outside.
    
    Also test the returned values in more detail. Testing only for
    non-equality doesn't capture other properties that we do care about.

--- lms/static/scripts/frontend_apps/utils/test/hooks-test.js
+++ lms/static/scripts/frontend_apps/utils/test/hooks-test.js
@@ -1,9 +1,23 @@
+import { createElement, render } from 'preact';
+
 import { useUniqueId } from '../hooks';
 
 describe('hooks', () => {
   it('generates unique ids each time useUniqueId is called', () => {
-    const id1 = useUniqueId('prefix:');
-    const id2 = useUniqueId('prefix:');
+    let id1;
+    let id2;
+
+    function Widget() {
+      id1 = useUniqueId('prefix:');
+      id2 = useUniqueId('prefix:');
+      return null;
+    }
+    render(<Widget />, document.createElement('div'));
+
+    assert.typeOf(id1, 'string');
+    assert.typeOf(id2, 'string');
+    assert.isTrue(id1.startsWith('prefix:'));
+    assert.isTrue(id2.startsWith('prefix:'));
     assert.notEqual(id1, id2);
   });
 });
