f0981138319610d8095da703b1f43157f6bab108
Sean Hammond <git@snhmnd.fastmail.com>
2020-08-19 18:32:42 +0100

    Move exception views into a view class
    This removes duplication of the `renderer="json"` in the view configs,
    and also makes the tests a little nicer. It's going to make future
    changes to these views nicer too.

--- tests/unit/lms/views/api/error_test.py
+++ tests/unit/lms/views/api/error_test.py
@@ -1,48 +1,51 @@
+import pytest
+
 from lms.services import CanvasAPIError, LTIOutcomesAPIError
 from lms.validation import ValidationError
-from lms.views.api import error
+from lms.views.api.error import ExceptionViews
 
 
 class TestSchemaValidationError:
-    def test_it(self, pyramid_request):
-        json_data = error.validation_error(
-            ValidationError(messages="foobar"), pyramid_request
-        )
+    def test_it(self, pyramid_request, views):
+        json_data = views.validation_error()
+
         assert pyramid_request.response.status_code == 422
         assert json_data == {
             "message": "Unable to process the contained instructions",
             "details": "foobar",
         }
 
+    @pytest.fixture
+    def context(self):
+        return ValidationError(messages="foobar")
+
 
 class TestCanvasAPIAccessTokenError:
-    def test_it(self, pyramid_request):
-        json_data = error.canvas_api_access_token_error(pyramid_request)
+    def test_it(self, pyramid_request, views):
+        json_data = views.canvas_api_access_token_error()
 
         assert pyramid_request.response.status_code == 400
         assert json_data == {"message": None, "details": None}
 
 
 class TestCanvasAPIError:
-    def test_it(self, pyramid_request):
-        json_data = error.proxy_api_error(
-            CanvasAPIError(explanation="test_explanation", details={"foo": "bar"}),
-            pyramid_request,
-        )
+    def test_it(self, pyramid_request, views):
+        json_data = views.proxy_api_error()
 
         assert pyramid_request.response.status_code == 400
         assert json_data == {
             "message": "test_explanation",
             "details": {"foo": "bar"},
         }
+
+    @pytest.fixture
+    def context(self):
+        return CanvasAPIError(explanation="test_explanation", details={"foo": "bar"})
 
 
 class TestLTIOutcomesAPIError:
-    def test_it(self, pyramid_request):
-        json_data = error.proxy_api_error(
-            LTIOutcomesAPIError(explanation="test_explanation", details={"foo": "bar"}),
-            pyramid_request,
-        )
+    def test_it(self, pyramid_request, views):
+        json_data = views.proxy_api_error()
 
         assert pyramid_request.response.status_code == 400
         assert json_data == {
@@ -50,36 +53,55 @@ class TestLTIOutcomesAPIError:
             "details": {"foo": "bar"},
         }
 
+    @pytest.fixture
+    def context(self):
+        return LTIOutcomesAPIError(
+            explanation="test_explanation", details={"foo": "bar"}
+        )
+
 
 class TestNotFound:
-    def test_it_sets_response_status(self, pyramid_request):
-        error.notfound(pyramid_request)
+    def test_it_sets_response_status(self, pyramid_request, views):
+        views.notfound()
 
         assert pyramid_request.response.status_int == 404
 
-    def test_it_shows_a_generic_error_message_to_the_user(self, pyramid_request):
-        result = error.notfound(pyramid_request)
+    def test_it_shows_a_generic_error_message_to_the_user(self, views):
+        result = views.notfound()
 
         assert result["message"] == "Endpoint not found"
 
 
 class TestForbidden:
-    def test_it_sets_response_status(self, pyramid_request):
-        error.forbidden(pyramid_request)
+    def test_it_sets_response_status(self, pyramid_request, views):
+        views.forbidden()
 
         assert pyramid_request.response.status_int == 403
 
-    def test_it_shows_a_generic_error_message_to_the_user(self, pyramid_request):
-        result = error.forbidden(pyramid_request)
+    def test_it_shows_a_generic_error_message_to_the_user(self, views):
+        result = views.forbidden()
 
         assert result["message"] == "You're not authorized to view this page"
 
 
 class TestAPIError:
-    def test_it(self, pyramid_request):
-        json_data = error.api_error(pyramid_request)
+    def test_it(self, pyramid_request, views):
+        json_data = views.api_error()
 
         assert pyramid_request.response.status_code == 500
         assert json_data == {
-            "message": "A problem occurred while handling this request. Hypothesis has been notified."
+            "message": (
+                "A problem occurred while handling this request. Hypothesis has been"
+                " notified."
+            )
         }
+
+
+@pytest.fixture
+def context():
+    return None
+
+
+@pytest.fixture
+def views(context, pyramid_request):
+    return ExceptionViews(context, pyramid_request)
