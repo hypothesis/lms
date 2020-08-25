35538db6d9f92201d38c81144a918c857057d69e
Sean Hammond <git@snhmnd.fastmail.com>
2020-08-25 10:50:12 +0100

    Revert "Enable Sentry's performance monitoring"
    This reverts commit 1a41a3ce7daebf7615e8540bf190da3b1640aec6.

--- lms/sentry.py
+++ lms/sentry.py
@@ -10,8 +10,6 @@ def filter_canvas_api_access_token_error(event):
 def includeme(config):
     config.add_settings(
         {
-            "h_pyramid_sentry.init.traces_sample_rate": 0.25,
-            "h_pyramid_sentry.init._experiments": {"auto_enabling_integrations": True},
             "h_pyramid_sentry.filters": [filter_canvas_api_access_token_error],
             "h_pyramid_sentry.retry_support": True,
             "h_pyramid_sentry.sqlalchemy_support": True,
