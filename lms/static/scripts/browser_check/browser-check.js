030b1d2c06eaa189e91a51f8f3a52f205cb26fce
Robert Knight <robertknight@gmail.com>
2020-10-29 07:48:50 +0000

    Make browser support test work better in Edge Legacy
    Add a browser feature test for `Element.prepend()` method. This fixes a
    failure of the "unsupported browser" banner to show in older,
    unsupported versions of Edge (<= 16). This test is also a good match for
    our minimum Chrome version (available in v54, our minimum is Chrome
    v55).

--- lms/static/scripts/browser_check/browser-check.js
+++ lms/static/scripts/browser_check/browser-check.js
@@ -1,7 +1,8 @@
 /**
  * Run a series of feature tests to see if the browser is new enough to support Hypothesis.
  *
- * We use feature tests to try to avoid false negatives.
+ * We use feature tests to try to avoid false negatives. These are only representative,
+ * not exhaustive, checks of required APIs.
  *
  * @return {boolean}
  */
@@ -14,6 +15,7 @@ export function isBrowserSupported() {
     // DOM API checks for APIs used by the LMS frontend and Hypothesis client.
     () => new URL(document.location.href), // URL constructor.
     () => new Request('https://hypothes.is'), // Part of the `fetch` API.
+    () => document.body.prepend.name, // Element.prepend() method.
   ];
 
   try {
