feec13b344a2a16bc06e9d5946963b08187e8dc7
Robert Knight <robertknight@gmail.com>
2020-10-29 07:19:22 +0000

    Align "Dismiss" button to right edge of banner

--- lms/static/scripts/browser_check/index.js
+++ lms/static/scripts/browser_check/index.js
@@ -8,6 +8,7 @@ if (!isBrowserSupported()) {
     <b>You may need to upgrade your browser to use Hypothesis.</b>
     See <a href="https://web.hypothes.is/help/which-browsers-are-supported-by-hypothesis/" target="_blank">this support article</a> for more information.
   </div>
+  <div class="u-stretch"></div>
   <button class="Button">Dismiss</button>
 `;
 
