d162c1a946ca6cbe1c318bae80d2594f0b456cd1
Robert Knight <robertknight@gmail.com>
2021-02-04 08:12:01 +0000

    Remove unused `trustMarkup` function
    This was used at one point for SVG icons, but that was replaced with an
    alternate method of ensuring that only trusted SVG markup is used.

--- lms/static/scripts/frontend_apps/utils/test/trusted-test.js
+++
@@ -1,9 +0,0 @@
-import { trustMarkup } from '../trusted';
-
-describe('trustMarkup', () => {
-  it('returns the provided object wrapped in a key called `trustedHTML`', () => {
-    assert.match(trustMarkup('some object'), {
-      trustedHTML: 'some object',
-    });
-  });
-});
