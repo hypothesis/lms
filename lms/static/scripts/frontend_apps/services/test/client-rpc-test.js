5b88f055e80e4b511f7abd99c3ddd4d9f735550c
Robert Knight <robertknight@gmail.com>
2021-03-10 12:45:40 +0000

    Refresh client's grant token if it has expired
    During an LTI launch the LMS backend generates a grant token for use by the
    Hypothesis client that is valid for 5 minutes.
    
    If the Hypothesis client attempts to fetch its configuration after this
    time has elapsed then the client will fail to log in. With the sidebar
    application this rare because the sidebar launches as soon as Via loads.
    It could happen however if the LMS frontend did not load the assignment
    content immediately but instead paused for Canvas OAuth and the user
    took more than 5 minutes to respond. With the client's "Notebook"
    application this is expected to be much more common however, because the
    notebook isn't loaded until the user explicitly chooses the "Open
    notebook" menu option.
    
    Fix the issue by modifying the `ClientRpc` class in the LMS frontend to
    request a new grant token if the current one has expired when responding
    to a `requestConfig` RPC request from the Hypothesis client.
    
     - Add a `JWT` helper class that wraps a server-generated JWT and
       provides methods to check whether it is still valid.
    
     - Modify `ClientRpc` to use the `JWT` helper to check if the JWT is
       still valid before returning config to the client, and fetch a new
       grant token otherwise.
    
    Part of https://github.com/hypothesis/client/issues/2801

--- lms/static/scripts/frontend_apps/services/test/client-rpc-test.js
+++ lms/static/scripts/frontend_apps/services/test/client-rpc-test.js
@@ -1,14 +1,28 @@
 import { $imports, ClientRpc } from '../client-rpc';
 
 describe('ClientRpc', () => {
+  let authToken;
   let clientConfig;
+  let fakeApiCall;
+
   let FakeServer;
-  let fakeSidebarWindow;
-  let fakeRpcCall;
   let fakeServerInstance;
 
+  let fakeSidebarWindow;
+  let fakeRpcCall;
+
+  let FakeJWT;
+  let fakeJwt;
+
   beforeEach(() => {
-    clientConfig = { showHighlights: true };
+    clientConfig = {
+      showHighlights: true,
+      services: [
+        {
+          grantToken: 'initial.grant.token',
+        },
+      ],
+    };
 
     fakeSidebarWindow = {
       frame: {},
@@ -24,7 +38,31 @@ describe('ClientRpc', () => {
 
     FakeServer = sinon.stub().returns(fakeServerInstance);
 
+    FakeJWT = sinon.spy(token => {
+      fakeJwt = {
+        hasExpired: sinon.stub().returns(false),
+        value: () => token,
+      };
+      return fakeJwt;
+    });
+
+    let grantTokenCounter = 0;
+    fakeApiCall = sinon.spy(async () => {
+      ++grantTokenCounter;
+      return {
+        grant_token: 'new.grant.token-' + grantTokenCounter,
+      };
+    });
+
+    authToken = 'dummy-auth-token';
+
     $imports.$mock({
+      '../utils/api': {
+        apiCall: fakeApiCall,
+      },
+      '../utils/jwt': {
+        JWT: FakeJWT,
+      },
       '../../postmessage_json_rpc': {
         Server: FakeServer,
         call: fakeRpcCall,
@@ -39,6 +77,7 @@ describe('ClientRpc', () => {
   function createClientRpc() {
     return new ClientRpc({
       allowedOrigins: ['https://client.hypothes.is'],
+      authToken,
       clientConfig,
     });
   }
@@ -48,13 +87,66 @@ describe('ClientRpc', () => {
     assert.calledWith(FakeServer, ['https://client.hypothes.is']);
   });
 
-  it('registers "requestConfig" RPC handler that returns client config', () => {
-    createClientRpc();
-    assert.calledWith(fakeServerInstance.register, 'requestConfig');
-    const [, callback] = fakeServerInstance.register.args.find(
-      ([method]) => method === 'requestConfig'
-    );
-    assert.equal(callback(), clientConfig);
+  describe('"requestConfig" RPC handler', () => {
+    it('is registered', () => {
+      createClientRpc();
+      assert.calledWith(fakeServerInstance.register, 'requestConfig');
+    });
+
+    it('returns client config', async () => {
+      createClientRpc();
+      const [, callback] = fakeServerInstance.register.args.find(
+        ([method]) => method === 'requestConfig'
+      );
+      assert.equal(await callback(), clientConfig);
+    });
+
+    it('returns initial grant token if still valid', async () => {
+      createClientRpc();
+
+      const [, callback] = fakeServerInstance.register.args.find(
+        ([method]) => method === 'requestConfig'
+      );
+      const config = await callback();
+
+      assert.notCalled(fakeApiCall);
+      assert.calledWith(FakeJWT, 'initial.grant.token', sinon.match.number);
+      assert.equal(config.services[0].grantToken, 'initial.grant.token');
+    });
+
+    it('fetches and returns new grant token if it has expired', async () => {
+      createClientRpc();
+      const [, callback] = fakeServerInstance.register.args.find(
+        ([method]) => method === 'requestConfig'
+      );
+
+      // Simulate initial grant token expiring. This should trigger fetching of
+      // a new token.
+      fakeJwt.hasExpired.returns(true);
+      let config = await callback();
+      assert.calledWith(fakeApiCall, {
+        authToken,
+        path: '/api/grant_token',
+      });
+      assert.equal(config.services[0].grantToken, 'new.grant.token-1');
+
+      // Re-fetch config before the new token expires.
+      fakeApiCall.resetHistory();
+      fakeJwt.hasExpired.returns(false);
+
+      config = await callback();
+
+      assert.notCalled(fakeApiCall);
+      assert.equal(config.services[0].grantToken, 'new.grant.token-1');
+
+      // Re-fetch config after the new token expires.
+      fakeJwt.hasExpired.returns(true);
+
+      config = await callback();
+
+      assert.calledWith(fakeApiCall, { authToken, path: '/api/grant_token' });
+      assert.equal(config.services[0].grantToken, 'new.grant.token-2');
+    });
   });
 
   it('registers "requestGroups" RPC handler', () => {
