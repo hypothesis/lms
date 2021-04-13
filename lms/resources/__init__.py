5d12bf1a5ab4e4da44ac852810f06514cb038e6d
Marcos Prieto <marcospri@gmail.com>
2021-04-13 13:10:29 +0200

    Delete DefaultResource. Used only for it's ACL

--- lms/resources/__init__.py
+++ lms/resources/__init__.py
@@ -8,6 +8,5 @@ See the traversal-related sections in the Pyramid docs:
 * https://docs.pylonsproject.org/projects/pyramid/en/latest/narr/traversal.html
 * https://docs.pylonsproject.org/projects/pyramid/en/latest/narr/hybrid.html
 """
-from lms.resources.default import DefaultResource
 from lms.resources.lti_launch import LTILaunchResource
 from lms.resources.oauth2_redirect import OAuth2RedirectResource
