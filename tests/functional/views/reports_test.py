a78d5e897ea8a8468086fb9888f815f33f4f27e9
Marcos Prieto <marcospri@gmail.com>
2021-04-14 12:39:51 +0200

    Extra functional tests

---
+++ tests/functional/views/reports_test.py
@@ -0,0 +1,31 @@
+from pyramid.authentication import AuthTicket
+
+
+class TestReports:
+    def test_it_allows_whitelisted_user(self, app):
+        cookie_token = AuthTicket(
+            "TEST_LMS_SECRET", "jeremy", "0.0.0.0", hashalg="sha512"
+        ).cookie_value()
+
+        app.set_cookie("auth_tkt", cookie_token)
+
+        response = app.get(
+            "/reports",
+            status=200,
+        )
+
+        assert "Application Instances" in response.text
+
+    def test_it_doesnt_allow_any_user(self, app):
+        cookie_token = AuthTicket(
+            "TEST_LMS_SECRET", "some-user", "0.0.0.0", hashalg="sha512"
+        ).cookie_value()
+
+        app.set_cookie("auth_tkt", cookie_token)
+
+        response = app.get(
+            "/reports",
+            status=200,
+        )
+        for field in ["came_from", "username", "password"]:
+            assert field in response.forms[0].fields
