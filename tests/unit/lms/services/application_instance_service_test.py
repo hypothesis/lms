afbf9f34214e51624bc4c54bd990fd1297b2e5c6
Sean Hammond <seanh@users.noreply.github.com>
2021-05-06 17:10:07 +0100

    Remove ApplicationInstanceService.update_settings()
    Not needed

--- tests/unit/lms/services/application_instance_service_test.py
+++ tests/unit/lms/services/application_instance_service_test.py
@@ -2,7 +2,6 @@ from unittest import mock
 
 import pytest
 
-from lms.models import ApplicationInstance
 from lms.services import ConsumerKeyError
 from lms.services.application_instance import factory
 from tests import factories
@@ -27,47 +26,6 @@ class TestApplicationInstanceService:
 
         with pytest.raises(ConsumerKeyError):
             svc.get(None)
-
-    def test_update_settings_no_update(
-        self, svc, test_application_instance, db_session
-    ):
-        svc.update_settings(test_application_instance)
-
-        # Let's re-query to get the state form the DB
-        ai = db_session.query(ApplicationInstance).get(test_application_instance.id)
-
-        assert ai.settings == test_application_instance.settings
-
-    @pytest.mark.parametrize(
-        "setting_name,setting_location",
-        [
-            ("canvas_sections_enabled", "canvas.sections_enabled"),
-            ("canvas_groups_enabled", "canvas.groups_enabled"),
-        ],
-    )
-    def test_update_settings_boolean_toggle(
-        self,
-        setting_name,
-        setting_location,
-        svc,
-        test_application_instance,
-        db_session,
-    ):
-        svc.update_settings(test_application_instance, **{setting_name: True})
-
-        # Let's re-query to get the state form the DB
-        ai = db_session.query(ApplicationInstance).get(test_application_instance.id)
-        assert ai.settings.get(*setting_location.split(".")) is True
-
-        svc.update_settings(test_application_instance, **{setting_name: False})
-
-        # Re-query again
-        ai = db_session.query(ApplicationInstance).get(test_application_instance.id)
-        assert (
-            # pylint: disable=compare-to-zero
-            ai.settings.get(*setting_location.split("."))
-            is False
-        )
 
     @pytest.fixture
     def svc(self, pyramid_request):
