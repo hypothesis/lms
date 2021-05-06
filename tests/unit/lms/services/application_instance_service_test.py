aeff434f79ddde40877eecfa195f828a737ae08f
Sean Hammond <seanh@users.noreply.github.com>
2021-05-06 14:05:44 +0100

    Add default consumer key to ApplicationInstanceService
    This adds an `ApplicationInstanceGetter` feature to
    `ApplicationInstanceService`, which should make it easier to replace
    `ApplicationInstanceGetter` with `ApplicationInstanceService` in a
    future commit.

--- tests/unit/lms/services/application_instance_service_test.py
+++ tests/unit/lms/services/application_instance_service_test.py
@@ -14,9 +14,19 @@ class TestApplicationInstanceGetter:
             svc.get(test_application_instance.consumer_key) == test_application_instance
         )
 
+    def test_get_with_default_consumer_key(self, svc, default_application_instance):
+        assert svc.get() == default_application_instance
+
     def test_get_raises_ConsumerKeyError_if_consumer_key_doesnt_exist(self, svc):
         with pytest.raises(ConsumerKeyError):
             assert svc.get("NOPE") is None
+
+    def test_get_raises_ConsumerKeyError_if_consumer_key_is_None(self, pyramid_request):
+        pyramid_request.lti_user = None
+        svc = factory(mock.sentinel.context, pyramid_request)
+
+        with pytest.raises(ConsumerKeyError):
+            svc.get(None)
 
     def test_update_settings_no_update(
         self, svc, test_application_instance, db_session
@@ -64,6 +74,14 @@ class TestApplicationInstanceGetter:
         return factory(mock.sentinel.context, pyramid_request)
 
     @pytest.fixture(autouse=True)
+    def default_application_instance(self, db_session, pyramid_request):
+        ai = factories.ApplicationInstance(
+            consumer_key=pyramid_request.lti_user.oauth_consumer_key
+        )
+        db_session.flush()
+        return ai
+
+    @pytest.fixture(autouse=True)
     def test_application_instance(self, db_session):
         ai = factories.ApplicationInstance()
         db_session.flush()
@@ -75,3 +93,25 @@ class TestApplicationInstanceGetter:
         # Add some "noise" application instances to the DB for every test, to
         # make the tests more realistic.
         factories.ApplicationInstance.create_batch(size=3)
+
+
+class TestFactory:
+    def test_it(self, ApplicationInstanceService, pyramid_request):
+        application_instance_service = factory(mock.sentinel.context, pyramid_request)
+
+        ApplicationInstanceService.assert_called_once_with(
+            pyramid_request.db, pyramid_request.lti_user.oauth_consumer_key
+        )
+        assert application_instance_service == ApplicationInstanceService.return_value
+
+    def test_it_with_no_lti_user(self, ApplicationInstanceService, pyramid_request):
+        pyramid_request.lti_user = None
+
+        application_instance_service = factory(mock.sentinel.context, pyramid_request)
+
+        ApplicationInstanceService.assert_called_once_with(pyramid_request.db, None)
+        assert application_instance_service == ApplicationInstanceService.return_value
+
+    @pytest.fixture(autouse=True)
+    def ApplicationInstanceService(self, patch):
+        return patch("lms.services.application_instance.ApplicationInstanceService")
