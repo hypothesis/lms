a17a83ad0de8c31f12c56f6b253c86627f0da541
Sean Hammond <seanh@users.noreply.github.com>
2021-05-06 14:05:44 +0100

    Add default consumer key to ApplicationInstanceService
    This adds an `ApplicationInstanceGetter` feature to
    `ApplicationInstanceService`, which should make it easier to replace
    `ApplicationInstanceGetter` with `ApplicationInstanceService` in a
    future commit.

--- lms/services/application_instance.py
+++ lms/services/application_instance.py
@@ -5,16 +5,22 @@ from lms.services import ConsumerKeyError
 
 
 class ApplicationInstanceService:
-    def __init__(self, db):
+    def __init__(self, db, default_consumer_key):
         self._db = db
+        self._default_consumer_key = default_consumer_key
 
     @lru_cache
-    def get(self, consumer_key: str) -> ApplicationInstance:
+    def get(self, consumer_key=None) -> ApplicationInstance:
         """
         Return the ApplicationInstance with the given consumer_key.
 
+        If no consumer_key is given return the current request's
+        ApplicationInstance (the ApplicationInstance whose consumer_key matches
+        request.lti_user.oauth_consumer_key).
+
         :raise ConsumerKeyError: if the consumer_key isn't in the database
         """
+        consumer_key = consumer_key or self._default_consumer_key
 
         application_instance = ApplicationInstance.get_by_consumer_key(
             self._db, consumer_key
@@ -42,6 +48,9 @@ class ApplicationInstanceService:
 
 
 def factory(_context, request):
-    return ApplicationInstanceService(
-        request.db,
-    )
+    if request.lti_user:
+        consumer_key = request.lti_user.oauth_consumer_key
+    else:
+        consumer_key = None
+
+    return ApplicationInstanceService(request.db, consumer_key)
