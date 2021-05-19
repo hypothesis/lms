47af9ce3a19f08111d6c74e3e2e1081fc7d178e7
Marcos Prieto <marcospri@gmail.com>
2021-05-19 09:31:54 +0200

    New model for Grouping

--- tests/factories/__init__.py
+++ tests/factories/__init__.py
@@ -17,6 +17,7 @@ from tests.factories.attributes import (
 )
 from tests.factories.course import Course
 from tests.factories.grading_info import GradingInfo
+from tests.factories.grouping import Grouping
 from tests.factories.h_group import HGroup
 from tests.factories.h_user import HUser
 from tests.factories.lti_user import LTIUser
