d042eb6742735e3b74af3f60caa14dac8bb4b88a
Marcos Prieto <marcospri@gmail.com>
2021-05-19 11:44:26 +0200

    Remove duplicate course on HGroup, moved to Grouping

--- lms/models/h_group.py
+++ lms/models/h_group.py
@@ -14,21 +14,6 @@ class HGroup(NamedTuple):
         return f"group:{self.authority_provided_id}@{authority}"
 
     @classmethod
-    def course_group(cls, course_name, tool_consumer_instance_guid, context_id):
-        """
-        Create an HGroup for a course.
-
-        :param course_name: The name of the course
-        :param tool_consumer_instance_guid: Tool consumer GUID
-        :param context_id: Course id
-        """
-        return HGroup(
-            cls._name(course_name),
-            hashed_id(tool_consumer_instance_guid, context_id),
-            type="course_group",
-        )
-
-    @classmethod
     def section_group(
         cls, section_name, tool_consumer_instance_guid, context_id, section_id
     ):
