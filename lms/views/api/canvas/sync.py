f0df5421e7cf74055d51bbc55900d4e595c8dce2
Marcos Prieto <marcospri@gmail.com>
2021-06-04 16:56:14 +0200

    Not a group launch if groups_enabled==False

--- lms/views/api/canvas/sync.py
+++ lms/views/api/canvas/sync.py
@@ -39,6 +39,12 @@ class Sync:
 
     @property
     def _is_group_launch(self):
+        application_instance = self._request.find_service(
+            name="application_instance"
+        ).get()
+        if not application_instance.settings.get("canvas", "groups_enabled"):
+            return False
+
         try:
             self.group_set
         except (KeyError, ValueError, TypeError):
