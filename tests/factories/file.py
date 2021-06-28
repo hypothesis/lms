edfe0239d1329893ac93a5217264fd5c4c51fcd9
Sean Hammond <seanh@users.noreply.github.com>
2021-06-28 15:23:22 +0100

    Add FileService
    This will be used by the Canvas service in future to look up matching
    files in the DB.

---
+++ tests/factories/file.py
@@ -0,0 +1,16 @@
+from factory import Faker, Sequence, SubFactory, make_factory
+from factory.alchemy import SQLAlchemyModelFactory
+
+from lms import models
+from tests.factories.application_instance import ApplicationInstance
+
+File = make_factory(
+    models.File,
+    FACTORY_CLASS=SQLAlchemyModelFactory,
+    application_instance=SubFactory(ApplicationInstance),
+    type="canvas_file",
+    lms_id=Sequence(lambda n: f"{n}"),
+    course_id=Faker("random_int"),
+    name=Faker("word"),
+    size=Faker("random_int"),
+)
