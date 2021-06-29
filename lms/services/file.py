193c942a20ff34a886a67b829f7259f152736f39
Sean Hammond <seanh@users.noreply.github.com>
2021-06-29 17:48:21 +0100

    Inject current ApplicationInstance into FileService
    I'm not sure [what I was thinking](https://github.com/hypothesis/lms/pull/2892#discussion_r660528549)
    but the higher-level code that's going to call `FileService` ends up
    simpler if `FileService`'s factory injects the `ApplicationInstance`.
    Without this change `CanvasService.public_url_for_file()` ends up with
    too many arguments. See https://github.com/hypothesis/lms/pull/2899/files#r660780163

--- lms/services/file.py
+++ lms/services/file.py
@@ -2,19 +2,25 @@ from lms.models import File
 
 
 class FileService:
-    def __init__(self, db):
+    def __init__(self, application_instance, db):
+        self._application_instance = application_instance
         self._db = db
 
-    def get(self, application_instance, lms_id, type_):
-        """Return the file with application_instance, lms_id and type_ or None."""
+    def get(self, lms_id, type_):
+        """Return the file with the given lms_id and type_ or None."""
         return (
             self._db.query(File)
             .filter_by(
-                application_instance=application_instance, lms_id=lms_id, type=type_
+                application_instance=self._application_instance,
+                lms_id=lms_id,
+                type=type_,
             )
             .one_or_none()
         )
 
 
 def factory(_context, request):
-    return FileService(db=request.db)
+    return FileService(
+        application_instance=request.find_service(name="application_instance").get(),
+        db=request.db,
+    )
