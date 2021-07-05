ac7655cd5957fba605d76b50d095b310da98b265
Jon Betts <jon.betts@hypothes.is>
2021-07-05 13:41:16 +0100

    Use parameterization and more fixtures to shorten the tests a bit
    This uses a single table class for all tests and toggles the on update
    value for it.
    
    It also parameterises the column under test and uses a helper to make
    the tests a bit shorter and snappier.

--- tests/unit/lms/db/_bulk_action_test.py
+++ tests/unit/lms/db/_bulk_action_test.py
@@ -19,19 +19,8 @@ class TestBulkAction:
         id = sa.Column(sa.Integer, primary_key=True)
         name = sa.Column(sa.String, nullable=False)
         other = sa.Column(sa.String)
-        onupdate_column = sa.Column(sa.String, onupdate=42)
 
-    class TableWithBulkUpsertOnUpdate(BASE):
-        __tablename__ = "test_table_with_bulk_upsert_onupdates"
-
-        BULK_CONFIG = BulkAction.Config(
-            upsert_index_elements=["id"],
-            upsert_update_elements=["name"],
-            upsert_trigger_onupdate=True,
-        )
-
-        id = sa.Column(sa.Integer, primary_key=True)
-        name = sa.Column(sa.String)
+        # Lots of auto update columns
         scalar = sa.Column(sa.Integer, onupdate=42)
         callable = sa.Column(sa.Integer, onupdate=lambda: 42)
         sql = sa.Column(sa.Integer, onupdate=sa.select([42]))
@@ -57,78 +46,37 @@ class TestBulkAction:
 
         assert isinstance(result, CursorResult)
 
-        rows = list(db_session.query(self.TableWithBulkUpsert))
-
-        assert (
-            rows
-            == Any.iterable.containing(
-                [
-                    Any.instance_of(self.TableWithBulkUpsert).with_attrs(expected)
-                    for expected in [
-                        {"id": 1, "name": "update_old", "other": "pre_1"},
-                        {"id": 2, "name": "pre_existing_2", "other": "pre_2"},
-                        {"id": 3, "name": "create_with_id", "other": "post_3"},
-                        {"id": 4, "name": "over_block_size", "other": "post_4"},
-                    ]
-                ]
-            ).only()
+        self.assert_has_rows(
+            db_session,
+            {"id": 1, "name": "update_old", "other": "pre_1"},
+            {"id": 2, "name": "pre_existing_2", "other": "pre_2"},
+            {"id": 3, "name": "create_with_id", "other": "post_3"},
+            {"id": 4, "name": "over_block_size", "other": "post_4"},
         )
 
     def test_upsert_does_nothing_if_given_an_empty_list_of_values(self, db_session):
         assert BulkAction(db_session).upsert(self.TableWithBulkUpsert, []) == []
 
-    def test_upsert_with_onupdate_columns(self, db_session):
+    @pytest.mark.parametrize("column", ("scalar", "callable", "sql", "default"))
+    @pytest.mark.usefixtures("with_upsert_trigger_onupdate")
+    def test_upsert_with_onupdate_columns(self, db_session, column):
         db_session.add_all(
             [
-                self.TableWithBulkUpsertOnUpdate(
-                    id=1, name="existing", scalar=0, callable=0, sql=0, default=0
-                ),
-                self.TableWithBulkUpsertOnUpdate(
-                    id=2, name="existing", scalar=1, callable=1, sql=1, default=1
-                ),
+                self.TableWithBulkUpsert(id=1, name="pre_existing_1", **{column: 0}),
+                self.TableWithBulkUpsert(id=2, name="pre_existing_2", **{column: 1}),
             ]
         )
         db_session.flush()
 
         BulkAction(db_session).upsert(
-            self.TableWithBulkUpsertOnUpdate,
-            [
-                {"id": 1, "name": "update_existing"},
-            ],
+            self.TableWithBulkUpsert, [{"id": 1, "name": "update_existing"}]
         )
 
-        rows = list(
-            db_session.query(self.TableWithBulkUpsertOnUpdate).order_by(
-                self.TableWithBulkUpsertOnUpdate.id.asc()
-            )
-        )
-        assert (
-            rows
-            == Any.iterable.containing(
-                [
-                    Any.instance_of(self.TableWithBulkUpsertOnUpdate).with_attrs(
-                        expected
-                    )
-                    for expected in [
-                        {
-                            "id": 1,
-                            "name": "update_existing",
-                            "scalar": 42,
-                            "callable": 42,
-                            "sql": 42,
-                            "default": 42,
-                        },
-                        {
-                            "id": 2,
-                            "name": "existing",
-                            "scalar": 1,
-                            "callable": 1,
-                            "sql": 1,
-                            "default": 1,
-                        },
-                    ]
-                ]
-            ).only()
+        self.assert_has_rows(
+            db_session,
+            # 42 is the onupdate default value
+            {"id": 1, "name": "update_existing", column: 42},
+            {"id": 2, "name": "pre_existing_2", column: 1},
         )
 
     def test_it_fails_with_missing_config(self, db_session):
@@ -145,3 +93,22 @@ class TestBulkAction:
                 NOT_THE_RIGHT_NAME = BulkAction.Config(
                     upsert_index_elements=["id"], upsert_update_elements=["name"]
                 )
+
+    def assert_has_rows(self, db_session, *attrs):
+        rows = list(db_session.query(self.TableWithBulkUpsert))
+
+        assert (
+            rows
+            == Any.iterable.containing(
+                [
+                    Any.instance_of(self.TableWithBulkUpsert).with_attrs(expected)
+                    for expected in attrs
+                ]
+            ).only()
+        )
+
+    @pytest.fixture
+    def with_upsert_trigger_onupdate(self):
+        self.TableWithBulkUpsert.BULK_CONFIG.upsert_trigger_onupdate = True
+        yield
+        self.TableWithBulkUpsert.BULK_CONFIG.upsert_trigger_onupdate = False
