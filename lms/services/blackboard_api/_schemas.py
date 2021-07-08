2a3e4a0d0e5a31c61f3b480cc8e72cd0cd889655
Sean Hammond <seanh@users.noreply.github.com>
2021-07-08 11:04:11 +0100

    Move "marshalling" code from schemas to views
    Make the BlackboardAPIClient service (and its schemas) just return
    Blackboard API dicts in the format given by the Blackboard API, without
    modifying them.
    
    The service *does* remove unwanted fields that we don't care about from
    the dicts. In the future we'll actually add Blackboard API query params
    to request only the fields we want and save network bytes.
    
    The service does also validate that the Blackboard dicts are as we
    expect.
    
    But it doesn't mutate them any further. Instead it's the view's job to
    not about our proxy API's response format and transform the dicts into
    that format.

--- lms/services/blackboard_api/_schemas.py
+++ lms/services/blackboard_api/_schemas.py
@@ -8,46 +8,32 @@ from marshmallow import EXCLUDE, Schema, fields, post_load
 from lms.validation._base import RequestsResponseSchema
 
 
-class _FileSchema(Schema):
-    """Shared base schema for Blackboard file dicts."""
-
-    class Meta:
-        unknown = EXCLUDE
-
-    id = fields.Str(required=True)
-    display_name = fields.Str(data_key="name", required=True)
-    updated_at = fields.Str(data_key="modified", required=True)
-    mime_type = fields.Str(data_key="mimeType")
-
-    @post_load
-    def post_load(self, data, **_kwargs):  # pylint:disable=no-self-use
-        data["id"] = f"blackboard://content-resource/{data['id']}/"
-        return data
-
-
 class BlackboardListFilesSchema(RequestsResponseSchema):
     """Schema for Blackboard API /courses/{courseId}/resources responses."""
 
-    results = fields.List(fields.Nested(_FileSchema), required=True)
+    class FileSchema(Schema):
+        """Schema for individual Blackboard file dicts."""
+
+        class Meta:
+            unknown = EXCLUDE
+
+        id = fields.Str(required=True)
+        name = fields.Str(required=True)
+        modified = fields.Str(required=True)
+        mimeType = fields.Str()
+
+    results = fields.List(fields.Nested(FileSchema), required=True)
 
     @post_load
     def post_load(self, data, **_kwargs):  # pylint:disable=no-self-use
-        pdf_files = []
-
-        for file in data["results"]:
-            if file.get("mime_type") == "application/pdf":
-                # Delete mime_type: we don't want to send it to the frontend.
-                del file["mime_type"]
-                pdf_files.append(file)
-
-        return pdf_files
+        return data["results"]
 
 
-class BlackboardPublicURLSchema(RequestsResponseSchema, _FileSchema):
+class BlackboardPublicURLSchema(RequestsResponseSchema):
     """Schema for Blackboard /courses/{courseId}/resources/{resourceId} responses."""
 
     downloadUrl = fields.Str(required=True)
 
     @post_load
-    def post_load(self, data, **_kwargs):
+    def post_load(self, data, **_kwargs):  # pylint:disable=no-self-use
         return data["downloadUrl"]
