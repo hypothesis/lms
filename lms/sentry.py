66b701027606d9df304226b4bc8b77f6c8edc158
Sean Hammond <seanh@users.noreply.github.com>
2021-07-14 15:41:03 +0100

    Remove filter_oauth2_token_error()
    This doesn't seem to be necessary anymore. These are caught by an
    exception view which seems to mean that they don't get reported to
    Sentry.

--- lms/sentry.py
+++ lms/sentry.py
@@ -1,16 +1,9 @@
 """Sentry crash reporting integration."""
-from lms.services import OAuth2TokenError
-
-
-def filter_oauth2_token_error(event):
-    """Filter out all OAuth2TokenError exceptions."""
-    return isinstance(event.exception, OAuth2TokenError)
 
 
 def includeme(config):
     config.add_settings(
         {
-            "h_pyramid_sentry.filters": [filter_oauth2_token_error],
             "h_pyramid_sentry.retry_support": True,
             "h_pyramid_sentry.sqlalchemy_support": True,
         }
