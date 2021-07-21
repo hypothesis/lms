80c83a8fd01b1a2a864e11cd41054bce5078e030
Sean Hammond <seanh@users.noreply.github.com>
2021-07-21 16:31:27 +0100

    Add Blackboard refresh-and-retry support
    When a Blackboard API request fails for any reason try to get a new
    access token without user interaction by doing a refresh token request
    and then re-try the original API request once more.
    
    If the refresh token request fails raise an OAuth2TokenError which will
    trigger the **Authorize Hypothesis** dialog to get a new access token
    with user interaction.
    
    If the refresh token request succeeds but then the second attempt at the
    API request still fails raise an HTTPError which will trigger an error
    dialog (with **Try again** button).

--- tests/unit/lms/services/blackboard_api/_basic_test.py
+++ tests/unit/lms/services/blackboard_api/_basic_test.py
@@ -1,4 +1,4 @@
-from unittest.mock import sentinel
+from unittest.mock import DEFAULT, call, sentinel
 
 import pytest
 
@@ -75,15 +75,62 @@ class TestBasicClientRequest:
             ("/foo/bar/", "https://blackboard.example.com/foo/bar/"),
         ],
     )
-    def test_it(self, basic_client, oauth_http_service, path, expected_url):
+    def test_it_sends_the_request_and_returns_the_response(
+        self, basic_client, oauth_http_service, path, expected_url
+    ):
         response = basic_client.request("GET", path)
 
         oauth_http_service.request.assert_called_once_with("GET", expected_url)
         assert response == oauth_http_service.request.return_value
 
-    def test_it_raises_OAuth2TokenError_if_our_access_token_isnt_working(
+    # If the request fails on first attempt request() sends a refresh token
+    # request and then re-tries the original request.
+    def test_it_refreshes_and_tries_again_if_the_first_request_fails(
         self, basic_client, oauth_http_service
     ):
+        # Make the first attempt at the request fail but the second succeed.
+        oauth_http_service.request.side_effect = [HTTPError, DEFAULT]
+
+        response = basic_client.request("GET", "/foo")
+
+        assert oauth_http_service.request.call_args_list == [
+            call("GET", "https://blackboard.example.com/foo"),
+            call("GET", "https://blackboard.example.com/foo"),
+        ]
+        oauth_http_service.refresh_access_token.assert_called_once_with(
+            token_url="https://blackboard.example.com/learn/api/public/v1/oauth2/token",
+            redirect_uri=sentinel.redirect_uri,
+            auth=(sentinel.client_id, sentinel.client_secret),
+        )
+        assert response == oauth_http_service.request.return_value
+
+    # If the request fails on first attempt and then the refresh token request
+    # also raises then request() raises.
+    def test_it_raises_if_the_refresh_request_fails(
+        self, basic_client, oauth_http_service
+    ):
+        # Make the API request fail.
+        oauth_http_service.request.side_effect = HTTPError
+        # Make the refresh token request also fail.
+        oauth_http_service.refresh_access_token.side_effect = HTTPError
+
+        with pytest.raises(HTTPError):
+            basic_client.request("GET", "/foo")
+
+    # If both attempts at the API request fail then request() raises.
+    def test_request_raises_if_the_second_request_fails(
+        self, basic_client, oauth_http_service
+    ):
+        # Make the API request fail both times.
+        oauth_http_service.request.side_effect = HTTPError
+
+        with pytest.raises(HTTPError):
+            basic_client.request("GET", "/foo")
+
+    def test_it_raises_OAuth2TokenError_if_the_request_fails_with_an_access_token_error(
+        self, basic_client, oauth_http_service
+    ):
+        # Make the API request fail both times.
         oauth_http_service.request.side_effect = HTTPError(
             factories.requests.Response(
                 json_data={"status": 401, "message": "Bearer token is invalid"}
@@ -91,20 +138,7 @@ class TestBasicClientRequest:
         )
 
         with pytest.raises(OAuth2TokenError):
-            basic_client.request("GET", "foo/bar/")
-
-    def test_it_raises_HTTPError_if_the_HTTP_request_fails(
-        self, basic_client, oauth_http_service
-    ):
-        oauth_http_service.request.side_effect = HTTPError(
-            factories.requests.Response(
-                # Just some unrecognized error response from Blackboard.
-                json_data={"foo": "bar"}
-            )
-        )
-
-        with pytest.raises(HTTPError):
-            basic_client.request("GET", "foo/bar/")
+            basic_client.request("GET", "/foo")
 
 
 @pytest.fixture
