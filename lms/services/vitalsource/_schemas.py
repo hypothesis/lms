2e67fcdefd5ebbba6b0d754e29d11ffc88dbfbfc
Marcos Prieto <marcospri@gmail.com>
2021-07-27 15:42:19 +0200

    VS API schemas module

---
+++ lms/services/vitalsource/_schemas.py
@@ -0,0 +1,33 @@
+"""
+Schemas for VitalSource API responses.
+
+See: https://developer.vitalsource.com/hc/en-us/categories/360001974433 
+"""
+from marshmallow import EXCLUDE, Schema, fields
+
+from lms.validation._base import RequestsResponseSchema
+
+
+class BookInfoSchema(RequestsResponseSchema):
+    class ResourceLinks(Schema):
+        class Meta:
+            unknown = EXCLUDE
+
+        cover_image = fields.Str(required=True)
+
+    vbid = fields.Str(required=True)
+    title = fields.Str(required=True)
+
+    resource_links = fields.Nested(ResourceLinks, required=True)
+
+
+class BookTOCSchema(RequestsResponseSchema):
+    class Chapter(Schema):
+        class Meta:
+            unknown = EXCLUDE
+
+        title = fields.Str(required=True)
+        cfi = fields.Str(required=True)
+        page = fields.Str(required=True)
+
+    table_of_contents = fields.List(fields.Nested(Chapter), required=True)
