66d314f4a10283d6bc0f4cf41858493502e41d15
Marcos Prieto <marcospri@gmail.com>
2021-07-27 15:50:41 +0200

    Use VS service in views

--- lms/views/api/vitalsource.py
+++ lms/views/api/vitalsource.py
@@ -1,32 +1,37 @@
+from pyramid.httpexceptions import HTTPNotFound
 from pyramid.view import view_config, view_defaults
 
 from lms.security import Permissions
 from lms.services.exceptions import ProxyAPIError
-from lms.views.api.vitalsource_sample_data import book_data, toc_data
 
 
 @view_defaults(renderer="json", permission=Permissions.API)
 class VitalSourceAPIViews:
     def __init__(self, request):
         self.request = request
+        self.vitalsource_service = request.find_service(name="vitalsource")
 
     @view_config(route_name="vitalsource_api.books.info")
     def book_info(self):
         book_id = self.request.matchdict["book_id"]
 
-        # In future this will fetch data from VitalSource.
-        if book_id not in book_data:
-            raise ProxyAPIError(f"Book {book_id} not found")
+        book_info = self.vitalsource_service.book_info(book_id)
 
-        return book_data[book_id]
+        if not book_info:
+            raise HTTPNotFound()
+
+        return {
+            "id": book_info["vbid"],
+            "title": book_info["title"],
+            "cover_image": book_info["resource_links"]["cover_image"],
+        }
 
     @view_config(route_name="vitalsource_api.books.toc")
     def table_of_contents(self):
         book_id = self.request.matchdict["book_id"]
 
-        # In future this will fetch the data from VitalSource using the
-        # https://api/vitalsource.com/v4/products/{BOOK_ID}/toc endpoint.
-        if book_id not in toc_data:
-            raise ProxyAPIError(f"Book {book_id} not found")
+        book_toc = self.vitalsource_service.book_toc(book_id)
+        if not book_toc:
+            raise HTTPNotFound()
 
-        return toc_data[book_id]
+        return book_toc["table_of_contents"]
