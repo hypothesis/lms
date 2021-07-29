71a9766b99e563404e5962681be8c599e92d00b5
Robert Knight <robertknight@gmail.com>
2021-07-29 17:03:23 +0100

    Update lms.assets to work the way it does in h
    Apply the most recent changes from https://github.com/hypothesis/h/pull/6752
    to asset serving in lms:
    
     - Reload assets when the asset manifest changes in development
     - Use `importlib_resources` to get the location of lms package files
    
    One difference is that we're trying to simplify configuration here by
    re-using the existing "dev" setting to control whether auto-reload is
    used rather than having a separate setting.

--- lms/assets.py
+++ lms/assets.py
@@ -1,15 +1,19 @@
-import os.path
+"""View for serving static assets under `/assets`"""
 
+import importlib_resources
 from h_assets import Environment, assets_view
 
 
 def includeme(config):
-    lms_root = os.path.dirname(__file__)
+    # Auto reload asset manifest when it changes in development.
+    auto_reload = config.registry.settings["dev"]
+    lms_files = importlib_resources.files("lms")
 
     assets_env = Environment(
         assets_base_url="/assets",
-        bundle_config_path=f"{lms_root}/assets.ini",
-        manifest_path=f"{lms_root}/../build/manifest.json",
+        bundle_config_path=lms_files / "assets.ini",
+        manifest_path=lms_files / "../build/manifest.json",
+        auto_reload=auto_reload,
     )
 
     # Store asset environment in registry for use in registering `asset_urls`
