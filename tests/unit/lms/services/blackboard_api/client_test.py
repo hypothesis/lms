40901aee42dc93ab55fa20bfd9dc22057c960c39
Marcos Prieto <marcospri@gmail.com>
2021-08-10 10:50:44 +0200

    Test asserting that an event is emitted for the discovered files

--- tests/unit/lms/services/blackboard_api/client_test.py
+++ tests/unit/lms/services/blackboard_api/client_test.py
@@ -2,9 +2,9 @@ from unittest.mock import MagicMock, call, create_autospec, sentinel
 
 import pytest
 from h_matchers import Any
+from pyramid.registry import Registry
 
 from lms.events import FilesDiscoveredEvent
-from pyramid.registry import Registry
 from lms.services.blackboard_api._basic import BasicClient
 from lms.services.blackboard_api.client import (
     PAGINATION_MAX_REQUESTS,
@@ -128,6 +128,50 @@ class TestListFiles:
         assert len(files) == PAGINATION_MAX_REQUESTS * len(
             blackboard_list_files_schema.parse.return_value
         )
+
+    @pytest.mark.parametrize("size,type_", [(1, "File"), (3, "File"), (3, "Folder")])
+    def test_it_emits_files_discover_event(
+        self, svc, basic_client, blackboard_list_files_schema, registry, size, type_
+    ):
+        # pylint: disable=protected-access
+        svc._request.registry = registry
+        basic_client.request.return_value = factories.requests.Response(json_data={})
+        blackboard_list_files_schema.parse.return_value = [
+            self.blackboard_file_dict(id_, type_=type_) for id_ in range(size)
+        ]
+
+        svc.list_files("COURSE_ID")
+
+        svc._request.registry.notify.assert_called_once_with(
+            FilesDiscoveredEvent(
+                request=svc._request,
+                values=[
+                    Any.dict.containing(
+                        {
+                            "lms_id": id_ + 1,
+                            "type": "blackboard_file"
+                            if type_ == "File"
+                            else "blackboard_folder",
+                        }
+                    )
+                    for id_ in range(size)
+                ],
+            )
+        )
+
+    def blackboard_file_dict(self, id_=1, type_="File"):
+        return {
+            "type": type_,
+            "course_id": "COURSE_ID",
+            "id": id_ + 1,
+            "name": f"file_entry_{id_}",
+            "size": id_ * 2,
+            "parentId": id_ * 4,
+        }
+
+    @pytest.fixture
+    def registry(self):
+        return create_autospec(Registry, instance=True, spec_set=True)
 
 
 class TestPublicURL:
