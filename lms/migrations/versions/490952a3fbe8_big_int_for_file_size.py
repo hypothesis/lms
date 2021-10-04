1756d0eeaa2122c186791f8e9411a03ddae774e4
Marcos Prieto <marcospri@gmail.com>
2021-10-04 17:18:15 +0200

    Migration to change file.size to BigInt

---
+++ lms/migrations/versions/490952a3fbe8_big_int_for_file_size.py
@@ -0,0 +1,34 @@
+"""
+Use a wider integer type for file.type .
+
+Revision ID: 490952a3fbe8
+Revises: d9c9e65c463e
+Create Date: 2021-10-04 17:13:14.982895
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "490952a3fbe8"
+down_revision = "d9c9e65c463e"
+
+
+def upgrade():
+    op.alter_column(
+        "file",
+        "size",
+        existing_type=sa.INTEGER(),
+        type_=sa.BigInteger(),
+        existing_nullable=True,
+    )
+
+
+def downgrade():
+    op.alter_column(
+        "file",
+        "size",
+        existing_type=sa.BigInteger(),
+        type_=sa.INTEGER(),
+        existing_nullable=True,
+    )
