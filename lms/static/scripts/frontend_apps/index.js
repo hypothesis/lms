d592c5dc1cdf6eabb13778e5e8928e8317b38b02
Robert Knight <robertknight@gmail.com>
2021-10-12 14:06:51 +0100

    Wrap LMS frontend entry code in a function
    This avoids a warning from Rollup in development due to Babel's JSX
    transform generating references to `this` outside of a function/method.
    
    It will also enable us to invoke the code with mocks in a test
    environment in future. The browser extension uses a similar pattern.

--- lms/static/scripts/frontend_apps/index.js
+++ lms/static/scripts/frontend_apps/index.js
@@ -19,61 +19,65 @@ import { registerIcons } from '@hypothesis/frontend-shared';
 import iconSet from './icons';
 registerIcons(iconSet);
 
-// Read configuration embedded into page by backend.
-const config = readConfig();
+function init() {
+  // Read configuration embedded into page by backend.
+  const config = readConfig();
 
-/**
- * Directory of services used by the current application.
- *
- * The necessary services for the different app types are currently initialized
- * manually before rendering. If we end up with many services in future we may
- * want to move to initializing them on-demand instead.
- *
- * @type {import('./services').ServiceMap}
- */
-const services = new Map();
+  /**
+   * Directory of services used by the current application.
+   *
+   * The necessary services for the different app types are currently initialized
+   * manually before rendering. If we end up with many services in future we may
+   * want to move to initializing them on-demand instead.
+   *
+   * @type {import('./services').ServiceMap}
+   */
+  const services = new Map();
 
-// Render main component for current route.
-let app;
-switch (config.mode) {
-  case 'basic-lti-launch':
-    services.set(
-      ClientRPC,
-      new ClientRPC({
-        authToken: config.api.authToken,
-        allowedOrigins: config.rpcServer.allowedOrigins,
-        clientConfig: /** @type {ClientConfig} */ (config.hypothesisClient),
-      })
-    );
-    services.set(
-      GradingService,
-      new GradingService({ authToken: config.api.authToken })
-    );
-    app = <BasicLTILaunchApp />;
-    break;
-  case 'content-item-selection':
-    services.set(
-      VitalSourceService,
-      new VitalSourceService({ authToken: config.api.authToken })
-    );
-    app = <FilePickerApp />;
-    break;
-  case 'error-dialog':
-    app = <ErrorDialogApp />;
-    break;
-  case 'oauth2-redirect-error':
-    app = <OAuth2RedirectErrorApp />;
-    break;
+  // Render main component for current route.
+  let app;
+  switch (config.mode) {
+    case 'basic-lti-launch':
+      services.set(
+        ClientRPC,
+        new ClientRPC({
+          authToken: config.api.authToken,
+          allowedOrigins: config.rpcServer.allowedOrigins,
+          clientConfig: /** @type {ClientConfig} */ (config.hypothesisClient),
+        })
+      );
+      services.set(
+        GradingService,
+        new GradingService({ authToken: config.api.authToken })
+      );
+      app = <BasicLTILaunchApp />;
+      break;
+    case 'content-item-selection':
+      services.set(
+        VitalSourceService,
+        new VitalSourceService({ authToken: config.api.authToken })
+      );
+      app = <FilePickerApp />;
+      break;
+    case 'error-dialog':
+      app = <ErrorDialogApp />;
+      break;
+    case 'oauth2-redirect-error':
+      app = <OAuth2RedirectErrorApp />;
+      break;
+  }
+
+  // Render frontend application.
+  const rootEl = document.querySelector('#app');
+  if (!rootEl) {
+    throw new Error('#app container for LMS frontend is missing');
+  }
+  render(
+    <Config.Provider value={config}>
+      <Services.Provider value={services}>{app}</Services.Provider>
+    </Config.Provider>,
+    rootEl
+  );
 }
 
-// Render frontend application.
-const rootEl = document.querySelector('#app');
-if (!rootEl) {
-  throw new Error('#app container for LMS frontend is missing');
-}
-render(
-  <Config.Provider value={config}>
-    <Services.Provider value={services}>{app}</Services.Provider>
-  </Config.Provider>,
-  rootEl
-);
+init();
