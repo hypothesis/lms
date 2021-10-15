ef5ffa41a36d60c0b4e1c452f5454350eaa595f6
Lyza Danger Gardner <lyza@lyza.com>
2021-10-15 09:30:07 -0400

    Make URLPicker not accept local file:// URIs
    Update the validation for the URLPicker to only accept URLs with
    protocols of 'http:' or 'https:'
    
    Fixes https://github.com/hypothesis/support/issues/230

--- lms/static/scripts/frontend_apps/components/URLPicker.js
+++ lms/static/scripts/frontend_apps/components/URLPicker.js
@@ -1,6 +1,11 @@
-import { LabeledButton, Modal, TextInput } from '@hypothesis/frontend-shared';
+import {
+  LabeledButton,
+  Modal,
+  SvgIcon,
+  TextInput,
+} from '@hypothesis/frontend-shared';
 
-import { useRef } from 'preact/hooks';
+import { useRef, useState } from 'preact/hooks';
 
 /**
  * @typedef URLPickerProps
@@ -19,13 +24,28 @@ export default function URLPicker({ onCancel, onSelectURL }) {
   const input = useRef(/** @type {HTMLInputElement|null} */ (null));
   const form = useRef(/** @type {HTMLFormElement|null} */ (null));
 
+  // Holds an error message corresponding to client-side validation of the
+  // input field
+  const [error, setError] = useState(/** @type {string|null} */ (null));
+
   /** @param {Event} event */
   const submit = event => {
     event.preventDefault();
-    if (form.current.checkValidity()) {
-      onSelectURL(input.current.value);
-    } else {
-      form.current.reportValidity();
+    try {
+      const url = new URL(input.current.value);
+      if (!url.protocol.startsWith('http')) {
+        if (url.protocol.startsWith('file')) {
+          setError(
+            'URLs that start with "file" are files on your own computer. Please use a URL that starts with "http" or "https".'
+          );
+        } else {
+          setError('Please use a URL that starts with "http" or "https"');
+        }
+      } else {
+        onSelectURL(input.current.value);
+      }
+    } catch (e) {
+      setError('Please enter a URL, e.g. "https://www.example.com"');
     }
   };
 
@@ -40,25 +60,41 @@ export default function URLPicker({ onCancel, onSelectURL }) {
       ]}
       initialFocus={input}
     >
-      <p>Enter the URL of any publicly available web page or PDF.</p>
-      <form
-        ref={form}
-        className="hyp-u-layout-row--align-center"
-        onSubmit={submit}
-      >
-        <label className="label" htmlFor="url">
-          URL:{' '}
-        </label>
+      <div className="hyp-u-vertical-spacing">
+        <p>Enter the URL of any publicly available web page or PDF:</p>
+        <form
+          ref={form}
+          className="hyp-u-layout-row--align-center"
+          onSubmit={submit}
+        >
+          <label className="label" htmlFor="url">
+            URL:{' '}
+          </label>
 
-        <TextInput
-          classes="hyp-u-stretch"
-          inputRef={input}
-          name="url"
-          placeholder="https://example.com/article.pdf"
-          required
-          type="url"
-        />
-      </form>
+          <TextInput
+            classes="hyp-u-stretch"
+            hasError={!!error}
+            inputRef={input}
+            name="url"
+            placeholder="e.g. https://example.com/article.pdf"
+            required
+          />
+        </form>
+        {/** setting a height here "preserves space" for this error display
+         * and prevents the dialog size from jumping when an error is rendered */}
+        <div
+          className="hyp-u-layout-row--center hyp-u-horizontal-spacing hyp-u-color--error"
+          data-testid="error-message"
+          style="height: 1rem"
+        >
+          {!!error && (
+            <>
+              <SvgIcon name="cancel" />
+              <div className="hyp-u-stretch">{error}</div>
+            </>
+          )}
+        </div>
+      </div>
     </Modal>
   );
 }
