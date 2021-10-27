aaf33d76d72669ff7398799de375cf1c08803610
Lyza Danger Gardner <lyza@lyza.com>
2021-10-27 12:05:19 -0400

    Add ErrorComponents page to local pattern library
    Establish an ErrorComponents component page for the local pattern library
    as a place to corral error-related UI patterns.
    
    Add `ErrorDisplay` and `ErrorDialog` to start.
    
    Part of https://github.com/hypothesis/lms/issues/3191

--- lms/static/scripts/ui-playground/index.js
+++ lms/static/scripts/ui-playground/index.js
@@ -1,9 +1,22 @@
 // Entry point for local webserver pattern-library bundle
 import { startApp } from '@hypothesis/frontend-shared/lib/pattern-library';
 
+import ErrorComponents from './components/ErrorComponents';
+
 import lmsIcons from '../frontend_apps/icons.js';
+
+/** @type {import('@hypothesis/frontend-shared/lib/pattern-library').PlaygroundRoute[]} */
+const extraRoutes = [
+  {
+    route: '/errors',
+    title: 'Errors',
+    component: ErrorComponents,
+    group: 'components',
+  },
+];
 
 startApp({
   baseURL: '/ui-playground',
+  extraRoutes,
   icons: lmsIcons,
 });
