ff76d34349cc81202f681d1936f4a7b772ffdeba
Lyza Danger Gardner <lyza@lyza.com>
2021-10-28 08:44:59 -0400

    Replace file and folder icons in file listings with shared icons
    Fixes #3189

--- lms/static/scripts/frontend_apps/icons.js
+++ lms/static/scripts/frontend_apps/icons.js
@@ -1,27 +1,20 @@
 // @ts-nocheck - TS doesn't understand SVG imports.
 
-// LMS icons
-// TODO: replace these with shared versions when updating the file picker:
-// https://github.com/hypothesis/lms/issues/3189
-import folderIcon from '../../images/folder.svg';
-import pdfIcon from '../../images/file-pdf.svg';
-
 // TODO: Remove when `frontend-shared` provides a FullScreenSpinner component
 import spinnerIcon from '../../images/spinner.svg';
 
-// Shared icons
 import {
   arrowLeft,
   arrowRight,
   cancel,
   caretDown,
   check,
+  filePDFFilled,
+  folder,
 } from '@hypothesis/frontend-shared/lib/icons';
 
 export default {
-  // LMS icons
-  folder: folderIcon,
-  pdf: pdfIcon,
+  // LMS local icons
   spinner: spinnerIcon,
 
   // Shared icons
@@ -30,4 +23,6 @@ export default {
   cancel,
   'caret-down': caretDown,
   check,
+  folder,
+  pdf: filePDFFilled,
 };
