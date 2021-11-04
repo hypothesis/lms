3a4d3f7cfeac8a31bc7ae623e529f4cfee2a3b33
Marcos Prieto <marcospri@gmail.com>
2021-11-04 14:00:17 +0100

    Handle VS assignments on creation API

--- tests/unit/lms/views/api/assignments_test.py
+++ tests/unit/lms/views/api/assignments_test.py
@@ -60,6 +60,24 @@ class TestAssignmentsAPIViews:
                     }
                 },
             ),
+            (
+                {
+                    "content": {
+                        "type": "vitalsource",
+                        "bookID": "BOOK_ID",
+                        "cfi": "/25",
+                    },
+                    "ext_lti_assignment_id": "EXT_LTI_ASSIGNMENT_ID",
+                    "course_id": "COURSE_ID",
+                },
+                "vitalsource://book/bookID/BOOK_ID/cfi//25",
+                {
+                    "vitalsource": {
+                        "bookID": "BOOK_ID",
+                        "cfi": "/25",
+                    }
+                },
+            ),
         ],
     )
     def test_create(
