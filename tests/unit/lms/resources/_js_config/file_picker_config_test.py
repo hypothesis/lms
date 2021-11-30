2cd3a54dc153342defea9c921e34df636b6a0b28
Marcos Prieto <marcospri@gmail.com>
2021-11-30 09:26:54 -0500

    JSConfig canvas and blackboard setting reorg

--- tests/unit/lms/resources/_js_config/file_picker_config_test.py
+++ tests/unit/lms/resources/_js_config/file_picker_config_test.py
@@ -8,61 +8,97 @@ from lms.resources._js_config import FilePickerConfig
 
 
 class TestFilePickerConfig:
-    def test_blackboard_config(self, context, pyramid_request, application_instance):
+    @pytest.mark.parametrize(
+        "files_enabled,groups_enabled",
+        [
+            (False, False),
+            (True, False),
+            (False, True),
+            (True, True),
+        ],
+    )
+    def test_blackboard_config(
+        self,
+        context,
+        pyramid_request,
+        blackboard_application_instance,
+        files_enabled,
+        groups_enabled,
+    ):
+
         pyramid_request.params["context_id"] = "COURSE_ID"
-        application_instance.settings.set(
-            "blackboard", "files_enabled", sentinel.enabled
+        blackboard_application_instance.settings.set(
+            "blackboard", "files_enabled", files_enabled
         )
+        context.blackboard_groups_enabled = groups_enabled
 
         config = FilePickerConfig.blackboard_config(
-            context, pyramid_request, application_instance
+            context, pyramid_request, blackboard_application_instance
         )
 
-        assert config == {
-            "enabled": sentinel.enabled,
-            "groupsEnabled": False,
-            "listFiles": {
-                "authUrl": "http://example.com/api/blackboard/oauth/authorize",
-                "path": "/api/blackboard/courses/COURSE_ID/files",
-            },
-            "listGroupSets": {
-                "authUrl": "http://example.com/api/blackboard/oauth/authorize",
-                "path": "/api/blackboard/courses/COURSE_ID/group_sets",
-            },
+        expected_config = {
+            "enabled": files_enabled,
+            "groupsEnabled": groups_enabled,
         }
 
-    def test_canvas_config(self, context, pyramid_request, application_instance):
+        if files_enabled:
+            expected_config["listFiles"] = {
+                "authUrl": "http://example.com/api/blackboard/oauth/authorize",
+                "path": "/api/blackboard/courses/COURSE_ID/files",
+            }
+
+        if groups_enabled:
+            expected_config["listGroupSets"] = {
+                "authUrl": "http://example.com/api/blackboard/oauth/authorize",
+                "path": "/api/blackboard/courses/COURSE_ID/group_sets",
+            }
+
+        assert config == expected_config
+
+    @pytest.mark.usefixtures("with_is_canvas")
+    @pytest.mark.parametrize(
+        "groups_enabled",
+        [
+            False,
+            True,
+        ],
+    )
+    def test_canvas_config(
+        self, context, pyramid_request, application_instance, groups_enabled
+    ):
         pyramid_request.params["custom_canvas_course_id"] = "COURSE_ID"
+        context.canvas_groups_enabled = groups_enabled
 
         config = FilePickerConfig.canvas_config(
             context, pyramid_request, application_instance
         )
 
-        assert config == {
+        expected_config = {
             "enabled": Any(),
-            "groupsEnabled": False,
+            "groupsEnabled": groups_enabled,
             "listFiles": {
                 "authUrl": "http://example.com/api/canvas/oauth/authorize",
                 "path": "/api/canvas/courses/COURSE_ID/files",
             },
-            "listGroupSets": {
+        }
+
+        if groups_enabled:
+            expected_config["listGroupSets"] = {
                 "authUrl": "http://example.com/api/canvas/oauth/authorize",
                 "path": "/api/canvas/courses/COURSE_ID/group_sets",
-            },
-            "ltiLaunchUrl": "http://example.com/lti_launches",
-        }
+            }
+
+        assert config == expected_config
 
     @pytest.mark.parametrize(
         "missing_value",
-        (None, "is_canvas", "course_id", "developer_key"),
+        (None, "course_id", "developer_key"),
     )
     @pytest.mark.usefixtures("canvas_files_enabled")
     def test_canvas_config_enabled(
         self, context, pyramid_request, application_instance, missing_value
     ):
-        if missing_value == "is_canvas":
-            context.is_canvas = False
-        elif missing_value == "course_id":
+        if missing_value == "course_id":
             pyramid_request.params.pop("custom_canvas_course_id")
         elif missing_value == "developer_key":
             application_instance.developer_key = None
@@ -137,6 +173,16 @@ class TestFilePickerConfig:
         application_instance.lms_url = None
 
         return application_instance
+
+    @pytest.fixture
+    def blackboard_application_instance(self, application_instance):
+        application_instance.tool_consumer_info_product_family_code = "BlackboardLearn"
+
+        return application_instance
+
+    @pytest.fixture
+    def with_is_canvas(self, context):
+        context.is_canvas = True
 
     @pytest.fixture
     def context(self):
