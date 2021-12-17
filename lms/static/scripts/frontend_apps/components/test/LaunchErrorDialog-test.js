978acf07ce6387f30c123c7afedf07059c20f064
Lyza Danger Gardner <lyza@lyza.com>
2021-12-17 13:50:57 -0500

    Replace custom Modal with ErrorModal in `LaunchErrorDialog`

--- lms/static/scripts/frontend_apps/components/test/LaunchErrorDialog-test.js
+++ lms/static/scripts/frontend_apps/components/test/LaunchErrorDialog-test.js
@@ -1,7 +1,5 @@
 import { mount } from 'enzyme';
 
-import { act } from 'preact/test-utils';
-
 import mockImportedComponents from '../../../test-util/mock-imported-components';
 import LaunchErrorDialog, { $imports } from '../LaunchErrorDialog';
 
@@ -38,65 +36,97 @@ describe('LaunchErrorDialog', () => {
     {
       errorState: 'error-authorizing',
       expectedText: 'Hypothesis needs your authorization',
-      hasDetailedError: false,
+      expectedTitle: 'Authorize Hypothesis',
+      hasRetry: true,
       retryAction: 'Authorize',
+      withError: false,
     },
     {
       errorState: 'blackboard_file_not_found_in_course',
       expectedText: 'The file has been deleted from Blackboard',
+      expectedTitle: "Hypothesis couldn't find the file in the course",
+      hasRetry: true,
+      withError: true,
     },
     {
       errorState: 'canvas_api_permission_error',
       expectedText: "Hypothesis couldn't get the assignment's file from Canvas",
+      expectedTitle: "Couldn't get the file from Canvas",
+      hasRetry: true,
+      withError: true,
     },
     {
       errorState: 'canvas_file_not_found_in_course',
       expectedText: 'edit the assignment and re-select the file',
-    },
-    {
-      errorState: 'error-fetching',
-      expectedText: 'There was a problem fetching this Hypothesis assignment',
-    },
-    {
-      errorState: 'error-reporting-submission',
-      expectedText: 'There was a problem submitting this Hypothesis assignment',
-      retryAction: null,
+      expectedTitle: "Hypothesis couldn't find the file in the course",
+      hasRetry: true,
+      withError: true,
     },
     {
       errorState: 'canvas_group_set_not_found',
       expectedText:
-        "Assignment's group set no longer exists in CanvasHypothesis couldn't load this assignment because the assignment's group set no longer exists in Canvas.To fix this problem, an instructor needs to edit the assignment settings and select a new group set.",
-      retryAction: null,
+        "Hypothesis couldn't load this assignment because the assignment's group set no longer exists in Canvas.To fix this problem, an instructor needs to edit the assignment settings and select a new group set.",
+      expectedTitle: "Assignment's group set no longer exists in Canvas",
+      hasRetry: false,
+      withError: true,
     },
     {
       errorState: 'canvas_group_set_empty',
-      expectedText: 'The group set for this Hypothesis assignment is empty.',
-      retryAction: null,
+      expectedText: 'The group set for this Hypothesis assignment is empty',
+      expectedTitle: 'Something went wrong',
+      hasRetry: false,
+      withError: true,
     },
     {
       errorState: 'canvas_student_not_in_group',
       expectedText:
-        "You're not in any of this assignment's groupsHypothesis couldn't launch this assignment because you aren't in any of the groups in the assignment's group set.To fix the problem, an instructor needs to add your Canvas user account to one of this assignment's groups.",
-      retryAction: null,
+        "you aren't in any of the groups in the assignment's group set",
+      expectedTitle: "You're not in any of this assignment's groups",
+      hasRetry: false,
+      withError: true,
+    },
+    {
+      errorState: 'error-fetching',
+      expectedText: 'There was a problem fetching this Hypothesis assignment',
+      expectedTitle: 'Something went wrong',
+      hasRetry: true,
+      withError: true,
+    },
+    {
+      errorState: 'error-reporting-submission',
+      expectedText: 'There was a problem submitting this Hypothesis assignment',
+      expectedTitle: 'Something went wrong',
+      hasRetry: false,
+      withError: true,
     },
   ].forEach(
     ({
       errorState,
       expectedText,
-      hasDetailedError = true,
-      retryAction = 'Try again',
+      expectedTitle,
+      hasRetry = false,
+      withError = true,
+      retryAction,
     }) => {
       it(`displays expected error for "${errorState}" error state`, () => {
         const error = new Error('Detailed error info');
+        const wrapper = renderDialog({ error, errorState: errorState });
 
-        const wrapper = renderDialog({ error, errorState });
-
-        assert.include(wrapper.text(), expectedText);
-        assert.equal(wrapper.exists('LabeledButton'), retryAction !== null);
-        if (retryAction) {
-          assert.include(wrapper.find('LabeledButton').text(), retryAction);
+        const modalProps = wrapper.find('ErrorModal').props();
+        assert.include(wrapper.find('ErrorModal').text(), expectedText);
+        assert.equal(modalProps.title, expectedTitle);
+        assert.equal(modalProps.retryLabel, retryAction);
+        if (hasRetry) {
+          assert.equal(modalProps.onRetry, retryStub);
+        } else {
+          assert.isUndefined(modalProps.onRetry);
         }
-        assert.equal(wrapper.exists('ErrorDisplay'), hasDetailedError);
+        if (retryAction) {
+          assert.equal(modalProps.retryLabel, retryAction);
+        }
+        if (!withError) {
+          assert.isUndefined(modalProps.error);
+        }
       });
     }
   );
@@ -120,32 +150,8 @@ describe('LaunchErrorDialog', () => {
     );
   });
 
-  it('initiates retry when "Try again" button is clicked', () => {
-    const wrapper = renderDialog();
-
-    act(() => {
-      wrapper.find('LabeledButton').prop('onClick')();
-    });
-
-    assert.called(retryStub);
-  });
-
-  it('enables "Try again" button if `busy` is false', () => {
-    const wrapper = renderDialog({ busy: false });
-    assert.isFalse(wrapper.find('LabeledButton').prop('disabled'));
-  });
-
-  it('disables "Try again" button if `busy` is true', () => {
+  it('forwards `busy` to `ErrorModal`', () => {
     const wrapper = renderDialog({ busy: true });
-    assert.isTrue(wrapper.find('LabeledButton').prop('disabled'));
-  });
-
-  it('shows error details', () => {
-    const error = new Error('Oh no');
-    const wrapper = renderDialog({
-      errorState: 'canvas_api_permission_error',
-      error,
-    });
-    assert.equal(wrapper.find('ErrorDisplay').prop('error'), error);
+    assert.isTrue(wrapper.find('ErrorModal').prop('busy'));
   });
 });
