b5cb7c7f2283a6c19533493a0c5b3a616993a5da
Sean Hammond <seanh@users.noreply.github.com>
2022-02-22 15:25:24 +0000

    Add Blackboard support to the refresh API

--- lms/views/api/refresh.py
+++ lms/views/api/refresh.py
@@ -1,5 +1,6 @@
 from pyramid.view import view_config
 
+from lms.models import ApplicationInstance
 from lms.security import Permissions
 
 
@@ -16,9 +17,24 @@ def get_refreshed_token(request):
     Send a request to get a refreshed access token for the authenticated user
     and save it to the DB.
     """
-    canvas_api = request.find_service(name="canvas_api_client")
-    oauth2_token_service = request.find_service(name="oauth2_token")
+    application_instance_service = request.find_service(name="application_instance")
+    application_instance = application_instance_service.get_current()
 
+    if application_instance.product == ApplicationInstance.Product.CANVAS:
+        refresh_canvas_access_token(request)
+    else:
+        assert application_instance.product == ApplicationInstance.Product.BLACKBOARD
+        refresh_blackboard_access_token(request)
+
+
+def refresh_canvas_access_token(request):
+    canvas_api_client = request.find_service(name="canvas_api_client")
+    oauth2_token_service = request.find_service(name="oauth2_token")
     refresh_token = oauth2_token_service.get().refresh_token
 
-    canvas_api.get_refreshed_token(refresh_token)
+    canvas_api_client.get_refreshed_token(refresh_token)
+
+
+def refresh_blackboard_access_token(request):
+    blackboard_api_client = request.find_service(name="blackboard_api_client")
+    blackboard_api_client.refresh_access_token()
--- tests/unit/lms/views/api/refresh_test.py
+++ tests/unit/lms/views/api/refresh_test.py
@@ -1,13 +1,36 @@
 import pytest
 
 from lms.views.api.refresh import get_refreshed_token
+from tests import factories
 
-pytestmark = pytest.mark.usefixtures("canvas_api_client", "oauth2_token_service")
 
+@pytest.mark.usefixtures("oauth2_token_service")
+def test_get_refreshed_token_with_Canvas(
+    pyramid_request,
+    application_instance_service,
+    canvas_api_client,
+    oauth2_token_service,
+):
+    application_instance_service.get_current.return_value = (
+        factories.ApplicationInstance(tool_consumer_info_product_family_code="canvas")
+    )
 
-def test_get_refreshed_token(pyramid_request, canvas_api_client, oauth2_token_service):
     get_refreshed_token(pyramid_request)
 
     canvas_api_client.get_refreshed_token.assert_called_once_with(
         oauth2_token_service.get.return_value.refresh_token
     )
+
+
+def test_get_refreshed_token_with_Blackboard(
+    pyramid_request, application_instance_service, blackboard_api_client
+):
+    application_instance_service.get_current.return_value = (
+        factories.ApplicationInstance(
+            tool_consumer_info_product_family_code="BlackboardLearn"
+        )
+    )
+
+    get_refreshed_token(pyramid_request)
+
+    blackboard_api_client.refresh_access_token.assert_called_once_with()
