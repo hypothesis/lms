9a6bc8d14fa7d4e6b79bade20d5714dd376721a7
Marcos Prieto <marcospri@gmail.com>
2022-03-16 09:38:51 +0100

    Stop using LegacyCourse

--- tests/unit/lms/services/course_test.py
+++ tests/unit/lms/services/course_test.py
@@ -3,7 +3,7 @@ from unittest.mock import sentinel
 
 import pytest
 
-from lms.models import Course, CourseGroupsExportedFromH, LegacyCourse
+from lms.models import Course, CourseGroupsExportedFromH
 from lms.services.course import course_service_factory
 from tests import factories
 
@@ -11,39 +11,6 @@ pytestmark = pytest.mark.usefixtures("application_instance_service")
 
 
 class TestCourseService:
-    @pytest.mark.parametrize("canvas_sections_enabled", [True, False])
-    def test_inserting_True_and_False(
-        self, application_instance_service, db_session, svc, canvas_sections_enabled
-    ):
-        application_instance_service.get_current.return_value.settings.set(
-            "canvas", "sections_enabled", canvas_sections_enabled
-        )
-
-        svc.get_or_create("test_authority_provided_id")
-
-        course = db_session.query(LegacyCourse).one()
-        assert (
-            course.settings.get("canvas", "sections_enabled") == canvas_sections_enabled
-        )
-
-    def test_it_does_nothing_if_theres_already_a_matching_row(
-        self, application_instance_service, application_instance, pyramid_request, svc
-    ):
-        existing_course = factories.LegacyCourse(
-            application_instance=application_instance,
-            authority_provided_id="test_authority_provided_id",
-            settings={},
-        )
-        existing_course.settings.set("canvas", "sections_enabled", False)
-        application_instance_service.get_current.return_value.settings.set(
-            "canvas", "sections_enabled", True
-        )
-
-        svc.get_or_create("test_authority_provided_id")
-
-        existing_course = pyramid_request.db.query(LegacyCourse).one()
-        assert not existing_course.settings.get("canvas", "sections_enabled")
-
     @pytest.mark.parametrize("canvas_sections_enabled", [True, False])
     def test_it_inserts_False_if_theres_a_matching_row_in_course_groups_exported_from_h(
         self, application_instance_service, db_session, svc, canvas_sections_enabled
@@ -58,9 +25,9 @@ class TestCourseService:
             "canvas", "sections_enabled", canvas_sections_enabled
         )
 
-        svc.get_or_create("test_authority_provided_id")
+        svc.upsert("test_authority_provided_id", "context_id", "new course name", {})
 
-        course = db_session.query(LegacyCourse).one()
+        course = db_session.query(Course).one()
         assert not course.settings.get("canvas", "sections_enabled")
 
     @pytest.mark.parametrize(
@@ -124,7 +91,7 @@ class TestCourseService:
             settings_set, application_instance=application_instance
         ):
             for settings in settings_set:
-                factories.LegacyCourse(
+                factories.Course(
                     application_instance=application_instance, settings=settings
                 )
 
