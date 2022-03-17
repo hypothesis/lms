2462a3dd0f89e61036a64d0dd5da308764bc7417
Marcos Prieto <marcospri@gmail.com>
2022-03-17 11:11:27 +0100

    Better AESService testing

--- tests/unit/lms/services/aes_service_test.py
+++ tests/unit/lms/services/aes_service_test.py
@@ -1,6 +1,7 @@
 from unittest.mock import sentinel
 
 import pytest
+from Cryptodome.Cipher import AES
 
 from lms.services.aes import AESService, factory
 
@@ -13,9 +14,71 @@ class TestAESService:
 
         assert svc.decrypt(iv, encrypted) == expected
 
+    @pytest.mark.parametrize(
+        "iv,encrypted,text",
+        [
+            (
+                b"\xa4\xb22?\x14\x11\xa1\x04\x80\xe5\xa6\x13\xb4\xdd\x7f\xf7",
+                b"2j\x1a\xbd",
+                b"TEXT",
+            ),
+            # Same text, different IV
+            (
+                b"M\xac\x95\xa4!\x93\xd9]\xfbRo\xc3\xe5\xb0\xd5X",
+                b".d\x1e]",
+                b"TEXT",
+            ),
+        ],
+    )
+    def test_decrypt(self, svc, iv, encrypted, text):
+        assert svc.decrypt(iv, encrypted) == text
+
+    @pytest.mark.parametrize(
+        "iv,text,encrypted",
+        [
+            (
+                b"\xa4\xb22?\x14\x11\xa1\x04\x80\xe5\xa6\x13\xb4\xdd\x7f\xf7",
+                b"TEXT",
+                b"2j\x1a\xbd",
+            ),
+            # Same text, different IV
+            (
+                b"M\xac\x95\xa4!\x93\xd9]\xfbRo\xc3\xe5\xb0\xd5X",
+                b"TEXT",
+                b".d\x1e]",
+            ),
+            # Same text, as str
+            (
+                b"M\xac\x95\xa4!\x93\xd9]\xfbRo\xc3\xe5\xb0\xd5X",
+                "TEXT",
+                b".d\x1e]",
+            ),
+        ],
+    )
+    def test_encrypt(self, svc, iv, encrypted, text):
+        assert svc.encrypt(iv, text) == encrypted
+
+    def test_encrypt_aes_settings(self, svc, AES, pyramid_request):
+        encrypted = svc.encrypt("IV", "TEXT")
+
+        AES.new.assert_called_once_with(
+            pyramid_request.registry.settings["aes_secret"], AES.MODE_CFB, "IV"
+        )
+        assert encrypted == AES.new.return_value.encrypt.return_value
+
+    def test_build_iv(self, svc):
+        iv = svc.build_iv()
+
+        assert isinstance(iv, bytes)
+        len(iv) == AES.block_size
+
     @pytest.fixture
     def svc(self, pyramid_request):
         return AESService(pyramid_request.registry.settings["aes_secret"])
+
+    @pytest.fixture
+    def AES(self, patch):
+        return patch("lms.services.aes.AES")
 
 
 class TestFactory:
