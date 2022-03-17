e4ecf0fd7455a8e903e9379db6fb0d782d8d4a07
Marcos Prieto <marcospri@gmail.com>
2022-03-17 11:24:34 +0100

    Better docs for AESService

--- lms/services/aes.py
+++ lms/services/aes.py
@@ -5,10 +5,20 @@ from Cryptodome.Cipher import AES
 
 
 class AESService:
+    """
+    Service to encrypt/decrypt strings with AES CFB.
+
+    CFB uses an "initialization vector", IV  which needs to:
+        - Have the same value in encrypt and decrypt
+        - Be a random value.
+
+    For those reason IV can't be an application setting (unlike secret) and needs to be stored separately.
+    """
+
     def __init__(self, secret):
         self.secret = secret
 
-    def decrypt(self, aes_iv, encrypted):
+    def decrypt(self, aes_iv, encrypted) -> bytes:
         cipher = AES.new(self.secret, AES.MODE_CFB, aes_iv)
         return cipher.decrypt(encrypted)
 
@@ -20,6 +30,12 @@ class AESService:
 
     @staticmethod
     def build_iv():
+        """
+        Return a random initialization vector (IV).
+
+        The same value must be used for encryption and decryption but not reused for different messages.
+        """
+        # Using the cryptographic secure Cryptodome.Random instead of the stlib random package.
         return Random.new().read(AES.block_size)
 
 
