9210a6f4f78b3ad3327b25255d1f39ddfcbed145
Marcos Prieto <marcospri@gmail.com>
2022-03-29 11:52:28 +0200

    Use CommonLTILaunchSchema for ConfigureAssignmentSchema

--- lms/validation/_assignment.py
+++ lms/validation/_assignment.py
@@ -2,19 +2,16 @@
 
 from webargs import fields
 
-from lms.validation._base import PyramidRequestSchema
+from lms.validation._lti_launch_params import CommonLTILaunchSchema
 
 
-class ConfigureAssignmentSchema(PyramidRequestSchema):
+class ConfigureAssignmentSchema(CommonLTILaunchSchema):
     """Schema for validating requests to the configure_assignment() view."""
 
     location = "form"
 
     document_url = fields.Str(required=True)
     resource_link_id = fields.Str(required=True)
-    tool_consumer_instance_guid = fields.Str(required=True)
-    oauth_consumer_key = fields.Str(required=True)
     user_id = fields.Str(required=True)
-    context_id = fields.Str(required=True)
     context_title = fields.Str(required=True)
     group_set = fields.Str(required=False, allow_none=True)
