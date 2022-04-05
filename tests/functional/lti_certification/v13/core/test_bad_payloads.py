1ddf35d6b0085d9509c397058b8434342d9e5f15
Marcos Prieto <marcospri@gmail.com>
2022-04-05 10:13:53 +0200

    Adapt parameters for mulitple payloads

--- tests/functional/lti_certification/v13/core/test_bad_payloads.py
+++ tests/functional/lti_certification/v13/core/test_bad_payloads.py
@@ -10,7 +10,7 @@ class TestBadPayloads:
     """
 
     def test_no_kid_sent_in_jwt_header(
-        self, jwt_headers, make_jwt, student_payload, do_lti_launch
+        self, jwt_headers, make_jwt, test_payload, do_lti_launch
     ):
         """
         The KID is missing from the header of the JWT (preventing the verification of the signing of the JWT)
@@ -18,7 +18,7 @@ class TestBadPayloads:
         del jwt_headers["kid"]
 
         response = do_lti_launch(
-            {"id_token": make_jwt(student_payload, jwt_headers)}, status=403
+            {"id_token": make_jwt(test_payload, jwt_headers)}, status=403
         )
 
         assert response.html
@@ -27,20 +27,20 @@ class TestBadPayloads:
         """The KID provided is incorrect (and signing verification is impossible)"""
         ...
 
-    def test_wrong_lti_version(self, make_jwt, student_payload, do_lti_launch):
+    def test_wrong_lti_version(self, make_jwt, test_payload, do_lti_launch):
         """The LTI version claim contains the wrong version"""
-        student_payload["https://purl.imsglobal.org/spec/lti/claim/version"] = "11.3"
+        test_payload["https://purl.imsglobal.org/spec/lti/claim/version"] = "11.3"
 
-        response = do_lti_launch({"id_token": make_jwt(student_payload)}, status=422)
+        response = do_lti_launch({"id_token": make_jwt(test_payload)}, status=422)
 
         assert "There were problems with these request parameters" in response.text
         assert "lti_version" in response.text
 
-    def test_no_lti_version(self, make_jwt, student_payload, do_lti_launch):
+    def test_no_lti_version(self, make_jwt, test_payload, do_lti_launch):
         """The LTI version claim is missing"""
-        del student_payload["https://purl.imsglobal.org/spec/lti/claim/version"]
+        del test_payload["https://purl.imsglobal.org/spec/lti/claim/version"]
 
-        response = do_lti_launch({"id_token": make_jwt(student_payload)}, status=422)
+        response = do_lti_launch({"id_token": make_jwt(test_payload)}, status=422)
 
         assert "There were problems with these request parameters" in response.text
         assert "lti_version" in response.text
@@ -53,7 +53,7 @@ class TestBadPayloads:
 
         assert response.status_code == 403
 
-    def test_missing_lti_claims(self, student_payload, do_lti_launch, make_jwt):
+    def test_missing_lti_claims(self, test_payload, do_lti_launch, make_jwt):
         """The provided 1.3 JWT launch is missing one or more required claims"""
         missing_claims = [
             "aud",
@@ -63,64 +63,62 @@ class TestBadPayloads:
             "https://purl.imsglobal.org/spec/lti/claim/roles",
         ]
         for missing_claim in missing_claims:
-            del student_payload[missing_claim]
+            del test_payload[missing_claim]
 
-        response = do_lti_launch({"id_token": make_jwt(student_payload)}, status=403)
+        response = do_lti_launch({"id_token": make_jwt(test_payload)}, status=403)
 
         assert response.status_code == 403
         assert response.html
 
     @pytest.mark.xfail(reason="JWT verfication disabled")
-    def test_timestamps_incorrect(self, student_payload, do_lti_launch, make_jwt):
+    def test_timestamps_incorrect(self, test_payload, do_lti_launch, make_jwt):
         """Incorrect JWT iat and exp timestamp Values are Invalid"""
-        student_payload["iat"] = 11111
-        student_payload["exp"] = 22222
+        test_payload["iat"] = 11111
+        test_payload["exp"] = 22222
 
-        response = do_lti_launch({"id_token": make_jwt(student_payload)}, status=403)
+        response = do_lti_launch({"id_token": make_jwt(test_payload)}, status=403)
         assert response.html
 
-    def test_message_type_claim_missing(self, student_payload, assert_missing_claim):
+    def test_message_type_claim_missing(self, test_payload, assert_missing_claim):
         """The Required message_type Claim Not Present"""
         response = assert_missing_claim(
-            student_payload, "https://purl.imsglobal.org/spec/lti/claim/message_type"
+            test_payload, "https://purl.imsglobal.org/spec/lti/claim/message_type"
         )
 
         assert "There were problems with these request parameters" in response.text
         assert "message_type" in response.text
 
-    def test_role_claim_missing(self, student_payload, assert_missing_claim):
+    def test_role_claim_missing(self, test_payload, assert_missing_claim):
         """The Required role Claim Not Present"""
         assert_missing_claim(
-            student_payload,
+            test_payload,
             "https://purl.imsglobal.org/spec/lti/claim/roles",
             status=403,
         )
 
-    def test_deployment_id_claim_missing(self, student_payload, assert_missing_claim):
+    def test_deployment_id_claim_missing(self, test_payload, assert_missing_claim):
         """The Required deployment_id Claim Not Present"""
         assert_missing_claim(
-            student_payload,
+            test_payload,
             "https://purl.imsglobal.org/spec/lti/claim/deployment_id",
             status=403,
         )
 
-    def test_resource_link_id_claim_missing(
-        self, student_payload, assert_missing_claim
-    ):
+    def test_resource_link_id_claim_missing(self, test_payload, assert_missing_claim):
         """The Required resource_link_id Claim Not Present"""
         assert_missing_claim(
-            student_payload, "https://purl.imsglobal.org/spec/lti/claim/resource_link"
+            test_payload, "https://purl.imsglobal.org/spec/lti/claim/resource_link"
         )
 
-    def test_user_claim_missing(self, student_payload, assert_missing_claim):
+    def test_user_claim_missing(self, test_payload, assert_missing_claim):
         """The Required sub Claim Not Present"""
         assert_missing_claim(test_payload, "sub", status=403)
 
-    @pytest.fixture(params=["student_payload", "teacher_payload"])
+    @pytest.fixture(params=["teacher_payload", "student_payload"])
     def test_payload(self, request):
         """Get an OAuthToken or None based on the fixture params."""
 
-        return request.getfixturevalue(request.param)
+        yield request.getfixturevalue(request.param)
 
     @pytest.fixture
     def assert_missing_claim(self, do_lti_launch, make_jwt):
