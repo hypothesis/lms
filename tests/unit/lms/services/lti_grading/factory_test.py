b4413f48928dcfd12762149ebc5e70ca755396bb
Marcos Prieto <marcospri@gmail.com>
2022-04-19 11:31:19 +0200

    Implement LTIGrading for LTI1.3/LTIA

--- tests/unit/lms/services/lti_grading/factory_test.py
+++ tests/unit/lms/services/lti_grading/factory_test.py
@@ -1,4 +1,4 @@
-from unittest.mock import sentinel
+from unittest.mock import Mock, sentinel
 
 import pytest
 
@@ -9,16 +9,37 @@ class TestFactory:
     def test_v11(
         self,
         pyramid_request,
+        application_instance_service,
         LTI11GradingService,
         http_service,
         oauth1_service,
     ):
+        application_instance_service.get_current.return_value = Mock(lti_version="1.1")
+
         svc = service_factory(sentinel.context, pyramid_request)
 
         LTI11GradingService.assert_called_once_with(
             sentinel.grading_url, http_service, oauth1_service
         )
         assert svc == LTI11GradingService.return_value
+
+    def test_v13(
+        self,
+        pyramid_request,
+        LTI13GradingService,
+        application_instance_service,
+        ltia_http_service,
+    ):
+        application_instance_service.get_current.return_value = Mock(
+            lti_version="1.3.0"
+        )
+
+        svc = service_factory(sentinel.context, pyramid_request)
+
+        LTI13GradingService.assert_called_once_with(
+            sentinel.grading_url, ltia_http_service
+        )
+        assert svc == LTI13GradingService.return_value
 
     @pytest.fixture
     def pyramid_request(self, pyramid_request):
@@ -30,3 +51,7 @@ class TestFactory:
     @pytest.fixture
     def LTI11GradingService(self, patch):
         return patch("lms.services.lti_grading.factory.LTI11GradingService")
+
+    @pytest.fixture
+    def LTI13GradingService(self, patch):
+        return patch("lms.services.lti_grading.factory.LTI13GradingService")
