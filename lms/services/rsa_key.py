b9d8821907ea8d650004a38c256d02373ade5be1
Marcos Prieto <marcospri@gmail.com>
2022-04-22 10:40:37 +0200

    Get random key in rsa key service

--- lms/services/rsa_key.py
+++ lms/services/rsa_key.py
@@ -4,6 +4,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from jose import constants, jwk
+from sqlalchemy import func
 
 from lms.models import RSAKey
 from lms.services import AESService
