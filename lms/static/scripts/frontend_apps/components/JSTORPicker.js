59f1b1ac943a252e63adad6188189584199d3c15
Robert Knight <robertknight@gmail.com>
2022-05-24 17:00:29 +0100

    Show article metadata and thumbnail in JSTOR picker
    Replace the thumbnail and title placeholders in JSTORPicker with the real
    thumbnail and metadata.
    
     - Change the JSTOR article URL => `jstor://{article_id}` functions in
       utils/jstor.js to instead return just the article ID. This is more
       convenient for use with the backend APIs for fetching article
       metadata.
    
     - Add `useAPIFetch` calls to fetch article metadata and thumbnail when
       an article is selected
    
     - Cancel metadata/thumbnail fetch if the user types in the URL field
       while the fetch is in-flight. This avoids potential confusion due to
       the thumbnail/metadata not matching the current field value.
    
     - Prevent confirmation of JSTOR picker until article metadata is
       successfully fetched. This will prevent the user from accidentally
       confirming the dialog with a non-working/wrong article ID before the fetch
       is completed.

--- lms/static/scripts/frontend_apps/components/JSTORPicker.js
+++ lms/static/scripts/frontend_apps/components/JSTORPicker.js
@@ -10,7 +10,28 @@ import {
 import classnames from 'classnames';
 import { useRef, useState } from 'preact/hooks';
 
-import { toJSTORUrl } from '../utils/jstor';
+import { formatErrorMessage } from '../errors';
+import { urlPath, useAPIFetch } from '../utils/api';
+import { articleIdFromURL, jstorURLFromArticleId } from '../utils/jstor';
+
+/**
+ * @template T
+ * @typedef {import('../utils/fetch').FetchResult<T>} FetchResult
+ */
+
+/**
+ * Response for an `/api/jstor/articles/{article_id}` call.
+ *
+ * @typedef Metadata
+ * @prop {string} title
+ */
+
+/**
+ * Response for an `/api/jstor/articles/{article_id}/thumbnail` call.
+ *
+ * @typedef Thumbnail
+ * @prop {string} image
+ */
 
 /**
  * @typedef JSTORPickerProps
@@ -26,24 +47,46 @@ import { toJSTORUrl } from '../utils/jstor';
  */
 export default function JSTORPicker({ onCancel, onSelectURL }) {
   const [error, setError] = useState(/** @type {string|null} */ (null));
-  const [selectedURL, setSelectedURL] = useState(
-    /** @type {string|null} */ (null)
+
+  // Selected JSTOR article ID or DOI, updated when the user confirms what
+  // they have pasted/typed into the input field.
+  const [articleId, setArticleId] = useState(/** @type {string|null} */ (null));
+
+  /** @type {FetchResult<Metadata>} */
+  const metadata = useAPIFetch(
+    articleId ? urlPath`/api/jstor/articles/${articleId}` : null
   );
 
+  /** @type {FetchResult<Thumbnail>} */
+  const thumbnail = useAPIFetch(
+    articleId ? urlPath`/api/jstor/articles/${articleId}/thumbnail` : null
+  );
+
+  let renderedError;
+  if (error) {
+    renderedError = error;
+  } else if (metadata.error) {
+    renderedError = formatErrorMessage(
+      metadata.error,
+      'Unable to fetch article details'
+    );
+  }
+
   const inputRef = /** @type {{ current: HTMLInputElement }} */ (useRef());
-  // The last value of the URL-entry text input
+  // The last confirmed value of the URL-entry text input
   const previousURL = useRef(/** @type {string|null} */ (null));
 
+  const canConfirmSelection = articleId && metadata.data !== null;
   const confirmSelection = () => {
-    if (selectedURL) {
-      onSelectURL(selectedURL);
+    if (canConfirmSelection) {
+      onSelectURL(jstorURLFromArticleId(articleId));
     }
   };
 
   /**
    * @param {boolean} [confirmSelectedUrl=false]
    */
-  const onUpdateURL = (confirmSelectedUrl = false) => {
+  const onURLChange = (confirmSelectedUrl = false) => {
     const url = inputRef?.current?.value;
     if (url && url === previousURL.current) {
       if (confirmSelectedUrl) {
@@ -53,21 +96,21 @@ export default function JSTORPicker({ onCancel, onSelectURL }) {
     }
 
     previousURL.current = url;
-    setSelectedURL(null);
+    setArticleId(null);
 
     if (!url) {
       // If the field is empty, there's nothing to do
       return;
     }
 
-    const jstorUrl = toJSTORUrl(url);
-
-    if (jstorUrl) {
-      setError(null);
-      setSelectedURL(jstorUrl);
-    } else {
+    const articleId = articleIdFromURL(url);
+    if (!articleId) {
       setError("That doesn't look like a JSTOR article link");
+      return;
     }
+
+    setError(null);
+    setArticleId(articleId);
   };
 
   /**
@@ -77,11 +120,13 @@ export default function JSTORPicker({ onCancel, onSelectURL }) {
    */
   const onKeyDown = event => {
     if (event.key === 'Enter') {
-      onUpdateURL(true /* confirmSelectedUrl */);
       event.preventDefault();
       event.stopPropagation();
+      onURLChange(true /* confirmSelection */);
     }
   };
+
+  const isLoading = thumbnail.isLoading || metadata.isLoading;
 
   return (
     <Modal
@@ -92,7 +137,7 @@ export default function JSTORPicker({ onCancel, onSelectURL }) {
       buttons={[
         <LabeledButton
           data-testid="select-button"
-          disabled={!selectedURL}
+          disabled={!canConfirmSelection}
           key="submit"
           onClick={confirmSelection}
           variant="primary"
@@ -102,7 +147,11 @@ export default function JSTORPicker({ onCancel, onSelectURL }) {
       ]}
     >
       <div className="flex flex-row space-x-3">
-        <Thumbnail classes="w-32 h-40" />
+        <Thumbnail classes="w-32 h-40" isLoading={isLoading}>
+          {thumbnail.data && (
+            <img alt="article thumbnail" src={thumbnail.data.image} />
+          )}
+        </Thumbnail>
         <div className="space-y-2 grow">
           <p>Paste a link to the JSTOR article you&apos;d like to use:</p>
 
@@ -110,35 +159,36 @@ export default function JSTORPicker({ onCancel, onSelectURL }) {
             <TextInput
               inputRef={inputRef}
               name="jstorURL"
-              onChange={() => onUpdateURL(false /* confirmSelectedUrl */)}
+              onChange={() => onURLChange()}
+              onInput={() => setArticleId(null)}
               onKeyDown={onKeyDown}
               placeholder="e.g. https://www.jstor.org/stable/1234"
             />
             <IconButton
               icon="arrowRight"
-              onClick={() => onUpdateURL(false /* confirmSelectedUrl */)}
+              onClick={() => onURLChange()}
               variant="dark"
               title="Find article"
             />
           </TextInputWithButton>
 
-          {selectedURL && (
+          {metadata.data && (
             <div
               className="flex flex-row items-center space-x-2"
               data-testid="selected-book"
             >
               <Icon name="check" classes="text-green-success" />
-              <div className="grow font-bold italic">JSTOR article</div>
+              <div className="grow font-bold italic">{metadata.data.title}</div>
             </div>
           )}
 
-          {error && (
+          {renderedError && (
             <div
               className="flex flex-row items-center space-x-2 text-red-error"
               data-testid="error-message"
             >
               <Icon name="cancel" />
-              <div className="grow">{error}</div>
+              <div className="grow">{renderedError}</div>
             </div>
           )}
         </div>
