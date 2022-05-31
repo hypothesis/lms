14eba5d1a5b93fc1fa792bb844c22537b5cb503c
Robert Knight <robertknight@gmail.com>
2022-05-31 15:33:15 +0100

    Add useFetch hook to make data fetching in components easier
    Whenever data is fetched for display in a component, there are a common
    set of states that we need to keep track of and side effects that need
    to be performed:
    
     - The render function needs to know whether a fetch is happening, or
       succeeded, or failed
     - If the inputs to the fetch change or the component is unmounted, the
       previous fetch needs to be canceled
    
    Add a hook to encapsulate this. The API is intentionally very similar to SWR
    [1]. That has a bunch of additional functionality that we don't yet need and
    that users need to be aware of, but a future migration should be easy if needed.
    
    [1] https://swr.vercel.app

---
+++ lms/static/scripts/frontend_apps/utils/fetch.js
@@ -0,0 +1,80 @@
+import { useEffect, useRef, useState } from 'preact/hooks';
+
+/**
+ * @template T
+ * @typedef FetchResult
+ * @prop {T|null} data
+ * @prop {Error|null} error
+ * @prop {boolean} isLoading
+ */
+
+/**
+ * Function that fetches the data.
+ *
+ * `signal` should be passed to the underlying data fetcher (eg. {@link fetch})
+ * to support cancelation if possible.
+ *
+ * @template T
+ * @typedef {(signal: AbortSignal) => Promise<T>} Fetcher
+ */
+
+/**
+ * Hook that fetches data from the backend API or some other async data source.
+ *
+ * The API is intentionally very similar to SWR (https://swr.vercel.app), so
+ * we can easily migrate if we need its additional functionality in future.
+ *
+ * This hook handles:
+ *
+ *  - Tracking the state of the fetch (idle, fetching, fetched, error)
+ *  - Initiating a fetch when the data to be fetched changes
+ *  - Canceling in-flight requests when the query changes or the component is
+ *    unmounted
+ *
+ * @template [T=unknown]
+ * @param {string|null} key - Key identifying the data to be fetched. The data
+ *   will be re-fetched whenever this changes. If `null`, nothing will be fetched.
+ * @param {Fetcher<T>} [fetcher] - Callback that fetches the data.
+ * @return {FetchResult<T>}
+ */
+export function useFetch(key, fetcher) {
+  const [result, setResult] = useState(
+    /** @type {FetchResult<T>} */ ({
+      data: null,
+      error: null,
+      isLoading: key !== null,
+    })
+  );
+
+  const lastFetcher = useRef(fetcher);
+  lastFetcher.current = fetcher;
+
+  useEffect(() => {
+    setResult({ data: null, error: null, isLoading: key !== null });
+
+    if (!lastFetcher.current) {
+      throw new Error('Fetch key provided but no fetcher set');
+    }
+
+    const controller = new AbortController();
+    lastFetcher
+      .current(controller.signal)
+      .then(data => {
+        if (!controller.signal.aborted) {
+          setResult({ data, error: null, isLoading: false });
+        }
+      })
+      .catch(error => {
+        if (!controller.signal.aborted) {
+          setResult({ data: null, error, isLoading: false });
+        }
+      });
+
+    return () => {
+      // Cancel in-flight request if query changes or component is unmounted.
+      controller.abort();
+    };
+  }, [key]);
+
+  return result;
+}
