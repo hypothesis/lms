6d46e8479114639af34cf6114e00014ef721ec0c
Marcos Prieto <marcospri@gmail.com>
2022-06-02 14:18:38 +0200

    Remove unused APICreateAssignmentSchema

--- lms/validation/_api.py
+++ lms/validation/_api.py
@@ -1,7 +1,7 @@
 """Schema for JSON APIs exposed to the frontend."""
 
 import marshmallow
-from marshmallow import INCLUDE, Schema, ValidationError, validates_schema
+from marshmallow import INCLUDE, Schema
 from webargs import fields
 
 from lms.validation._base import JSONPyramidRequestSchema, PyramidRequestSchema
@@ -84,55 +84,6 @@ class APIRecordResultSchema(JSONPyramidRequestSchema):
     """
     Score — i.e. grade — for this submission. A value between 0 and 1, inclusive.
     """
-
-
-class APICreateAssignmentSchema(PyramidRequestSchema):
-    """Schema for validating assignment creation requests made by our frontend."""
-
-    class Content(Schema):
-        class File(Schema):
-            display_name = fields.Str(required=True)
-            id = fields.Int(required=True)
-            updated_at = fields.Str(required=True)
-            size = fields.Int(required=True)
-
-        type = fields.Str(
-            required=True,
-            validate=marshmallow.validate.OneOf(["url", "file", "vitalsource"]),
-        )
-        url = fields.Url(required=False, allow_none=True)
-
-        file = fields.Nested(File, required=False, allow_none=True)
-        bookID = fields.Str(required=False, allow_none=True)
-        cfi = fields.Str(required=False, allow_none=True)
-        """VitalSource IDs"""
-
-    ext_lti_assignment_id = fields.Str(required=True)
-    """Canvas only assignment unique identifier"""
-
-    course_id = fields.Str(required=True)
-    """Course ID for the assignment. We'd need it to construct the document_url"""
-
-    groupset = fields.Integer(required=False, allow_none=True)
-    """Groupset the assignment belongs to if created as a smalls groups assignment"""
-
-    content = fields.Nested(Content, required=True)
-
-    @validates_schema
-    def validate(self, data, **_kwargs):
-        content = data["content"]
-        type_ = data["content"]["type"]
-
-        if type_ == "url" and not content.get("url"):
-            raise ValidationError("url is mandatory with type is 'url'")
-        if type_ == "file" and not content.get("file"):
-            raise ValidationError("content is mandatory with type is 'file'")
-        if type_ == "vitalsource" and not all(
-            [content.get("bookID"), content.get("cfi")]
-        ):
-            raise ValidationError(
-                "both bookID and cfi are mandatory with type is 'vitalsource'"
-            )
 
 
 class APIBlackboardSyncSchema(PyramidRequestSchema):
