d9ba6506f560d6d019ac76943feaa395374bf51a
Jon Betts <jon.betts@hypothes.is>
2022-06-08 18:20:07 +0100

    Add a model for assignment membership

---
+++ tests/unit/lms/models/assignment_membership_test.py
@@ -0,0 +1,49 @@
+import pytest
+from sqlalchemy.exc import IntegrityError
+
+from lms.models import AssignmentMembership
+from tests import factories
+
+
+class TestAssignmentMembership:
+    def test_it(self, db_session, user, assignment, lti_role):
+        db_session.commit()  # Ensure our objects have ids
+
+        membership = AssignmentMembership(
+            user=user, assignment=assignment, lti_role=lti_role
+        )
+        db_session.add(membership)
+
+        db_session.flush()
+        assert membership.user == user
+        assert membership.user_id == user.id
+        assert membership.assignment == assignment
+        assert membership.assignment_id == assignment.id
+        assert membership.lti_role == lti_role
+        assert membership.lti_role_id == lti_role.id
+
+    def test_it_does_not_allow_duplicates(self, db_session, user, assignment, lti_role):
+        db_session.add(
+            AssignmentMembership(user=user, assignment=assignment, lti_role=lti_role)
+        )
+        db_session.commit()
+
+        with pytest.raises(IntegrityError):
+            db_session.add(
+                AssignmentMembership(
+                    user=user, assignment=assignment, lti_role=lti_role
+                )
+            )
+            db_session.commit()
+
+    @pytest.fixture
+    def user(self):
+        return factories.User.create()
+
+    @pytest.fixture
+    def assignment(self):
+        return factories.Assignment.create()
+
+    @pytest.fixture
+    def lti_role(self):
+        return factories.LTIRole.create()
