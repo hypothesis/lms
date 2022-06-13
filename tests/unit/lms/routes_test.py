88c05ef60aa2e8676c9960edd8a74d622d0513c5
Robert Knight <robertknight@gmail.com>
2022-06-13 13:36:29 +0100

    Fix handling of JSTOR metadata/thumbnail requests that include a DOI prefix
    Metadata/thumbnail requests for JSTOR article IDs with a DOI prefix
    (eg. 10.5325/jafrideve.18.2.0019) did not match the expected route. It
    seems this is because the path is URL-decoded before being matched
    against the route's path pattern [1], and the default regex for path
    parameters is `[^/]+`. Fix this by specifying a custom pattern for the
    path parameter.
    
    [1] Per https://docs.pylonsproject.org/projects/pyramid/en/latest/narr/urldispatch.html#route-pattern-syntax:
    
        > Literal strings in the path segment should represent the decoded value of the PATH_INFO provided to Pyramid

---
+++ tests/unit/lms/routes_test.py
@@ -0,0 +1,48 @@
+import pytest
+from pyramid.interfaces import IRoutesMapper
+from pyramid.testing import DummyRequest
+
+
+# This test does not cover all routes, only cases that have non-trivial route
+# patterns.
+#
+# Note that the `path` test param is the path _after_ URL decoding.
+# See https://docs.pylonsproject.org/projects/pyramid/en/latest/narr/urldispatch.html#route-pattern-syntax
+@pytest.mark.parametrize(
+    "path,expected_route,expected_match",
+    [
+        (
+            "/api/jstor/articles/1234",
+            "jstor_api.articles.metadata",
+            {"article_id": "1234"},
+        ),
+        (
+            "/api/jstor/articles/10.123/456",
+            "jstor_api.articles.metadata",
+            {"article_id": "10.123/456"},
+        ),
+        ("/api/jstor/articles/10/456", None, None),
+        (
+            "/api/jstor/articles/1234/thumbnail",
+            "jstor_api.articles.thumbnail",
+            {"article_id": "1234"},
+        ),
+        (
+            "/api/jstor/articles/10.123/456/thumbnail",
+            "jstor_api.articles.thumbnail",
+            {"article_id": "10.123/456"},
+        ),
+        ("/api/jstor/articles/10/456/thumbnail", None, None),
+    ],
+)
+def test_request_matches_expected_route(
+    pyramid_config, path, expected_route, expected_match
+):
+    route_mapper = pyramid_config.registry.queryUtility(IRoutesMapper)
+    request = DummyRequest(path=path)
+
+    route = route_mapper(request)
+    route_name = route["route"].name if route["route"] else None
+
+    assert route_name == expected_route
+    assert route["match"] == expected_match
