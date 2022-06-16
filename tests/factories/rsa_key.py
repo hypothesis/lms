6ac18d00a97add275dbf852f3c848a86643474c8
Marcos Prieto <marcospri@gmail.com>
2022-06-16 15:42:47 +0200

    Add RSAKeyService.rotate
    Not yet used as this is meant to be called from a Celery task.

--- tests/factories/rsa_key.py
+++ tests/factories/rsa_key.py
@@ -4,5 +4,5 @@ from factory.alchemy import SQLAlchemyModelFactory
 from lms import models
 
 RSAKey = make_factory(
-    models.RSAKey, FACTORY_CLASS=SQLAlchemyModelFactory, public_key="{}"
+    models.RSAKey, FACTORY_CLASS=SQLAlchemyModelFactory, public_key="{}", expired=False
 )
--- tests/unit/lms/services/rsa_key_service_test.py
+++ tests/unit/lms/services/rsa_key_service_test.py
@@ -1,8 +1,10 @@
+from datetime import datetime, timedelta
 from unittest.mock import sentinel
 
 import pytest
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
+from freezegun import freeze_time
 from h_matchers import Any
 from jose import constants
 
@@ -12,6 +14,37 @@ from tests import factories
 
 
 class TestAESService:
+    @freeze_time("2022-1-15")
+    def test_rotate(self, svc, valid_keys, expired_keys, db_session):
+        # Keys created the 10th, max ages of 2 and 4 both expire valid and
+        # deleted expired on the 15th.
+        target_keys = 5
+
+        svc.rotate(
+            target_keys, max_age=timedelta(days=2), max_expired_age=timedelta(days=4)
+        )
+
+        assert all((key.expired for key in valid_keys))
+        assert not any((key in db_session for key in expired_keys))
+        assert db_session.query(RSAKey).filter_by(expired=False).count() == target_keys
+
+    @freeze_time("2022-1-11")
+    @pytest.mark.usefixtures("expired_keys")
+    def test_rotate_doesnt_expire_valid(self, svc, valid_keys):
+        # Keys created the 10th, max age of 2, doesn't expire the keys on the
+        # 11th.
+        svc.rotate(3, max_age=timedelta(days=2), max_expired_age=timedelta(days=5))
+
+        assert not any((key.expired for key in valid_keys))
+
+    @freeze_time("2022-1-11")
+    @pytest.mark.usefixtures("valid_keys")
+    def test_rotate_doesnt_delete_recent_expires(self, svc, expired_keys, db_session):
+        # Keys created the 10th, max expired age of 5, doesn't delete the keys
+        # on the 11th.
+        svc.rotate(3, max_age=timedelta(days=2), max_expired_age=timedelta(days=5))
+        assert all((key in db_session for key in expired_keys))
+
     def test_generate(self, svc, aes_service, rsa, jwk):
         jwk.RSAKey.return_value.to_dict.return_value = {}
 
@@ -45,7 +78,6 @@ class TestAESService:
             algorithm=constants.Algorithms.RS256,
             key=public_bytes.return_value.decode("utf-8"),
         )
-
         assert key.private_key == aes_service.encrypt.return_value
         assert key.aes_cipher_iv == aes_service.build_iv.return_value
 
@@ -57,11 +89,12 @@ class TestAESService:
         aes_service.decrypt.assert_called_once_with(sentinel.iv, sentinel.private_key)
         assert private_key == aes_service.decrypt.return_value
 
-    def test_get_all_public_jwks(self, svc, valid_keys):
+    def test_get_all_public_jwks(self, svc, valid_keys, expired_keys):
         keys = svc.get_all_public_jwks()
 
         expected_keys = [
-            {"use": "sig", "kid": key.kid, "key": "value"} for key in valid_keys
+            {"use": "sig", "kid": key.kid, "key": "value"}
+            for key in (valid_keys + expired_keys)
         ]
         assert keys == Any.list.containing(expected_keys).only()
 
@@ -72,10 +105,19 @@ class TestAESService:
 
     @pytest.fixture
     def valid_keys(self):
-        # Create some noise with some expired keys.
-        factories.RSAKey.create_batch(size=3, expired=True)
         return factories.RSAKey.create_batch(
-            size=3, expired=False, public_key='{"key": "value"}'
+            size=3,
+            public_key='{"key": "value"}',
+            created=datetime(2022, 1, 10),
+        )
+
+    @pytest.fixture
+    def expired_keys(self):
+        return factories.RSAKey.create_batch(
+            size=3,
+            expired=True,
+            public_key='{"key": "value"}',
+            created=datetime(2022, 1, 10),
         )
 
     @pytest.fixture
