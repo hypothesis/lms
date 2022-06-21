bbfc5a7cc882755b88baf2f8a6e77942f2038a56
Marcos Prieto <marcospri@gmail.com>
2022-06-21 16:26:56 +0200

    Fix and adapt canvas plugin tests

--- tests/unit/lms/services/grouping/_plugin/blackboard_test.py
+++ tests/unit/lms/services/grouping/_plugin/blackboard_test.py
@@ -1,182 +1,96 @@
-from unittest.mock import Mock
+from unittest.mock import Mock, sentinel
 
 import pytest
 
 from lms.models import Grouping
 from lms.services.exceptions import ExternalRequestError
-from tests.conftest import TEST_SETTINGS
-
-pytestmark = pytest.mark.usefixtures(
-    "application_instance_service",
-    "assignment_service",
-    "lti_h_service",
-    "grouping_service",
-    "course_service",
-    "blackboard_api_client",
-)
+from lms.services.grouping._plugin.blackboard import BlackboardGroupingPlugin
+from lms.services.grouping._plugin.exceptions import GroupError
+from tests import factories
 
 
-@pytest.mark.usefixtures("user_is_instructor")
-def test_it_when_instructor(
-    pyramid_request, assert_sync_and_return_groups, blackboard_api_client, groups
-):
-    blackboard_api_client.group_set_groups.return_value = groups
+class TestBlackboardGroupingPlugin:
+    def test_get_groups_for_learner(
+        self, blackboard_api_client, plugin, grouping_service, course
+    ):
+        api_groups = plugin.get_groups_for_learner(
+            grouping_service, course, sentinel.group_set_id
+        )
 
-    result = Sync(pyramid_request).sync()
+        blackboard_api_client.course_groups.assert_called_once_with(
+            course.lms_id, sentinel.group_set_id, current_student_own_groups_only=True
+        )
+        assert api_groups == blackboard_api_client.course_groups.return_value
 
-    assert_sync_and_return_groups(result, groups=groups)
+    def test_get_groups_for_learner_when_no_groups(
+        self, blackboard_api_client, plugin, grouping_service, course
+    ):
+        blackboard_api_client.course_groups.return_value = []
 
+        with pytest.raises(GroupError) as err:
+            plugin.get_groups_for_learner(
+                grouping_service, course, sentinel.group_set_id
+            )
+        assert (
+            err.value.error_code
+            == GroupError.ErrorCodes.BLACKBOARD_STUDENT_NOT_IN_GROUP
+        )
 
-@pytest.mark.usefixtures("user_is_instructor")
-def test_it_when_instructor_and_group_set_empty(pyramid_request, blackboard_api_client):
-    blackboard_api_client.group_set_groups.return_value = []
+    def test_get_groups_for_grading(self, plugin, grouping_service, course):
+        api_groups = plugin.get_groups_for_grading(
+            grouping_service, course, sentinel.group_set_id, sentinel.grading_student_id
+        )
 
-    with pytest.raises(BlackboardGroupSetEmpty):
-        Sync(pyramid_request).sync()
-
-
-@pytest.mark.usefixtures("user_is_instructor")
-def test_it_when_instructor_and_group_set_not_found(
-    pyramid_request, blackboard_api_client
-):
-    blackboard_api_client.group_set_groups.side_effect = ExternalRequestError(
-        response=Mock(status_code=404)
-    )
-
-    with pytest.raises(BlackboardGroupSetNotFound):
-        Sync(pyramid_request).sync()
-
-
-@pytest.mark.usefixtures("user_is_instructor")
-def test_it_when_instructor_and_group_set_raises(
-    pyramid_request, blackboard_api_client
-):
-    blackboard_api_client.group_set_groups.side_effect = ExternalRequestError(
-        response=Mock(status_code=500)
-    )
-
-    with pytest.raises(ExternalRequestError):
-        Sync(pyramid_request).sync()
-
-
-@pytest.mark.usefixtures("user_is_learner", "application_instance_service")
-def test_it_when_student(
-    pyramid_request,
-    assert_sync_and_return_groups,
-    blackboard_api_client,
-    groups,
-    grouping_service,
-    user_service,
-):
-    blackboard_api_client.course_groups.return_value = groups
-
-    result = Sync(pyramid_request).sync()
-
-    assert_sync_and_return_groups(result, groups=groups)
-    blackboard_api_client.course_groups.assert_called_once_with(
-        pyramid_request.parsed_params["course"]["context_id"],
-        "GROUP_SET_ID",
-        current_student_own_groups_only=True,
-    )
-    grouping_service.upsert_grouping_memberships.assert_called_once_with(
-        user_service.get.return_value,
-        grouping_service.upsert_groupings.return_value,
-    )
-
-
-@pytest.mark.usefixtures(
-    "user_is_learner", "application_instance_service", "user_service"
-)
-def test_it_when_student_not_in_group(blackboard_api_client, pyramid_request):
-    blackboard_api_client.course_groups.return_value = []
-
-    with pytest.raises(BlackboardStudentNotInGroup):
-        Sync(pyramid_request).sync()
-
-
-@pytest.mark.usefixtures("user_service", "user_is_instructor")
-def test_it_when_grading(
-    pyramid_request,
-    grouping_service,
-    course_service,
-    assignment_service,
-):
-    pyramid_request.parsed_params["gradingStudentId"] = "GRADING_STUDENT_ID"
-    grouping_service.get_course_groupings_for_user.return_value = [Mock()]
-
-    result = Sync(pyramid_request).sync()
-
-    grouping_service.get_course_groupings_for_user.assert_called_once_with(
-        course_service.get_by_context_id.return_value,
-        "GRADING_STUDENT_ID",
-        type_=Grouping.Type.BLACKBOARD_GROUP,
-        group_set_id=assignment_service.get_assignment.return_value.extra[
-            "group_set_id"
-        ],
-    )
-    assert result == [
-        group.groupid()
-        for group in grouping_service.get_course_groupings_for_user.return_value
-    ]
-
-
-@pytest.fixture
-def groups():
-    return [{"name": "GROUP", "id": "1", "groupSetId": "2"}]
-
-
-@pytest.fixture
-def assert_sync_and_return_groups(
-    lti_h_service, request_json, grouping_service, course_service
-):
-    def assert_return_values(groupids, groups):
-        expected_groups = grouping_service.upsert_groupings(
-            [
-                {
-                    "lms_id": group["id"],
-                    "lms_name": group.get("name", f"Group {group['id']}"),
-                    "extra": {"group_set_id": group["groupSetId"]},
-                }
-                for group in groups
-            ],
+        grouping_service.get_course_groupings_for_user.assert_called_once_with(
+            course,
+            sentinel.grading_student_id,
             type_=Grouping.Type.BLACKBOARD_GROUP,
-            parent=course_service.get_by_context_id.return_value,
+            group_set_id=sentinel.group_set_id,
+        )
+        assert api_groups == grouping_service.get_course_groupings_for_user.return_value
+
+    def test_get_groups_for_instructor(
+        self, blackboard_api_client, plugin, grouping_service, course
+    ):
+        api_groups = plugin.get_groups_for_instructor(
+            grouping_service, course, sentinel.group_set_id
         )
 
-        lti_h_service.sync.assert_called_once_with(
-            expected_groups, request_json["group_info"]
+        blackboard_api_client.group_set_groups.assert_called_once_with(
+            course.lms_id, sentinel.group_set_id
+        )
+        assert api_groups == blackboard_api_client.group_set_groups.return_value
+
+    def test_get_groups_for_instructor_group_set_not_found(
+        self, blackboard_api_client, plugin, grouping_service, course
+    ):
+        blackboard_api_client.group_set_groups.side_effect = ExternalRequestError(
+            response=Mock(status_code=404)
         )
 
-        assert groupids == [
-            group.groupid(TEST_SETTINGS["h_authority"]) for group in expected_groups
-        ]
+        with pytest.raises(GroupError) as err:
+            plugin.get_groups_for_instructor(
+                grouping_service, course, sentinel.group_set_id
+            )
+        assert (
+            err.value.error_code == GroupError.ErrorCodes.BLACKBOARD_GROUP_SET_NOT_FOUND
+        )
 
-    return assert_return_values
+    def test_get_groups_for_instructor_group_set_empty(
+        self, blackboard_api_client, plugin, grouping_service, course
+    ):
+        blackboard_api_client.group_set_groups.return_value = []
 
+        with pytest.raises(GroupError) as err:
+            plugin.get_groups_for_instructor(
+                grouping_service, course, sentinel.group_set_id
+            )
+        assert err.value.error_code == GroupError.ErrorCodes.BLACKBOARD_GROUP_SET_EMPTY
 
-@pytest.fixture
-def request_json():
-    return {
-        "course": {
-            "context_id": "test_context_id",
-        },
-        "assignment": {
-            "resource_link_id": "test_resource_link_id",
-        },
-        "lms": {"tool_consumer_instance_guid": "test_tool_consumer_instance_guid"},
-        "group_info": {"foo": "bar"},
-    }
+    @pytest.fixture
+    def plugin(self, blackboard_api_client):
+        return BlackboardGroupingPlugin(blackboard_api_client)
 
-
-@pytest.fixture(autouse=True)
-def assignment_service(assignment_service):
-    assignment_service.get_assignment.return_value.extra = {
-        "group_set_id": "GROUP_SET_ID"
-    }
-    return assignment_service
-
-
-@pytest.fixture
-def pyramid_request(pyramid_request, request_json):
-    pyramid_request.parsed_params = request_json
-    return pyramid_request
+    @pytest.fixture
+    def course(self):
+        return factories.Course()
--- tests/unit/lms/services/grouping/_plugin/canvas_test.py
+++ tests/unit/lms/services/grouping/_plugin/canvas_test.py
@@ -7,16 +7,6 @@ from lms.services import CanvasAPIError
 from lms.services.grouping._plugin.canvas import CanvasGroupingPlugin
 from lms.services.grouping._plugin.exceptions import GroupError
 from tests import factories
-from tests.conftest import TEST_SETTINGS
-
-pytestmark = pytest.mark.usefixtures(
-    "canvas_api_client",
-    "lti_h_service",
-    "grouping_service",
-    "course_service",
-    "application_instance_service",
-    "user_service",
-)
 
 
 class TestCanvasGroupingPlugin:
@@ -47,7 +37,9 @@ class TestCanvasGroupingPlugin:
             {"id": canvas_api_client.course_sections.return_value}
         ]
 
-        api_groups = plugin.get_sections_for_grading(grouping_service, course)
+        api_groups = plugin.get_sections_for_grading(
+            grouping_service, course, sentinel.grading_student_id
+        )
 
         canvas_api_client.course_sections.assert_called_once_with(
             sentinel.canvas_course_id
@@ -55,7 +47,7 @@ class TestCanvasGroupingPlugin:
         canvas_api_client.users_sections.assert_called_once_with(
             sentinel.grading_student_id, sentinel.canvas_course_id
         )
-        assert groupings == grouping_service.upsert_groupings.return_value
+        assert "TODO" == "assert result sections"
 
     def test_get_groups_for_learner(
         self, canvas_api_client, grouping_service, plugin, course
@@ -83,52 +75,19 @@ class TestCanvasGroupingPlugin:
                 grouping_service, course, sentinel.group_set_id
             )
 
-        assert 1 == 2
+        assert err.value.error_code == GroupError.ErrorCodes.CANVAS_STUDENT_NOT_IN_GROUP
 
-    def test_get_groups_for_grading(
-        pyramid_request, canvas_api_client, assert_sync_and_return_groups
-    ):
-        groups = [{"name": "group", "id": 1, "group_category_id": 2}]
-        canvas_api_client.user_groups.return_value = groups
-        group_set = 1
-        course_id = "test_custom_canvas_course_id"
-        learner_id = 111
-
-        groupids = Sync(pyramid_request).sync()
-
-        canvas_api_client.user_groups.assert_called_once_with(
-            course_id, learner_id, group_set
-        )
-        assert_sync_and_return_groups(groupids, groups=groups)
-
-    @pytest.mark.usefixtures("user_is_instructor", "is_group_launch")
     def test_get_groups_for_instructor(
-        pyramid_request, canvas_api_client, assert_sync_and_return_groups
+        self, canvas_api_client, grouping_service, plugin, course
     ):
-        groups = [{"name": "group", "id": 1, "group_category_id": 2}]
-        canvas_api_client.group_category_groups.return_value = groups
-        group_set = 1
+        api_groups = plugin.get_groups_for_instructor(
+            grouping_service, course, sentinel.group_set_id
+        )
 
-        groupids = Sync(pyramid_request).sync()
-
-        canvas_api_client.group_category_groups.assert_called_once_with(group_set)
-        assert_sync_and_return_groups(groupids, groups=groups)
-
-    @pytest.mark.usefixtures("user_is_instructor", "is_group_launch")
-    def test_get_canvas_groups_instructor_empty(pyramid_request, canvas_api_client):
-        canvas_api_client.group_category_groups.return_value = []
-
-        with pytest.raises(CanvasGroupSetEmpty):
-            Sync(pyramid_request).sync()
-
-    @pytest.mark.usefixtures("user_is_instructor", "is_group_launch")
-    def test_get_canvas_groups_instructor_not_found_group_set(
-        pyramid_request, canvas_api_client
-    ):
-        canvas_api_client.group_category_groups.side_effect = CanvasAPIError
-
-        with pytest.raises(CanvasGroupSetNotFound):
-            Sync(pyramid_request).sync()
+        canvas_api_client.group_category_groups.assert_called_once_with(
+            sentinel.group_set_id
+        )
+        assert canvas_api_client.group_category_groups.return_value == api_groups
 
     @pytest.fixture
     def course(self):
