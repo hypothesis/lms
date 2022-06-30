a6cf8b859d7f9d5d2829abb282f956e68b8a1179
Jon Betts <jon.betts@hypothes.is>
2022-06-30 16:28:43 +0100

    Refactor the sync tests to make it a bit easier to extend
    There's going to be some additions to the end here which it would be
    better not to have to duplicate.

--- tests/unit/lms/views/api/sync_test.py
+++ tests/unit/lms/views/api/sync_test.py
@@ -7,70 +7,64 @@ from tests import factories
 from tests.conftest import TEST_SETTINGS
 
 
-@pytest.mark.usefixtures("application_instance_service", "lti_h_service")
+@pytest.mark.usefixtures("application_instance_service")
 class TestSync:
     def test_with_groups(
-        self, lti_h_service, grouping_service, lti_user, course_service, pyramid_request
+        self, pyramid_request, grouping_service, course_service, assert_ids_returned
     ):
-        groups = factories.BlackboardGroup.create_batch(5)
-        grouping_service.get_groups.return_value = groups
+        pyramid_request.parsed_params["group_set_id"] = sentinel.group_set_id
+        groupings = factories.BlackboardGroup.create_batch(5)
+        grouping_service.get_groups.return_value = groupings
 
-        groupids = sync(pyramid_request)
+        returned_ids = sync(pyramid_request)
 
         course_service.get_by_context_id.assert_called_once_with(sentinel.context_id)
         grouping_service.get_groups.assert_called_once_with(
-            sentinel.user,
-            lti_user,
-            course_service.get_by_context_id.return_value,
-            sentinel.group_set_id,
-            sentinel.grading_student_id,
+            user=sentinel.user,
+            lti_user=pyramid_request.lti_user,
+            course=course_service.get_by_context_id.return_value,
+            grading_student_id=sentinel.grading_student_id,
+            group_set_id=sentinel.group_set_id,
         )
-        lti_h_service.sync.assert_called_once_with(
-            grouping_service.get_groups.return_value, sentinel.group_info
-        )
-        assert groupids == [
-            group.groupid(TEST_SETTINGS["h_authority"]) for group in groups
-        ]
+        assert_ids_returned(returned_ids, groupings)
 
-    @pytest.mark.usefixtures("with_sections")
     def test_with_sections(
-        self, lti_h_service, grouping_service, lti_user, course_service, pyramid_request
+        self, pyramid_request, grouping_service, course_service, assert_ids_returned
     ):
-        sections = factories.CanvasSection.create_batch(5)
-        grouping_service.get_sections.return_value = sections
+        groupings = factories.CanvasSection.create_batch(5)
+        grouping_service.get_sections.return_value = groupings
 
-        groupids = sync(pyramid_request)
+        returned_ids = sync(pyramid_request)
 
         course_service.get_by_context_id.assert_called_once_with(sentinel.context_id)
         grouping_service.get_sections.assert_called_once_with(
-            sentinel.user,
-            lti_user,
-            course_service.get_by_context_id.return_value,
-            sentinel.grading_student_id,
+            user=sentinel.user,
+            lti_user=pyramid_request.lti_user,
+            course=course_service.get_by_context_id.return_value,
+            grading_student_id=sentinel.grading_student_id,
         )
-        lti_h_service.sync.assert_called_once_with(
-            grouping_service.get_sections.return_value, sentinel.group_info
-        )
-        assert groupids == [
-            group.groupid(TEST_SETTINGS["h_authority"]) for group in sections
-        ]
+        assert_ids_returned(returned_ids, groupings)
 
     @pytest.fixture
-    def pyramid_request(self, pyramid_request, request_json):
-        pyramid_request.parsed_params = request_json
-        pyramid_request.user = sentinel.user
-        return pyramid_request
+    def assert_ids_returned(self, lti_h_service):
+        # This name is a bit cheeky, as we are really testing everything which
+        # is the same after the main if.
+        def assert_ids_returned(returned_ids, groupings):
+            lti_h_service.sync.assert_called_once_with(groupings, sentinel.group_info)
+
+            assert returned_ids == [
+                group.groupid(TEST_SETTINGS["h_authority"]) for group in groupings
+            ]
+
+        return assert_ids_returned
 
     @pytest.fixture
-    def request_json(self):
-        return {
+    def pyramid_request(self, pyramid_request):
+        pyramid_request.parsed_params = {
             "lms": {"tool_consumer_instance_guid": sentinel.guid},
             "context_id": sentinel.context_id,
-            "group_set_id": sentinel.group_set_id,
             "gradingStudentId": sentinel.grading_student_id,
             "group_info": sentinel.group_info,
         }
-
-    @pytest.fixture
-    def with_sections(self, request_json):
-        del request_json["group_set_id"]
+        pyramid_request.user = sentinel.user
+        return pyramid_request
