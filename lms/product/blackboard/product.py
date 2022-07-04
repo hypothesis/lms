cc26675c96fb64c72286bfa0c338e687dba9a4cf
Jon Betts <jon.betts@hypothes.is>
2022-07-04 13:11:48 +0100

    Fix route instantiation for Canvas and Blackboard
    For dataclasses class level variables will work as expected when accessing
    at a class level, but unless you declare them correctly, the dataclass
    generated init will ignore them and fill out the last correctly defined thing.
    
    For safety it's best to fully declare them all.

--- lms/product/blackboard/product.py
+++ lms/product/blackboard/product.py
@@ -1,15 +1,20 @@
+from dataclasses import dataclass
+
 from lms.product.blackboard._plugin.grouping import BlackboardGroupingPlugin
 from lms.product.product import PluginConfig, Product, Routes
 
 
+@dataclass
 class Blackboard(Product):
     """A product for Blackboard specific settings and tweaks."""
 
-    family = Product.Family.BLACKBOARD
+    family: Product.Family = Product.Family.BLACKBOARD
 
-    route = Routes(
+    route: Routes = Routes(
         oauth2_authorize="blackboard_api.oauth.authorize",
         oauth2_refresh="blackboard_api.oauth.refresh",
     )
 
-    plugin_config = PluginConfig(grouping_service=BlackboardGroupingPlugin)
+    plugin_config: PluginConfig = PluginConfig(
+        grouping_service=BlackboardGroupingPlugin
+    )
--- lms/product/canvas/product.py
+++ lms/product/canvas/product.py
@@ -1,15 +1,18 @@
+from dataclasses import dataclass
+
 from lms.product.canvas._plugin.grouping import CanvasGroupingPlugin
 from lms.product.product import PluginConfig, Product, Routes
 
 
+@dataclass
 class Canvas(Product):
     """A product for Canvas specific settings and tweaks."""
 
-    family = Product.Family.CANVAS
+    family: Product.Family = Product.Family.CANVAS
 
-    route = Routes(
+    route: Routes = Routes(
         oauth2_authorize="canvas_api.oauth.authorize",
         oauth2_refresh="canvas_api.oauth.refresh",
     )
 
-    plugin_config = PluginConfig(grouping_service=CanvasGroupingPlugin)
+    plugin_config: PluginConfig = PluginConfig(grouping_service=CanvasGroupingPlugin)
