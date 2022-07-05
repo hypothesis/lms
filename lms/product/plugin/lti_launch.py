50c9f988ac02c08c3a73bafb2164071a277ab4cf
Jon Betts <jon.betts@hypothes.is>
2022-07-05 20:48:54 +0100

    Grading bar flag fixup

--- lms/product/plugin/lti_launch.py
+++ lms/product/plugin/lti_launch.py
@@ -3,6 +3,7 @@ from tests.unit.lms.resources.oauth2_redirect_test import JSConfig
 
 class LTILaunchPlugin:
     supports_grading_bar = True
+    """Does this product support our built in grading bar?"""
 
     def add_to_launch_js_config(
         self, js_config: JSConfig, document_url, assignment_gradable
