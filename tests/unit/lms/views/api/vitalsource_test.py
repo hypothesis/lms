a49fd2ae977d5acd3661815a5ecb9b5537ba6ab8
Jon Betts <jon.betts@hypothes.is>
2022-07-22 18:04:24 +0100

    Inline fixtures which are only used in one place

--- tests/unit/lms/views/api/vitalsource_test.py
+++ tests/unit/lms/views/api/vitalsource_test.py
@@ -6,9 +6,13 @@ from lms.views.api.vitalsource import VitalSourceAPIViews
 
 @pytest.mark.usefixtures("vitalsource_service")
 class TestVitalSourceAPIViews:
-    def test_book_info_returns_metadata(
-        self, pyramid_request, vitalsource_service, api_book_info
-    ):
+    def test_book_info_returns_metadata(self, pyramid_request, vitalsource_service):
+        api_book_info = {
+            "vbid": "VBID",
+            "title": "BOOK  TITLE",
+            "resource_links": {"cover_image": "http://cover_image/url"},
+        }
+
         vitalsource_service.get_book_info.return_value = api_book_info
 
         pyramid_request.matchdict["book_id"] = "BOOKSHELF-TUTORIAL"
@@ -22,13 +26,18 @@ class TestVitalSourceAPIViews:
         }
 
     def test_table_of_contents_returns_chapter_data(
-        self, pyramid_request, vitalsource_service, api_book_table_of_contents
+        self, pyramid_request, vitalsource_service
     ):
-        vitalsource_service.get_book_toc.return_value = api_book_table_of_contents
+        vitalsource_service.get_book_toc.return_value = {
+            "table_of_contents": [
+                {"title": "chapter 1", "cfi": "XXXX", "page": "1"},
+                ...,
+            ]
+        }
 
         pyramid_request.matchdict["book_id"] = "BOOKSHELF-TUTORIAL"
         toc = VitalSourceAPIViews(pyramid_request).table_of_contents()
-        assert toc == api_book_table_of_contents["table_of_contents"]
+        assert toc == vitalsource_service.get_book_toc.return_value["table_of_contents"]
 
     @pytest.mark.parametrize("method", ("book_info", "table_of_contents"))
     @pytest.mark.parametrize("invalid_book_id", ["", "lowercase", "OTHER#CHARS-OTHER"])
@@ -39,24 +48,3 @@ class TestVitalSourceAPIViews:
 
         with pytest.raises(HTTPBadRequest):
             getattr(view, method)()
-
-    @pytest.fixture
-    def api_book_table_of_contents(self):
-        return {
-            "table_of_contents": [
-                {"title": "chapter 1", "cfi": "XXXX", "page": "1"},
-                {"title": "chapter 2", "cfi": "XXXX", "page": "2"},
-                {"title": "chapter 3", "cfi": "XXXX", "page": "3"},
-                {"title": "chapter 4", "cfi": "XXXX", "page": "4"},
-            ]
-        }
-
-    @pytest.fixture
-    def api_book_info(self):
-        return {
-            "vbid": "VBID",
-            "title": "BOOK  TITLE",
-            "resource_links": {
-                "cover_image": "http://cover_image/url",
-            },
-        }
