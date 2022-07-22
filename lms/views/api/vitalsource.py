30b5db1cf1a82dc79e910be011a2dd2b0e24ac17
Jon Betts <jon.betts@hypothes.is>
2022-07-22 19:37:14 +0100

    Use a schema for validation instead of rolling our own

--- lms/views/api/vitalsource.py
+++ lms/views/api/vitalsource.py
@@ -1,10 +1,15 @@
-import re
-
-from pyramid.httpexceptions import HTTPBadRequest
+from marshmallow import fields, validate
 from pyramid.view import view_config, view_defaults
 
 from lms.security import Permissions
 from lms.services import VitalSourceService
+from lms.validation import PyramidRequestSchema
+
+
+class _BookSchema(PyramidRequestSchema):
+    location = "matchdict"
+
+    book_id = fields.Str(required=True, validate=validate.Regexp(r"^[\dA-Z-]+$"))
 
 
 @view_defaults(renderer="json", permission=Permissions.API)
@@ -13,21 +18,10 @@ class VitalSourceAPIViews:
         self.request = request
         self.svc = request.find_service(VitalSourceService)
 
-    @view_config(route_name="vitalsource_api.books.info")
+    @view_config(route_name="vitalsource_api.books.info", schema=_BookSchema)
     def book_info(self):
-        return self.svc.get_book_info(self._book_id)
+        return self.svc.get_book_info(self.request.matchdict["book_id"])
 
-    @view_config(route_name="vitalsource_api.books.toc")
+    @view_config(route_name="vitalsource_api.books.toc", schema=_BookSchema)
     def table_of_contents(self):
-        return self.svc.get_table_of_contents(self._book_id)
-
-    _VALID_BOOK_ID = re.compile(r"^[\dA-Z-]+$")
-
-    @property
-    def _book_id(self):
-        book_id = self.request.matchdict["book_id"]
-
-        if not self._VALID_BOOK_ID.match(book_id):
-            raise HTTPBadRequest("Invalid `book_id`. It must only contain [0-9A-Z-].")
-
-        return book_id
+        return self.svc.get_table_of_contents(self.request.matchdict["book_id"])
