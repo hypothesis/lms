f6d8564a903fbe87a7d9c760b537e3bec543e1b8
Jon Betts <jon.betts@hypothes.is>
2022-07-25 19:30:46 +0100

    Use the new methods and settings to return a SSO redirect instead of direct link
    This means the URL we return will now automatically sign the user in if possible

--- tests/unit/lms/resources/_js_config/__init___test.py
+++ tests/unit/lms/resources/_js_config/__init___test.py
@@ -121,12 +121,18 @@ class TestAddDocumentURL:
             "path": "/api/blackboard/courses/test_course_id/via_url?document_url=blackboard%3A%2F%2Fcontent-resource%2Fxyz123",
         }
 
-    def test_vitalsource_sets_config(self, js_config, vitalsource_service):
-        vitalsource_url = "vitalsource://book/bookID/book-id/cfi//abc"
+    def test_vitalsource_sets_config(
+        self, js_config, pyramid_request, vitalsource_service
+    ):
+        document_url = "vitalsource://book/bookID/book-id/cfi//abc"
+        vitalsource_service.user_lti_param = "user_id"
+        pyramid_request.lti_params["user_id"] = "USER_REF"
 
-        js_config.add_document_url(vitalsource_url)
+        js_config.add_document_url(document_url)
 
-        vitalsource_service.get_launch_url.assert_called_with(vitalsource_url)
+        vitalsource_service.get_launch_url.assert_called_with(
+            user_reference="USER_REF", document_url=document_url
+        )
         assert (
             js_config.asdict()["viaUrl"]
             == vitalsource_service.get_launch_url.return_value
--- tests/unit/lms/services/vitalsource/service_test.py
+++ tests/unit/lms/services/vitalsource/service_test.py
@@ -3,24 +3,46 @@ from unittest.mock import create_autospec, sentinel
 import pytest
 
 from lms.services.vitalsource._client import VitalSourceClient
+from lms.services.vitalsource.exceptions import VitalSourceError
 from lms.services.vitalsource.service import VitalSourceService
 
 
 class TestVitalSourceService:
     @pytest.mark.parametrize("client", (sentinel.client, None))
     @pytest.mark.parametrize("enabled", (sentinel.client, None))
-    def test_enabled(self, client, enabled):
-        svc = VitalSourceService(client=client, enabled=enabled)
-
-        assert svc.enabled == bool(client and enabled)
-
-    def test_get_launch_url(self, svc):
-        document_url = "vitalsource://book/bookID/book-id/cfi//abc"
-
-        assert (
-            svc.get_launch_url(document_url)
-            == "https://hypothesis.vitalsource.com/books/book-id/cfi//abc"
+    @pytest.mark.parametrize("user_lti_param", (sentinel.user_lti_param, None))
+    def test_enabled(self, client, enabled, user_lti_param):
+        svc = VitalSourceService(
+            client=client, enabled=enabled, user_lti_param=user_lti_param
         )
+
+        assert svc.enabled == bool(client and enabled and user_lti_param)
+
+    def test_get_launch_url(self, svc, client):
+        result = svc.get_launch_url(
+            sentinel.user_reference,
+            document_url="vitalsource://book/bookID/BOOK-ID/cfi/CFI",
+        )
+
+        client.get_user_book_license.assert_called_once_with(
+            sentinel.user_reference, "BOOK-ID"
+        )
+        client.get_sso_redirect.assert_called_once_with(
+            sentinel.user_reference,
+            "https://hypothesis.vitalsource.com/books/BOOK-ID/cfi/CFI",
+        )
+        assert result == client.get_sso_redirect.return_value
+
+    def test_get_launch_url_with_no_book_license(self, svc, client):
+        client.get_user_book_license.return_value = None
+
+        with pytest.raises(VitalSourceError) as exc:
+            svc.get_launch_url(
+                sentinel.user_reference,
+                document_url="vitalsource://book/bookID/BOOK-ID/cfi/CFI",
+            )
+
+        assert exc.value.error_code == "vitalsource_no_book_license"
 
     @pytest.mark.parametrize(
         "proxy_method,args",
@@ -42,4 +64,6 @@ class TestVitalSourceService:
 
     @pytest.fixture
     def svc(self, client):
-        return VitalSourceService(client=client, enabled=True)
+        return VitalSourceService(
+            client=client, enabled=True, user_lti_param=sentinel.user_lti_param
+        )
