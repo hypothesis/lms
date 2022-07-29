7d6d2c4d7e78112b7288cfa265df5b77887494dc
Marcos Prieto <marcospri@gmail.com>
2022-07-29 16:30:45 +0200

    Refactor events
    Use a common dataclass for all events and specialise for LTIEvents
    where we can pick the values for user, course, assignment... from the
    request parameters.

--- lms/services/event.py
+++ lms/services/event.py
@@ -1,27 +1,41 @@
+from __future__ import annotations
+
 from functools import lru_cache
+from typing import TYPE_CHECKING
 
 from sqlalchemy.orm import Session
 
 from lms.models import Event, EventData, EventType, EventUser
+
+if TYPE_CHECKING:
+    from lms.events import BaseEvent
 
 
 class EventService:
     def __init__(self, db: Session):
         self._db = db
 
-    def insert_event(
-        self, type_: EventType, user: None, lti_roles=None, data=None, **kwargs
-    ):
+    def insert_event(self, event: BaseEvent):
 
-        event = Event(type_id=self._get_type_pk(type_), **kwargs)
-        self._db.add(event)
+        db_event = Event(
+            type_id=self._get_type_pk(event.type),
+            application_instance_id=event.application_instance_id,
+            course_id=event.course_id,
+            assignment_id=event.assignment_id,
+            grouping_id=event.grouping_id,
+        )
+        self._db.add(db_event)
 
-        if user:
-            for lti_role in lti_roles or [None]:
-                self._db.add(EventUser(event=event, user=user, lti_role=lti_role))
+        if event.user_id:
+            for role_id in event.role_ids or [None]:
+                self._db.add(
+                    EventUser(
+                        event=db_event, user_id=event.user_id, lti_role_id=role_id
+                    )
+                )
 
-        if data:
-            self._db.add(EventData(event=event, data=data))
+        if event.data:
+            self._db.add(EventData(event=db_event, data=event.data))
 
     @lru_cache
     def _get_type_pk(self, type_: EventType.Type) -> int:
