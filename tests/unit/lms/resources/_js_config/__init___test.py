b733d486b11650842f1bac080071ef9c8ff9a675
Robert Knight <robertknight@gmail.com>
2022-08-01 11:45:39 +0100

    Set `contentBanner` config in JSTOR assignments

--- tests/unit/lms/resources/_js_config/__init___test.py
+++ tests/unit/lms/resources/_js_config/__init___test.py
@@ -138,6 +138,10 @@ class TestAddDocumentURL:
         js_config.add_document_url(jstor_url)
 
         jstor_service.via_url.assert_called_with(pyramid_request, jstor_url)
+        assert js_config.asdict()["contentBanner"] == {
+            "source": "jstor",
+            "itemId": "DOI",
+        }
         assert js_config.asdict()["viaUrl"] == jstor_service.via_url.return_value
 
     def test_it_adds_the_viaUrl_api_config_for_Canvas_documents(
