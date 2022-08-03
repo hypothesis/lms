8f51ebfe1cb335996c3fcbd2010f7ab979ae3b2a
Jon Betts <jon.betts@hypothes.is>
2022-08-03 17:01:18 +0100

    Use a simpler proxy for JSTOR article metadata

--- lms/views/api/jstor.py
+++ lms/views/api/jstor.py
@@ -12,20 +12,16 @@ class JSTORAPIViews:
 
     @view_config(route_name="jstor_api.articles.metadata")
     def article_metadata(self):
-        article_id = self.request.matchdict["article_id"]
-        article_info = self.jstor_service.metadata(article_id)
-        content_status = article_info["content_status"]
-
-        return {
-            "title": article_info["title"],
-            "content_status": content_status.name.lower(),
-        }
+        return self.jstor_service.get_article_metadata(
+            article_id=self.request.matchdict["article_id"]
+        )
 
     @view_config(route_name="jstor_api.articles.thumbnail")
     def article_thumbnail(self):
-        article_id = self.request.matchdict["article_id"]
-        data_uri = self.jstor_service.thumbnail(article_id)
-
         # The image is wrapped in an object to make API responses more uniform
         # for consumers.
-        return {"image": data_uri}
+        return {
+            "image": self.jstor_service.thumbnail(
+                article_id=self.request.matchdict["article_id"]
+            )
+        }
