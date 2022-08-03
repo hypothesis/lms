c439f4d0a22cfa97f6fb3a626437e07c0d7df269
Jon Betts <jon.betts@hypothes.is>
2022-08-03 17:01:18 +0100

    Use a simpler proxy for JSTOR article metadata

--- tests/unit/lms/views/api/jstor_test.py
+++ tests/unit/lms/views/api/jstor_test.py
@@ -5,25 +5,22 @@ from lms.views.api.jstor import JSTORAPIViews
 
 @pytest.mark.usefixtures("jstor_service")
 class TestJSTORAPIViews:
-    def test_article_metadata(self, jstor_service, pyramid_request):
-        views = JSTORAPIViews(pyramid_request)
+    def test_article_metadata(self, views, jstor_service, pyramid_request):
         pyramid_request.matchdict["article_id"] = "test-article"
 
         metadata = views.article_metadata()
 
-        jstor_service.metadata.assert_called_once_with("test-article")
-        assert metadata == {
-            "title": jstor_service.metadata.return_value["title"],
-            "content_status": jstor_service.metadata.return_value[
-                "content_status"
-            ].name.lower(),
-        }
+        jstor_service.get_article_metadata.assert_called_once_with("test-article")
+        assert metadata == jstor_service.get_article_metadata.return_value
 
-    def test_article_thumbnail(self, jstor_service, pyramid_request):
-        views = JSTORAPIViews(pyramid_request)
+    def test_article_thumbnail(self, views, jstor_service, pyramid_request):
         pyramid_request.matchdict["article_id"] = "test-article"
 
         thumbnail = views.article_thumbnail()
 
         jstor_service.thumbnail.assert_called_once_with("test-article")
         assert thumbnail == {"image": jstor_service.thumbnail.return_value}
+
+    @pytest.fixture
+    def views(self, pyramid_request):
+        return JSTORAPIViews(pyramid_request)
