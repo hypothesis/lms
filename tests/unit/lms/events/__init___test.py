21f7bef80ace78a05630ffab58a19c995afb7e12
Marcos Prieto <marcospri@gmail.com>
2022-08-03 16:31:28 +0200

    Fix LTIEvent default values for deep linking
    Take the parameters from the JSON body instead of the LTIParams
    location.

--- tests/unit/lms/events/__init___test.py
+++ tests/unit/lms/events/__init___test.py
@@ -1,4 +1,5 @@
-from unittest.mock import sentinel
+import json
+from unittest.mock import Mock, sentinel
 
 import pytest
 
@@ -32,7 +33,13 @@ class TestLTIEvent:
             event.application_instance_id
             == application_instance_service.get_current.return_value.id
         )
+
+        course_service.get_by_context_id.assert_called_once_with(sentinel.context_id)
         assert event.course_id == course_service.get_by_context_id.return_value.id
+
+        assignment_service.get_assignment.assert_called_once_with(
+            sentinel.tool_guid, sentinel.resource_link_id
+        )
         assert event.assignment_id == assignment_service.get_assignment.return_value.id
 
     @pytest.mark.usefixtures("lti_role_service", "course_service", "assignment_service")
@@ -43,6 +50,33 @@ class TestLTIEvent:
 
         event = LTIEvent(request=pyramid_request, type=sentinel.type)
         assert not event.application_instance_id
+
+    @pytest.mark.usefixtures(
+        "lti_role_service", "application_instance_service", "assignment_service"
+    )
+    def test_lti_event_when_context_id_from_json(self, pyramid_request, course_service):
+        del pyramid_request.lti_params["context_id"]
+        pyramid_request.json = {"context_id": sentinel.json_context_id}
+
+        event = LTIEvent(request=pyramid_request, type=sentinel.type)
+
+        course_service.get_by_context_id.assert_called_once_with(
+            sentinel.json_context_id
+        )
+        assert event.course_id == course_service.get_by_context_id.return_value.id
+
+    @pytest.mark.usefixtures(
+        "lti_role_service", "application_instance_service", "assignment_service"
+    )
+    def test_lti_event_when_context_id_from_invalid_json(
+        self, pyramid_request, course_service
+    ):
+        del pyramid_request.lti_params["context_id"]
+
+        event = LTIEvent(request=pyramid_request, type=sentinel.type)
+
+        course_service.get_by_context_id.assert_called_once_with(None)
+        assert event.course_id == course_service.get_by_context_id.return_value.id
 
     @pytest.mark.usefixtures(
         "lti_role_service", "application_instance_service", "assignment_service"
@@ -78,3 +112,20 @@ class TestLTIEvent:
         assert event.application_instance_id == sentinel.application_instance_id
         assert event.course_id == sentinel.course_id
         assert event.assignment_id == sentinel.assignment_id
+
+    @pytest.fixture
+    def pyramid_request(self, pyramid_request):
+        pyramid_request.json = Mock()
+        pyramid_request.json.get.side_effect = json.decoder.JSONDecodeError(
+            msg="", doc="", pos=0
+        )
+
+        pyramid_request.lti_params.update(
+            {
+                "context_id": sentinel.context_id,
+                "tool_consumer_instance_guid": sentinel.tool_guid,
+                "resource_link_id": sentinel.resource_link_id,
+            }
+        )
+
+        return pyramid_request
