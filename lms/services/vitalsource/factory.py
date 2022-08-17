ea14451366ecb3079d2ef275a7cf7d17cef919c5
Jon Betts <jon.betts@hypothes.is>
2022-08-17 14:30:03 +0100

    Re-instate the non-SSO VitalSource path as a fallback

--- lms/services/vitalsource/factory.py
+++ lms/services/vitalsource/factory.py
@@ -5,13 +5,15 @@ from lms.services.vitalsource.service import VitalSourceService
 def service_factory(_context, request):
     settings = request.find_service(name="application_instance").get_current().settings
 
+    global_key = request.registry.settings["vitalsource_api_key"]
     customer_key = settings.get("vitalsource", "api_key")
 
     return VitalSourceService(
         # It's important to pass None here if there's no key, so the service
         # knows it's been disabled. The client will raise an error anyway if
         # you try and create one with no API key.
-        client=VitalSourceClient(customer_key) if customer_key else None,
-        user_lti_param=settings.get("vitalsource", "user_lti_param"),
         enabled=settings.get("vitalsource", "enabled", False),
+        global_client=VitalSourceClient(global_key) if global_key else None,
+        customer_client=VitalSourceClient(customer_key) if customer_key else None,
+        user_lti_param=settings.get("vitalsource", "user_lti_param"),
     )
