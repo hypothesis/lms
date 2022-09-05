ee094a42414988038e057de861265aab79fba717
Robert Knight <robertknight@gmail.com>
2022-09-05 13:27:06 +0100

    Add Tracking-User-ID and Tracking-User-Agent headers to JSTOR API requests
    These are needed by JSTOR for usage counting purposes.
    
    Fixes https://github.com/hypothesis/lms/issues/4340

--- lms/services/jstor/factory.py
+++ lms/services/jstor/factory.py
@@ -8,10 +8,20 @@ def service_factory(_context, request):
 
     app_settings = request.registry.settings
 
+    # The h username is passed to JSTOR as a tracking ID because it is a
+    # conveniently available unique user ID in a standardized format and doesn't
+    # contain private info (email addresses etc.).
+    if request.lti_user:
+        tracking_user_id = request.lti_user.h_user.username
+    else:
+        tracking_user_id = None
+
     return JSTORService(
         api_url=app_settings.get("jstor_api_url"),
         secret=app_settings.get("jstor_api_secret"),
         enabled=ai_settings.get("jstor", "enabled"),
         site_code=ai_settings.get("jstor", "site_code"),
         http_service=request.find_service(name="http"),
+        tracking_user_agent=request.headers.get("User-Agent", None),
+        tracking_user_id=tracking_user_id,
     )
