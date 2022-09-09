82c9ea19dd12607daa4f209544a359dc3f33aa26
Marcos Prieto <marcospri@gmail.com>
2022-09-09 13:10:03 +0200

    Allow name editing on organization detail

--- lms/views/admin/organization.py
+++ lms/views/admin/organization.py
@@ -17,9 +17,22 @@ class AdminLTIRegistrationViews:
         request_method="GET",
         renderer="lms:templates/admin/organization.html.jinja2",
     )
-    def org_detail(self):
+    def org_details(self):
         public_id = self.request.matchdict["public_id"]
         return {
             "org": self.organization_service.get_by_public_id(public_id),
-            "public_id": public_id,
         }
+
+    @view_config(
+        route_name="admin.organization",
+        request_method="POST",
+        renderer="lms:templates/admin/organization.html.jinja2",
+    )
+    def org_update(self):
+        public_id = self.request.matchdict["public_id"]
+        org = self.organization_service.get_by_public_id(public_id)
+
+        if name := self.request.params.get("name", "").strip():
+            org.name = name
+
+        return {"org": org}
