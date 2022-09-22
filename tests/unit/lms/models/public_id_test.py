d844677e5d34549d23d5f97206f40bd3f0667e84
Jon Betts <jon.betts@hypothes.is>
2022-09-22 17:23:39 +0100

    Add a method for parsing PublicID strings

--- tests/unit/lms/models/public_id_test.py
+++ tests/unit/lms/models/public_id_test.py
@@ -1,7 +1,7 @@
 import pytest
 from h_matchers import Any
 
-from lms.models.public_id import PublicId
+from lms.models.public_id import InvalidPublicId, PublicId
 from lms.models.region import Regions
 
 
@@ -15,6 +15,38 @@ class TestPublicId:
         assert PublicId.generate_instance_id() == Any.string.matching(
             r"[A-Za-z0-9-_]{22}"
         )
+
+    def test_parse(self):
+        public_id = PublicId.parse(
+            "us.APP_CODE.MODEL_CODE.INSTANCE_ID", expect_app_code=None
+        )
+
+        assert isinstance(public_id, PublicId)
+        assert public_id.region == Regions.US
+        assert public_id.model_code == "MODEL_CODE"
+        assert public_id.app_code == "APP_CODE"
+        assert public_id.instance_id == "INSTANCE_ID"
+
+    @pytest.mark.parametrize("bad_value", ("too.few", "too.many.dots.in.this"))
+    def test_parse_raises_with_bad_dots(self, bad_value):
+        with pytest.raises(InvalidPublicId):
+            PublicId.parse(bad_value)
+
+    def test_parse_raises_with_bad_region(self):
+        with pytest.raises(InvalidPublicId):
+            PublicId.parse("NOT_A_REGION_CODE.lms.model.id")
+
+    @pytest.mark.parametrize(
+        "expect,value",
+        (
+            ("expect_app_code", "not_lms"),
+            ("expect_model_code", "not_model"),
+            ("expect_region", Regions.CA),
+        ),
+    )
+    def test_parse_raises_with_expectations_missed(self, expect, value):
+        with pytest.raises(InvalidPublicId):
+            PublicId.parse("us.lms.model.id", **{expect: value})
 
     def test___str__(self, public_id):
         assert str(public_id) == "us.APP_CODE.MODEL_CODE.INSTANCE_ID"
