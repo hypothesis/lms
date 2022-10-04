9c64a2b8d59182b97299cca11077d4b2ec588508
Jon Betts <jon.betts@hypothes.is>
2022-10-04 15:22:07 +0100

    Use a global singleton instead of a request method for region
    This keeps the request method for now, but makes it optional. This
    allows us to remove the region injection bit by bit.

--- tests/unit/lms/models/region_test.py
+++ tests/unit/lms/models/region_test.py
@@ -1,20 +1,24 @@
-from unittest.mock import create_autospec
+from unittest import mock
+from unittest.mock import create_autospec, sentinel
 
 import pytest
+from h_matchers import Any
 from pyramid.config import Configurator
 
 from lms.models.region import Regions, includeme
 
 
 class TestRegions:
-    @pytest.mark.parametrize(
-        "authority,region",
-        (("lms.hypothes.is", Regions.US), ("lms.ca.hypothes.is", Regions.CA)),
-    )
-    def test_from_request(self, pyramid_request, authority, region):
-        pyramid_request.registry.settings["h_authority"] = authority
+    def test_get_region(self, current_region):
+        current_region.return_value = Regions.CA
 
-        assert Regions.from_request(pyramid_request) == region
+        assert Regions.get_region() == Regions.CA
+
+    def test_get_region_with_no_region(self, current_region):
+        current_region.return_value = None
+
+        with pytest.raises(ValueError):
+            Regions.get_region()
 
     @pytest.mark.parametrize("bad_authority", (None, "UNRECOGNIZED"))
     def test_from_request_raises_ValueError_for_bad_authorities(
@@ -23,17 +27,34 @@ class TestRegions:
         pyramid_request.registry.settings["h_authority"] = bad_authority
 
         with pytest.raises(ValueError):
-            Regions.from_request(pyramid_request)
+            Regions.set_region(pyramid_request)
+
+    @pytest.fixture
+    def current_region(self):
+        with mock.patch.object(Regions, "_current_region") as _current_region:
+            _current_region.__get__ = mock.Mock(return_value=None)
+            yield _current_region.__get__
 
 
 class TestIncludeMe:
-    def test_it(self, configurator):
+    def test_it(self, configurator, Regions):
         includeme(configurator)
 
+        any_callable = Any.callable()
+
         configurator.add_request_method.assert_called_once_with(
-            Regions.from_request, name="region", property=True, reify=True
+            any_callable, name="region", property=True, reify=True
         )
 
-    @pytest.fixture()
+        # You can use h-matchers as spies to capture what they last matched
+        response = any_callable.last_matched()(sentinel.request)
+        Regions.get_region.assert_called_once_with()
+        assert response == Regions.get_region.return_value
+
+    @pytest.fixture
     def configurator(self):
         return create_autospec(Configurator, spec_set=True, instance=True)
+
+    @pytest.fixture
+    def Regions(self, patch):
+        return patch("lms.models.region.Regions")
