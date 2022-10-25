c139fb32e9f386595c7b15361e994db0b9b2f4d5
Marcos Prieto <marcospri@gmail.com>
2022-10-25 11:39:49 +0200

    Directly inject the settings on product creation

--- tests/unit/lms/product/factory_test.py
+++ tests/unit/lms/product/factory_test.py
@@ -30,12 +30,16 @@ class TestGetProductFromRequest:
     ]
 
     @pytest.mark.parametrize("value,family,class_", PRODUCT_MAP)
-    def test_from_request_exact_match_lti(self, pyramid_request, value, family, class_):
+    def test_from_request_exact_match_lti(
+        self, pyramid_request, value, family, class_, application_instance
+    ):
         pyramid_request.lti_params["tool_consumer_info_product_family_code"] = value
 
         product = get_product_from_request(pyramid_request)
 
-        class_.from_request.assert_called_once_with(pyramid_request)
+        class_.from_request.assert_called_once_with(
+            pyramid_request, application_instance.settings
+        )
         assert product == class_.from_request.return_value
         assert product.family == family
 
@@ -51,24 +55,30 @@ class TestGetProductFromRequest:
         assert get_product_from_request(pyramid_request).family == family
 
     @pytest.mark.parametrize("value,family,class_", PRODUCT_MAP)
-    def test_from_request_api(self, pyramid_request, value, family, class_):
+    def test_from_request_api(
+        self, pyramid_request, value, family, class_, application_instance
+    ):
         pyramid_request.content_type = "application/json"
         pyramid_request.json = {"lms": {"product": value}}
 
         product = get_product_from_request(pyramid_request)
 
-        class_.from_request.assert_called_once_with(pyramid_request)
+        class_.from_request.assert_called_once_with(
+            pyramid_request, application_instance.settings
+        )
         assert product == class_.from_request.return_value
         assert product.family == family
 
-    def test_from_request_canvas_custom(self, pyramid_request):
+    def test_from_request_canvas_custom(self, pyramid_request, application_instance):
         pyramid_request.lti_params = {"custom_canvas_course_id": "course_id"}
 
         product = get_product_from_request(pyramid_request)
 
         # Pylint doesn't know we patched this
         # pylint: disable=no-member
-        Canvas.from_request.assert_called_once_with(pyramid_request)
+        Canvas.from_request.assert_called_once_with(
+            pyramid_request, application_instance.settings
+        )
         assert product == Canvas.from_request.return_value
         assert product.family == Product.Family.CANVAS
 
@@ -80,7 +90,9 @@ class TestGetProductFromRequest:
 
         product = get_product_from_request(pyramid_request)
 
-        class_.from_request.assert_called_once_with(pyramid_request)
+        class_.from_request.assert_called_once_with(
+            pyramid_request, application_instance.settings
+        )
         assert product == class_.from_request.return_value
         assert product.family == family
 
