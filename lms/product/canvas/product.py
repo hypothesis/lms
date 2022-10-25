2c8d5981e1933e5da2254a6d4e8e918476bd3f0a
Marcos Prieto <marcospri@gmail.com>
2022-10-25 10:42:00 +0200

    Expose api end points at the product level

--- lms/product/canvas/product.py
+++ lms/product/canvas/product.py
@@ -13,6 +13,7 @@ class Canvas(Product):
     route: Routes = Routes(
         oauth2_authorize="canvas_api.oauth.authorize",
         oauth2_refresh="canvas_api.oauth.refresh",
+        list_group_sets="canvas_api.courses.group_sets.list",
     )
 
     plugin_config: PluginConfig = PluginConfig(grouping_service=CanvasGroupingPlugin)
