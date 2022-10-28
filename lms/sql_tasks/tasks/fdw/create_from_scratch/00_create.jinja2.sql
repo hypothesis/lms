2b1ee51bb1d81902692be47380672e30b38c3e86
Marcos Prieto <marcospri@gmail.com>
2022-10-28 12:59:51 +0200

    Test FDW creation and GHA password masking

---
+++ lms/sql_tasks/tasks/fdw/create_from_scratch/00_create.jinja2.sql
@@ -0,0 +1,15 @@
+DROP SERVER IF EXISTS {{h_fdw_server_name}} CASCADE;
+CREATE SERVER {{h_fdw_server_name}} FOREIGN DATA WRAPPER postgres_fdw 
+    OPTIONS (host '{{h_fdw_host}}', port '{{h_fdw_port}}', dbname '{{h_fdw_dbname}}');
+
+
+DROP USER MAPPING IF EXISTS FOR {{db_user}} SERVER {{h_fdw_server_name}};
+CREATE USER MAPPING IF NOT EXISTS FOR {{db_user}} 
+    SERVER {{h_fdw_server_name}}
+    OPTIONS (user '{{h_fdw_user}}', password '{{h_fdw_password}}');
+
+DROP SCHEMA IF EXISTS h CASCADE;
+-- Keep foreign tables on its own schema
+CREATE SCHEMA h;
+
+IMPORT FOREIGN SCHEMA "public" LIMIT TO (annotation) FROM SERVER {{h_fdw_server_name}} INTO h;
