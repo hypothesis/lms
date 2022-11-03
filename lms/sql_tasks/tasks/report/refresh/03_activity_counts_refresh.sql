a66fb8a26c41fdf800ec0a965c8350c5db066fe1
Jon Betts <jon.betts@hypothes.is>
2022-11-03 18:51:58 +0000

    Add user activity and calculate teacher annotations from it

--- lms/sql_tasks/tasks/report/refresh/03_activity_counts_refresh.sql
+++ lms/sql_tasks/tasks/report/refresh/03_activity_counts_refresh.sql
@@ -1,5 +1,8 @@
 REFRESH MATERIALIZED VIEW CONCURRENTLY report.group_activity;
 ANALYSE report.group_activity;
 
+REFRESH MATERIALIZED VIEW CONCURRENTLY report.user_activity;
+ANALYSE report.organization_activity;
+
 REFRESH MATERIALIZED VIEW CONCURRENTLY report.organization_activity;
 ANALYSE report.organization_activity;
