917a314b5da5e9c584e0db5f7e6b089b72381e14
Jon Betts <jon.betts@hypothes.is>
2022-11-08 15:17:35 +0000

    Expose the raw timestamp to allow normal date operations on it

--- lms/sql_tasks/tasks/report/create_from_scratch/04_activity_counts/02_organization_activity/02_initial_fill.sql
+++ lms/sql_tasks/tasks/report/create_from_scratch/04_activity_counts/02_organization_activity/02_initial_fill.sql
@@ -1,4 +1,5 @@
 DROP INDEX IF EXISTS report.organization_activity_timescale_period_role_organization_id_idx;
+DROP INDEX IF EXISTS report.organization_activity_calendar_date_idx;
 
 REFRESH MATERIALIZED VIEW report.organization_activity;
 
@@ -6,3 +7,4 @@ ANALYSE report.organization_activity;
 
 -- A unique index is mandatory for concurrent updates used in the refresh
 CREATE UNIQUE INDEX organization_activity_timescale_period_role_organization_id_idx ON report.organization_activity (timescale, period, role, organization_id);
+CREATE INDEX organization_activity_calendar_date_idx ON report.organization_activity (calendar_date);
