9cbec7333025316db79f6e1a32f9f4858da0af75
Marcos Prieto <marcospri@gmail.com>
2022-11-17 11:13:09 +0100

    D2L grouping plugin

---
+++ tests/unit/lms/product/d2l/_plugin/grouping_test.py
@@ -0,0 +1,122 @@
+from unittest.mock import Mock, sentinel
+
+import pytest
+
+from lms.models import Grouping
+from lms.product.d2l._plugin.grouping import D2LGroupingPlugin, ErrorCodes
+from lms.product.plugin.grouping_service import GroupError
+from lms.services.exceptions import ExternalRequestError
+from tests import factories
+
+
+class TestD2LGroupingPlugin:
+    def test_get_groups_for_learner(
+        self, d2l_api_client, plugin, grouping_service, course
+    ):
+        api_groups = plugin.get_groups_for_learner(
+            grouping_service, course, sentinel.group_set_id
+        )
+
+        d2l_api_client.group_set_groups.assert_called_once_with(
+            course.lms_id, sentinel.group_set_id, sentinel.api_user_id
+        )
+        assert api_groups == d2l_api_client.group_set_groups.return_value
+
+    def test_get_groups_for_learner_when_no_groups(
+        self, d2l_api_client, plugin, grouping_service, course
+    ):
+        d2l_api_client.group_set_groups.return_value = []
+
+        with pytest.raises(GroupError) as err:
+            plugin.get_groups_for_learner(
+                grouping_service, course, sentinel.group_set_id
+            )
+        assert err.value.error_code == ErrorCodes.STUDENT_NOT_IN_GROUP
+
+    def test_get_groups_for_grading(self, plugin, grouping_service, course):
+        group_set_id = 100
+        api_groups = plugin.get_groups_for_grading(
+            grouping_service, course, group_set_id, sentinel.grading_student_id
+        )
+
+        grouping_service.get_course_groupings_for_user.assert_called_once_with(
+            course,
+            sentinel.grading_student_id,
+            type_=Grouping.Type.D2L_GROUP,
+            group_set_id=group_set_id,
+        )
+        assert api_groups == grouping_service.get_course_groupings_for_user.return_value
+
+    def test_get_groups_for_instructor(
+        self, d2l_api_client, plugin, grouping_service, course
+    ):
+        api_groups = plugin.get_groups_for_instructor(
+            grouping_service, course, sentinel.group_set_id
+        )
+
+        d2l_api_client.group_set_groups.assert_called_once_with(
+            course.lms_id, sentinel.group_set_id
+        )
+        assert api_groups == d2l_api_client.group_set_groups.return_value
+
+    def test_get_groups_for_instructor_raises(
+        self, d2l_api_client, plugin, grouping_service, course
+    ):
+        d2l_api_client.group_set_groups.side_effect = ExternalRequestError(
+            response=Mock(status_code=500)
+        )
+
+        with pytest.raises(ExternalRequestError):
+            plugin.get_groups_for_instructor(
+                grouping_service, course, sentinel.group_set_id
+            )
+
+    def test_get_groups_for_instructor_group_set_not_found(
+        self, d2l_api_client, plugin, grouping_service, course
+    ):
+        d2l_api_client.group_set_groups.side_effect = ExternalRequestError(
+            response=Mock(status_code=404)
+        )
+
+        with pytest.raises(GroupError) as err:
+            plugin.get_groups_for_instructor(
+                grouping_service, course, sentinel.group_set_id
+            )
+        assert err.value.error_code == ErrorCodes.GROUP_SET_NOT_FOUND
+
+    def test_get_groups_for_instructor_group_set_empty(
+        self, d2l_api_client, plugin, grouping_service, course
+    ):
+        d2l_api_client.group_set_groups.return_value = []
+
+        with pytest.raises(GroupError) as err:
+            plugin.get_groups_for_instructor(
+                grouping_service, course, sentinel.group_set_id
+            )
+        assert err.value.error_code == ErrorCodes.GROUP_SET_EMPTY
+
+    @pytest.mark.parametrize(
+        "user_id,api_user_id",
+        [
+            ("API-USER-ID", "API-USER-ID"),
+            ("USERID_API-USER-ID", "API-USER-ID"),
+            ("USERID_API-USER-ID", "API-USER-ID"),
+        ],
+    )
+    def test__get_api_user_id(self, user_id, api_user_id):
+        # pylint: disable=protected-access
+        assert D2LGroupingPlugin._get_api_user_id(user_id) == api_user_id
+
+    def test_factory(self, pyramid_request, d2l_api_client):
+        plugin = D2LGroupingPlugin.factory(sentinel.context, pyramid_request)
+        assert isinstance(plugin, D2LGroupingPlugin)
+        # pylint: disable=protected-access
+        assert plugin._d2l_api == d2l_api_client
+
+    @pytest.fixture
+    def plugin(self, d2l_api_client):
+        return D2LGroupingPlugin(d2l_api_client, sentinel.api_user_id)
+
+    @pytest.fixture
+    def course(self):
+        return factories.Course()
