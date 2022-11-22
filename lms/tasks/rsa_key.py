770dc7783d678af0e8927313756e4a57857f3ff0
Marcos Prieto <marcospri@gmail.com>
2022-11-22 12:31:57 +0100

    Allow disabling key rotation task with a new setting
    `DISABLE_KEY_ROTATION` allows disabling RSA key rotation in one
    environment.
    
    This is useful in QA, where we could disable key rotation and point
    external servers to the QA environment and share the same keys in the
    local environments.

--- lms/tasks/rsa_key.py
+++ lms/tasks/rsa_key.py
@@ -1,5 +1,9 @@
+import logging
+
 from lms.services import RSAKeyService
 from lms.tasks.celery import app
+
+LOG = logging.getLogger(__name__)
 
 TARGET_KEYS = 5
 
@@ -9,6 +13,10 @@ def rotate_keys():
     """Periodically (based on h-periodic) rotate RSA keys."""
 
     with app.request_context() as request:  # pylint: disable=no-member
+        if request.registry.settings["disable_key_rotation"]:
+            LOG.info("RSA Key rotation is disabled")
+            return
+
         with request.tm:
             # Using a hardcoded value for target keys
             # and relying on the service's defaults for max_age and max_expired_age
