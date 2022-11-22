694ef51bf8b12fe6d36ef31f59d6ef41b5d23730
Jon Betts <jon.betts@hypothes.is>
2022-11-22 14:01:45 +0000

    Limit the FDW permissions and ensure old permissions are removed

--- lms/sql_tasks/tasks/report/create_from_scratch/05_grant_permissions/01_grant_schema.jinja2.sql
+++ lms/sql_tasks/tasks/report/create_from_scratch/05_grant_permissions/01_grant_schema.jinja2.sql
@@ -1,11 +1,22 @@
--- The public schema already exists but
--- we need to grant usage to the user will map via fdw.
+-- The public schema already exists but we need to grant usage to the user we
+-- will map via FDW.
+
 {% for fdw_user in fdw_users %}
-GRANT USAGE ON SCHEMA public TO "{{fdw_user}}";
-GRANT SELECT ON public.organization TO "{{fdw_user}}";
+    -- Permissions exist independently of the schema, so dropping the schema
+    -- does not revoke permissions. We need to remove the existing permissions
+    -- otherwise removing something the list below does not remove the
+    -- permission.
+    REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM "{{fdw_user}}";
+
+    GRANT USAGE ON SCHEMA public TO "{{fdw_user}}";
+
+    GRANT SELECT ON public.organization TO "{{fdw_user}}";
 {% endfor %}
 
 {% for fdw_user in fdw_users %}
-GRANT USAGE ON SCHEMA report TO "{{fdw_user}}";
-GRANT SELECT ON ALL TABLES IN SCHEMA report TO "{{fdw_user}}";
+    REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA report FROM "{{fdw_user}}";
+
+    GRANT USAGE ON SCHEMA report TO "{{fdw_user}}";
+
+    GRANT SELECT ON report.organization_activity TO "{{fdw_user}}";
 {% endfor %}
