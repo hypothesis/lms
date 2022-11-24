97a0a35771023eb7f938a7b4f0ce069df89ce6c2
Marcos Prieto <marcospri@gmail.com>
2022-11-24 11:15:48 +0100

    Use data-task library

--- bin/run_sql_task.py
+++ bin/run_sql_task.py
@@ -8,12 +8,13 @@ reporting.
 
 from argparse import ArgumentParser
 
+import data_tasks
 import importlib_resources
+from data_tasks.python_script import PythonScript
 from psycopg2.extensions import parse_dsn
 from pyramid.paster import bootstrap
 
 from lms.models import Regions
-from lms.sql_tasks.sql_script import SQLScript
 
 TASK_ROOT = importlib_resources.files("lms.sql_tasks") / "tasks"
 
@@ -29,6 +30,21 @@ parser.add_argument(
 
 parser.add_argument("-t", "--task", required=True, help="The SQL task name to run")
 
+parser.add_argument(
+    "--no-python",
+    action="store_const",
+    default=False,
+    const=True,
+    help="Skip Python executables",
+)
+parser.add_argument(
+    "--dry-run",
+    action="store_const",
+    default=False,
+    const=True,
+    help="Run through the task without executing anything for real",
+)
+
 
 def main():
     args = parser.parse_args()
@@ -39,7 +55,7 @@ def main():
 
         Regions.set_region(settings["h_authority"])
 
-        scripts = SQLScript.from_dir(
+        scripts = data_tasks.from_dir(
             task_dir=TASK_ROOT / args.task,
             template_vars={
                 "db_user": parse_dsn(settings["database_url"].strip())["user"],
@@ -53,11 +69,15 @@ def main():
         with request.db.bind.connect() as connection:
             with connection.begin():
                 for script in scripts:
-                    print(f"Executing: {script.path}")
+                    if args.no_python and isinstance(script, PythonScript):
+                        print(f"Skipping: {script}")
+                        continue
 
-                    for query in script.queries:
-                        query.execute(connection)
-                        print(query.dump(indent="    ") + "\n")
+                    for step in script.execute(connection, dry_run=args.dry_run):
+                        if args.dry_run:
+                            print("Dry run!")
+
+                        print(step.dump(indent="    ") + "\n")
 
 
 if __name__ == "__main__":
