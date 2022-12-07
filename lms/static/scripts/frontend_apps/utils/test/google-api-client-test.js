2fc1c652272b1f955e34b23904097da2740fb060
Robert Knight <robertknight@gmail.com>
2022-12-07 16:14:48 +0000

    Migrate to Google Identity Services JS client library
    Migrate from the "gapi.auth2" module to the Google Identity Services
    client library for getting an access token for use with Google Drive.
    
    This migration fixes a warning about use of deprecated libraries when
    clicking the "Select PDF from Google Drive" option in the assignment
    picker:
    
    ```
    Your client application uses libraries for user authentication or authorization that will soon be deprecated. See the [Migration Guide](https://developers.google.com/identity/gsi/web/guides/gis-migration) for more information.
    ```
    
    Hopefully this will also be a step towards resolving
    https://github.com/hypothesis/lms/issues/4696.
    
    See https://developers.google.com/identity/oauth2/web/guides/migration-to-gis.

--- lms/static/scripts/frontend_apps/utils/test/google-api-client-test.js
+++ lms/static/scripts/frontend_apps/utils/test/google-api-client-test.js
@@ -1,6 +1,9 @@
-import { loadLibraries } from '../google-api-client';
+import {
+  loadLibraries,
+  loadIdentityServicesLibrary,
+} from '../google-api-client';
 
-describe('loadLibraries', () => {
+describe('google-api-client', () => {
   let dummyScript;
 
   beforeEach(() => {
@@ -14,65 +17,111 @@ describe('loadLibraries', () => {
   afterEach(() => {
     document.createElement.restore();
     dummyScript.remove();
+
+    // Unset globals that Google API scripts would create.
+    delete window.gapi;
+    delete window.google;
   });
 
-  it('loads API script', async () => {
-    // Call `loadLibraries` before `window.gapi` is set. This will cause
-    // it to load the API script first.
-    const libs = loadLibraries(['test']);
-    window.gapi = {
-      load: sinon.stub().callsFake((libs, { callback }) => {
-        callback();
-      }),
-    };
-    // Simulate API script load completing.
-    dummyScript.dispatchEvent(new Event('load'));
-    await libs;
+  describe('loadIdentityServicesLibrary', () => {
+    it('loads API script', async () => {
+      const libPromise = loadIdentityServicesLibrary();
+
+      // Simulate script load completing.
+      window.google = { accounts: {} };
+      dummyScript.dispatchEvent(new Event('load'));
+
+      const lib = await libPromise;
+
+      assert.equal(dummyScript.src, 'https://accounts.google.com/gsi/client');
+      assert.equal(lib, window.google.accounts);
+    });
+
+    it('does not load script if already loaded', async () => {
+      window.google = { accounts: {} };
+      const lib = await loadIdentityServicesLibrary();
+      assert.notCalled(document.createElement);
+      assert.equal(lib, window.google.accounts);
+    });
+
+    it('rejects if API script fails to load', async () => {
+      const lib = loadIdentityServicesLibrary();
+
+      // Simulate script failing to load.
+      dummyScript.onerror();
+
+      let err;
+      try {
+        await lib;
+      } catch (e) {
+        err = e;
+      }
+      assert.equal(
+        err.message,
+        'Failed to load Google Identity Services client'
+      );
+    });
   });
 
-  it('loads requested Google client libraries', async () => {
-    // Set `window.gapi` before calling `loadLibraries` as if API script had
-    // already been loaded.
-    window.gapi = {
-      load: sinon.stub().callsFake((libs, { callback }) => {
-        callback();
-      }),
-    };
-    await loadLibraries(['one', 'two']);
-    assert.calledWith(window.gapi.load, 'one:two');
-  });
-
-  it('rejects if API script fails to load', async () => {
-    const libs = loadLibraries(['one', 'two']);
-
-    // Simulate api.js failing to load.
-    dummyScript.onerror();
-
-    let err;
-    try {
+  describe('loadLibraries', () => {
+    it('loads API script', async () => {
+      // Call `loadLibraries` before `window.gapi` is set. This will cause
+      // it to load the API script first.
+      const libs = loadLibraries(['test']);
+      window.gapi = {
+        load: sinon.stub().callsFake((libs, { callback }) => {
+          callback();
+        }),
+      };
+      // Simulate API script load completing.
+      dummyScript.dispatchEvent(new Event('load'));
       await libs;
-    } catch (e) {
-      err = e;
-    }
-    assert.equal(err.message, 'Failed to load Google API client');
-  });
+    });
 
-  it('rejects if loading requested libraries fails', async () => {
-    window.gapi = {
-      load: sinon.stub().callsFake((libs, { onerror }) => {
-        onerror(new Error('Loading failed'));
-      }),
-    };
+    it('loads requested Google client libraries', async () => {
+      // Set `window.gapi` before calling `loadLibraries` as if API script had
+      // already been loaded.
+      window.gapi = {
+        load: sinon.stub().callsFake((libs, { callback }) => {
+          callback();
+        }),
+      };
+      await loadLibraries(['one', 'two']);
+      assert.calledWith(window.gapi.load, 'one:two');
+    });
 
-    const libs = loadLibraries(['one', 'two']);
-    dummyScript.dispatchEvent(new Event('load'));
-    let err;
-    try {
-      await libs;
-    } catch (e) {
-      err = e;
-    }
+    it('rejects if API script fails to load', async () => {
+      const libs = loadLibraries(['one', 'two']);
 
-    assert.equal(err.message, 'Loading failed');
+      // Simulate api.js failing to load.
+      dummyScript.onerror();
+
+      let err;
+      try {
+        await libs;
+      } catch (e) {
+        err = e;
+      }
+      assert.equal(err.message, 'Failed to load Google API client');
+    });
+
+    it('rejects if loading requested libraries fails', async () => {
+      window.gapi = {
+        load: sinon.stub().callsFake((libs, { onerror }) => {
+          onerror(new Error('Loading failed'));
+        }),
+      };
+
+      const libs = loadLibraries(['one', 'two']);
+      dummyScript.dispatchEvent(new Event('load'));
+      let err;
+      try {
+        await libs;
+      } catch (e) {
+        err = e;
+      }
+
+      assert.equal(err.message, 'Loading failed');
+    });
   });
 });
