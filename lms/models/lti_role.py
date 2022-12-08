adbd98aeedc1c9571d5078c9cd70971ec2c3f998
Marcos Prieto <marcospri@gmail.com>
2022-12-08 17:29:08 +0100

    Simplify role parsing

--- lms/models/lti_role.py
+++ lms/models/lti_role.py
@@ -99,98 +99,61 @@ class _RoleParser:
 
     @classmethod
     def parse_role(cls, role) -> RoleType:
-        if role := cls._parse_v13_role(role) or cls._parse_v11_role(role):
+        if role := cls._parse_v13_role(role):
             return role
 
         # If we can't match any, pick the most restrictive
         LOG.debug("Can't find role type for %s", role)
         return RoleType.LEARNER
 
-    _V11_INSTRUCTOR_STRINGS = (
-        "instructor",
-        "teachingassistant",
-        "contentdeveloper",
-        "faculty",
-        "mentor",
-        "staff",
-    )
-
-    @classmethod
-    def _parse_v11_role(cls, role) -> RoleType:
-        role = role.lower()
-
-        for string in cls._V11_INSTRUCTOR_STRINGS:
-            if string in role:
-                return RoleType.INSTRUCTOR
-
-        if "learner" in role:
-            return RoleType.LEARNER
-
-        if "admin" in role:
-            return RoleType.ADMIN
-
-        return None
-
-    _V13_PREFIX = "http://purl.imsglobal.org/vocab/lis/v2"
     _V13_ROLE_MAPPINGS = {
-        # https://www.imsglobal.org/spec/lti/v1p3/#lis-vocabulary-for-context-roles
-        # A.2.1 LIS vocabulary for system roles
-        # Core system roles
-        f"{_V13_PREFIX}/system/person#Administrator": RoleType.ADMIN,
-        f"{_V13_PREFIX}/system/person#None": RoleType.LEARNER,
-        # Non‑core system roles
-        f"{_V13_PREFIX}/system/person#AccountAdmin": RoleType.ADMIN,
-        f"{_V13_PREFIX}/system/person#Creator": RoleType.INSTRUCTOR,
-        f"{_V13_PREFIX}/system/person#SysAdmin": RoleType.ADMIN,
-        f"{_V13_PREFIX}/system/person#SysSupport": RoleType.ADMIN,
-        f"{_V13_PREFIX}/system/person#User": RoleType.LEARNER,
-        # A.2.2 LIS vocabulary for institution roles
-        # Core institution roles
-        f"{_V13_PREFIX}/institution/person#Administrator": RoleType.ADMIN,
-        f"{_V13_PREFIX}/institution/person#Faculty": RoleType.INSTRUCTOR,
-        f"{_V13_PREFIX}/institution/person#Guest": RoleType.LEARNER,
-        f"{_V13_PREFIX}/institution/person#None": RoleType.LEARNER,
-        f"{_V13_PREFIX}/institution/person#Other": RoleType.LEARNER,
-        f"{_V13_PREFIX}/institution/person#Staff": RoleType.INSTRUCTOR,
-        f"{_V13_PREFIX}/institution/person#Student": RoleType.LEARNER,
-        # Non‑core institution roles
-        f"{_V13_PREFIX}/institution/person#Alumni": RoleType.LEARNER,
-        f"{_V13_PREFIX}/institution/person#Instructor": RoleType.INSTRUCTOR,
-        f"{_V13_PREFIX}/institution/person#Learner": RoleType.LEARNER,
-        f"{_V13_PREFIX}/institution/person#Member": RoleType.LEARNER,
-        f"{_V13_PREFIX}/institution/person#Mentor": RoleType.INSTRUCTOR,
-        f"{_V13_PREFIX}/institution/person#Observer": RoleType.LEARNER,
-        f"{_V13_PREFIX}/institution/person#ProspectiveStudent": RoleType.LEARNER,
-        # A.2.3 LIS vocabulary for context roles
-        # Core context roles
-        f"{_V13_PREFIX}/membership/Administrator": RoleType.ADMIN,
-        f"{_V13_PREFIX}/membership/ContentDeveloper": RoleType.INSTRUCTOR,
-        f"{_V13_PREFIX}/membership/Instructor": RoleType.INSTRUCTOR,
+        "Administrator": RoleType.ADMIN,
+        "ContentDeveloper": RoleType.INSTRUCTOR,
+        "Instructor": RoleType.INSTRUCTOR,
+        "Learner": RoleType.LEARNER,
         # Look out for this weirdo!
-        f"{_V13_PREFIX}/membership/Learner#Instructor": RoleType.INSTRUCTOR,
-        f"{_V13_PREFIX}/membership/Learner": RoleType.LEARNER,
-        f"{_V13_PREFIX}/membership/Mentor": RoleType.INSTRUCTOR,
-        # Non‑core context roles
-        f"{_V13_PREFIX}/membership/Manager": RoleType.INSTRUCTOR,
-        f"{_V13_PREFIX}/membership/Member": RoleType.LEARNER,
-        f"{_V13_PREFIX}/membership/Office": RoleType.INSTRUCTOR,
+        "Learner#Instructor": RoleType.INSTRUCTOR,
+        "Manager": RoleType.INSTRUCTOR,
+        "Member": RoleType.LEARNER,
+        "Mentor": RoleType.INSTRUCTOR,
+        "Office": RoleType.INSTRUCTOR,
+        "Grader": RoleType.INSTRUCTOR,
+        "Staff": RoleType.INSTRUCTOR,
+        "Faculty": RoleType.INSTRUCTOR,
+        "SysAdmin": RoleType.ADMIN,
+        "TeachingAssistant": RoleType.INSTRUCTOR,
+        "person#AccountAdmin": RoleType.ADMIN,
+        "person#Administrator": RoleType.ADMIN,
+        "person#Alumni": RoleType.LEARNER,
+        "person#Creator": RoleType.INSTRUCTOR,
+        "person#Faculty": RoleType.INSTRUCTOR,
+        "person#Guest": RoleType.LEARNER,
+        "person#Instructor": RoleType.INSTRUCTOR,
+        "person#Learner": RoleType.LEARNER,
+        "person#Member": RoleType.LEARNER,
+        "person#Mentor": RoleType.INSTRUCTOR,
+        "person#None": RoleType.LEARNER,
+        "person#Observer": RoleType.LEARNER,
+        "person#Other": RoleType.LEARNER,
+        "person#ProspectiveStudent": RoleType.LEARNER,
+        "person#Staff": RoleType.INSTRUCTOR,
+        "person#Student": RoleType.LEARNER,
+        "person#SysAdmin": RoleType.ADMIN,
+        "person#SysSupport": RoleType.ADMIN,
+        "person#User": RoleType.LEARNER,
     }
 
     @classmethod
     def _parse_v13_role(cls, role):
         role = role.strip()
+        role_mapping = dict(
+            sorted(
+                cls._V13_ROLE_MAPPINGS.items(), key=lambda x: len(x[0]), reverse=True
+            )
+        )
 
-        # Save some time if we aren't going to match
-        if not role.startswith(cls._V13_PREFIX):
-            return None
-
-        # Try a quick match
-        if type_ := cls._V13_ROLE_MAPPINGS.get(role):
-            return type_
-
-        # Do a thorough prefix match
-        for prefix, type_ in cls._V13_ROLE_MAPPINGS.items():
-            if role.startswith(prefix):
+        for suffix, type_ in role_mapping.items():
+            if role.endswith(suffix):
                 return type_
 
         return None
