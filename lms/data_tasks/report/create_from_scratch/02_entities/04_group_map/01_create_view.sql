baf1aa8c8faddea6b530d52b33250ab0a9323e0f
Jon Betts <jon.betts@hypothes.is>
2022-12-12 14:53:53 +0000

    WIP

--- lms/data_tasks/report/create_from_scratch/02_entities/04_group_map/01_create_view.sql
+++ lms/data_tasks/report/create_from_scratch/02_entities/04_group_map/01_create_view.sql
@@ -7,6 +7,7 @@ CREATE MATERIALIZED VIEW report.group_map AS (
         all_groups AS (
             SELECT
                 grouping.id AS lms_grouping_id,
+                grouping.parent_id AS lms_grouping_parent_id,
                 -- Coalesce the ids in case the left or right side is missing
                 COALESCE(
                     grouping.authority_provided_id,
@@ -22,16 +23,31 @@ CREATE MATERIALIZED VIEW report.group_map AS (
             FULL OUTER JOIN group_info ON
                 group_info.authority_provided_id = grouping.authority_provided_id
                 AND group_info.application_instance_id = grouping.application_instance_id
+        ),
+
+        group_map AS (
+            SELECT
+                lms_grouping_id,
+                lms_grouping_parent_id,
+                groups.id AS group_id,
+                application_instances.organization_id
+            FROM all_groups
+            JOIN report.groups ON
+                all_groups.authority_provided_id = groups.authority_provided_id
+            JOIN application_instances ON
+                all_groups.application_instance_id = application_instances.id
+            ORDER BY lms_grouping_id, group_id
         )
 
     SELECT
-        lms_grouping_id,
-        groups.id AS group_id,
-        application_instances.organization_id
-    FROM all_groups
-    JOIN report.groups ON
-        all_groups.authority_provided_id = groups.authority_provided_id
-    JOIN application_instances ON
-        all_groups.application_instance_id = application_instances.id
-    ORDER BY lms_grouping_id, group_id
+        group_map.lms_grouping_id,
+        group_map.lms_grouping_parent_id,
+        group_map.group_id,
+        parent_group_map.group_id AS group_parent_id,
+        group_map.organization_id
+    FROM group_map
+    LEFT OUTER JOIN group_map AS parent_group_map ON
+        parent_group_map.lms_grouping_id = group_map.lms_grouping_parent_id
+        AND parent_group_map.lms_grouping_id is NOT NULL
+    ORDER BY lms_grouping_id
 ) WITH NO DATA;
