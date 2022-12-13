bd4a9a4ba3cca7a955d77b4e8119061a455e3ac8
Marcos Prieto <marcospri@gmail.com>
2022-12-13 11:36:19 +0100

    Simplify BearerTokenSchema
    Follow the behaviour of OAuthCallbackSchema leaving the responsibility
    of serialization to LTIUser itself.

--- lms/validation/authentication/__init__.py
+++ lms/validation/authentication/__init__.py
@@ -1,10 +1,7 @@
 from lms.validation.authentication._bearer_token import BearerTokenSchema
 from lms.validation.authentication._exceptions import (
-    ExpiredSessionTokenError,
     ExpiredStateParamError,
-    InvalidSessionTokenError,
     InvalidStateParamError,
-    MissingSessionTokenError,
     MissingStateParamError,
 )
 from lms.validation.authentication._lti import LTI11AuthSchema, LTI13AuthSchema
--- lms/validation/authentication/_exceptions.py
+++ lms/validation/authentication/_exceptions.py
@@ -4,25 +4,10 @@ from lms.validation._exceptions import ValidationError
 # ValidationError has a large hierarchy, but we need to inherit from it
 
 __all__ = [
-    "ExpiredSessionTokenError",
-    "MissingSessionTokenError",
-    "InvalidSessionTokenError",
     "MissingStateParamError",
     "ExpiredStateParamError",
     "InvalidStateParamError",
 ]
-
-
-class ExpiredSessionTokenError(ValidationError):
-    """Raised when the request has an expired session token."""
-
-
-class MissingSessionTokenError(ValidationError):
-    """Raised when the request has no session token."""
-
-
-class InvalidSessionTokenError(ValidationError):
-    """Raised when the request has an invalid session token."""
 
 
 class MissingStateParamError(ValidationError):
