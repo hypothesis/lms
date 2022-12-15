c94e0fdf6c2433adb24330795b29dafcc3f2cc68
Jon Betts <jon.betts@hypothes.is>
2022-12-15 20:25:55 +0000

    Remove the non-bubbled group counts now we don't use them

--- lms/data_tasks/report/create_from_scratch/04_activity_counts/01_groups/03_group_counts/02_initial_fill.sql
+++
@@ -1,8 +0,0 @@
-DROP INDEX IF EXISTS report.group_counts_group_id_idx;
-
-REFRESH MATERIALIZED VIEW report.group_counts;
-
-ANALYSE report.group_counts;
-
--- A unique index is mandatory for concurrent updates used in the refresh
-CREATE UNIQUE INDEX group_counts_group_id_idx ON report.group_counts (group_id);
