b3d8af84ada66bf20e2743231f6b0a9160d08dc9
Marcos Prieto <marcospri@gmail.com>
2022-12-19 10:35:48 +0100

    Add email and name field to User model change

--- tests/unit/lms/services/user_test.py
+++ tests/unit/lms/services/user_test.py
@@ -24,19 +24,45 @@ class TestUserService:
                 "user_id": lti_user.user_id,
                 "roles": lti_user.roles,
                 "h_userid": lti_user.h_user.userid("authority.example.com"),
+                "email": lti_user.email,
+                "display_name": lti_user.display_name,
             }
         )
         assert saved_user == user
 
-    def test_upsert_user_with_an_existing_user(
-        self, service, user, lti_user, db_session
+    def test_upsert_user_doesnt_save_personal_details_for_students(
+        self, service, lti_user, db_session
     ):
+        lti_user = lti_user._replace(roles="Student")
+
+        service.upsert_user(lti_user)
+
+        saved_user = db_session.query(User).order_by(User.id.desc()).first()
+        assert saved_user.roles == lti_user.roles
+        assert not saved_user.email
+        assert not saved_user.display_name
+
+    @pytest.mark.usefixtures("user")
+    def test_upsert_user_with_an_existing_user(self, service, lti_user, db_session):
         user = service.upsert_user(lti_user)
 
         saved_user = db_session.query(User).get(user.id)
         assert saved_user.id == user.id
         assert saved_user.roles == lti_user.roles
         assert user == saved_user
+
+    @pytest.mark.usefixtures("user")
+    def test_upsert_user_doesnt_save_personal_details_for_existing_students(
+        self, service, lti_user, db_session
+    ):
+        lti_user = lti_user._replace(roles="Student")
+
+        service.upsert_user(lti_user)
+
+        saved_user = db_session.query(User).order_by(User.id.desc()).first()
+        assert saved_user.roles == lti_user.roles
+        assert not saved_user.email
+        assert not saved_user.display_name
 
     def test_get(self, user, service):
         db_user = service.get(user.application_instance, user.user_id)
