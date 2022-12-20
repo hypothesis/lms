208176da42ca792646ebb2b2c617866cdd58dad2
Jon Betts <jon.betts@hypothes.is>
2022-12-20 19:00:43 +0000

    Bubble down launch counts instead of up
    Courses have launch counts, but individual sub-groups tend not to, so
    we want to bubble down launch counts instead of up.

--- lms/data_tasks/report/create_from_scratch/04_activity_counts/01_groups/02_group_bubbled_activity/01_create_view.sql
+++ lms/data_tasks/report/create_from_scratch/04_activity_counts/01_groups/02_group_bubbled_activity/01_create_view.sql
@@ -5,17 +5,44 @@ DROP VIEW IF EXISTS report.group_bubbled_activity CASCADE;
 
 CREATE MATERIALIZED VIEW report.group_bubbled_activity AS (
     SELECT
-        created_week,
-        group_to_group.parent_id AS group_id,
-        SUM(annotation_count) as annotation_count,
-        SUM(annotation_shared_count) as annotation_shared_count,
-        SUM(annotation_replies_count) as annotation_replies_count,
-        SUM(launch_count) as launch_count
-    FROM report.group_to_group
-    -- The join on group to group without a relation includes self,
-    -- so the child_id here is all children and ourselves
-    JOIN report.group_activity ON
-        group_activity.group_id = group_to_group.child_id
-    GROUP BY created_week, group_to_group.parent_id
-    ORDER BY created_week, group_to_group.parent_id
+        bubble_up.created_week,
+        bubble_up.group_id,
+        bubble_up.annotation_count,
+        bubble_up.annotation_shared_count,
+        bubble_up.annotation_replies_count,
+        bubble_down.launch_count
+
+    -- Numbers we want to move from child to parent
+    FROM (
+        SELECT
+            created_week,
+            group_to_group.parent_id AS group_id,
+            SUM(annotation_count) as annotation_count,
+            SUM(annotation_shared_count) as annotation_shared_count,
+            SUM(annotation_replies_count) as annotation_replies_count
+        FROM report.group_to_group
+        -- The join on group to group without a relation includes self,
+        -- so the child_id here is all children and ourselves
+        JOIN report.group_activity ON
+            group_activity.group_id = group_to_group.child_id
+        GROUP BY created_week, group_to_group.parent_id
+    ) as bubble_up
+
+     -- Numbers we want to move from parent to child
+    JOIN (
+        SELECT
+            created_week,
+            group_to_group.child_id AS group_id,
+            SUM(launch_count) as launch_count
+        FROM report.group_to_group
+        -- The join on group to group without a relation includes self,
+        -- so the parent_id here is our parent and ourselves
+        JOIN report.group_activity ON
+            group_activity.group_id = group_to_group.parent_id
+        GROUP BY created_week, group_to_group.child_id
+    ) AS bubble_down ON
+        bubble_up.created_week = bubble_down.created_week
+        AND bubble_up.group_id = bubble_down.group_id
+
+    ORDER BY bubble_up.created_week, bubble_up.group_id
 ) WITH NO DATA;
