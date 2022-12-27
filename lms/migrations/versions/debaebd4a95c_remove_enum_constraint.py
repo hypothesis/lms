9cb4e09ad59e5b5be5d856a83e42aeaaae1ec5c5
Marcos Prieto <marcospri@gmail.com>
2022-12-27 14:27:57 +0100

    Migration to disable the check constraint on Grouping.type
    Changes to the type are cumbersome and we cover the correctness of these
    values in the tests.

---
+++ lms/migrations/versions/debaebd4a95c_remove_enum_constraint.py
@@ -0,0 +1,29 @@
+"""
+Remove enum DB constraint.
+
+Revision ID: debaebd4a95c
+Revises: 0b7391f5b1e3
+Create Date: 2022-12-27 14:19:35.278944
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "debaebd4a95c"
+down_revision = "0b7391f5b1e3"
+
+constraint_name = "grouping_type_must_be_a_valid_value"
+table_name = "grouping"
+
+
+def upgrade():
+    op.drop_constraint(constraint_name, table_name=table_name, type_="check")
+
+
+def downgrade():
+    op.create_check_constraint(
+        constraint_name,
+        table_name=table_name,
+        condition="type in ('course', 'canvas_section', 'canvas_group', 'blackboard_group', 'd2l_group')",
+    )
