4900f7b147eecba515e8e42b9a36d875ac4d8ccc
Marcos Prieto <marcospri@gmail.com>
2023-01-20 17:24:00 +0100

    Course copy plugin for D2L

--- lms/product/d2l/__init__.py
+++ lms/product/d2l/__init__.py
@@ -1,3 +1,4 @@
+from lms.product.d2l._plugin.course_copy import D2LCourseCopyPlugin
 from lms.product.d2l._plugin.grouping import D2LGroupingPlugin
 from lms.product.d2l._plugin.misc import D2LMiscPlugin
 from lms.product.d2l.product import D2L
@@ -7,3 +8,6 @@ def includeme(config):  # pragma: nocover
     """Register all of our plugins."""
     config.register_service_factory(D2LGroupingPlugin.factory, iface=D2LGroupingPlugin)
     config.register_service_factory(D2LMiscPlugin.factory, iface=D2LMiscPlugin)
+    config.register_service_factory(
+        D2LCourseCopyPlugin.factory, iface=D2LCourseCopyPlugin
+    )
---
+++ lms/product/d2l/_plugin/course_copy.py
@@ -0,0 +1,33 @@
+from lms.product.plugin.course_copy import CourseCopyPlugin
+from lms.services.d2l_api import D2LAPIClient
+
+
+class D2LCourseCopyPlugin(CourseCopyPlugin):
+    """
+        select application_instance_id, type, lms_id, count(*) from file where type in ('blackboard_file', 'blackboard_folder')
+    group by application_instance_id, type, lms_id
+    """
+
+    file_type = "d2l_file"
+    folder_type = "d2l_folder"
+
+    def __init__(self, db, api, file_service):
+        self._db = db
+        self._api = api
+        self._file_service = file_service
+
+    def assert_file_in_course(self, course_id, file_id):
+        """Raise if the current user can't see file_id in course_id."""
+        if self._file_service.get(file_id, type_=self.file_type).course_id != course_id:
+            raise CanvasFileNotFoundInCourse(file_id)
+
+    def _store_new_course_files(self, course_id):
+        return self._api.list_files(course_id)
+
+    @classmethod
+    def factory(cls, _context, request):
+        return cls(
+            request.db,
+            request.find_service(D2LAPIClient),
+            file_service=request.find_service(name="file"),
+        )
