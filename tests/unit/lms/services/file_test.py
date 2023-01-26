875d01b57d3948fdcfffcafa5a72587111c98032
Marcos Prieto <marcospri@gmail.com>
2023-01-26 18:34:30 +0100

    Fix class naming for FileService tests

--- tests/unit/lms/services/file_test.py
+++ tests/unit/lms/services/file_test.py
@@ -7,18 +7,18 @@ from lms.services.file import FileService, factory
 from tests import factories
 
 
-class TestGet:
-    def test_it_returns_the_matching_file_if_there_is_one(
+class TestFileService:
+    def test_get_returns_the_matching_file_if_there_is_one(
         self, application_instance, svc
     ):
         file_ = factories.File(application_instance=application_instance)
 
         assert svc.get(file_.lms_id, file_.type) == file_
 
-    def test_it_returns_None_if_theres_no_matching_file(self, svc):
+    def test_get_returns_None_if_theres_no_matching_file(self, svc):
         assert not svc.get("unknown_file_id", "canvas_file")
 
-    def test_it_doesnt_return_matching_files_from_other_application_instances(
+    def test_get_doesnt_return_matching_files_from_other_application_instances(
         self, svc
     ):
         file_ = factories.File()
