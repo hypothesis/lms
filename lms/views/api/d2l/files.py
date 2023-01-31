2e63718de857b7b9cefff3b794d2e293f3ad997d
Marcos Prieto <marcospri@gmail.com>
2023-01-31 14:55:22 +0100

    Use the course copy plugin in D2L file views

--- lms/views/api/d2l/files.py
+++ lms/views/api/d2l/files.py
@@ -4,6 +4,7 @@ from pyramid.view import view_config
 
 from lms.security import Permissions
 from lms.services.d2l_api import D2LAPIClient
+from lms.services.exceptions import FileNotFoundInCourse
 from lms.views import helpers
 
 DOCUMENT_URL_REGEX = re.compile(
@@ -29,15 +30,37 @@ def list_files(_context, request):
     permission=Permissions.API,
 )
 def via_url(_context, request):
+    course_copy_plugin = request.product.plugin.course_copy
+    api_client = request.find_service(D2LAPIClient)
+
     course_id = request.matchdict["course_id"]
     document_url = request.params["document_url"]
-    file_id = DOCUMENT_URL_REGEX.search(document_url)["file_id"]
 
-    public_url = request.find_service(D2LAPIClient).public_url(course_id, file_id)
+    course = request.find_service(name="course").get_by_context_id(course_id)
+
+    file_id = course_copy_plugin.get_mapped_file_id(
+        course, DOCUMENT_URL_REGEX.search(document_url)["file_id"]
+    )
+    try:
+        if request.lti_user.is_instructor:
+            if not course_copy_plugin.is_file_in_course(course_id, file_id):
+                raise FileNotFoundInCourse("d2l_file_not_found_in_course", file_id)
+
+        public_url = api_client.public_url(course_id, file_id)
+
+    except FileNotFoundInCourse:
+        found_file = course_copy_plugin.find_matching_file_in_course(file_id, course_id)
+        if not found_file:
+            raise
+
+        # Try again to return a public URL, this time using found_file_id.
+        public_url = api_client.public_url(course_id, found_file.lms_id)
+
+        # Store a mapping so we don't have to re-search next time.
+        course_copy_plugin.set_mapped_file_id(course, file_id, found_file.lms_id)
 
     access_token = request.find_service(name="oauth2_token").get().access_token
     headers = {"Authorization": f"Bearer {access_token}"}
-
     return {
         "via_url": helpers.via_url(
             request, public_url, content_type="pdf", headers=headers
