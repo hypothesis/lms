38582cab7dc14e066b159101beb1109381076f29
Robert Knight <robertknight@gmail.com>
2023-02-03 14:17:07 +0000

    Remove new-application-instance.js
    The templates that use this JS were removed in https://github.com/hypothesis/lms/pull/4876.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -61,10 +61,6 @@ function bundleConfig(name, entryFile) {
 
 export default [
   bundleConfig('frontend_apps', 'lms/static/scripts/frontend_apps/index.js'),
-  bundleConfig(
-    'new_application_instance',
-    'lms/static/scripts/new-application-instance.js'
-  ),
   bundleConfig('browser_check', 'lms/static/scripts/browser_check/index.ts'),
   bundleConfig('ui-playground', 'lms/static/scripts/ui-playground/index.js'),
 ];
