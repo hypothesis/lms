80684754a9bf17e8f97f68e5ca88d8e78985da49
Marcos Prieto <marcospri@gmail.com>
2023-02-14 15:32:59 +0100

    Fix course copy issues for groups in Blackboard

--- lms/product/blackboard/_plugin/course_copy.py
+++ lms/product/blackboard/_plugin/course_copy.py
@@ -1,4 +1,4 @@
-from lms.product.plugin.course_copy import CourseCopyFilesHelper
+from lms.product.plugin.course_copy import CourseCopyFilesHelper, CourseCopyGroupsHelper
 
 
 class BlackboardCourseCopyPlugin:
@@ -6,9 +6,15 @@ class BlackboardCourseCopyPlugin:
 
     file_type = "blackboard_file"
 
-    def __init__(self, api, files_helper: CourseCopyFilesHelper):
+    def __init__(
+        self,
+        api,
+        files_helper: CourseCopyFilesHelper,
+        groups_helper: CourseCopyGroupsHelper,
+    ):
         self._api = api
         self._files_helper = files_helper
+        self._groups_helper = groups_helper
 
     def is_file_in_course(self, course_id, file_id):
         return self._files_helper.is_file_in_course(course_id, file_id, self.file_type)
@@ -18,9 +24,15 @@ class BlackboardCourseCopyPlugin:
             self._api.list_all_files, self.file_type, original_file_id, new_course_id
         )
 
+    def find_matching_group_set_in_course(self, course, group_set_id):
+        return self._groups_helper.find_matching_group_set_in_course(
+            course, group_set_id
+        )
+
     @classmethod
     def factory(cls, _context, request):
         return cls(
-            request.find_service(name="blackboard_api_client"),
-            request.find_service(CourseCopyFilesHelper),
+            api=request.find_service(name="blackboard_api_client"),
+            files_helper=request.find_service(CourseCopyFilesHelper),
+            groups_helper=request.find_service(CourseCopyGroupsHelper),
         )
