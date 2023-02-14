99726151f43caf17c31298dcfe6801c605380e91
Jon Betts <jon.betts@hypothes.is>
2023-02-14 19:21:24 +0000

    Expand the functional tests to cover all ai admin end-points

--- tests/functional/views/admin_test.py
+++ tests/functional/views/admin_test.py
@@ -1,5 +1,30 @@
-def test_admin_authentication_redirects_to_google(app):
-    response = app.get("/admin/instances/")
+import pytest
+
+
+@pytest.mark.parametrize(
+    "method,path",
+    (
+        # Create
+        ("get", "/admin/instance/create"),
+        ("post", "/admin/instance/create"),
+        # Downgrade
+        ("post", "/admin/instance/1234/downgrade"),
+        # Move org
+        ("post", "/admin/instance/1234/move_org"),
+        # Search
+        ("get", "/admin/instances/"),
+        ("post", "/admin/instances/"),
+        # Show
+        ("get", "/admin/instance/1234/"),
+        # Update
+        ("post", "/admin/instance/1234/"),
+        # Upgrade
+        ("get", "/admin/instance/upgrade"),
+        ("post", "/admin/instance/upgrade"),
+    ),
+)
+def test_admin_authentication_redirects_to_google(app, method, path):
+    response = getattr(app, method)(path)
 
     assert response.status_code == 302
     assert response.location.startswith("http://localhost/googleauth/login")
