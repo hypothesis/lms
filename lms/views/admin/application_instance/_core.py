c5a5870313bb60feda39155140f26bdeb2bd4d34
Jon Betts <jon.betts@hypothes.is>
2023-02-15 15:52:20 +0000

    Use the fields specified in the application settings object
    This makes the application settings object the sole source of truth
    about where these settings come from.

--- lms/views/admin/application_instance/_core.py
+++ lms/views/admin/application_instance/_core.py
@@ -1,31 +1,7 @@
 from pyramid.httpexceptions import HTTPFound, HTTPNotFound
-from pyramid.settings import asbool
 
 from lms.models import ApplicationInstance
 from lms.services import ApplicationInstanceNotFound
-
-# Helper to declare settings as secret
-AES_SECRET = object()
-APPLICATION_INSTANCE_SETTINGS = {
-    ("blackboard", "files_enabled"): asbool,
-    ("blackboard", "groups_enabled"): asbool,
-    ("canvas", "sections_enabled"): asbool,
-    ("canvas", "groups_enabled"): asbool,
-    ("desire2learn", "client_id"): str,
-    ("desire2learn", "client_secret"): AES_SECRET,
-    ("desire2learn", "groups_enabled"): asbool,
-    ("desire2learn", "files_enabled"): asbool,
-    ("desire2learn", "create_line_item"): asbool,
-    ("microsoft_onedrive", "files_enabled"): asbool,
-    ("vitalsource", "enabled"): asbool,
-    ("vitalsource", "user_lti_param"): str,
-    ("vitalsource", "user_lti_pattern"): str,
-    ("vitalsource", "api_key"): str,
-    ("vitalsource", "disable_licence_check"): asbool,
-    ("jstor", "enabled"): asbool,
-    ("jstor", "site_code"): str,
-    ("hypothesis", "notes"): str,
-}
 
 
 class BaseApplicationInstanceView:
