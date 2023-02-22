1cdf1a6a8affb7649c6eb60dc0948070fe21924b
Lyza Danger Gardner <lyza@lyza.com>
2023-02-22 15:09:56 -0500

    Convert `OAuth2RedirectErrorApp` to TSX

--- lms/static/scripts/frontend_apps/components/OAuth2RedirectErrorApp.tsx
+++ lms/static/scripts/frontend_apps/components/OAuth2RedirectErrorApp.tsx
@@ -3,13 +3,10 @@ import { Link } from '@hypothesis/frontend-shared';
 import { useConfig } from '../config';
 import ErrorModal from './ErrorModal';
 
-/** @typedef {import('../config').OAuthErrorConfig} OAuthErrorConfig */
-
-/**
- * @typedef OAuth2RedirectErrorAppProps
- * @prop {Location} [location] - Test seam
- */
-
+export type OAuth2RedirectErrorAppProps = {
+  /* Test seam */
+  location?: Location;
+};
 /**
  * Error Modal displayed when authorization with a third-party API via OAuth
  * fails.
@@ -18,13 +15,15 @@ import ErrorModal from './ErrorModal';
  *
  * @param {OAuth2RedirectErrorAppProps} props
  */
-export default function OAuth2RedirectErrorApp({ location = window.location }) {
+export default function OAuth2RedirectErrorApp({
+  location = window.location,
+}: OAuth2RedirectErrorAppProps) {
   const {
     OAuth2RedirectError: {
       authUrl = null,
       errorCode,
       errorDetails = '',
-      canvasScopes = /** @type {string[]} */ ([]),
+      canvasScopes = [],
     },
   } = useConfig(['OAuth2RedirectError']);
 
@@ -46,7 +45,7 @@ export default function OAuth2RedirectErrorApp({ location = window.location }) {
   }
 
   const retry = () => {
-    location.href = /** @type {string} */ (authUrl);
+    location.href = authUrl!;
   };
   const onRetry = authUrl ? retry : undefined;
 
