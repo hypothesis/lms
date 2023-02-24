4fa77cdd5a5755cbb85c06f368b01eb9e7507e40
Robert Knight <robertknight@gmail.com>
2023-02-24 15:54:38 +0000

    Persist frontend app mode in hash to allow Back/Forwards to work
    Enable the browser's back button to work for transitioning from editing
    an assignment to viewing it. We don't currently have a proper set of
    URLs for the different frontend app routes (eg. `/assignments/{id}` for
    viewing, `/assignments/{id}/edit` for editing) so we just use a hash.

--- lms/static/scripts/frontend_apps/components/AppRoot.tsx
+++ lms/static/scripts/frontend_apps/components/AppRoot.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'preact/hooks';
+import { useEffect, useState } from 'preact/hooks';
 
-import type { ConfigObject } from '../config';
+import type { AppMode, ConfigObject } from '../config';
 import { Config } from '../config';
 import type { ServiceMap } from '../services';
 import { Services } from '../services';
@@ -15,11 +15,47 @@ export type AppRootProps = {
   services: ServiceMap;
 };
 
+const APP_MODES: readonly AppMode[] = [
+  'basic-lti-launch',
+  'content-item-selection',
+  'error-dialog',
+  'oauth2-redirect-error',
+] as const;
+
 export default function AppRoot({ initialConfig, services }: AppRootProps) {
   const [config, setConfig] = useState(initialConfig);
   config.update = (newConfig: Partial<ConfigObject>) => {
     setConfig({ ...config, ...newConfig });
   };
+
+  // Preserve the current frontend app mode in the window hash. This allows
+  // the browser's back button to work for eg. transitioning from editing an
+  // assignment back to viewing it.
+  //
+  // It would nice nicer to use proper URLs.
+  useEffect(() => {
+    const onHashChange = (event: HashChangeEvent) => {
+      const mode = new URL(event.newURL).hash.slice(1) as AppMode;
+      if (!APP_MODES.includes(mode)) {
+        return;
+      }
+
+      setConfig(config => {
+        if (config.mode === mode) {
+          return config;
+        }
+        return { ...config, mode };
+      });
+    };
+    window.addEventListener('hashchange', onHashChange);
+    return () => {
+      window.removeEventListener('hashchange', onHashChange);
+    };
+  }, []);
+
+  useEffect(() => {
+    window.location.hash = config.mode;
+  }, [config.mode]);
 
   let root;
   switch (config.mode) {
