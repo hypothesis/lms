7def6c3d1d696a3c369d8c1959ef2c13c951c431
Lyza Danger Gardner <lyza@lyza.com>
2023-02-28 15:31:54 -0500

    Remove unused legacy icons

--- lms/static/scripts/bootstrap.js
+++ lms/static/scripts/bootstrap.js
@@ -1,18 +1,12 @@
-import { registerIcons } from '@hypothesis/frontend-shared';
 import { configure } from 'enzyme';
 import { Adapter } from 'enzyme-adapter-preact-pure';
 import 'preact/debug';
-
-import iconSet from './frontend_apps/icons';
 
 // Expose sinon assertions.
 sinon.assert.expose(assert, { prefix: null });
 
 // Configure Enzyme for UI tests.
 configure({ adapter: new Adapter() });
-
-// Register the same set of icons that is available in the app.
-registerIcons(iconSet);
 
 // Ensure that uncaught exceptions between tests result in the tests failing.
 // This works around an issue with mocha / karma-mocha, see
