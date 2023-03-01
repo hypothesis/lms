e06254d31cddbf63251609e40c2e922ea2705f72
Marcos Prieto <marcospri@gmail.com>
2023-03-01 16:06:34 +0100

    Return the response instead of json for record_result
    The spec allows the response to be empty (204 No Content) so this
    was causing issues in some LMS.
    
    Some LMS do return json all the time and others return it only for
    errors.

--- tests/unit/lms/services/lti_grading/_v13_test.py
+++ tests/unit/lms/services/lti_grading/_v13_test.py
@@ -82,7 +82,7 @@ class TestLTI13GradingService:
             },
             headers={"Content-Type": "application/vnd.ims.lis.v1.score+json"},
         )
-        assert response == ltia_http_service.request.return_value.json.return_value
+        assert response == ltia_http_service.request.return_value
 
     def test_record_result_raises_ExternalRequestError(self, svc, ltia_http_service):
         ltia_http_service.request.side_effect = ExternalRequestError(
@@ -102,7 +102,7 @@ class TestLTI13GradingService:
 
         response = svc.record_result(sentinel.user_id, sentinel.score)
 
-        assert response == {}
+        assert not response
 
     def test_create_line_item(self, svc, ltia_http_service):
         response = svc.create_line_item(
