6fe1b6861b2a4ddf7b9cfa68209f4694188f6c91
Robert Knight <robertknight@gmail.com>
2023-03-08 12:09:56 +0000

    Convert google-picker-client.js to TypeScript
     - The `google.picker.View` class that was instantiated is declared as
       an abstract class in the API docs [1] and not constructable according
       to the types in `@types/google.picker`. Instead there is a `DocsView`
       subclass which can be constructed [2].
    
     - There are partial types for `gapi.client.drive` in
       `@types/gapi.drive`, but these don't include the APIs we use, so use
       of this API is untyped for now.
    
    [1] https://developers.google.com/drive/picker/reference#view
    [2] https://developers.google.com/drive/picker/reference#docs-view

--- lms/static/scripts/frontend_apps/utils/google-picker-client.ts
+++ lms/static/scripts/frontend_apps/utils/google-picker-client.ts
@@ -13,10 +13,8 @@ export const GOOGLE_DRIVE_SCOPE = 'https://www.googleapis.com/auth/drive';
  * `custom_canvas_api_domain` LTI launch parameter on the backend, in which
  * case it may be a domain rather than a URL. If so, this should really be handled
  * on the backend.
- *
- * @param {string} origin
  */
-function addHttps(origin) {
+function addHTTPS(origin: string) {
   if (origin.indexOf('://') !== -1) {
     return origin;
   }
@@ -24,24 +22,46 @@ function addHttps(origin) {
 }
 
 /**
- * Subset of the `Document` type returned by the Google Picker.
+ * `Document` type returned by the Google Picker.
  *
  * See https://developers.google.com/picker/docs/reference#document
- *
- * @typedef PickerDocument
- * @prop {string} id
- * @prop {string} name - Filename
- * @prop {string} url
- * @prop {string} [resourceKey] - A key which is present on a subset of older
- *   Google Drive files. If present this needs to be provided to various
- *   Google Drive APIs and share links (via the `resourcekey` query param).
- *
- *   As far as we understand, this key was added to old files to guard against
- *   their IDs being guessed.
- *
- *   See https://developers.google.com/drive/api/v3/resource-keys and
- *   https://support.google.com/drive/answer/10729743.
  */
+export type PickerDocument = {
+  id: string;
+
+  /** Filename of document. */
+  name: string;
+
+  url: string;
+
+  /**
+   * A key which is present on a subset of older Google Drive files. If present
+   * this needs to be provided to various Google Drive APIs and share links (via
+   * the `resourcekey` query param).
+   *
+   * As far as we understand, this key was added to old files to guard against
+   * their IDs being guessed.
+   *
+   * See https://developers.google.com/drive/api/v3/resource-keys and
+   * https://support.google.com/drive/answer/10729743.
+   */
+  resourceKey?: string;
+};
+
+export type GooglePickerOptions = {
+  /** API key obtained from the Google API console. */
+  developerKey: string;
+
+  /** Client ID obtained from the Google API console. */
+  clientId: string;
+
+  /**
+   * The origin of the top-most page in the frame where this picker will
+   * be served. This is needed when the picker is served inside an iframe
+   * with a different origin than the top-level page
+   */
+  origin: string;
+};
 
 /**
  * A wrapper around the Google Picker API client libraries.
@@ -50,52 +70,44 @@ function addHttps(origin) {
  * underlying libraries.
  */
 export class GooglePickerClient {
-  /**
-   * @param {object} options
-   * @param {string} options.developerKey -
-   *   API key obtained from the Google API console.
-   * @param {string} options.clientId -
-   *   Client ID obtained from the Google API console.
-   * @param {string} options.origin -
-   *   The origin of the top-most page in the frame where this picker will
-   *   be served. This is needed when the picker is served inside an iframe
-   *   with a different origin than the top-level page
-   */
-  constructor({ developerKey, clientId, origin }) {
+  private _accessToken: string | null;
+  private _clientId: string;
+  private _developerKey: string;
+  private _gapiClient: Promise<typeof gapi.client>;
+  private _gapiPicker: Promise<typeof google.picker>;
+  private _identityServices: Promise<typeof google.accounts>;
+  private _origin: string;
+
+  constructor({ developerKey, clientId, origin }: GooglePickerOptions) {
+    this._accessToken = null;
     this._clientId = clientId;
     this._developerKey = developerKey;
-    this._origin = addHttps(origin);
-
-    const libs = loadLibraries(['client', 'picker']);
+    this._origin = addHTTPS(origin);
 
     this._identityServices = loadIdentityServicesLibrary();
 
+    const libs = loadLibraries(['client', 'picker']);
     this._gapiClient = libs.then(({ client }) => client);
     this._gapiPicker = libs.then(({ picker }) => picker.api);
-
-    /** @type {string|null} */
-    this._accessToken = null;
   }
 
   /**
    * Authorize this application's access to the user's files in Google Drive.
    *
-   * @return {Promise<string>} - An access token for making Google Drive API requests.
-   *   The access token is cached for use in future Drive API calls.
+   * @return An access token for making Google Drive API requests. The access
+   *   token is cached for use in future Drive API calls.
    */
-  async requestAuthorization() {
+  async requestAuthorization(): Promise<string> {
     if (this._accessToken) {
       return this._accessToken;
     }
 
     const idServices = await this._identityServices;
 
-    /** @type {(err: Error) => void} */
-    let rejectAccessToken;
+    let rejectAccessToken: (err: Error) => void;
+    let resolveAccessToken: (token: string) => void;
 
-    /** @type {(token: string) => void} */
-    let resolveAccessToken;
-    const accessToken = new Promise((resolve, reject) => {
+    const accessToken = new Promise<string>((resolve, reject) => {
       resolveAccessToken = resolve;
       rejectAccessToken = reject;
     });
@@ -146,27 +158,26 @@ export class GooglePickerClient {
    * This will automatically call {@link requestAuthorization} to acquire
    * an access token.
    *
-   * @return {Promise<{ id: string, name: string, url: string }>}
+   * @return
    *   Document ID, filename and download URL of the selected file.
    *   The download URL is only available by users who have access to the file.
    *   To make it accessible to everyone, use `enablePublicViewing`.
    */
-  async showPicker() {
+  async showPicker(): Promise<{ id: string; name: string; url: string }> {
     const pickerLib = await this._gapiPicker;
     const accessToken = await this.requestAuthorization();
 
-    /** @type {(doc: PickerDocument) => void} */
-    let resolve;
+    let resolve: (doc: PickerDocument) => void;
 
-    /** @type {(e: Error) => void} */
-    let reject;
+    let reject: (e: Error) => void;
 
-    /**
-     * @param {object} args
-     *   @param {string} args.action
-     *   @param {PickerDocument[]} args.docs
-     */
-    function pickerCallback({ action, docs }) {
+    function pickerCallback({
+      action,
+      docs,
+    }: {
+      action: string;
+      docs: PickerDocument[];
+    }) {
       if (action === pickerLib.Action.PICKED) {
         const doc = docs[0];
         // nb. It would be better to get this URL from Google Drive instead of
@@ -183,7 +194,7 @@ export class GooglePickerClient {
       }
     }
 
-    const view = new pickerLib.View(pickerLib.ViewId.DOCS);
+    const view = new pickerLib.DocsView(pickerLib.ViewId.DOCS);
     view.setMimeTypes('application/pdf');
     const picker = new pickerLib.PickerBuilder()
       .addView(view)
@@ -203,7 +214,7 @@ export class GooglePickerClient {
   }
 
   /** Prepare Google API client library for making Google Drive API requests. */
-  async _initAPIClient() {
+  private async _initAPIClient() {
     const gapiClient = await this._gapiClient;
 
     if (!this._accessToken) {
@@ -224,20 +235,20 @@ export class GooglePickerClient {
   /**
    * Change the sharing settings on a document in Google Drive to make it
    * publicly viewable to anyone with the link.
-   *
-   * @param {string} docId
    */
-  async enablePublicViewing(docId) {
+  async enablePublicViewing(docId: string): Promise<void> {
     const gapiClient = await this._initAPIClient();
     const body = {
       type: 'anyone',
       role: 'reader',
     };
-    const request = gapiClient.drive.permissions.create({
+    // @ts-expect-error - We're missing types for `gapiClient.drive`.
+    const request: unknown = gapiClient.drive.permissions.create({
       fileId: docId,
       resource: body,
     });
     return new Promise((resolve, reject) => {
+      // @ts-expect-error - We're missing types for this.
       request.execute(resolve, reject);
     });
   }
