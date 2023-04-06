386439dbbd5d6634ab8283cfcba24e8fff166592
Marcos Prieto <marcospri@gmail.com>
2023-04-06 15:11:38 +0200

    Migration for the email_unsubscribe table

---
+++ lms/migrations/versions/9f1e88367596_email_unsubscribe.py
@@ -0,0 +1,35 @@
+"""
+Add the email_unsubscribe table.
+
+Revision ID: 9f1e88367596
+Revises: f3d631c110bf
+Create Date: 2023-04-06 14:44:40.077112
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "9f1e88367596"
+down_revision = "f3d631c110bf"
+
+
+def upgrade():
+    op.create_table(
+        "email_unsubscribe",
+        sa.Column(
+            "created", sa.DateTime(), server_default=sa.text("now()"), nullable=False
+        ),
+        sa.Column(
+            "updated", sa.DateTime(), server_default=sa.text("now()"), nullable=False
+        ),
+        sa.Column("id", sa.Integer(), autoincrement=True, nullable=False),
+        sa.Column("tag", sa.UnicodeText(), nullable=False),
+        sa.Column("email", sa.Unicode(), nullable=False),
+        sa.PrimaryKeyConstraint("id", name=op.f("pk__email_unsubscribe")),
+        sa.UniqueConstraint("email", "tag", name=op.f("uq__email_unsubscribe__email")),
+    )
+
+
+def downgrade():
+    op.drop_table("email_unsubscribe")
