be32792f0e10138f527e3fd224cbe439f09344ba
Marcos Prieto <marcospri@gmail.com>
2023-04-12 15:14:45 +0200

    Use varchar_enum for EmailUnsubscribe.Tag

--- lms/models/email_unsubscribe.py
+++ lms/models/email_unsubscribe.py
@@ -1,11 +1,16 @@
+from enum import Enum
+
 import sqlalchemy as sa
 
-from lms.db import BASE
+from lms.db import BASE, varchar_enum
 from lms.models._mixins import CreatedUpdatedMixin
 
 
 class EmailUnsubscribe(CreatedUpdatedMixin, BASE):
     """Store a denylist of for transactional emails."""
+
+    class Tag(str, Enum):
+        INSTRUCTOR_DIGEST = "instructor_digest"
 
     __tablename__ = "email_unsubscribe"
     __table_args__ = (sa.UniqueConstraint("h_userid", "tag"),)
@@ -15,5 +20,5 @@ class EmailUnsubscribe(CreatedUpdatedMixin, BASE):
     h_userid = sa.Column(sa.Unicode, nullable=False)
     """Which H user unsubscribed from the email"""
 
-    tag = sa.Column(sa.UnicodeText(), nullable=False)
+    tag = varchar_enum(Tag, nullable=False)
     """Identify the type of email to not receive anymore"""
