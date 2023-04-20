d2afaef7cad3ee77a36e34b93932a251005fb81e
Jon Betts <jon.betts@hypothes.is>
2023-04-20 14:03:17 +0100

    Remove the update to `users_sensitive` which is now a view
    This was a materialized view, but now it's not, and so can't be
    updated.

--- lms/data_tasks/report/refresh/01_entities_refresh.sql
+++ lms/data_tasks/report/refresh/01_entities_refresh.sql
@@ -4,9 +4,6 @@ ANALYSE report.raw_users;
 REFRESH MATERIALIZED VIEW CONCURRENTLY report.user_map;
 ANALYSE report.user_map;
 
-REFRESH MATERIALIZED VIEW CONCURRENTLY report.users_sensitive;
-ANALYSE report.users_sensitive;
-
 REFRESH MATERIALIZED VIEW CONCURRENTLY report.group_map;
 ANALYSE report.group_map;
 
