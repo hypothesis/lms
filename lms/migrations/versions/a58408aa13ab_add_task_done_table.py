f273507d844f6ed3ade4f5cc66f17bf989ea2d2e
Sean Hammond <seanh@users.noreply.github.com>
2023-05-01 11:14:42 +0100

    Add migration script for task_done table

---
+++ lms/migrations/versions/a58408aa13ab_add_task_done_table.py
@@ -0,0 +1,39 @@
+"""
+Add task_done table.
+
+Revision ID: a58408aa13ab
+Revises: 9f1e88367596
+Create Date: 2023-05-01 10:37:08.209572
+
+"""
+from alembic import op
+from sqlalchemy import Column, DateTime, Integer, UnicodeText, func, text
+
+revision = "a58408aa13ab"
+down_revision = "9f1e88367596"
+
+
+def upgrade():
+    op.create_table(
+        "task_done",
+        Column("created", DateTime, server_default=func.now(), nullable=False),
+        Column(
+            "updated",
+            DateTime,
+            server_default=func.now(),
+            onupdate=func.now(),
+            nullable=False,
+        ),
+        Column("id", Integer, autoincrement=True, primary_key=True),
+        Column("key", UnicodeText, nullable=False, unique=True),
+        Column(
+            "expires_at",
+            DateTime,
+            nullable=False,
+            server_default=text("now() + interval '30 days'"),
+        ),
+    )
+
+
+def downgrade():
+    op.drop_table("task_done")
