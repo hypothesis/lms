a5cd57119f48b57a343a9802e8a32550f8c7e9c1
Jon Betts <jon.betts@hypothes.is>
2023-05-01 15:41:15 +0100

    Add permission for report to read the event and user tables
    This works fine in production, but if you follow the correct instructions
    locally, it doesn't work. This implies we have the wrong user in prod report,
    but we should fix this regardless.

--- lms/data_tasks/report/create_from_scratch/99_grant_permissions/01_grant_schema.jinja2.sql
+++ lms/data_tasks/report/create_from_scratch/99_grant_permissions/01_grant_schema.jinja2.sql
@@ -18,12 +18,14 @@
 
     GRANT USAGE ON SCHEMA report TO "{{fdw_user}}";
 
-    GRANT SELECT ON report.organization_activity TO "{{fdw_user}}";
-    GRANT SELECT ON report.organization_roles TO "{{fdw_user}}";
+    GRANT SELECT ON report.events TO "{{fdw_user}}";
     GRANT SELECT ON report.groups TO "{{fdw_user}}";
     GRANT SELECT ON report.group_map TO "{{fdw_user}}";
     GRANT SELECT ON report.group_bubbled_activity TO "{{fdw_user}}";
     GRANT SELECT ON report.group_bubbled_counts TO "{{fdw_user}}";
     GRANT SELECT ON report.group_roles TO "{{fdw_user}}";
+    GRANT SELECT ON report.organization_activity TO "{{fdw_user}}";
+    GRANT SELECT ON report.organization_roles TO "{{fdw_user}}";
+    GRANT SELECT ON report.users TO "{{fdw_user}}";
     GRANT SELECT ON report.users_sensitive TO "{{fdw_user}}";
 {% endfor %}
