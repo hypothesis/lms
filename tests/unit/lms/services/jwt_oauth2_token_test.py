7d356ba9a3ed2c17cfcd786586b39555ec4d01a9
Marcos Prieto <marcospri@gmail.com>
2023-06-07 09:44:03 +0200

    Use a list of scopes instead of strings in the service API

--- tests/unit/lms/services/jwt_oauth2_token_test.py
+++ tests/unit/lms/services/jwt_oauth2_token_test.py
@@ -10,65 +10,84 @@ from tests import factories
 
 class TestJWTOAuth2TokenServiceTest:
     @freeze_time("2022-04-04")
-    def test_save_new_token(self, svc, lti_registration):
-        token = svc.save(lti_registration, "SCOPES", "ACCESS_TOKEN", 3600)
+    @pytest.mark.parametrize("scopes", [("1", "2"), ("2", "1")])
+    def test_it(self, svc, lti_registration, scopes):
+        token = svc.save(lti_registration, scopes, "ACCESS_TOKEN", 3600)
+
+        assert svc.get(lti_registration, scopes) == token
+        assert token.scopes == "1 2"
+
+    @freeze_time("2022-04-04")
+    def test_save_new_token(self, svc, lti_registration, scopes):
+        token = svc.save(lti_registration, scopes, "ACCESS_TOKEN", 3600)
 
         assert token.access_token == "ACCESS_TOKEN"
         assert token.received_at == datetime(2022, 4, 4, 0)
         assert token.expires_at == token.received_at + timedelta(seconds=3600)
 
     @freeze_time("2022-04-04")
-    def test_save_existing_token(self, svc, lti_registration):
+    def test_save_existing_token(self, svc, lti_registration, scopes):
         existing_token = factories.JWTOAuth2Token(
             lti_registration=lti_registration,
-            scopes="SCOPES",
+            scopes=" ".join(scopes),
             expires_at=datetime.now(),
         )
 
-        token = svc.save(lti_registration, "SCOPES", "ACCESS_TOKEN", 3600)
+        token = svc.save(lti_registration, scopes, "ACCESS_TOKEN", 3600)
 
         assert existing_token == token
         assert token.received_at == datetime(2022, 4, 4, 0)
         assert token.expires_at == datetime(2022, 4, 4, 1)
 
     @freeze_time("2022-04-04")
-    def test_get(self, svc, lti_registration, db_session):
+    def test_get(self, svc, lti_registration, db_session, scopes):
         existing_token = factories.JWTOAuth2Token(
             lti_registration=lti_registration,
-            scopes="SCOPES",
+            scopes=" ".join(scopes),
             expires_at=datetime.now() + timedelta(hours=1),
         )
         db_session.flush()
 
-        token = svc.get(lti_registration, "SCOPES")
+        token = svc.get(lti_registration, scopes)
 
         assert existing_token == token
 
     @freeze_time("2022-04-04")
-    def test_get_doesnt_return_expired(self, svc, lti_registration, db_session):
+    def test_get_doesnt_return_expired(self, svc, lti_registration, db_session, scopes):
         factories.JWTOAuth2Token(
             lti_registration=lti_registration,
-            scopes="SCOPES",
+            scopes=" ".join(scopes),
             expires_at=datetime.now() - timedelta(hours=1),
         )
         db_session.flush()
 
-        token = svc.get(lti_registration, "SCOPES")
+        token = svc.get(lti_registration, scopes)
 
         assert not token
 
     @freeze_time("2022-04-04")
-    def test_get_return_expired_with_flag(self, svc, lti_registration, db_session):
+    def test_get_return_expired_with_flag(
+        self, svc, lti_registration, db_session, scopes
+    ):
         expired_token = factories.JWTOAuth2Token(
             lti_registration=lti_registration,
-            scopes="SCOPES",
+            scopes=" ".join(scopes),
             expires_at=datetime.now() - timedelta(hours=1),
         )
         db_session.flush()
 
-        token = svc.get(lti_registration, "SCOPES", exclude_expired=False)
+        token = svc.get(lti_registration, scopes, exclude_expired=False)
 
         assert token == expired_token
+
+    @pytest.fixture
+    def scopes(self):
+        return ["SCOPE_1", "SCOPE_2"]
+
+    @pytest.fixture(autouse=True)
+    def with_existing_group_infos(self):
+        # Add some "noise" tokens to make the tests more realistic
+        factories.JWTOAuth2Token.build_batch(3)
 
     @pytest.fixture
     def svc(self, db_session):
