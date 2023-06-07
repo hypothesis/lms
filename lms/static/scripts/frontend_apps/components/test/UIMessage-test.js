2d198f8548f8fcba8b261d56f31c753a99b2e6cf
Lyza Danger Gardner <lyza@lyza.com>
2023-06-07 12:38:09 -0400

    Extract common UIMessage component

---
+++ lms/static/scripts/frontend_apps/components/test/UIMessage-test.js
@@ -0,0 +1,33 @@
+import { mount } from 'enzyme';
+
+import { checkAccessibility } from '../../../test-util/accessibility';
+import UIMessage from '../UIMessage';
+
+describe('UIMessage', () => {
+  const renderComponent = (props = {}) =>
+    mount(<UIMessage {...props}>This is a message</UIMessage>);
+
+  it('renders a checkmark next to success messages', () => {
+    const wrapper = renderComponent({ status: 'success' });
+    assert.isTrue(wrapper.find('[data-testid="uimessage-icon"]').exists());
+    assert.isTrue(wrapper.find('CheckIcon').exists());
+  });
+
+  it('renders a cancel icon next to error messages', () => {
+    const wrapper = renderComponent({ status: 'error' });
+    assert.isTrue(wrapper.find('[data-testid="uimessage-icon"]').exists());
+    assert.isTrue(wrapper.find('CancelIcon').exists());
+  });
+
+  it('does not render an icon next to info messages', () => {
+    const wrapper = renderComponent();
+    assert.isFalse(wrapper.find('[data-testid="uimessage-icon"]').exists());
+  });
+
+  it(
+    'should pass a11y checks',
+    checkAccessibility({
+      content: () => renderComponent(),
+    })
+  );
+});
