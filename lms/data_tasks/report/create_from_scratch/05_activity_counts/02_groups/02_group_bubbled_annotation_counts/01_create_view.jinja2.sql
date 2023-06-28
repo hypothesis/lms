90a9a43135daa96f5d09a2c073e14529f7d06172
Jon Betts <jon.betts@hypothes.is>
2023-06-28 15:18:27 +0100

    Use the user counts to create rolled up group counts
    There are way too many tables treading on basically the same stuff. We
    could really do with cutting this down.

---
+++ lms/data_tasks/report/create_from_scratch/05_activity_counts/02_groups/02_group_bubbled_annotation_counts/01_create_view.jinja2.sql
@@ -0,0 +1,21 @@
+DROP MATERIALIZED VIEW IF EXISTS report.group_bubbled_annotation_counts CASCADE;
+
+-- A weekly count of groups with annotation activity, but with counts summed
+-- up and down between parents and children as appropriate.
+
+CREATE MATERIALIZED VIEW report.group_bubbled_annotation_counts AS (
+    SELECT
+        group_id,
+        created_week,
+        role,
+        sub_type,
+        shared,
+        SUM(count) AS count
+    FROM report.group_to_group
+    -- The join on group to group without a relation includes self,
+    -- so the child_id here is all children and ourselves
+    JOIN report.group_annotation_counts ON
+        group_annotation_counts.group_id = group_to_group.child_id
+    GROUP BY group_id, created_week, role, sub_type, shared
+    ORDER BY group_id, created_week, count DESC
+) WITH NO DATA;
--- lms/data_tasks/report/refresh/04_activity_counts_refresh.sql
+++ lms/data_tasks/report/refresh/04_activity_counts_refresh.sql
@@ -4,6 +4,12 @@ ANALYSE report.user_annotation_counts;
 REFRESH MATERIALIZED VIEW CONCURRENTLY report.user_activity;
 ANALYSE report.user_activity;
 
+REFRESH MATERIALIZED VIEW CONCURRENTLY report.group_annotation_counts;
+ANALYSE report.group_annotation_counts;
+
+REFRESH MATERIALIZED VIEW CONCURRENTLY report.group_bubbled_annotation_counts;
+ANALYSE report.group_bubbled_annotation_counts;
+
 REFRESH MATERIALIZED VIEW CONCURRENTLY report.group_activity;
 ANALYSE report.group_activity;
 
