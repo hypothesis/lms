c1596989fbeff81564e6c2df8835ca62c3c137c6
Marcos Prieto <marcospri@gmail.com>
2023-07-06 11:24:16 +0200

    Remove LMS family from the grading table, migration.

---
+++ lms/migrations/versions/106d94be7705_remove_family_grading.py
@@ -0,0 +1,30 @@
+"""
+Remove tool_consumer_info_product_family_code from GradingInfo.
+
+Revision ID: 106d94be7705
+Revises: 973c9358b616
+Create Date: 2023-07-06 11:23:10.850486
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "106d94be7705"
+down_revision = "973c9358b616"
+
+
+def upgrade():
+    op.drop_column("lis_result_sourcedid", "tool_consumer_info_product_family_code")
+
+
+def downgrade():
+    op.add_column(
+        "lis_result_sourcedid",
+        sa.Column(
+            "tool_consumer_info_product_family_code",
+            sa.TEXT(),
+            autoincrement=False,
+            nullable=True,
+        ),
+    )
