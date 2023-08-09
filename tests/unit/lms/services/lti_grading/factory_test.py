37c7eff0836cd89ec39c041a5b796c8ad2c1fb8f
Marcos Prieto <marcospri@gmail.com>
2023-08-09 10:14:50 +0200

    Pick a value for line_item_url in all contexts
    The grading service might operate in two different contexts:
    
    - While on a launch
    
    Here take the value directly from LTIParams
    
    - On an API call from the front end
    
    Here we will forward the value to the frontend which will send it back
    in `parsed_params`

--- tests/unit/lms/services/lti_grading/factory_test.py
+++ tests/unit/lms/services/lti_grading/factory_test.py
@@ -28,12 +28,28 @@ class TestFactory:
         )
         assert svc == LTI13GradingService.return_value
 
+    def test_v13_line_item_url_from_lti_params(
+        self, pyramid_request, LTI13GradingService, ltia_http_service
+    ):
+        del pyramid_request.parsed_params["lis_outcome_service_url"]
+        pyramid_request.lti_user.application_instance = Mock(lti_version="1.3.0")
+
+        svc = service_factory(sentinel.context, pyramid_request)
+
+        LTI13GradingService.assert_called_once_with(
+            sentinel.grading_url, sentinel.lineitems, ltia_http_service
+        )
+        assert svc == LTI13GradingService.return_value
+
     @pytest.fixture
     def pyramid_request(self, pyramid_request):
         pyramid_request.parsed_params = {
             "lis_outcome_service_url": sentinel.grading_url
         }
-        pyramid_request.lti_params = {"lineitems": sentinel.lineitems}
+        pyramid_request.lti_params = {
+            "lineitems": sentinel.lineitems,
+            "lis_outcome_service_url": sentinel.grading_url,
+        }
 
         return pyramid_request
 
