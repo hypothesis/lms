9fdd97555c966ca2fc4bfbff35ea331f62c429ba
Marcos Prieto <marcospri@gmail.com>
2023-09-20 12:24:22 +0200

    Add testing factory for model.Event

--- tests/factories/__init__.py
+++ tests/factories/__init__.py
@@ -19,6 +19,7 @@ from tests.factories.attributes import (
     USER_ID,
 )
 from tests.factories.email_unsubscribe import EmailUnsubscribe
+from tests.factories.event import Event
 from tests.factories.file import File
 from tests.factories.grading_info import GradingInfo
 from tests.factories.group_info import GroupInfo
