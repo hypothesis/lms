3273e8e84e7672299693d50c347cb777a7b8c385
Marcos Prieto <marcospri@gmail.com>
2023-09-22 09:59:53 +0200

    View to proxy canvas pages API calls

---
+++ lms/views/api/canvas/pages.py
@@ -0,0 +1,31 @@
+from pyramid.view import view_config, view_defaults
+
+from lms.security import Permissions
+from lms.services.canvas import CanvasService
+
+
+@view_defaults(permission=Permissions.API, renderer="json")
+class PagesAPIViews:
+    def __init__(self, request):
+        self.request = request
+        self.canvas = request.find_service(CanvasService)
+
+    @view_config(request_method="GET", route_name="canvas_api.courses.pages.list")
+    def list_pages(self):
+        """
+        Return the list of pages in the given course.
+
+        :raise lms.services.CanvasAPIError: if the Canvas API request fails.
+            This exception is caught and handled by an exception view.
+        """
+        course_id = self.request.matchdict["course_id"]
+        return [
+            {
+                "id": f"canvas://page/course/{course_id}/page_id/{page.id}",
+                "lms_id": page.id,
+                "display_name": page.title,
+                "type": "File",
+                "updated_at": page.updated_at,
+            }
+            for page in self.canvas.api.pages.list(course_id)
+        ]
---
+++ tests/unit/lms/views/api/canvas/pages_test.py
@@ -0,0 +1,35 @@
+import pytest
+
+from lms.services.canvas_api._pages import CanvasPage
+from lms.views.api.canvas.pages import PagesAPIViews
+
+
+@pytest.mark.usefixtures(
+    "application_instance_service", "assignment_service", "canvas_service"
+)
+class TestPageAPIViews:
+    def test_list_pages(self, canvas_service, pyramid_request, pages):
+        course_id = "COURSE_ID"
+        pyramid_request.matchdict = {"course_id": course_id}
+        canvas_service.api.pages.list.return_value = pages
+
+        result = PagesAPIViews(pyramid_request).list_pages()
+
+        assert result == [
+            {
+                "id": f"canvas://page/course/{course_id}/page_id/{page.id}",
+                "lms_id": page.id,
+                "display_name": page.title,
+                "type": "File",
+                "updated_at": page.updated_at,
+            }
+            for page in pages
+        ]
+        canvas_service.api.pages.list.assert_called_once_with(course_id)
+
+    @pytest.fixture
+    def pages(self):
+        return [
+            CanvasPage(id=i, title=f"title {i}", updated_at=f"updated {i}")
+            for i in range(5)
+        ]
