2bdabc480cf9037eb8f291f705b9a09d001f36b9
Sean Hammond <seanh@users.noreply.github.com>
2023-11-14 12:35:43 +0000

    Add task_done.data column

--- lms/models/task_done.py
+++ lms/models/task_done.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, DateTime, Integer, UnicodeText, text
+from sqlalchemy.dialects.postgresql import JSONB
 
 from lms.db import BASE
 from lms.models._mixins import CreatedUpdatedMixin
@@ -9,6 +10,7 @@ class TaskDone(CreatedUpdatedMixin, BASE):
 
     id = Column(Integer, autoincrement=True, primary_key=True)
     key = Column(UnicodeText, nullable=False, unique=True)
+    data = Column(JSONB, server_default=text("'{}'::jsonb"), nullable=True)
     expires_at = Column(
         DateTime, nullable=False, server_default=text("now() + interval '30 days'")
     )
