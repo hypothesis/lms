af1506ae92acd26f8f438ff4ab4128e0f9cb3244
Marcos Prieto <marcospri@gmail.com>
2023-11-15 12:21:09 +0100

    Generate usage reports

--- lms/services/organization.py
+++ lms/services/organization.py
@@ -1,13 +1,35 @@
+from dataclasses import dataclass
+from datetime import datetime
 from logging import getLogger
 from typing import List, Optional
 
 import sqlalchemy as sa
-from sqlalchemy.orm import Session
+from sqlalchemy import Date, func, select
+from sqlalchemy.orm import Session, aliased
 
 from lms.db import full_text_match
-from lms.models import ApplicationInstance, GroupInfo, Organization
+from lms.models import (
+    ApplicationInstance,
+    GroupInfo,
+    Grouping,
+    GroupingMembership,
+    Organization,
+    User,
+)
+from lms.services.h_api import HAPI
 
 LOG = getLogger(__name__)
+
+
+@dataclass
+class UsageReportRow:
+    name: Optional[str]
+    email: Optional[str]
+    h_userid: str
+
+    course_name: str
+    course_created: datetime
+    authority_provided_id: str
 
 
 class InvalidOrganizationParent(Exception):
@@ -17,8 +39,9 @@ class InvalidOrganizationParent(Exception):
 class OrganizationService:
     """A service for dealing with organization actions."""
 
-    def __init__(self, db_session: Session):
+    def __init__(self, db_session: Session, h_api: HAPI):
         self._db_session = db_session
+        self._h_api = h_api
 
     def get_by_id(self, id_: int) -> Optional[Organization]:
         """
@@ -216,6 +239,82 @@ class OrganizationService:
 
         return organization
 
+    def usage_report(
+        self,
+        organization: Organization,
+        since: datetime,
+        until: datetime,
+    ):
+        # Organizations that are children of the current one.
+        # It includes the current org ID.
+        organization_children = self._get_hierarchy_ids(
+            organization.id, include_parents=False
+        )
+        # All the groups that can hold annotations (courses and segments) from this org
+        groups_from_org = self._db_session.scalars(
+            select(Grouping.authority_provided_id)
+            .join(ApplicationInstance)
+            .where(
+                ApplicationInstance.organization_id.in_(organization_children),
+                # If a group was created after the date we are interested, exclude it
+                Grouping.created <= until,
+            )
+        ).all()
+
+        # Of those groups, get the ones that do have annotations in the time period
+        groups_with_annos = [
+            group.authority_provided_id
+            for group in self._h_api.get_groups(groups_from_org, since, until)
+        ]
+        # Based on those groups generate the usage report based on the definition of unique user:
+        # Users that belong to a course in which there are annotations in the time period
+        # pylint:disable=not-callable
+        parent = aliased(Grouping)
+        query = (
+            select(
+                func.coalesce(User.display_name, "<STUDENT>").label("name"),
+                func.coalesce(User.email, "<STUDENT>").label("email"),
+                User.h_userid.label("h_userid"),
+                Grouping.lms_name.label("course_name"),
+                func.date_trunc("day", Grouping.created)
+                .cast(Date)
+                .label("course_created"),
+                Grouping.authority_provided_id,
+            )
+            .select_from(User)
+            .join(GroupingMembership)
+            .join(Grouping)
+            .distinct()
+            .where(
+                Grouping.authority_provided_id.in_(
+                    # The report is based in courses so we query either
+                    # groupings with no parent (courses) or the parents of segments (courses)
+                    select(
+                        func.coalesce(
+                            parent.authority_provided_id, Grouping.authority_provided_id
+                        )
+                    )
+                    .select_from(Grouping)
+                    .outerjoin(parent, Grouping.parent_id == parent.id)
+                    .where(Grouping.authority_provided_id.in_(groups_with_annos))
+                ),
+                # We can't exactly know the state of membership in the past but we can
+                # know if someone was added to the group after the date we are interested
+                GroupingMembership.created <= until,
+            )
+        )
+        return [
+            UsageReportRow(
+                name=row.name,
+                email=row.email,
+                h_userid=row.h_userid,
+                course_name=row.course_name,
+                course_created=row.course_created,
+                authority_provided_id=row.authority_provided_id,
+            )
+            for row in self._db_session.execute(query).all()
+        ]
+
     def _move_organization_parent(self, organization: Organization, parent_public_id):
         """Change an organizations parent, without creating loops."""
 
@@ -280,4 +379,7 @@ class OrganizationService:
 def service_factory(_context, request) -> OrganizationService:
     """Get a new instance of OrganizationService."""
 
-    return OrganizationService(db_session=request.db)
+    return OrganizationService(
+        db_session=request.db,
+        h_api=request.find_service(HAPI),
+    )
