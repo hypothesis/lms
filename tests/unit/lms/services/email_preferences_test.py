0cea8a8cb0c3d9812cfe37002df516de9305a0d9
Sean Hammond <seanh@users.noreply.github.com>
2023-11-21 17:43:59 +0000

    Add `EmailPreferencesService.h_userid(url)` method
    The upcoming `EmailPreferencesSecurityPolicy` is going to need to
    extract the `h_userid` from an unsubscribe URL or email preferences URL
    generated by `EmailPreferencesService.unsubscribe_url()` or the upcoming
    `EmailPreferencesService.preferences_url()`. Add a new
    `EmailPreferencesService.h_userid(url)` method that returns the
    `h_userid` extracted from one of `EmailPreferencesService`'s URLs.
    
    Keep the generating of unsubscribe and email preferences URLs and the
    parsing of those URLs to extract the `h_userid`'s together in
    `EmailPreferencesService`.

--- tests/unit/lms/services/email_preferences_test.py
+++ tests/unit/lms/services/email_preferences_test.py
@@ -4,7 +4,13 @@ from unittest.mock import sentinel
 import pytest
 
 from lms.models import EmailUnsubscribe
-from lms.services.email_preferences import EmailPreferencesService, factory
+from lms.services.email_preferences import (
+    EmailPreferencesService,
+    InvalidTokenError,
+    UnrecognisedURLError,
+    factory,
+)
+from lms.services.exceptions import ExpiredJWTError, InvalidJWTError
 
 
 class TestEmailPreferencesService:
@@ -41,6 +47,34 @@ class TestEmailPreferencesService:
             index_elements=["h_userid", "tag"],
             update_columns=["updated"],
         )
+
+    def test_h_userid(self, svc, jwt_service):
+        jwt_service.decode_with_secret.return_value = {"h_userid": sentinel.h_userid}
+
+        h_userid = svc.h_userid("https://example.com?token=test_token")
+
+        jwt_service.decode_with_secret.assert_called_once_with("test_token", "SECRET")
+        assert h_userid == sentinel.h_userid
+
+    @pytest.mark.parametrize(
+        "url",
+        ["https://example.com?foo=bar", "https://example.com", "http://["],
+    )
+    def test_h_userid_if_URL_is_unrecognised_or_invalid(self, svc, url):
+        with pytest.raises(UnrecognisedURLError):
+            svc.h_userid(url)
+
+    @pytest.mark.parametrize(
+        "exception_class",
+        [ExpiredJWTError, InvalidJWTError],
+    )
+    def test_h_userid_if_token_is_invalid_or_expired(
+        self, svc, jwt_service, exception_class
+    ):
+        jwt_service.decode_with_secret.side_effect = exception_class
+
+        with pytest.raises(InvalidTokenError):
+            svc.h_userid("https://example.com?token=test_token")
 
     @pytest.fixture
     def svc(self, db_session, jwt_service, pyramid_request):
