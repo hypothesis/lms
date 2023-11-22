830ab9024947eb74bdf89551b74fe0558fb54bdf
Marcos Prieto <marcospri@gmail.com>
2023-11-22 12:56:59 +0100

    Handle missing permissions and logged off users differently
    Without this change, missing permissions redirects to the google oauth
    page which in turns redirects to the original page the user was trying
    to access which results in another failed permissions check creating
    too many redirects.
    
    Here we take anonymous users rto the google oauth page and redirect to
    the admin pages index users that are missing one particular permission.

--- tests/unit/lms/views/admin/__init___test.py
+++ tests/unit/lms/views/admin/__init___test.py
@@ -1,17 +1,28 @@
-from lms.views.admin import index, logged_out, notfound
+import pytest
+
+from lms.security import Identity
+from lms.views.admin import forbidden, index, notfound
 from tests.matchers import temporary_redirect_to
 
 
-def test_logged_out_redirects_to_login(pyramid_request):
-    response = logged_out(pyramid_request)
+def test_forbidden_redirects_to_login(pyramid_request_without_userid):
+    response = forbidden(pyramid_request_without_userid)
 
     assert response.status_code == 302
 
     assert response == temporary_redirect_to(
-        pyramid_request.route_url(
-            "pyramid_googleauth.login", _query={"next": pyramid_request.url}
+        pyramid_request_without_userid.route_url(
+            "pyramid_googleauth.login",
+            _query={"next": pyramid_request_without_userid.url},
         )
     )
+
+
+def test_forbidden_redirects_to_index_when_permission(pyramid_request):
+    response = forbidden(pyramid_request)
+
+    assert response.status_code == 302
+    assert response == temporary_redirect_to(pyramid_request.route_url("admin.index"))
 
 
 def test_not_found_view(pyramid_request):
@@ -26,3 +37,11 @@ def test_index(pyramid_request):
     assert response == temporary_redirect_to(
         pyramid_request.route_url("admin.instance.search")
     )
+
+
+@pytest.fixture
+def pyramid_request_without_userid(pyramid_config, pyramid_request, lti_user):
+    pyramid_config.testing_securitypolicy(
+        userid="", identity=Identity("", [], lti_user)
+    )
+    return pyramid_request
