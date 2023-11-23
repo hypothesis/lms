8b6e34b3cb47de4ff5c7c1f14c60509d37eeba54
Sean Hammond <seanh@users.noreply.github.com>
2023-11-23 15:28:17 +0000

    Add DB migration for task_done.data column

---
+++ lms/migrations/versions/6d72ce7efdeb_add_the_task_done_data_column.py
@@ -0,0 +1,27 @@
+"""Add the task_done.data column.
+
+Revision ID: 6d72ce7efdeb
+Revises: 74318dc2de37
+"""
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.dialects import postgresql
+
+revision = "6d72ce7efdeb"
+down_revision = "74318dc2de37"
+
+
+def upgrade() -> None:
+    op.add_column(
+        "task_done",
+        sa.Column(
+            "data",
+            postgresql.JSONB(astext_type=sa.Text()),
+            server_default=sa.text("'{}'::jsonb"),
+            nullable=True,
+        ),
+    )
+
+
+def downgrade() -> None:
+    op.drop_column("task_done", "data")
