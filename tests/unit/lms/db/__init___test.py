c97971d89adb64963281672ea23872f2ee4d4ec9
Marcos Prieto <marcospri@gmail.com>
2023-12-05 10:36:32 +0100

    Explicitly list of GroupInfo fields being updated.
    Remove the last usage of BASE.update_from_dict and delete the method.

--- tests/unit/lms/db/__init___test.py
+++ tests/unit/lms/db/__init___test.py
@@ -1,4 +1,3 @@
-import pytest
 import sqlalchemy as sa
 
 from lms.db import BASE
@@ -25,31 +24,6 @@ class TestBase:
             "id",
         ]
 
-    def test_we_can_update_from_dict(self, model):
-        model.update_from_dict(
-            {
-                "id": 4321,
-                "column": "new_value",
-                "relationship": "something",
-                "missing": "Another value",
-            }
-        )
-
-        assert model.id == 1234
-        assert model.column == "new_value"
-
-    def test_we_can_specify_keys_to_skip(self, model):
-        model.update_from_dict(
-            {"id": 4321, "column": "new_value"}, skip_keys={"column"}
-        )
-
-        assert model.id == 4321
-        assert model.column == "original_value"
-
-    def test_we_fail_to_update_when_skip_keys_is_not_a_set(self):
-        with pytest.raises(TypeError):
-            ModelClass().update_from_dict({}, skip_keys=["a"])
-
     def test_repr(self):
         model = ModelClass(_aliased_column=77, id=23, column=46)
 
@@ -65,7 +39,3 @@ class TestBase:
             "column",
         ):
             assert getattr(deserialized_model, attr) == getattr(model, attr)
-
-    @pytest.fixture
-    def model(self):
-        return ModelClass(id=1234, column="original_value")
