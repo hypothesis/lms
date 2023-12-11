ac439ba691a7c88500c51f25515f8b02bb1ced5e
Alejandro Celaya <alejandrocelaya@gmail.com>
2023-12-11 12:54:08 +0100

    Style email preferences app layout (#5909)

--- lms/static/scripts/frontend_apps/components/EmailPreferencesApp.tsx
+++ lms/static/scripts/frontend_apps/components/EmailPreferencesApp.tsx
@@ -1,3 +1,5 @@
+import { InfoIcon, Link } from '@hypothesis/frontend-shared';
+import classnames from 'classnames';
 import { useCallback, useState } from 'preact/hooks';
 
 import { useConfig } from '../config';
@@ -10,16 +12,41 @@ export default function EmailPreferencesApp() {
   const onSave = useCallback(() => setSaving(true), []);
 
   return (
-    <div className="h-full grid place-items-center">
-      <div className="w-96">
-        <EmailPreferences
-          selectedDays={selectedDays}
-          updateSelectedDays={newSelectedDays =>
-            setSelectedDays(prev => ({ ...prev, ...newSelectedDays }))
-          }
-          onSave={onSave}
-          saving={saving}
+    <div className="min-h-full bg-grey-2">
+      <div
+        className={classnames(
+          'flex justify-center p-3 w-full',
+          'bg-white border-b shadow'
+        )}
+      >
+        <img
+          alt="Hypothesis logo"
+          src="/static/images/email_header.png"
+          className="h-10"
         />
+      </div>
+      <div className="flex flex-col gap-y-4 py-8">
+        <div className="max-w-[450px] mx-auto">
+          <EmailPreferences
+            selectedDays={selectedDays}
+            updateSelectedDays={newSelectedDays =>
+              setSelectedDays(prev => ({ ...prev, ...newSelectedDays }))
+            }
+            onSave={onSave}
+            saving={saving}
+          />
+        </div>
+        <p className="text-center text-grey-8">
+          <InfoIcon className="inline" /> Do you need help? Visit our{' '}
+          <Link
+            variant="custom"
+            underline="always"
+            href="https://web.hypothes.is/help/"
+          >
+            help center
+          </Link>
+          .
+        </p>
       </div>
     </div>
   );
