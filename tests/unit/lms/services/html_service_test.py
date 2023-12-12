fb96a1cf1e5fc221f31d85420f0e64befab1c843
Marcos Prieto <marcospri@gmail.com>
2023-12-12 14:58:57 +0100

    Remove tags_to_newline option on strip_html_tags
    It turns out that LMS that support HTML also keep the new lines. We only
    need to remove the HTML tags to display the text in our textarea.

--- tests/unit/lms/services/html_service_test.py
+++ tests/unit/lms/services/html_service_test.py
@@ -4,16 +4,13 @@ from lms.services.html_service import strip_html_tags
 
 
 @pytest.mark.parametrize(
-    "text,expected,tags_to_new_line",
+    "text,expected",
     [
-        ("<b>COLON :</b>", "COLON :", None),
-        ("A <em>B</em>", "A B", None),
-        (" C <em>D</em> E", "C D E", None),
-        ("A<B", "A<B", None),
-        ("<p>PARAGRAPH</p>OTHER", "PARAGRAPH\nOTHER", ["p"]),
-        ("<p>PARAGRAPH</p>OTHER<br/>ANOTHER", "PARAGRAPH\nOTHER\nANOTHER", ["p", "br"]),
-        ("<div>1\n <p>2</p>\n<p>3 </p>\n</div>", "1\n2\n3", ["p", "br"]),
+        ("<b>COLON :</b>", "COLON :"),
+        ("A <em>B</em>", "A B"),
+        (" C <em>D</em> E", "C D E"),
+        ("A<B", "A<B"),
     ],
 )
-def test_strip_html_tags(text, expected, tags_to_new_line):
-    assert strip_html_tags(text, tags_to_new_line) == expected
+def test_strip_html_tags(text, expected):
+    assert strip_html_tags(text) == expected
