ac3f738f8c1686d0b0ec274faba744d0690b96c4
Marcos Prieto <marcospri@gmail.com>
2023-12-12 14:58:57 +0100

    Remove tags_to_newline option on strip_html_tags
    It turns out that LMS that support HTML also keep the new lines. We only
    need to remove the HTML tags to display the text in our textarea.

--- lms/services/html_service.py
+++ lms/services/html_service.py
@@ -3,17 +3,12 @@ from html.parser import HTMLParser
 
 
 class WhiteSpaceHTMLParser(HTMLParser):  # pylint:disable=abstract-method
-    def __init__(self, tags_to_newline):
+    def __init__(self):
         super().__init__()
         self._chunks = []
-        self._tags_to_new_line = tags_to_newline or []
 
     def handle_data(self, data):
         self._chunks.append(data)
-
-    def handle_endtag(self, tag):
-        if tag in self._tags_to_new_line:
-            self._chunks.append("\n")
 
     def get_text(self) -> str:
         joined = "".join(self._chunks).strip()
@@ -21,9 +16,8 @@ class WhiteSpaceHTMLParser(HTMLParser):  # pylint:disable=abstract-method
         return re.sub(r"\n\s", "\n", joined)
 
 
-def strip_html_tags(html: str, tags_to_newline=None) -> str:
-    parser = WhiteSpaceHTMLParser(tags_to_newline)
+def strip_html_tags(html: str) -> str:
+    parser = WhiteSpaceHTMLParser()
     parser.feed(html)
     parser.close()
-
     return parser.get_text()
