e44e5a47c66a29945405c8043926b08132bcabd5
Marcos Prieto <marcospri@gmail.com>
2024-01-16 15:08:32 +0100

    New moodle product

--- lms/product/factory.py
+++ lms/product/factory.py
@@ -1,9 +1,12 @@
 from lms.product.blackboard import Blackboard
 from lms.product.canvas import Canvas
 from lms.product.d2l import D2L
+from lms.product.moodle import Moodle
 from lms.product.product import Product
 
-_PRODUCT_MAP = {product.family: product for product in (Blackboard, Canvas, D2L)}
+_PRODUCT_MAP = {
+    product.family: product for product in (Blackboard, Canvas, D2L, Moodle)
+}
 
 
 def get_product_from_request(request) -> Product:
