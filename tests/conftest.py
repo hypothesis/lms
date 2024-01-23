b7bffa6092273c9039f88cdd7729262428df13c2
Sean Hammond <seanh@users.noreply.github.com>
2024-01-23 12:08:29 +0000

    Use h-testkit instead of duplicating test fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,7 @@
 import functools
-from os import environ
 from unittest import mock
 
 import pytest
-from sqlalchemy.orm import sessionmaker
-
-from lms.db import create_engine
-from tests import factories
 
 TEST_SETTINGS = {
     "dev": False,
@@ -49,44 +44,6 @@ def autopatcher(request, target, **kwargs):
     obj = patcher.start()
     request.addfinalizer(patcher.stop)
     return obj
-
-
-@pytest.fixture(scope="session")
-def db_engine():
-    return create_engine(environ["DATABASE_URL"])
-
-
-@pytest.fixture(scope="session")
-def db_sessionfactory():
-    return sessionmaker()
-
-
-@pytest.fixture
-def db_session(db_engine, db_sessionfactory):
-    """
-    Return the SQLAlchemy database session.
-
-    This returns a session that is wrapped in an external transaction that is
-    rolled back after each test, so tests can't make database changes that
-    affect later tests.  Even if the test (or the code under test) calls
-    session.commit() this won't touch the external transaction.
-
-    This is the same technique as used in SQLAlchemy's own CI:
-    https://docs.sqlalchemy.org/en/20/orm/session_transaction.html#joining-a-session-into-an-external-transaction-such-as-for-test-suites
-    """
-    connection = db_engine.connect()
-    transaction = connection.begin()
-    session = db_sessionfactory(
-        bind=connection, join_transaction_mode="create_savepoint"
-    )
-    factories.set_sqlalchemy_session(session)
-
-    yield session
-
-    factories.clear_sqlalchemy_session()
-    session.close()
-    transaction.rollback()
-    connection.close()
 
 
 @pytest.fixture
