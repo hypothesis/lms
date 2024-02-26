f0cba268fa6e1d8eaf9aba1c3b0541e0ac574aa1
Marcos Prieto <marcospri@gmail.com>
2024-02-26 15:31:11 +0100

    Revert "Remove support for file proxing"
    This reverts commit fad8495815642d49b16d8c78fd8e38566323c88e.

--- lms/views/helpers/_via.py
+++ lms/views/helpers/_via.py
@@ -5,7 +5,9 @@ from h_vialib import ViaClient
 __all__ = ["via_url"]
 
 
-def via_url(request, document_url, content_type=None, options=None, headers=None):
+def via_url(
+    request, document_url, content_type=None, options=None, headers=None, query=None
+):
     """
     Return the Via URL for annotating the given ``document_url``.
 
@@ -19,6 +21,7 @@ def via_url(request, document_url, content_type=None, options=None, headers=None
     :param content_type: Either "pdf" or "html" if known, None if not
     :param options: Any extra options for the url
     :param headers: Headers for the request to `document_url`
+    :param query: Query parameters for the request to `document_url`. Encrypted on transit.
     :return: A URL string
     """
     if not options:
@@ -35,5 +38,10 @@ def via_url(request, document_url, content_type=None, options=None, headers=None
         service_url=request.registry.settings["via_url"],
         secret=request.registry.settings["via_secret"],
     ).url_for(
-        document_url, content_type, blocked_for="lms", options=options, headers=headers
+        document_url,
+        content_type,
+        blocked_for="lms",
+        options=options,
+        headers=headers,
+        query=query,
     )
