12ecb1d3eec0305da6fc290e0b2fa8e62d6db7a3
Marcos Prieto <marcospri@gmail.com>
2024-02-26 15:31:11 +0100

    Proxy Moodle files using Via

--- tests/unit/lms/views/api/moodle/files_test.py
+++ tests/unit/lms/views/api/moodle/files_test.py
@@ -1,6 +1,8 @@
 from unittest.mock import sentinel
 
-from lms.views.api.moodle.files import list_files
+import pytest
+
+from lms.views.api.moodle.files import list_files, via_url
 
 
 def test_course_group_sets(pyramid_request, moodle_api_client):
@@ -10,3 +12,27 @@ def test_course_group_sets(pyramid_request, moodle_api_client):
 
     moodle_api_client.list_files.assert_called_once_with("test_course_id")
     assert result == moodle_api_client.list_files.return_value
+
+
+def test_via_url(
+    moodle_api_client,
+    helpers,
+    pyramid_request,
+):
+    type(moodle_api_client).token = "TOKEN"
+    pyramid_request.params["document_url"] = "moodle://file/url/URL"
+
+    response = via_url(sentinel.context, pyramid_request)
+
+    helpers.via_url.assert_called_once_with(
+        pyramid_request,
+        "URL",
+        content_type="pdf",
+        query={"token": moodle_api_client.token},
+    )
+    assert response == {"via_url": helpers.via_url.return_value}
+
+
+@pytest.fixture(autouse=True)
+def helpers(patch):
+    return patch("lms.views.api.moodle.files.helpers")
