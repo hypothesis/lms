52dd2a3187943a0309b2e2ebba7aa21a17afa80d
Marcos Prieto <marcospri@gmail.com>
2024-02-26 15:31:11 +0100

    Proxy Moodle files using Via

--- lms/views/api/moodle/files.py
+++ lms/views/api/moodle/files.py
@@ -4,6 +4,7 @@ from pyramid.view import view_config
 
 from lms.security import Permissions
 from lms.services.moodle import MoodleAPIClient
+from lms.views import helpers
 
 DOCUMENT_URL_REGEX = re.compile(r"moodle:\/\/file\/url\/(?P<url>.*)")
 
@@ -19,3 +20,21 @@ def list_files(_context, request):
     return request.find_service(MoodleAPIClient).list_files(
         request.matchdict["course_id"]
     )
+
+
+@view_config(
+    request_method="GET",
+    route_name="moodle_api.courses.files.via_url",
+    renderer="json",
+    permission=Permissions.API,
+)
+def via_url(_context, request):
+    token = request.find_service(MoodleAPIClient).token
+
+    document_url = request.params["document_url"]
+    url = DOCUMENT_URL_REGEX.search(document_url)["url"]
+    return {
+        "via_url": helpers.via_url(
+            request, url, content_type="pdf", query={"token": token}
+        )
+    }
