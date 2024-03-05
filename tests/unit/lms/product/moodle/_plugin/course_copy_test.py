722b6f44aedb21baa3caa31f6115566255ca9477
Marcos Prieto <marcospri@gmail.com>
2024-03-05 12:43:52 +0100

    Fix for Moodle files course copy

--- tests/unit/lms/product/moodle/_plugin/course_copy_test.py
+++ tests/unit/lms/product/moodle/_plugin/course_copy_test.py
@@ -20,6 +20,42 @@ class TestMoodleCourseCopyPlugin:
             == course_copy_groups_helper.find_matching_group_set_in_course.return_value
         )
 
+    def test_find_matching_file_in_course(
+        self, plugin, course_copy_files_helper, d2l_api_client
+    ):
+        result = plugin.find_matching_file_in_course(
+            sentinel.original_file_id, sentinel.new_course_id
+        )
+
+        course_copy_files_helper.find_matching_file_in_course(
+            d2l_api_client.list_files,
+            "moodle_file",
+            sentinel.original_file_id,
+            sentinel.new_course_id,
+        )
+
+        assert (
+            result == course_copy_files_helper.find_matching_file_in_course.return_value
+        )
+
+    def test_find_matching_page_in_course(
+        self, plugin, course_copy_files_helper, moodle_api_client
+    ):
+        result = plugin.find_matching_page_in_course(
+            sentinel.page_id, sentinel.course_id
+        )
+
+        course_copy_files_helper.find_matching_file_in_course.assert_called_once_with(
+            moodle_api_client.list_pages,
+            "moodle_page",
+            sentinel.page_id,
+            sentinel.course_id,
+        )
+
+        assert (
+            result == course_copy_files_helper.find_matching_file_in_course.return_value
+        )
+
     @pytest.mark.usefixtures(
         "moodle_api_client", "course_copy_files_helper", "course_copy_groups_helper"
     )
