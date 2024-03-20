c0aea1c6806a868070d62ba068affa772ab2c4ce
Marcos Prieto <marcospri@gmail.com>
2024-03-20 11:54:28 +0100

    Use function level imports to avoid circular dependencies with product

--- lms/product/factory.py
+++ lms/product/factory.py
@@ -1,20 +1,23 @@
-from lms.product.blackboard import Blackboard
-from lms.product.canvas import Canvas
-from lms.product.d2l import D2L
 from lms.product.family import Family
-from lms.product.moodle import Moodle
 from lms.product.product import Product
-
-_PRODUCT_MAP = {
-    product.family: product for product in (Blackboard, Canvas, D2L, Moodle)
-}
 
 
 def get_product_from_request(request) -> Product:
     """Get the correct product object from the provided request."""
+
+    # pylint:disable=import-outside-toplevel
+    from lms.product.blackboard import Blackboard
+    from lms.product.canvas import Canvas
+    from lms.product.d2l import D2L
+    from lms.product.moodle import Moodle
+
     ai = request.lti_user.application_instance
     family = Family(request.lti_user.lti.product_family)
 
-    product = _PRODUCT_MAP.get(family, Product).from_request(request, dict(ai.settings))
+    product_class = {
+        product.family: product for product in (Blackboard, Canvas, D2L, Moodle)
+    }.get(family, Product)
+
+    product = product_class.from_request(request, dict(ai.settings))
     product.family = family
     return product
