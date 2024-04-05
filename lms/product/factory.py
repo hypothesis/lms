a7fc09a89cd000222af2fb665c143fb0837b35c4
Marcos Prieto <marcospri@gmail.com>
2024-04-05 09:31:21 +0200

    Fix bug to get product family while on the admin pages
    In the admin pages, where there's no LTIUSer using any of the services
    that are built differently per-product we need to set the product to
    UNKNOWN to get the default behaviour

--- lms/product/factory.py
+++ lms/product/factory.py
@@ -5,19 +5,23 @@ from lms.product.product import Product
 def get_product_from_request(request) -> Product:
     """Get the correct product object from the provided request."""
 
-    # pylint:disable=import-outside-toplevel
+    # pylint:disable=import-outside-toplevel,redefined-variable-type
     from lms.product.blackboard import Blackboard
     from lms.product.canvas import Canvas
     from lms.product.d2l import D2L
     from lms.product.moodle import Moodle
 
-    ai = request.lti_user.application_instance
-    family = Family(request.lti_user.lti.product_family)
+    family = Family.UNKNOWN
+    settings = {}
+    if request.lti_user:
+        ai = request.lti_user.application_instance
+        family = Family(request.lti_user.lti.product_family)
+        settings = ai.settings
 
     product_class = {
         product.family: product for product in (Blackboard, Canvas, D2L, Moodle)
     }.get(family, Product)
 
-    product = product_class.from_request(request, dict(ai.settings))
+    product = product_class.from_request(request, dict(settings))
     product.family = family
     return product
