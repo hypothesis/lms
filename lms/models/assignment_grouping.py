9ded157f4611c8d0298d3f0024a44a05cff778d4
Marcos Prieto <marcospri@gmail.com>
2024-04-08 16:07:46 +0200

    Add relationship from assignment to groupings

--- lms/models/assignment_grouping.py
+++ lms/models/assignment_grouping.py
@@ -14,11 +14,15 @@ class AssignmentGrouping(CreatedUpdatedMixin, Base):
         sa.ForeignKey("assignment.id", ondelete="cascade"),
         primary_key=True,
     )
-    assignment = sa.orm.relationship("Assignment", foreign_keys=[assignment_id])
+    assignment = sa.orm.relationship(
+        "Assignment", foreign_keys=[assignment_id], backref="assignment_grouping"
+    )
     """The assignment."""
 
     grouping_id = sa.Column(
         sa.Integer(), sa.ForeignKey("grouping.id", ondelete="cascade"), primary_key=True
     )
-    grouping = sa.orm.relationship("Grouping", foreign_keys=[grouping_id])
+    grouping = sa.orm.relationship(
+        "Grouping", foreign_keys=[grouping_id], backref="groupings"
+    )
     """The grouping the assignment is a part of."""
