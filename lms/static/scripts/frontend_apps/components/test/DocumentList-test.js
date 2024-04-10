f52b710aa9353b2ed5eb55b032e0907632554a16
Robert Knight <robertknight@gmail.com>
2024-04-10 13:30:30 +0100

    Add support for rendering thumbnails in file picker
    The initial use case is for Canvas Studio videos.

--- lms/static/scripts/frontend_apps/components/test/DocumentList-test.js
+++ lms/static/scripts/frontend_apps/components/test/DocumentList-test.js
@@ -44,6 +44,13 @@ describe('DocumentList', () => {
     // Folder
     { type: 'Folder', icon: 'FolderIcon' },
 
+    // Files with a thumbnail
+    {
+      type: 'File',
+      mime_type: 'video',
+      thumbnail_url: 'https://example.local/thumbnail.jpg',
+    },
+
     // Files with known mime type
     { type: 'File', mime_type: 'application/pdf', icon: 'FilePdfFilledIcon' },
     { type: 'File', mime_type: 'text/html', icon: 'FileGenericIcon' },
@@ -51,20 +58,53 @@ describe('DocumentList', () => {
 
     // File with unknown mime type
     { type: 'File', icon: 'FileGenericIcon' },
-  ].forEach(({ type, mime_type, icon }) => {
+  ].forEach(({ type, mime_type, icon, thumbnail_url }) => {
     it('renders documents with an icon, document name and date', () => {
       const wrapper = renderDocumentList({
-        documents: [{ ...testDocuments[0], type, mime_type }],
+        documents: [{ ...testDocuments[0], type, mime_type, thumbnail_url }],
       });
       const formattedDate = new Date(testDocuments[0]).toLocaleDateString();
       const dataRow = wrapper.find('tbody tr').at(0);
-      assert.isTrue(dataRow.find(icon).exists());
       assert.equal(
         dataRow.find('td').at(0).text(),
         testDocuments[0].display_name,
       );
+
+      if (icon) {
+        assert.isTrue(dataRow.exists(icon));
+      } else {
+        assert.isFalse(dataRow.exists('[data-testid="icon"]'));
+      }
+
+      if (thumbnail_url) {
+        const thumbnail = dataRow.find('[data-testid="thumbnail"]');
+        assert.isTrue(thumbnail.exists());
+        assert.equal(thumbnail.prop('src'), thumbnail_url);
+      }
+
       assert.equal(dataRow.find('td').at(1).text(), formattedDate);
     });
+  });
+
+  it('renders fallback if thumbnail fails to load', () => {
+    const wrapper = renderDocumentList({
+      documents: [
+        {
+          ...testDocuments[0],
+          type: 'File',
+          mime_type: 'video',
+          thumbnail_url: 'https://example.local/thumbnail.jpg',
+        },
+      ],
+    });
+    const thumbnail = wrapper.find('img[data-testid="thumbnail"]');
+    assert.isTrue(thumbnail.exists());
+
+    thumbnail.simulate('error');
+    wrapper.update();
+
+    const fallback = wrapper.find('[data-testid="thumbnail-fallback"]');
+    assert.isTrue(fallback.exists());
   });
 
   it('renders a explanatory message when there are no documents', () => {
