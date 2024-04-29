0c20af65e3becdba8085143f6a9eb455760d95ea
Marcos Prieto <marcospri@gmail.com>
2024-04-29 14:57:30 +0200

    Add a new test for RSA serialization with a hardcoded values
    To make it easier to change RSA / JWK backends

--- tests/unit/lms/services/rsa_key_service_test.py
+++ tests/unit/lms/services/rsa_key_service_test.py
@@ -103,6 +103,15 @@ class TestAESService:
 
         assert key in valid_keys
 
+    def test__as_jwk_public_key(self, svc, rsa_key):
+        # pylint: disable=protected-access
+        assert svc._as_jwk_public_key(rsa_key) == {
+            "alg": "RS256",
+            "kty": "RSA",
+            "n": "2BNk2-UPDI3ihR8MXMi11xNzaaS0NiVDTzA7MczlRh9WAqRW3onaCmNgFXmq-hHy1HKIPohMn_N9TvmtrvhTQV_skn4lADYyfxFTsKZkFPJWoOx3sbRJPZVx7HiPaHb72zpTfhwsg5HHa3Sn5Qs8Fybyo33sjbTX1sGZUCq3JGbtEgOii2j19ICS-87AXcMFmCGkaTCFwDoQ38swYDU1YjwhINxx-N5hU19vxA8ZmNexnrpKhvF9C2ejS63xJxMCInfJZFb-Yh2zrzExMLQLpTUEBd0tw1ng66xqkB2LUwr0j-Zdo5_swEepQuOtuLq9omFP345kYIk6IIl0NMcnpQ",
+            "e": "AQAB",
+        }
+
     @pytest.fixture
     def valid_keys(self):
         return factories.RSAKey.create_batch(
@@ -131,6 +140,11 @@ class TestAESService:
     @pytest.fixture
     def rsa(self, patch):
         return patch("lms.services.rsa_key.rsa")
+
+    @pytest.fixture
+    def rsa_key(self):
+        key_data = b"-----BEGIN RSA PRIVATE KEY-----\nMIIEpAIBAAKCAQEA2BNk2+UPDI3ihR8MXMi11xNzaaS0NiVDTzA7MczlRh9WAqRW\n3onaCmNgFXmq+hHy1HKIPohMn/N9TvmtrvhTQV/skn4lADYyfxFTsKZkFPJWoOx3\nsbRJPZVx7HiPaHb72zpTfhwsg5HHa3Sn5Qs8Fybyo33sjbTX1sGZUCq3JGbtEgOi\ni2j19ICS+87AXcMFmCGkaTCFwDoQ38swYDU1YjwhINxx+N5hU19vxA8ZmNexnrpK\nhvF9C2ejS63xJxMCInfJZFb+Yh2zrzExMLQLpTUEBd0tw1ng66xqkB2LUwr0j+Zd\no5/swEepQuOtuLq9omFP345kYIk6IIl0NMcnpQIDAQABAoIBAAWMdyXVcg3WU+rF\nIBeYeODy/F79YYciOe8ETYGku364PcikfAcyPr6CP3yo5FZ2uqFj51jeit2XQcPG\nK7fHSgRvAex5UrQFcEXTX4goOBnSM+rLsvKSu/tZ0kEQ3H/oGrIa9pUE6xi3pOMY\nlECQiaSMXCVAjdNN9LZfwAyvHocpYf8GFo/NBmf1vheDReFiO9Rpx9QzYOO8G6i7\n26aBIiQOHHd+utXF6UXaOW+CHT0fTUWLbUVeaCIWJWdcFzkTcaVGBdEj+H8wfjb3\nW6xwZ/0b7Nma4z+OB1nQ9mCc6+OVoPRylpjP8KAkqGg3sD0TXdolSxdriKGGCKNv\nBqf0iSECgYEA7mIw1hS0hElhlAZqdMFEc761Kect/8btGOZ8vEoVVea4d7a+NJuZ\n8NKRZ7ROcYZjirE3QXAVgGQZTP9S4XuZITVF5DFsR1jjHSKjO/X8LTjo4WzTH1ol\nAJW6zEC8Wpx8QRwfjPQi5IJfnLf1cAJiLuQzrk+XRNvUUa+Am+cEu0UCgYEA6Ast\ng5R7sHSqHnXlMppPNvJQy4bK70+G2VEJF4nY+yVo8n8l/D5zG83X4feepuUvoiAV\nE9/1DCHUSj/9XJlPpgaA/QSVuSoCzLkVFv3QnwMV82hRr6sBw+QlO0mnGfyE/Xl1\niLKSGhCEqGMXuqvyunViheX/QOIpxnTnVffFUOECgYEAtM+bdTXdOh2iurEnHAnf\nzt8O8Iqd9EzBV8qzP+n9RtuqfXsJyENhvy0oBv3XJfqmE/OZErReSrUiD6n2BntG\nSc7rhUsLcw5zrYyxPXC15uMsmJd/h19Lg6cOOZC8jQn2oTggojwnHyXYObm6m0vj\nhuemX4eVGDCZWaABr77JcvUCgYAWjree+flIPx8mlOlyEOQwgD/weSsTNpTyXVlX\n63OnfoyEPm4P5nZENq+M7QiESvVlel7yLqxgwI0lprDXpqPCjRFPB3oSpQ3enwN4\n17XHL4KbxgFi5WnnhC9GYzOWaCD5jywo3MstM3vh7rgo0nxnOfAY+jHlOdc7zrOK\nfOq2oQKBgQDtSLLelV/u24iHfsOVMO5Wpdytdkls3uG410vlgMteytn682vg4knT\n+0x4M3CnQoZ6l9MpuEFB/a9Ya6kZsL3n3duQwMcOYj9YF5Oj6xzlVTGdTM/+1SFK\nAHCI/NHS3Ap2RljTSiMKpN3eRAdp3KoOJOvMCIHl6MMoRp2bVPgplw==\n-----END RSA PRIVATE KEY-----\n"
+        return serialization.load_pem_private_key(key_data, None)
 
 
 class TestFactory:
