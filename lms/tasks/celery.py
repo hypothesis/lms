e8ba64758d99933000d3edfedace1e34ceb82a33
Marcos Prieto <marcospri@gmail.com>
2024-05-08 13:49:38 +0200

    Check broad-exception with ruff

--- lms/tasks/celery.py
+++ lms/tasks/celery.py
@@ -82,7 +82,7 @@ def bootstrap_worker(sender, **_kwargs):  # pragma: no cover
     try:
         lms = create_app(None, **{})
 
-    except Exception:  # pylint: disable=broad-except
+    except Exception:  # noqa: BLE001
         # If we don't bail out here ourselves, Celery just hides the error
         # and continues. This means `request_context` will not be available
         # when attempting to run tasks, which is impossible to debug.
