4d3991e5a89598215f36244719bc56fd52e87580
Robert Knight <robertknight@gmail.com>
2024-05-30 13:16:49 +0100

    Omit authorization route from Canvas Studio `viaUrl` config for non-admins
    Certain errors [1] when launching an assignment can result in an error
    dialog in the frontend with a "Try again" button which re-authorizes the
    user and then re-fetches the Via URL.
    
    For Canvas Studio assignments, the Via URL fetch uses the Canvas Studio admin's
    token. We don't want to make other users go through the authorization flow.
    Omit the `authUrl` from the `viaUrl` configuration for non-admin users to
    prevent this.
    
    [1] Unfortunately error handling is set up to initiate a re-authorization in
        the default error handling path just in case it might help, instead of
        only initiating the launch flow when there is definitely an authorization
        problem. As a result we end up initiating a re-authorization unnecessarily
        in many cases. I'd like to change this in future.

--- lms/resources/_js_config/__init__.py
+++ lms/resources/_js_config/__init__.py
@@ -11,7 +11,14 @@ from lms.product.blackboard import Blackboard
 from lms.product.canvas import Canvas
 from lms.product.d2l import D2L
 from lms.resources._js_config.file_picker_config import FilePickerConfig
-from lms.services import HAPI, EventService, HAPIError, JSTORService, VitalSourceService
+from lms.services import (
+    HAPI,
+    CanvasStudioService,
+    EventService,
+    HAPIError,
+    JSTORService,
+    VitalSourceService,
+)
 from lms.validation.authentication import BearerTokenSchema
 from lms.views.helpers import via_url
 
@@ -82,8 +89,18 @@ class JSConfig:
             }
 
         elif document_url.startswith("canvas-studio://media"):
+            canvas_studio_svc = self._request.find_service(CanvasStudioService)
+
+            # Requests for the transcript and video are made as the admin user.
+            # Only set `authUrl` if the current user is the admin to make sure
+            # we don't unnecessarily launch the auth flow for other users.
+            if canvas_studio_svc.is_admin():
+                auth_url = self._request.route_url("canvas_studio_api.oauth.authorize")
+            else:
+                auth_url = None
+
             self._config["api"]["viaUrl"] = {
-                "authUrl": self._request.route_url("canvas_studio_api.oauth.authorize"),
+                "authUrl": auth_url,
                 "path": self._request.route_path("canvas_studio_api.via_url"),
             }
 
