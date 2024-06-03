464097e1b852f608cee2ec7468e5e2831cf94600
Marcos Prieto <marcospri@gmail.com>
2024-06-03 15:05:18 +0200

    Option to generate usage report from a celery task
    Store the results in the DB for:
    
    - Retrieval of the report without having to do the query work
    - Option to graph the results in metabase over a period of time.
    
    This commit add the option to generate these in an async (celery) task
    but not code uses it yet.
    
    The plan is to write a second  task that schedules periodic reports for
    ongoing company deals.

---
+++ lms/models/organization_usage.py
@@ -0,0 +1,37 @@
+from datetime import date
+
+from sqlalchemy import ForeignKey, Integer
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from lms.db import Base
+from lms.models._mixins import CreatedUpdatedMixin
+from lms.models.organization import Organization
+
+
+class OrganizationUsageReport(CreatedUpdatedMixin, Base):
+    __tablename__ = "organization_usage_report"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+
+    organization_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("organization.id", ondelete="cascade")
+    )
+    organization: Mapped[Organization] = relationship(backref="reports")
+
+    tag: Mapped[str] = mapped_column()
+
+    key: Mapped[str] = mapped_column(unique=True)
+    """Key that idenfities this report"""
+
+    since: Mapped[date | None]
+    until: Mapped[date | None]
+
+    unique_users: Mapped[int | None] = mapped_column()
+
+    report: Mapped[list[dict] | None] = mapped_column(JSONB())
+    """Actual report, in JSON format."""
+
+    @classmethod
+    def generate_key(cls, organization, tag, report_start, report_end):
+        return f"{organization.public_id}-{tag}-{report_start}-{report_end}"
