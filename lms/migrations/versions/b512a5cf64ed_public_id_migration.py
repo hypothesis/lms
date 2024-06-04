e2c1bce4b7fccfb3e45c5e0cbfdb0a5462c31d58
Marcos Prieto <marcospri@gmail.com>
2024-06-04 13:11:35 +0200

    Migration to include the full "us.lms.org" in Organization.public_id

---
+++ lms/migrations/versions/b512a5cf64ed_public_id_migration.py
@@ -0,0 +1,33 @@
+"""Public ID migration.
+
+Revision ID: b512a5cf64ed
+Revises: 329313b38de1
+"""
+
+import os
+
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.dialects import postgresql
+
+revision = "b512a5cf64ed"
+down_revision = "329313b38de1"
+
+
+def upgrade() -> None:
+    region = os.environ["REGION_CODE"]
+    conn = op.get_bind()
+    conn.execute(
+        sa.text(
+            f"""UPDATE "organization" set public_id = '{region}.lms.org.' || "public_id";"""
+        )
+    )
+
+
+def downgrade() -> None:
+    conn = op.get_bind()
+    conn.execute(
+        sa.text(
+            """UPDATE "organization" set public_id = split_part("public_id", '.', 4);"""
+        )
+    )
