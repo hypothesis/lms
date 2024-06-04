1b91216c61b56aac77d2c28919dd084d1ad6a7d1
Alejandro Celaya <alejandrocelaya@gmail.com>
2024-06-04 10:33:57 +0200

    Add breadcrumbs to the dashboard views (#6298)

--- lms/static/scripts/frontend_apps/components/dashboard/test/AssignmentActivity-test.js
+++ lms/static/scripts/frontend_apps/components/dashboard/test/AssignmentActivity-test.js
@@ -37,7 +37,14 @@ describe('AssignmentActivity', () => {
   beforeEach(() => {
     fakeUseAPIFetch = sinon.stub().callsFake(url => ({
       isLoading: false,
-      data: url.endsWith('stats') ? students : { title: 'The title' },
+      data: url.endsWith('stats')
+        ? students
+        : {
+            title: 'The title',
+            course: {
+              title: 'The course',
+            },
+          },
     }));
     fakeConfig = {
       dashboard: {
@@ -72,7 +79,7 @@ describe('AssignmentActivity', () => {
     fakeUseAPIFetch.returns({ isLoading: true });
 
     const wrapper = createComponent();
-    const titleElement = wrapper.find('CardTitle[data-testid="title"]');
+    const titleElement = wrapper.find('[data-testid="title"]');
     const tableElement = wrapper.find('OrderableActivityTable');
 
     assert.equal(titleElement.text(), 'Loading...');
@@ -83,7 +90,7 @@ describe('AssignmentActivity', () => {
     fakeUseAPIFetch.returns({ error: new Error('Something failed') });
 
     const wrapper = createComponent();
-    const titleElement = wrapper.find('CardTitle[data-testid="title"]');
+    const titleElement = wrapper.find('[data-testid="title"]');
     const tableElement = wrapper.find('OrderableActivityTable');
 
     assert.equal(titleElement.text(), 'Could not load assignment title');
@@ -92,9 +99,9 @@ describe('AssignmentActivity', () => {
 
   it('shows expected title', () => {
     const wrapper = createComponent();
-    const titleElement = wrapper.find('CardTitle[data-testid="title"]');
+    const titleElement = wrapper.find('[data-testid="title"]');
     const tableElement = wrapper.find('OrderableActivityTable');
-    const expectedTitle = `Assignment: The title`;
+    const expectedTitle = 'Assignment: The title';
 
     assert.equal(titleElement.text(), expectedTitle);
     assert.equal(tableElement.prop('title'), expectedTitle);
---
+++ lms/static/scripts/frontend_apps/components/dashboard/test/DashboardBreadcrumbs-test.js
@@ -0,0 +1,33 @@
+import { checkAccessibility } from '@hypothesis/frontend-testing';
+import { mount } from 'enzyme';
+
+import DashboardBreadcrumbs from '../DashboardBreadcrumbs';
+
+describe('DashboardBreadcrumbs', () => {
+  function createComponent(props = {}) {
+    return mount(<DashboardBreadcrumbs {...props} />);
+  }
+
+  [['foo', 'bar'], [], ['one', 'two', 'three']].forEach(links => {
+    it('shows expected amount of links', () => {
+      const wrapper = createComponent({
+        links: links.map(title => ({ title, href: `/${title}` })),
+      });
+
+      assert.equal(wrapper.find('BreadcrumbLink').length, links.length);
+    });
+  });
+
+  it(
+    'should pass a11y checks',
+    checkAccessibility({
+      content: () =>
+        createComponent({
+          links: [
+            { title: 'Foo', href: '/foo' },
+            { title: 'Bar', href: '/bar' },
+          ],
+        }),
+    }),
+  );
+});
