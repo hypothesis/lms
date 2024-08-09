1805e8efa5ee79e2dddc429d0e0d1872f33c20c2
Marcos Prieto <marcospri@gmail.com>
2024-08-09 16:38:44 +0200

    Create indexes for foreign keys missing them
    Non-unique FKs don't get automated indexes on the referencing side.
    AssignmentGrouping and AssignmentMembership have a primary key composed
    of two foreign keys, while the PK has index there's no index on the
    individual columns that we often use in isolation.
    
    - Assignment.course_id FK without an index
    - AssignmentGrouping.assignment_id, FK without and index, although part
      of the PK of AssignmentGrouping
    - AssignmentGrouping.groping_id, FK without and index, although part
      of the PK of AssignmentGrouping
    - AssignmentMembership.assignment_id, FK without and index, although part
      of the PK of AssignmentMembership
    - GroupingMembership.user_id, FK without an index

---
+++ lms/migrations/versions/3d0c022c716c_add_missing_indexes_for_fks.py
@@ -0,0 +1,57 @@
+"""Add missing indexes for FKs."""
+
+import sqlalchemy as sa
+from alembic import op
+
+revision = "3d0c022c716c"
+down_revision = "7858a12c4e4a"
+
+
+def upgrade() -> None:
+    # CONCURRENTLY can't be used inside a transaction. Finish the current one.
+    op.execute("COMMIT")
+
+    op.create_index(
+        op.f("ix__assignment_grouping_assignment_id"),
+        "assignment_grouping",
+        ["assignment_id"],
+        unique=False,
+        postgresql_concurrently=True,
+    )
+    op.create_index(
+        op.f("ix__assignment_grouping_grouping_id"),
+        "assignment_grouping",
+        ["grouping_id"],
+        unique=False,
+        postgresql_concurrently=True,
+    )
+    op.create_index(
+        op.f("ix__assignment_membership_assignment_id"),
+        "assignment_membership",
+        ["assignment_id"],
+        unique=False,
+        postgresql_concurrently=True,
+    )
+    op.create_index(
+        op.f("ix__grouping_membership_user_id"),
+        "grouping_membership",
+        ["user_id"],
+        unique=False,
+        postgresql_concurrently=True,
+    )
+
+
+def downgrade() -> None:
+    op.drop_index(
+        op.f("ix__grouping_membership_user_id"), table_name="grouping_membership"
+    )
+    op.drop_index(
+        op.f("ix__assignment_membership_assignment_id"),
+        table_name="assignment_membership",
+    )
+    op.drop_index(
+        op.f("ix__assignment_grouping_grouping_id"), table_name="assignment_grouping"
+    )
+    op.drop_index(
+        op.f("ix__assignment_grouping_assignment_id"), table_name="assignment_grouping"
+    )
