4f3cc1a07ec202ac78b27db06c52db60409dc173
Marcos Prieto <marcospri@gmail.com>
2024-08-14 16:37:18 +0200

    Model for LMSUSer

--- lms/models/user.py
+++ lms/models/user.py
@@ -47,3 +47,44 @@ class User(CreatedUpdatedMixin, Base):
 
     display_name: Mapped[str | None] = mapped_column(sa.Unicode, index=True)
     """The user's display name."""
+
+
+class LMSUser(CreatedUpdatedMixin, Base):
+    """A user record for each LMS user."""
+
+    __tablename__ = "lms_user"
+    __table_args__ = (sa.UniqueConstraint("tool_consumer_instance_guid", "lti_id"),)
+
+    id = sa.Column(sa.Integer, autoincrement=True, primary_key=True)
+
+    tool_consumer_instance_guid: Mapped[str | None] = mapped_column(
+        sa.UnicodeText, index=True
+    )
+
+    h_userid: Mapped[str] = mapped_column(sa.Unicode, unique=True, index=True)
+    """The H userid which is created from LTI provided values."""
+
+    lti_id: Mapped[str] = mapped_column(sa.Unicode, index=True)
+    """ID of this user in the LMS, via LTI"""
+
+    email: Mapped[str | None] = mapped_column(sa.Unicode)
+    """Email address of the user"""
+
+    name: Mapped[str | None] = mapped_column(sa.Unicode, index=True)
+    """The user's display name."""
+
+
+class LMSUserApplicationInstance(CreatedUpdatedMixin, Base):
+    __tablename__ = "lms_user_application_instance"
+
+    __table_args__ = (sa.UniqueConstraint("application_instance_id", "lms_user_id"),)
+
+    id = sa.Column(sa.Integer, autoincrement=True, primary_key=True)
+
+    application_instance_id: Mapped[int] = sa.Column(
+        sa.ForeignKey("application_instances.id", ondelete="cascade"),
+    )
+
+    lms_user_id: Mapped[int] = sa.Column(
+        sa.ForeignKey("lms_user.id", ondelete="cascade"),
+    )
