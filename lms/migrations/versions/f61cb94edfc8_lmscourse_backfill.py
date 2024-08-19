567fcd3847237d517589d677ed49793b61e5be82
Marcos Prieto <marcospri@gmail.com>
2024-08-19 13:33:48 +0200

    Backfill LMSCourse

---
+++ lms/migrations/versions/f61cb94edfc8_lmscourse_backfill.py
@@ -0,0 +1,51 @@
+"""LMSCourse backfill
+
+Revision ID: f61cb94edfc8
+Revises: 9f31d4427a34
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = "f61cb94edfc8"
+down_revision = "9f31d4427a34"
+
+
+def upgrade() -> None:
+    conn = op.get_bind()
+    conn.execute(
+        sa.text(
+            """
+        WITH backfill as (
+            select distinct on (authority_provided_id)
+                "grouping".created, "grouping".updated,
+                tool_consumer_instance_guid, authority_provided_id, lms_name, lms_id
+            from "grouping"
+            join application_instances on application_instances.id = "grouping".application_instance_id
+            where grouping.type ='course'
+            order by authority_provided_id, "grouping".created desc
+        )
+        INSERT INTO lms_course (
+             created,
+             updated,
+             tool_consumer_instance_guid,
+             authority_provided_id,
+             lti_id,
+             name
+        )
+        SELECT
+           created,
+           updated,
+           tool_consumer_instance_guid,
+           authority_provided_id,
+           lms_id,
+           lms_name
+        FROM backfill
+        ON CONFLICT (tool_consumer_instance_guid, lti_id) DO NOTHING
+    """
+        )
+    )
+
+
+def downgrade() -> None:
+    pass
