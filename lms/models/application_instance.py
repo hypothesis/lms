d9d3665ba338300b88fe5dbe6a2cb9bf1084a525
Marcos Prieto <marcospri@gmail.com>
2024-09-16 10:26:25 +0200

    Add a feature flag for the sync grade side of auto grading
    This will allow us to expose the configuring and calculating side of
    auto grading while we work on the syncing part.

--- lms/models/application_instance.py
+++ lms/models/application_instance.py
@@ -56,6 +56,7 @@ class ApplicationSettings(JSONSettings):
         JSONSetting("hypothesis", "instructor_email_digests_enabled", asbool),
         JSONSetting("hypothesis", "lti_13_sourcedid_for_grading", asbool),
         JSONSetting("hypothesis", "auto_grading_enabled", asbool),
+        JSONSetting("hypothesis", "auto_grading_sync_enabled", asbool),
     )
 
 
