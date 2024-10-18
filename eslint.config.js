cdbbd4626272508039b8a23794204bfe78d8b3f7
Alejandro Celaya <alejandro.celaya@hypothes.is>
2024-10-18 14:53:46 +0200

    Use TS and JSX shared configs from eslint-config-hypothesis

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
-import hypothesis from 'eslint-config-hypothesis';
-import jsxA11y from 'eslint-plugin-jsx-a11y';
+import hypothesisBase from 'eslint-config-hypothesis/base';
+import hypothesisJSX from 'eslint-config-hypothesis/jsx';
+import hypothesisTS from 'eslint-config-hypothesis/ts';
 import globals from 'globals';
-import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default [
   {
     ignores: [
       '.tox/**/*',
@@ -14,38 +14,10 @@ export default tseslint.config(
       'docs/_build/*',
     ],
   },
-  ...hypothesis,
-  ...tseslint.configs.recommended,
-  jsxA11y.flatConfigs.recommended,
-  {
-    rules: {
-      'prefer-arrow-callback': 'error',
-      'prefer-const': ['error', { destructuring: 'all' }],
 
-      // Prop validation is performed by TypeScript + JSDoc.
-      'react/prop-types': 'off',
-
-      // Upgrade TS rules from warning to error.
-      '@typescript-eslint/no-unused-vars': 'error',
-
-      // Disable TS rules that we dislike.
-      '@typescript-eslint/ban-ts-comment': 'off',
-      '@typescript-eslint/no-empty-function': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-non-null-assertion': 'off',
-      '@typescript-eslint/no-this-alias': 'off',
-
-      // Enforce consistency in cases where TypeScript supports old and new
-      // syntaxes for the same thing.
-      //
-      // - Require `<var> as <type>` for casts
-      // - Require `import type` for type imports. The corresponding rule for
-      //   exports is not enabled yet because that requires setting up type-aware
-      //   linting.
-      '@typescript-eslint/consistent-type-assertions': 'error',
-      '@typescript-eslint/consistent-type-imports': 'error',
-    },
-  },
+  ...hypothesisBase,
+  ...hypothesisJSX,
+  ...hypothesisTS,
 
   // Additional rules that require type information. These can only be run
   // on files included in the TS project by tsconfig.json.
@@ -72,4 +44,4 @@ export default tseslint.config(
       },
     },
   },
-);
+];
