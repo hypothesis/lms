d1e73904aaa0d3d67b9bb55dfa55b33fabdcf320
Marcos Prieto <marcospri@gmail.com>
2024-10-30 12:45:24 +0100

    PoC segments new table

---
+++ lms/models/lms_segment.py
@@ -0,0 +1,84 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import ForeignKey, UniqueConstraint
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from lms.db import Base, varchar_enum
+from lms.models import Grouping
+from lms.models._mixins import CreatedUpdatedMixin
+
+if TYPE_CHECKING:
+    from lms.models import LMSUser, LTIRole
+
+
+class LMSGroupSet(CreatedUpdatedMixin, Base):
+    """Group sets are differt ways to device a course's roster into groups.
+
+    Segments that represent groups belong to one group set.
+
+    Some LMS call this concept a "Group category" instead.
+    """
+
+    __tablename__ = "lms_group_set"
+
+    __table_args__ = (UniqueConstraint("lms_course_id", "lms_id"),)
+
+    id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True)
+
+    lms_id: Mapped[str] = mapped_column(index=True)
+    """ID of this group set in the LMS"""
+
+    lms_course_id: Mapped[int | None] = mapped_column(ForeignKey("lms_course.id"))
+    """ID of the course this group set belongs to"""
+
+
+class LMSSegment(CreatedUpdatedMixin, Base):
+    """Segments represent subdivisions of students in a course (i.e sections and groups)."""
+
+    __tablename__ = "lms_segment"
+
+    id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True)
+
+    type = varchar_enum(Grouping.Type, nullable=False)
+
+    lms_id: Mapped[str] = mapped_column(index=True)
+    """ID of this segment in the LMS"""
+
+    h_authority_provided_id: Mapped[str] = mapped_column(unique=True, index=True)
+    """The Group.authority_provided_id value in H."""
+
+    lms_course_id: Mapped[int | None] = mapped_column(ForeignKey("lms_course.id"))
+    """ID of the course this segment belongs to"""
+
+    name: Mapped[str] = mapped_column()
+
+    lms_group_set_id: Mapped[int | None] = mapped_column(
+        ForeignKey("lms_group_set.id", ondelete="cascade"), index=True
+    )
+    # TODO CHECK constraint for require this groups
+    """For groups, the group set they belong to"""
+
+
+class LMSSegmentMembership(CreatedUpdatedMixin, Base):
+    """Membership information we have acquired via LTI launches."""
+
+    __tablename__ = "lms_segment_membership"
+    __table_args__ = (UniqueConstraint("lms_segment_id", "lms_user_id", "lti_role_id"),)
+
+    id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True)
+
+    lms_segment_id: Mapped[int] = mapped_column(
+        ForeignKey("lms_segment.id", ondelete="cascade"), index=True
+    )
+    lms_segment: Mapped[LMSSegment] = relationship()
+
+    lms_user_id: Mapped[int] = mapped_column(
+        ForeignKey("lms_user.id", ondelete="cascade"), index=True
+    )
+    lms_user: Mapped["LMSUser"] = relationship()
+
+    lti_role_id: Mapped[int] = mapped_column(
+        ForeignKey("lti_role.id", ondelete="cascade"),
+        index=True,
+    )
+    lti_role: Mapped["LTIRole"] = relationship()
