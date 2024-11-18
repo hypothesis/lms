9efc1a9e9122fabfbfce8ccfa459dcf21ee7e8e9
Marcos Prieto <marcospri@gmail.com>
2024-11-18 15:44:05 +0100

    Alias user_id to lms_user_id on LMSUSer for compatibility with User

---
+++ tests/unit/lms/models/lms_user_test.py
@@ -0,0 +1,10 @@
+from lms.models import RSAKey
+from tests import factories
+
+
+class TestLMSUser:
+    def test_user_id(self):
+        lms_user = factories.LMSUser()
+
+        assert lms_user.lti_user_id
+        assert lms_user.lti_user_id == lms_user.user_id
