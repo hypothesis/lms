8380f29b0fe6d032c2a037eaed87a3623c727d31
Marcos Prieto <marcospri@gmail.com>
2024-11-21 16:20:33 +0100

    Revert "Expose basic roster information on the admin pages"
    We are starting exposing this directly on the dashboards, no need to
    include them on the admin pages as well.
    
    This reverts commit 9393b2f7f78db5eb0de462e7102324cd9b5f2060.

--- tests/unit/lms/views/admin/assignment_test.py
+++ tests/unit/lms/views/admin/assignment_test.py
@@ -9,21 +9,16 @@ from lms.views.admin.assignment import AdminAssignmentViews
 from tests import factories
 
 
-@pytest.mark.usefixtures("roster_service")
 class TestAdminAssignmentViews:
-    def test_show(self, pyramid_request, assignment_service, views, roster_service):
+    def test_show(self, pyramid_request, assignment_service, views):
         pyramid_request.matchdict["id_"] = sentinel.id
 
         response = views.show()
 
         assignment_service.get_by_id.assert_called_once_with(id_=sentinel.id)
-        roster_service.get_assignment_roster.assert_called_once_with(
-            assignment_service.get_by_id.return_value
-        )
 
         assert response == {
             "assignment": assignment_service.get_by_id.return_value,
-            "roster": roster_service.get_assignment_roster.return_value,
         }
 
     def test_show_not_found(self, pyramid_request, assignment_service, views):
