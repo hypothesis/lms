52498bd6e0b468c916aaffb1f3c6ed608b08771f
Marcos Prieto <marcospri@gmail.com>
2024-12-06 14:35:17 +0100

    Schedule fetching canvas groups rosters
    Use the same logic as for assignments and courses.

--- tests/unit/lms/tasks/roster_test.py
+++ tests/unit/lms/tasks/roster_test.py
@@ -7,9 +7,11 @@ from freezegun import freeze_time
 from lms.tasks.roster import (
     fetch_assignment_roster,
     fetch_course_roster,
+    fetch_segment_roster,
     schedule_fetching_assignment_rosters,
     schedule_fetching_course_rosters,
     schedule_fetching_rosters,
+    schedule_fetching_segment_rosters,
 )
 from tests import factories
 
@@ -31,13 +33,25 @@ class TestRosterTasks:
 
         roster_service.fetch_assignment_roster.assert_called_once_with(assignment)
 
+    def test_fetch_segment_roster(self, roster_service, db_session):
+        lms_segment = factories.LMSSegment()
+        db_session.flush()
+
+        fetch_segment_roster(lms_segment_id=lms_segment.id)
+
+        roster_service.fetch_canvas_group_roster.assert_called_once_with(lms_segment)
+
     def test_schedule_fetching_rosters(
-        self, schedule_fetching_assignment_rosters, schedule_fetching_course_rosters
+        self,
+        schedule_fetching_assignment_rosters,
+        schedule_fetching_course_rosters,
+        schedule_fetching_segment_rosters,
     ):
         schedule_fetching_rosters()
 
         schedule_fetching_course_rosters.assert_called_once_with()
         schedule_fetching_assignment_rosters.assert_called_once_with()
+        schedule_fetching_segment_rosters.assert_called_once_with()
 
     @freeze_time("2024-08-28")
     @pytest.mark.usefixtures(
@@ -75,6 +89,24 @@ class TestRosterTasks:
 
         fetch_assignment_roster.delay.assert_called_once_with(
             assignment_id=assignment_with_recent_launch.id
+        )
+
+    @freeze_time("2024-08-28")
+    @pytest.mark.usefixtures(
+        "lms_segment_with_no_launch",
+        "lms_segment_with_no_recent_launch",
+        "lms_segment_with_recent_launch_and_task_done_row",
+        "lms_segment_with_launch_and_recent_roster",
+    )
+    def test_schedule_fetching_segment_rosters(
+        self, lms_segment_with_recent_launch, db_session, fetch_segment_roster
+    ):
+        db_session.flush()
+
+        schedule_fetching_segment_rosters()
+
+        fetch_segment_roster.delay.assert_called_once_with(
+            lms_segment_id=lms_segment_with_recent_launch.id
         )
 
     @pytest.fixture
@@ -214,12 +246,65 @@ class TestRosterTasks:
         return assignment
 
     @pytest.fixture
+    def lms_segment_with_no_launch(self, lms_course_with_no_launch):
+        return factories.LMSSegment(
+            lms_course=lms_course_with_no_launch, type="canvas_group"
+        )
+
+    @pytest.fixture
+    def lms_segment_with_no_recent_launch(self, lms_course_with_no_recent_launch):
+        return factories.LMSSegment(
+            lms_course=lms_course_with_no_recent_launch, type="canvas_group"
+        )
+
+    @pytest.fixture
+    def lms_segment_with_recent_launch(self, lms_course_with_recent_launch):
+        return factories.LMSSegment(
+            lms_course=lms_course_with_recent_launch, type="canvas_group"
+        )
+
+    @pytest.fixture
+    def lms_segment_with_recent_launch_and_task_done_row(
+        self, lms_course_with_recent_launch, db_session
+    ):
+        lms_segment = factories.LMSSegment(
+            lms_course=lms_course_with_recent_launch, type="canvas_group"
+        )
+        db_session.flush()  # Make sure we have an ID for the course
+        factories.TaskDone(key=f"roster::segment::scheduled::{lms_segment.id}")
+
+        return lms_segment
+
+    @pytest.fixture
+    def lms_segment_with_launch_and_recent_roster(self, lms_course_with_recent_launch):
+        lms_segment = factories.LMSSegment(
+            lms_course=lms_course_with_recent_launch, type="canvas_group"
+        )
+        factories.LMSSegmentRoster(
+            lms_segment=lms_segment,
+            lms_user=factories.LMSUser(),
+            lti_role=factories.LTIRole(),
+            active=True,
+            updated=datetime(2024, 8, 25),
+        )
+
+        return lms_segment
+
+    @pytest.fixture
     def fetch_course_roster(self, patch):
         return patch("lms.tasks.roster.fetch_course_roster")
 
     @pytest.fixture
     def fetch_assignment_roster(self, patch):
         return patch("lms.tasks.roster.fetch_assignment_roster")
+
+    @pytest.fixture
+    def fetch_segment_roster(self, patch):
+        return patch("lms.tasks.roster.fetch_segment_roster")
+
+    @pytest.fixture
+    def schedule_fetching_segment_rosters(self, patch):
+        return patch("lms.tasks.roster.schedule_fetching_segment_rosters")
 
     @pytest.fixture
     def schedule_fetching_assignment_rosters(self, patch):
