288b5f8381a85ec7fa6e58a7f063dfedb31301ff
Marcos Prieto <marcospri@gmail.com>
2024-12-13 16:27:38 +0100

    Functional test for the DeepLinking fields API
    Just covering the LTI1.1 version on this commit.

--- tests/factories/lti_user.py
+++ tests/factories/lti_user.py
@@ -4,6 +4,12 @@ from lms.models.lti_user import LTI, LTIUser
 from tests.factories.application_instance import ApplicationInstance
 from tests.factories.attributes import TOOL_CONSUMER_INSTANCE_GUID, USER_ID
 
+_LTI = make_factory(
+    LTI,
+    course_id=Faker("hexify", text="^" * 40),
+    product_family="UNKNOWN",
+)
+
 LTIUser = make_factory(
     LTIUser,
     user_id=USER_ID,
@@ -13,6 +19,6 @@ LTIUser = make_factory(
     effective_lti_roles=[],
     tool_consumer_instance_guid=TOOL_CONSUMER_INSTANCE_GUID,
     display_name=Faker("name"),
-    lti=LTI(course_id=Faker("hexify", text="^" * 40), product_family="UNKNOWN"),
+    lti=SubFactory(_LTI),
     application_instance=SubFactory(ApplicationInstance),
 )
