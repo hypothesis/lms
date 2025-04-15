342932996d6bc63163c3fa3d7b059b46fd5818ae
Marcos Prieto <marcospri@gmail.com>
2025-04-15 16:29:50 +0200

    Expose latest git commit as sentry release

--- lms/sentry.py
+++ lms/sentry.py
@@ -1,11 +1,14 @@
 """Sentry crash reporting integration."""
 
+from lms._version import get_version
+
 
 def includeme(config):
     config.add_settings(
         {
             "h_pyramid_sentry.retry_support": True,
             "h_pyramid_sentry.sqlalchemy_support": True,
+            "h_pyramid_sentry.init.release": get_version(),
         }
     )
     config.include("h_pyramid_sentry")
