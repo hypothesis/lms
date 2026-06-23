f49e8a8d4d4800c07e56036b134b701e4557cc3c
Gabriel Morador <gmorador@tribu.com.uy>
2026-06-23 15:26:34 -0300

    hide and reveal frontend

--- lms/static/scripts/frontend_apps/components/FilePickerApp.tsx
+++ lms/static/scripts/frontend_apps/components/FilePickerApp.tsx
@@ -600,6 +600,15 @@ export default function FilePickerApp({ onSubmit }: FilePickerAppProps) {
                       inputRef={dueDateInputRef}
                     />
                   )}
+                  {currentStep === 'checkpoint' && (
+                    <CheckpointSelector
+                      selected={checkpointType}
+                      onChange={setCheckpointType}
+                    />
+                  )}
+                  {currentStep === 'due-date' && (
+                    <DueDateSelector dueDate={dueDate} onChange={setDueDate} />
+                  )}
                 </div>
               ) : (
                 /* 1-col grid for very narrow screens; 2-col for everyone else */
